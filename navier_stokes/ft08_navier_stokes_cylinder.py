58dcab1b057b194fa23bd73f3e253dabb25de188
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-03-28 10:09:18 +0100

    Tested by varying the boundary conditions, it works

--- navier_stokes/ft08_navier_stokes_cylinder.py
+++ navier_stokes/ft08_navier_stokes_cylinder.py
@@ -18,7 +18,7 @@ import ufl as ufl
 T = 0.1        # final time
 num_steps = 1000  # number of time steps
 dt = T / num_steps # time step size
-mu = 0.001         # dynamic viscosity
+mu = 0.005         # dynamic viscosity
 rho = 1            # density
 # L = 10.0
 # H = 2.0
@@ -63,7 +63,7 @@ walls    = 'near(x[1], -1.0) || near(x[1], 1.0)'
 cylinder = 'on_boundary && (x[0]*x[0] + x[1]*x[1] < (0.5*0.5))'
 
 # Define inflow profile
-inflow_profile = ('4.0*1.5*(x[1]*x[1]-1.0) / pow(2.0, 2)', '0')
+inflow_profile = ('-2.0*(x[0]-0.5)', '-2.0*(x[1]-0.0)')
 # outflow_profile = ('4.0*1.5*x[1]*(2.0 - x[1]) / pow(2.0, 2)', '0')
 
 # Define boundary conditions
