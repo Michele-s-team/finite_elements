a3cb8c1362b73d2dc9f53184283ba8443297642d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-02 18:55:05 +0200

    Update ft08_navier_stokes_cylinder.py

--- navier_stokes/fenics_example/ft08_navier_stokes_cylinder.py
+++ navier_stokes/fenics_example/ft08_navier_stokes_cylinder.py
@@ -74,14 +74,14 @@ bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
 bcp = [bcp_outflow]
 
 # Define trial and test functions
-u = TrialFunction(V)
 v = TestFunction(V)
-p = TrialFunction(Q)
 q = TestFunction(Q)
 
 # Define functions for solutions at previous and current time steps
+u = Function(V)
 u_n = Function(V)
 u_  = Function(V)
+p = Function(Q)
 p_n = Function(Q)
 p_  = Function(Q)
 
@@ -107,25 +107,21 @@ F1 = rho*dot((u - u_n) / k, v)*dx \
    + inner(sigma(U, p_n), epsilon(v))*dx \
    + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
    - dot(f, v)*dx
-a1 = lhs(F1)
-L1 = rhs(F1)
 
 # Define variational problem for step 2
-a2 = dot(nabla_grad(p), nabla_grad(q))*dx
-L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx - (1/k)*div(u_)*q*dx
+F2 = dot(nabla_grad(p), nabla_grad(q))*dx - (dot(nabla_grad(p_n), nabla_grad(q))*dx - (1/k)*div(u_)*q*dx)
 
 # Define variational problem for step 3
-a3 = dot(u, v)*dx
-L3 = dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx
+F3 = dot(u, v)*dx - (dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx)
 
 # Assemble matrices
-A1 = assemble(a1)
-A2 = assemble(a2)
-A3 = assemble(a3)
+#A1 = assemble(a1)
+#A2 = assemble(a2)
+#A3 = assemble(a3)
 
 # Apply boundary conditions to matrices
-[bc.apply(A1) for bc in bcu]
-[bc.apply(A2) for bc in bcp]
+#[bc.apply(A1) for bc in bcu]
+#[bc.apply(A2) for bc in bcp]
 
 # Create XDMF files for visualization output
 xdmffile_u = XDMFFile('velocity.xdmf')
@@ -155,18 +151,18 @@ for n in range(num_steps):
     t += dt
 
     # Step 1: Tentative velocity step
-    b1 = assemble(L1)
-    [bc.apply(b1) for bc in bcu]
-    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
+#    b1 = assemble(L1)
+#    [bc.apply(b1) for bc in bcu]
+    solve(F1==0, u, bcu)
 
     # Step 2: Pressure correction step
-    b2 = assemble(L2)
-    [bc.apply(b2) for bc in bcp]
-    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
+#    b2 = assemble(L2)
+#    [bc.apply(b2) for bc in bcp]
+    solve(A2, p, bcp)
 
     # Step 3: Velocity correction step
-    b3 = assemble(L3)
-    solve(A3, u_.vector(), b3, 'cg', 'sor')
+#    b3 = assemble(L3)
+    solve(A3, u)
 
     # Plot solution
     plot(u_, title='Velocity')
