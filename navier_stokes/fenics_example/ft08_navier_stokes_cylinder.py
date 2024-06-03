49f4122c435d2419208908bd8717fb3562575262
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-03 15:11:51 +0200

    Wrote quantitites for trisal function

--- navier_stokes/fenics_example/ft08_navier_stokes_cylinder.py
+++ navier_stokes/fenics_example/ft08_navier_stokes_cylinder.py
@@ -94,24 +94,24 @@ vs = TestFunction(V)
 
 
 # Define functions for solutions at previous and current time steps
-up = Function(VQ)
+up = TrialFunction(VQ)
 u, p = split(up)
 
 
 u_n = Function(V)
 p_n = Function(Q)
-_u_ = Function(V)
-_p_ = Function(Q)
+# _u_ = Function(V)
+# _p_ = Function(Q)
 
-up_ = Function(VQ)
-u_, p_ = split(up_)
+# up_ = Function(VQ)
+# u_, p_ = split(up_)
 
-us = Function(V)
-ps = Function(Q)
+us = TrialFunction(V)
+ps_ = Function(Q)
 
 
 # Define expressions used in variational forms
-U  = 0.5*(u_n + u_)
+U  = 0.5*(u_n + u)
 n  = FacetNormal(mesh)
 f  = Constant((0, 0))
 k  = Constant(dt)
@@ -127,17 +127,24 @@ def sigma(u, p):
     return 2*mu*epsilon(u) - p*Identity(len(u))
 
 # Define variational problem for step 1
-F1 = rho*dot((u_ - u_n) / k, v)*dx \
+F1 = rho*dot((u - u_n) / k, v)*dx \
    + rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx \
    + inner(sigma(U, p_n), epsilon(v))*dx \
    + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
    - dot(f, v)*dx
 # Define variational problem for step 2
-F2 = (dot(nabla_grad(p_), nabla_grad(q)) - (dot(nabla_grad(p_n), nabla_grad(q)) - (1/k)*div(u_)*q))*dx
+F2 = (dot(nabla_grad(p), nabla_grad(q)) - (dot(nabla_grad(p_n), nabla_grad(q)) - (1/k)*div(u)*q))*dx
 F12 = F1 + F2
 
+a12 = lhs(F12)
+L12 = rhs(F12)
+
+
 # Define variational problem for step 3
-F3 = (dot(us, vs) - (dot(_u_, vs) - k*dot(nabla_grad(_p_ - p_n), vs))) * dx
+F3 = (dot(us, vs) - (dot(us, vs) - k*dot(nabla_grad(ps_ - p_n), vs))) * dx
+
+a3 = lhs(F3)
+L3 = rhs(F3)
 
 
 # Create XDMF files for visualization output
