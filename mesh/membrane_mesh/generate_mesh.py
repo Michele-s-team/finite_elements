5ec611e6687d92677d34b3e4b09a16668f011186
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-07-04 09:06:37 +0200

    Switching to a disk domain: updated generate_mesh.py ...

--- mesh/membrane_mesh/generate_mesh.py
+++ mesh/membrane_mesh/generate_mesh.py
@@ -15,13 +15,17 @@ resolution = (float)(args.resolution)
 
 
 # Channel parameters
-L = 1
-h = 1
+# L = 2.2
+# h = 0.41
+r = 1
+c_r = [0.2, 0.2, 0]
 
 
 
-print("L = ", L)
-print("h = ", h)
+# print("L = ", L)
+# print("h = ", h)
+print("r = ", r)
+print("c_r = ", c_r)
 print("resolution = ", resolution)
 
 
@@ -30,27 +34,31 @@ geometry = pygmsh.geo.Geometry()
 # Fetch model we would like to add data to
 model = geometry.__enter__()
 
-my_points = [ model.add_point((0, 0, 0), mesh_size=resolution),
-             model.add_point((L, 0, 0), mesh_size=resolution),
-             model.add_point((L, h, 0), mesh_size=resolution),
-             model.add_point((0, h, 0), mesh_size=resolution)]
+# my_points = [ model.add_point((0, 0, 0), mesh_size=resolution),
+#              model.add_point((L, 0, 0), mesh_size=resolution),
+#              model.add_point((L, h, 0), mesh_size=resolution),
+#              model.add_point((0, h, 0), mesh_size=resolution)]
 
-channel_lines = [model.add_line(my_points[0], my_points[1]),
-                   model.add_line(my_points[1], my_points[2]),
-                   model.add_line(my_points[2], my_points[3]),
-                   model.add_line(my_points[3], my_points[0])]
+# Add lines between all points creating the rectangle
+# channel_lines = [model.add_line(my_points[i], my_points[i+1])
+                #   for i in range(-1, len(my_points)-1)]
 
-channel_loop = model.add_curve_loop(channel_lines)
-plane_surface = model.add_plane_surface(channel_loop, holes=[])
+# channel_loop = model.add_curve_loop(channel_lines)
+
+circle_r = model.add_circle(c_r, r, mesh_size=resolution/2)
+
+
+# plane_surface = model.add_plane_surface(channel_loop, holes=[circle_r.curve_loop])
+plane_surface = model.add_plane_surface(circle_r.curve_loop, holes=[])
 
 
 model.synchronize()
+
 model.add_physical([plane_surface], "Volume")
-model.add_physical([channel_lines[3]], "Inflow")
-model.add_physical([channel_lines[1]], "Outflow")
-model.add_physical([channel_lines[2]], "TopWall")
-model.add_physical([channel_lines[0]], "BottomWall")
-# model.add_physical(circle_r.curve_loop.curves, "Circle")
+# model.add_physical([channel_lines[0]], "Inflow")
+# model.add_physical([channel_lines[2]], "Outflow")
+# model.add_physical([channel_lines[1], channel_lines[3]], "Walls")
+model.add_physical(circle_r.curve_loop.curves, "Boundary")
 
 geometry.generate_mesh(dim=2)
 gmsh.write("membrane_mesh.msh")
