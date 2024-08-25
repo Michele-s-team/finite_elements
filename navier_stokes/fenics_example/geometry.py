37a50803c0f1ab03456f1c8f069517aa44a34a8d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-08-25 19:00:39 +0200

    Found another, more complex parametewr configuration, where it does not blow up
    'python3 generate_mesh.py 0.2' + 'python3 example.py /home/fenics/shared/mesh/membrane_mesh/ /home/fenics/shared/navier_stokes/fenics_example/solution/'

--- navier_stokes/fenics_example/geometry.py
+++ navier_stokes/fenics_example/geometry.py
@@ -97,8 +97,8 @@ class z_Expression(UserExpression):
 #trial analytical expression for a vector
 class grad_r_Expression(UserExpression):
     def eval(self, values, x):
-        values[0] = 0
-        values[1] = 0
+        values[0] = C*x[0]
+        values[1] = C*x[1]
         return (2,)
     
 class grad_R_Expression(UserExpression):
