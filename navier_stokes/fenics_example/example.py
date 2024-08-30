9c7d5a2cd5965631fbb58214b62706103cc33b96
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-08-30 15:20:48 +0200

    Update example.py

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -83,7 +83,7 @@ grad_R = interpolate(grad_R_Expression(element=Q_omega.ufl_element()), Q_omega)
 F_z = ( kappa * ( g_c(omega)[i, j] * (H(omega).dx(j)) * (nu_z.dx(i)) - 2.0 * H(omega) * ( (H(omega))**2 - K(omega) ) * nu_z ) + sigma * H(omega) * nu_z ) * sqrt_detg(omega) * dx \
     - ( \
         ( kappa * (n_lr(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth(omega) * (ds_l + ds_r) + \
-        ( kappa * (n_tb(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth(omega) * (ds_t + ds_b) + \
+        ( kappa * (n_tb(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth(omega) * (ds_t + ds_b) \
     ) 
 F_omega = ( - z * Nabla_v(nu_omega, omega)[i, i] - omega[i] * nu_omega[i] ) *  sqrt_detg(omega) * dx + \
           ( (n(omega))[i] * g(omega)[i, j] * z * nu_omega[j] ) * sqrt_deth(omega) * ds
