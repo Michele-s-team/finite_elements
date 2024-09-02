7215c288257668a4011d9581845281fac139cff7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-02 22:02:32 +0200

    Wrote F_z

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -99,13 +99,14 @@ F_v = ( rho * (  v[j]*Nabla_v(v, omega)[i, j] * nu_v[i] ) + \
 
 
 F_z = ( \
-        rho * v[i] * v[k] * b(omega)[k, i] * nu_z - \ 
+        rho * v[i] * v[k] * b(omega)[k, i] * nu_z - \
         kappa * ( 2 * g_c(omega)[i, j] * (H(omega).dx(j)) * (nu_z.dx(i)) - 4.0 * H(omega) * ( (H(omega))**2 - K(omega) ) * nu_z ) - \
             2 * ( sigma * H(omega) + eta * Nabla_v(v, omega)[j, i] * g_c(omega)[i, k] * b(omega)[k, j] ) * nu_z \
         ) * sqrt_detg(omega) * dx \
-    - ( \
-        ( kappa * (n_lr(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_square(omega) * (ds_l + ds_r) + \
-        ( kappa * (n_tb(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_square(omega) * (ds_t + ds_b) \
+    + ( \
+        ( 2 * kappa * (n_lr(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_square(omega) * (ds_l + ds_r) + \
+        ( 2 * kappa * (n_tb(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_square(omega) * (ds_t + ds_b) + \
+        ( 2 * kappa * (n(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_circle(omega) * ds_circle 
     ) 
 '''
 F_omega = ( - z * Nabla_v(nu_omega, omega)[i, i] - omega[i] * nu_omega[i] ) *  sqrt_detg(omega) * dx + \
