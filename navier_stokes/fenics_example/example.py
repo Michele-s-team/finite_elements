4dfb9f302ff731df77cc47ebd20dc2db7fb4e052
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-02 22:06:07 +0200

    Update example.py

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -97,7 +97,6 @@ F_v = ( rho * (  v[j]*Nabla_v(v, omega)[i, j] * nu_v[i] ) + \
         ( 2 * eta * d(v, omega)[i, j] * n(omega)[i] * g_c(omega)[j, k] * nu_v[k] ) * sqrt_deth_circle(omega) * ds_circle
     )
 
-
 F_z = ( \
         rho * v[i] * v[k] * b(omega)[k, i] * nu_z - \
         kappa * ( 2 * g_c(omega)[i, j] * (H(omega).dx(j)) * (nu_z.dx(i)) - 4.0 * H(omega) * ( (H(omega))**2 - K(omega) ) * nu_z ) - \
@@ -108,6 +107,7 @@ F_z = ( \
         ( 2 * kappa * (n_tb(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_square(omega) * (ds_t + ds_b) + \
         ( 2 * kappa * (n(omega))[i] * nu_z * (H(omega).dx(i)) ) * sqrt_deth_circle(omega) * ds_circle 
     ) 
+
 F_omega = ( z * Nabla_v(nu_omega, omega)[i, i] + omega[i] * nu_omega[i] ) * sqrt_detg(omega) * dx - \
           ( \
             ( (n_lr(omega))[i] * g(omega)[i, j] * z * nu_omega[j] ) * sqrt_deth_square(omega) * (ds_l + ds_r) + \
