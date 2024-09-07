5180b8e632d9c470a0b07598114a338fbe6b2303
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-07 22:50:53 +0200

    Update example.py

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -134,31 +134,31 @@ To be safe, I explicitly wrote the each term on each part of the boundary with i
 the surface elements are ds_l + ds_r, and the normal is n_lr(omega) ~ {+-1 , 0}: this avoids odd interpolations at the corners of the rectangle edges. 
 '''
 
-F_vbar = ( \
+F_v_bar = ( \
                      rho * (((v_bar[i] - v_n_1[i]) / Deltat \
                                + ( 3.0/2.0*v_n_1[j] - 1.0/2.0*v_n_2[j] ) * Nabla_v(V, omega_n_12)[i, j] \
                                - 2.0 * V[j] * W * g_c(omega_n_12)[i, k] * b(omega_n_12)[k, j]) * nu_v_bar[i] \
                             + 1.0 / 2.0 * (W**2) * g_c(omega_n_12)[i, j] * Nabla_f( nu_v_bar, omega_n_12 )[i, j])  \
                      + sigma_ast * g_c(omega_n_12)[i, j] * Nabla_f( nu_v_bar, omega_n_12 )[i, j] \
                      + 2.0 * eta * d_c(V, W, omega_n_12)[i, j] * Nabla_f( nu_v_bar, omega_n_12 )[j, i]
-         )* sqrt_detg(omega_n_12) * dx \
-         - rho/2.0 * ( \
+         ) * sqrt_detg(omega_n_12) * dx \
+          - rho / 2.0 * ( \
                      ((W**2) * (n_lr(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_square( omega_n_12 ) * (ds_l + ds_r) + \
                      ((W**2) * (n_tb(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_square( omega_n_12 ) * (ds_t + ds_b) + \
                      ((W**2) * (n(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_circle( omega_n_12, c_r ) * ds_circle
          ) \
-         - ( \
+          - ( \
                      (sigma_ast * (n_lr(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_square( omega_n_12 ) * (ds_l + ds_r) + \
                      (sigma_ast * (n_tb(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_square( omega_n_12 ) * (ds_t + ds_b) + \
                      (sigma_ast * (n(omega_n_12))[i] * nu_v_bar[i]) * sqrt_deth_circle( omega_n_12, c_r ) * ds_circle
          ) \
-         - 2.0 * eta * ( \
+          - 2.0 * eta * ( \
                      (d_c(V, W, omega_n_12)[i, j] * g(omega_n_12)[i, k] * (n_lr(omega_n_12))[k] * nu_v_bar[j]) * sqrt_deth_square( omega_n_12 ) * ds_l + \
                      (d_c(V, W, omega_n_12)[i, j] * g(omega_n_12)[i, k] * (n_tb(omega_n_12))[k] * nu_v_bar[j]) * sqrt_deth_square( omega_n_12 ) * (ds_t + ds_b) + \
                      (d_c(V, W, omega_n_12)[i, j] * g(omega_n_12)[i, k] * (n(omega_n_12))[k] * nu_v_bar[j]) * sqrt_deth_circle( omega_n_12, c_r ) * ds_circle
          )
 
-F_wbar = ( \
+F_w_bar = ( \
                      rho * ((w_bar - w_n_1) / Deltat + V[i] * V[k] * b( omega_n_12 )[k, i]) * nu_w_bar \
                      - rho * W * Nabla_v( (3.0/2.0*v_n_1 - 1.0/2.0*v_n_2) * nu_w_bar, omega_n_12 )[i, i] \
                      + 2.0 * kappa * ( \
@@ -170,7 +170,7 @@ F_wbar = ( \
                                  + 2.0 * eta * ( g_c(omega_n_12)[i, k] * Nabla_v(V, omega_n_12)[j, k] * (b(omega_n_12))[i, j] - 2.0 * W * ( 2.0 * (H(omega_n_12))**2 - K(omega_n_12) ) )
                      )
          ) * sqrt_detg(omega_n_12) * dx \
-         + rho * ( \
+          + rho * ( \
                      (W * nu_w_bar * n_lr( omega_n_12 )[j] * g( omega_n_12 )[j, i] * (3.0 / 2.0 * v_n_1[i] - 1.0 / 2.0 * v_n_2[i])) * sqrt_deth_square( omega_n_12 ) * (ds_l + ds_r) \
                      + (W * nu_w_bar * n_tb( omega_n_12 )[j] * g( omega_n_12 )[j, i] * (3.0 / 2.0 * v_n_1[i] - 1.0 / 2.0 * v_n_2[i])) * sqrt_deth_square( omega_n_12 ) * (ds_t + ds_b) \
                      + (W * nu_w_bar * n( omega_n_12 )[j] * g( omega_n_12 )[j, i] * (3.0 / 2.0 * v_n_1[i] - 1.0 / 2.0 * v_n_2[i])) * sqrt_deth_circle( omega_n_12, c_r ) * ds_circle
