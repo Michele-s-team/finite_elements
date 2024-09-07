8f4f8ba5ac48e573333d3c8d7ff9ce85fcfccdc1
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-07 22:36:16 +0200

    Update example.py

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -167,6 +167,7 @@ F_wbar = ( \
                          ) \
                      - ( \
                                  2.0 * sigma_n_12 * H(omega_n_12) \
+                 + 2.0 * eta * ( g_c(omega_n_12)[i, k] * Nabla_v(V, omega_n_12)[j, k] * (b(omega_n_12))[i, j]  )
                          )
          ) * sqrt_detg(omega_n_12) * dx
 
