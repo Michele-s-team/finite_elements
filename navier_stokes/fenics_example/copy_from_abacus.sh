32ffbbde7dad0161e5b010a9a3e548c1da0862cd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-19 20:34:32 +0200

    Update copy_from_abacus.sh

--- navier_stokes/fenics_example/copy_from_abacus.sh
+++ navier_stokes/fenics_example/copy_from_abacus.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-IN_DIR="/mnt/beegfs/home/mcastel1/fenics_example/solution"
-OUT_DIR="/Users/michelecastellana/Desktop"
+IN_DIR="/mnt/beegfs/home/mcastel1/$1/solution"
+OUT_DIR=$2
+
+echo "Input directory = " $IN_DIR
+echo "Output directory = " $OUT_DIR
 
 rsync --size-only -P -v -e ssh mcastel1@abacus:$IN_DIR/z_n.\* $OUT_DIR
