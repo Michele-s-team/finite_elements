f8dde40bb0a95a41e7b2bc87ee0835277d4fc874
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-21 10:30:04 +0200

    I can now write the solution at each time step and read it back even if the code crashes
    python3 example.py

--- navier_stokes/fenics_example/example.py
+++ navier_stokes/fenics_example/example.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from fenics import *
 import numpy as np
+import time
 
 mesh = UnitSquareMesh(96, 96)
 V = FunctionSpace(mesh, "Lagrange", 1)
@@ -11,16 +12,19 @@ v  = Function( V )
 expr = Expression("p*x[0]", degree = 1, p = 1.0)
 
 
-# Create XDMF files for visualization output
-xdmffile_v = XDMFFile( 'v.xdmf' )
+
 
 # Time-stepping
 for step in range(10):
 
     print("\n* step = ", step, "\n")
 
+    time.sleep( 5 )  # Makes Python wait for 5 seconds
+
     expr.p = step;
     #this is the correct way to interpolate an expression and write it into v
     v.interpolate(expr)
 
-    xdmffile_v.write( v, step )
+    # Create XDMF files  and write to it the solution for each time step
+    xdmffile_v = XDMFFile( 'v_n' + str( step ) + '.xdmf' )
+    xdmffile_v.write(v)
