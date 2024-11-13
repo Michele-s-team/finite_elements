6e16d23ae387db20bb84ca239d748f1e1f7f4ed3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-11-13 11:41:52 +0100

    Started revising to incorporate N_t ...

--- navier_stokes/fenics_example/geometry.py
+++ navier_stokes/fenics_example/geometry.py
@@ -228,8 +228,9 @@ def sqrt_deth_square(omega):
 #the normal vector on the l and r boundaries of a rectangle  (0,0) - (L,0) - (L, h) - (0,h), normalized according to g and pointing outside Omega
 def n_lr(omega):
     x = ufl.SpatialCoordinate(mesh)
-    u = as_tensor([conditional(lt(x[0], L/2.0), -1.0, 1.0), 0.0] )
-    return as_tensor(u[k]/sqrt(g(omega)[i,j]*u[i]*u[j]), (k))
+    N3d = as_tensor([conditional(lt(x[0], L/2.0), -1.0, 1.0), 0.0, 0.0] )
+    Nt = as_tensor(g_c(omega)[i, j] * N3d[k] * e(omega)[j, k], (i))
+    return as_tensor(Nt[k]/sqrt(g(omega)[i, j]*Nt[i]*Nt[j]), (k))
 
 #the normal vector on the t  and b boundaries of a rectangle  (0,0) - (L,0) - (L, h) - (0,h), normalized according to g and pointing outside Omega
 def n_tb(omega):
