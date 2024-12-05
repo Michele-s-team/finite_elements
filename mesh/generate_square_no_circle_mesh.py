25e432ebd140162ead6b5a52a28adc678546b365
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-12-05 14:58:21 +0100

    Adding case for square mesh with no circle ...

---
+++ mesh/generate_square_no_circle_mesh.py
@@ -0,0 +1,72 @@
+import meshio
+import gmsh
+import pygmsh
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("resolution")
+args = parser.parse_args()
+
+#mesh resolution
+resolution = (float)(args.resolution)
+
+# Channel parameters
+#CHANGE PARAMETERS HERE
+L = 0.5
+h = L
+#CHANGE PARAMETERS HERE
+
+
+print("L = ", L)
+print("h = ", h)
+print("resolution = ", resolution)
+
+
+# Initialize empty geometry using the build in kernel in GMSH
+geometry = pygmsh.geo.Geometry()
+# Fetch model we would like to add data to
+model = geometry.__enter__()
+
+my_points = [ model.add_point((0, 0, 0), mesh_size=resolution),
+             model.add_point((L, 0, 0), mesh_size=resolution),
+             model.add_point((L, h, 0), mesh_size=resolution),
+             model.add_point((0, h, 0), mesh_size=resolution)]
+
+# Add lines between all points creating the rectangle
+channel_lines = [model.add_line(my_points[i], my_points[i+1])
+                  for i in range(-1, len(my_points)-1)]
+
+channel_loop = model.add_curve_loop(channel_lines)
+
+plane_surface = model.add_plane_surface(channel_loop, holes=[])
+
+
+model.synchronize()
+
+model.add_physical([plane_surface], "Volume")
+model.add_physical([channel_lines[0]], "i")
+model.add_physical([channel_lines[2]], "o")
+model.add_physical([channel_lines[3]], "t")
+model.add_physical([channel_lines[1]], "b")
+
+geometry.generate_mesh(dim=2)
+gmsh.write("mesh.msh")
+gmsh.clear()
+geometry.__exit__()
+
+mesh_from_file = meshio.read("mesh.msh")
+
+def create_mesh(mesh, cell_type, prune_z=False):
+    cells = mesh.get_cells_type(cell_type)
+    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+    points = mesh.points[:, :2] if prune_z else mesh.points
+    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={
+                           "name_to_read": [cell_data]})
+    return out_mesh
+
+line_mesh = create_mesh(mesh_from_file, "line", prune_z=True)
+meshio.write("line_mesh.xdmf", line_mesh)
+
+triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=True)
+meshio.write("triangle_mesh.xdmf", triangle_mesh)
