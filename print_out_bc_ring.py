1f69a802d44c7f4bbd7a3d8f3587412626c85938
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-12-05 08:50:58 +0100

    Started incorporating ring problem ..

---
+++ print_out_bc_ring.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from fenics import *
+from mshr import *
+from variational_problem_bc_square_a import *
+
+# copy the data of the  solution psi into v_output, ..., z_output, which will be allocated or re-allocated here
+omega_output, z_output = psi.split( deepcopy=True )
+
+print( "\int_{\partial \Omega} (n^i \omega_i - psi )^2 dS = ", \
+       assemble( ( ((n_lr( omega_output ))[i] * omega_output[i] - omega_square)) ** 2 * (ds_l + ds_r) ) \
+       + assemble( ( ((n_tb( omega_output ))[i] * omega_output[i] - omega_square)) ** 2 * (ds_t + ds_b) ) \
+       + assemble( ( ((n_circle( omega_output ))[i] * omega_output[i] - omega_circle)) ** 2 * ds_circle ) \
+       )
