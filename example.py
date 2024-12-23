689bbeb0b65867710c611362fcfc32d36739acac
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-12-23 12:51:39 +0100

    Reformulating variational problem as nonlinear one ....

--- example.py
+++ example.py
@@ -54,20 +54,46 @@ class laplacian_u_expression(UserExpression):
 
 
 # Define variational problem
-u_ = Function( Q )
-u = TrialFunction( Q )
-v = TestFunction( Q )
+u = Function( Q )
+nu = TestFunction( Q )
 f = Function( Q )
 grad_u = Function( V )
+J_u = TrialFunction( Q )
 
 grad_u.interpolate( grad_u_expression( element=V.ufl_element() ) )
 f.interpolate( laplacian_u_expression( element=Q.ufl_element() ) )
 
-a = dot(grad(u), grad(v))*dx
-L = -f*v*dx + dot(n,grad_u)*v*ds
+F = (dot( grad(u), grad( nu ) ) + f * nu) * dx - dot( n, grad_u ) * nu * ds
+bcs= []
+J = derivative( F, u, J_u )
+problem = NonlinearVariationalProblem( F, u, bcs, J )
+solver = NonlinearVariationalSolver( problem )
 
-solve(a == L, u_)
+'''
+#set the solver parameters here
+params = {'nonlinear_solver': 'newton',
+           'newton_solver':
+            {
+                'linear_solver'           : 'mumps',
+                'absolute_tolerance'      : 1e-6,
+                'relative_tolerance'      : 1e-6,
+                'maximum_iterations'      : 1000000,
+                'relaxation_parameter'    : 0.95,
+             }
+}
+solver.parameters.update(params)
+'''
+'''
+#set the solver parameters here
+params ={"newton_solver": {"linear_solver": 'superlu'}}
+solver.parameters.update(params)
+'''
 
-xdmffile_u.write(u_, 0)
+solver.solve()
 
-print("\int (n[i] \partial_i u - n[i] grad_u[i])^2 dS = ", assemble(((n[i]*grad_u[i]) - (n[i]*u_.dx(i))) **2 * ds))
+
+# solve( a == L, u )
+
+xdmffile_u.write( u, 0 )
+
+print("\int (n[i] \partial_i u - n[i] grad_u[i])^2 dS = ", assemble( ((n[i]*grad_u[i]) - (n[i] * u.dx( i ))) ** 2 * ds ) )
