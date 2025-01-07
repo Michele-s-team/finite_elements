c712fc64d321303672789707c04c4f9800b8bb05
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-01-07 12:48:57 +0100

    Update generate_3dmesh.py

--- mesh/generate_3dmesh.py
+++ mesh/generate_3dmesh.py
@@ -1,81 +1,35 @@
-'''
-run with
-clear; clear; python3 generate_3dmesh.py [resolution]
-example:
-clear; clear; python3 generate_3dmesh.py 0.1
-
-
-
-'''
-
 import meshio
-import gmsh
-import pygmsh
-import argparse
+import numpy as np
+from dolfin import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("resolution")
-args = parser.parse_args()
+dim = 3
+N = 2**5
+mesh = UnitCubeMesh(N, N, N)
 
-#mesh resolution
-resolution = (float)(args.resolution)
+bdim = dim-1
+bmesh = BoundaryMesh(mesh, "exterior")
+mapping = bmesh.entity_map(bdim)
+part_of_bot = MeshFunction("size_t", bmesh, bdim)
+for cell in cells(bmesh):
+    curr_facet_normal = Facet(mesh, mapping[cell.index()]).normal()
+    if near(curr_facet_normal.y(), -1.0):  # On bot boundary
+        part_of_bot[cell] = 1
 
-#mesh parameters
-#CHANGE PARAMETERS HERE
-r = 1.0
-c_r = [0, 0, 0]
-#CHANGE PARAMETERS HERE
-
-print("r = ", r)
-print("c_r = ", c_r)
-print("resolution = ", resolution)
-
-
-# Initialize empty geometry using the build in kernel in GMSH
-geometry = pygmsh.occ.Geometry()
-# Fetch model we would like to add data to
-model = geometry.__enter__()
+bot_boundary = SubMesh(bmesh, part_of_bot, 1)
+#File('bot_boundary.pvd') << bot_boundary
+with XDMFFile("bot_mesh.xdmf") as xdmf:
+    xdmf.write(bot_boundary)
 
 
-#add a 3d object:
-ball = model.add_ball(c_r, r,  mesh_size=resolution)
+in_mesh = meshio.read("bot_mesh.xdmf")
+
+cells = in_mesh.get_cells_type("triangle")
+points = np.delete(in_mesh.points, 1, axis=1)
+out_mesh = meshio.Mesh(points=points, cells={"triangle": cells})
+meshio.write("pruned_mesh.xdmf", out_mesh)
 
 
-
-model.synchronize()
-
-model.add_physical([ball], "ball")
-
-geometry.generate_mesh(dim=3)
-gmsh.write("solution/mesh.msh")
-model.__exit__()
-
-def create_mesh(mesh, cell_type, prune_z=False):
-    cells = mesh.get_cells_type(cell_type)
-    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
-    out_mesh = meshio.Mesh(points=mesh.points, cells={
-                           cell_type: cells}, cell_data={"name_to_read": [cell_data]})
-    return out_mesh
-
-
-mesh_from_file = meshio.read("solution/mesh.msh")
-
-
-#create a tetrahedron mesh
-tetrahedron_mesh = create_mesh(mesh_from_file, "tetra", True)
-meshio.write("solution/tetrahedron_mesh.xdmf", tetrahedron_mesh)
-
-'''
-#create a triangle mesh
-triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=False)
-meshio.write("solution/triangle_mesh.xdmf", triangle_mesh)
-
-
-#create a line mesh
-line_mesh = create_mesh(mesh_from_file, "line", True)
-meshio.write("solution/line_mesh.xdmf", line_mesh)
-
-#create a vertex mesh
-vertex_mesh = create_mesh(mesh_from_file, "vertex", True)
-meshio.write("solution/vertex_mesh.xdmf", vertex_mesh)
-'''
+mesh2D = Mesh()
+with XDMFFile("pruned_mesh.xdmf") as xdmf:
+    xdmf.read(mesh2D)
+print(mesh2D.geometry().dim())
