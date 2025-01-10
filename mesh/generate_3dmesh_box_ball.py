d4dca20f9092b39e1ff4d82bd75e0fbe2af433c5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-01-10 11:42:00 +0100

    The mesh is generated
     clear; clear; rm -r solution; mkdir solution; python3 generate_3dmesh_box_ball.py 0.1

--- mesh/generate_3dmesh_box_ball.py
+++ mesh/generate_3dmesh_box_ball.py
@@ -10,14 +10,94 @@ clear; clear; rm -r solution; mkdir solution; python3 generate_3dmesh_box_ball.p
 '''
 
 import meshio
-import gmsh
-import pygmsh
 import argparse
+import numpy as np
+import gmsh
+import warnings
 
 parser = argparse.ArgumentParser()
 parser.add_argument("resolution")
 args = parser.parse_args()
 
+warnings.filterwarnings("ignore")
+gmsh.initialize()
+
+gmsh.model.add("my model")
+L, B, H, r = 2.5, 0.41, 0.41, 0.05
+resolution = r / 10
+
+channel = gmsh.model.occ.addBox(0, 0, 0, L, B, H)
+cylinder = gmsh.model.occ.addCylinder(0.5, 0, 0.2, 0, B, 0, r)
+fluid = gmsh.model.occ.cut([(3, channel)], [(3, cylinder)])
+
+gmsh.model.occ.synchronize()
+volumes = gmsh.model.getEntities(dim=3)
+assert volumes == fluid[0]
+fluid_marker = 11
+gmsh.model.addPhysicalGroup(volumes[0][0], [volumes[0][1]], fluid_marker)
+gmsh.model.setPhysicalName(volumes[0][0], fluid_marker, "Fluid volume")
+
+surfaces = gmsh.model.occ.getEntities(dim=2)
+inlet_marker, outlet_marker, wall_marker, obstacle_marker = 1, 3, 5, 7
+walls = []
+obstacles = []
+for surface in surfaces:
+    com = gmsh.model.occ.getCenterOfMass(surface[0], surface[1])
+    if np.allclose(com, [0, B / 2, H / 2]):
+        gmsh.model.addPhysicalGroup(surface[0], [surface[1]], inlet_marker)
+        inlet = surface[1]
+        gmsh.model.setPhysicalName(surface[0], inlet_marker, "Fluid inlet")
+    elif np.allclose(com, [L, B / 2, H / 2]):
+        gmsh.model.addPhysicalGroup(surface[0], [surface[1]], outlet_marker)
+        gmsh.model.setPhysicalName(surface[0], outlet_marker, "Fluid outlet")
+    elif (
+        np.isclose(com[2], 0)
+        or np.isclose(com[1], B)
+        or np.isclose(com[2], H)
+        or np.isclose(com[1], 0)
+    ):
+        walls.append(surface[1])
+    else:
+        obstacles.append(surface[1])
+gmsh.model.addPhysicalGroup(2, walls, wall_marker)
+gmsh.model.setPhysicalName(2, wall_marker, "Walls")
+gmsh.model.addPhysicalGroup(2, obstacles, obstacle_marker)
+gmsh.model.setPhysicalName(2, obstacle_marker, "Obstacle")
+
+distance = gmsh.model.mesh.field.add("Distance")
+gmsh.model.mesh.field.setNumbers(distance, "FacesList", obstacles)
+
+threshold = gmsh.model.mesh.field.add("Threshold")
+gmsh.model.mesh.field.setNumber(threshold, "IField", distance)
+gmsh.model.mesh.field.setNumber(threshold, "LcMin", resolution)
+gmsh.model.mesh.field.setNumber(threshold, "LcMax", 20 * resolution)
+gmsh.model.mesh.field.setNumber(threshold, "DistMin", 0.5 * r)
+gmsh.model.mesh.field.setNumber(threshold, "DistMax", r)
+
+inlet_dist = gmsh.model.mesh.field.add("Distance")
+gmsh.model.mesh.field.setNumbers(inlet_dist, "FacesList", [inlet])
+
+inlet_thre = gmsh.model.mesh.field.add("Threshold")
+gmsh.model.mesh.field.setNumber(inlet_thre, "IField", inlet_dist)
+gmsh.model.mesh.field.setNumber(inlet_thre, "LcMin", 5 * resolution)
+gmsh.model.mesh.field.setNumber(inlet_thre, "LcMax", 10 * resolution)
+gmsh.model.mesh.field.setNumber(inlet_thre, "DistMin", 0.1)
+gmsh.model.mesh.field.setNumber(inlet_thre, "DistMax", 0.5)
+
+minimum = gmsh.model.mesh.field.add("Min")
+gmsh.model.mesh.field.setNumbers(minimum, "FieldsList", [threshold, inlet_thre])
+gmsh.model.mesh.field.setAsBackgroundMesh(minimum)
+
+gmsh.model.occ.synchronize()
+gmsh.model.mesh.generate(3)
+
+gmsh.write("mesh3D.msh")
+
+
+
+
+
+'''
 #mesh resolution
 resolution = (float)(args.resolution)
 print("resolution = ", resolution)
@@ -60,17 +140,7 @@ model.add_physical( box_minus_ball, "difference" )
 
 #find out the sphere surface and add it to the model
 #this is name_to_read which will be shown in paraview and subdomain_id which will be used in the code which reads the mesh in `ds_custom = Measure("ds", domain=mesh, subdomain_data=sf, subdomain_id=1)`
-'''
-the  surfaces are tagged with the following subdomain_ids: 
-- left: subdomain_id = 1
-- right: subdomain_id = 6
-- front: subdomain_id = 2
-- back: subdomain_id = 4
-- top: subdomain_id = 3
-- bottom: subdomain_id = 5
-- sphere: subdomain_id = 7
-If you have a doubt about the subdomain_ids, see name_to_read in tetrahedron_mesh.xdmf with Paraview
-'''
+
 sphere_tag = 1
 dim_facet = 2 # for facets in 3D
 sphere_boundaries = []
@@ -107,3 +177,4 @@ meshio.write("solution/tetrahedron_mesh.xdmf", tetrahedron_mesh)
 #create a triangle mesh in which the surfaces will be stored
 triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=False)
 meshio.write("solution/triangle_mesh.xdmf", triangle_mesh)
+'''
