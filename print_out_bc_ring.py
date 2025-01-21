0224a2fe2ea1805145cb89ca1b7fbcdfce4b4beb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-01-21 20:26:57 +0100

    Adding check whether the PDE is satisfied ...

--- print_out_bc_ring.py
+++ print_out_bc_ring.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from fenics import *
 from mshr import *
+from physics import *
 # from variational_problem_bc_a import *
 from variational_problem_bc_ring import *
 
@@ -9,14 +10,19 @@ z_output, omega_output, mu_output, nu_output = psi.split( deepcopy=True )
 
 print("Check of BCs:")
 print( "\t<<(z - phi)^2>>_r = ", \
-   sqrt(assemble( ( ((z_output - z_r_const) ) ** 2 * ds_r ) ) / assemble(Constant(1.0) * ds_r))
+   sqrt(assemble( ( (z_output - z_r_const ) ** 2 * ds_r ) ) / assemble(Constant(1.0) * ds_r))
   )
 print( "\t<<(z - phi)^2>>_R = ", \
-   sqrt(assemble( ( ((z_output - z_R_const) ) ** 2 * ds_R ) ) / assemble(Constant(1.0) * ds_R))
+   sqrt(assemble( ( (z_output - z_R_const ) ** 2 * ds_R ) ) / assemble(Constant(1.0) * ds_R))
   )
 print( "\t<<(n^i \omega_i - psi )^2>>_r = ", \
-   sqrt(assemble( ( (((n_circle( omega_output ))[i] * omega_output[i] - omega_r) ) ** 2 * ds_r ) ) / assemble(Constant(1.0) * ds_r))
+   sqrt(assemble( ( ((n_circle( omega_output ))[i] * omega_output[i] - omega_r ) ** 2 * ds_r ) ) / assemble(Constant(1.0) * ds_r))
   )
 print( "\t<<(n^i \omega_i - psi )^2>>_R = ", \
-   sqrt(assemble( ( (((n_circle( omega_output ))[i] * omega_output[i] - omega_R) ) ** 2 * ds_R ) ) / assemble( Constant(1.0) * ds_R))
+   sqrt(assemble( ( ((n_circle( omega_output ))[i] * omega_output[i] - omega_R ) ** 2 * ds_R ) ) / assemble( Constant(1.0) * ds_R))
+  )
+
+print("Check if the PDE is satisfied:")
+print( "\t<<(fel - flaplace)^2>> = ", \
+   sqrt(assemble( ( (  fel_n( omega_output, mu_output, nu_output, kappa ) + flaplace( sigma, omega_output) ) ** 2 * dx ) ) / assemble(Constant(1.0) * dx))
   )
