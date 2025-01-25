6c08f9c24dc9ef5465f483c3e103e347e2ffd72a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-01-25 19:24:03 +0100

    ... nu -> fsp.nu ...

--- steady-state-no-flow/variational_problem_bc_ring.py
+++ steady-state-no-flow/variational_problem_bc_ring.py
@@ -124,10 +124,10 @@ bcs = [bc_z_r, bc_z_R]
 
 # Define variational problem
 
-F_z = (kappa * (geo.g_c( fsp.omega )[i, j] * nu[j] * (nu_z.dx( i )) - 2.0 * fsp.mu * ((fsp.mu ** 2) - K( fsp.omega )) * nu_z) + sigma * fsp.mu * nu_z) * sqrt_detg( fsp.omega ) * dx \
+F_z = (kappa * (geo.g_c( fsp.omega )[i, j] * fsp.nu[j] * (nu_z.dx( i )) - 2.0 * fsp.mu * ((fsp.mu ** 2) - K( fsp.omega )) * nu_z) + sigma * fsp.mu * nu_z) * sqrt_detg( fsp.omega ) * dx \
       - ( \
-                  + (kappa * (n_circle( fsp.omega ))[i] * nu_z * nu[i]) * sqrt_deth_circle( fsp.omega, c_r ) * (1.0 / r) * ds_r \
-                  + (kappa * (n_circle( fsp.omega ))[i] * nu_z * nu[i]) * sqrt_deth_circle( fsp.omega, c_R ) * (1.0 / R) * ds_R
+                  + (kappa * (n_circle( fsp.omega ))[i] * nu_z * fsp.nu[i]) * sqrt_deth_circle( fsp.omega, c_r ) * (1.0 / r) * ds_r \
+                  + (kappa * (n_circle( fsp.omega ))[i] * nu_z * fsp.nu[i]) * sqrt_deth_circle( fsp.omega, c_R ) * (1.0 / R) * ds_R
       )
 
 F_omega = (- z * Nabla_v( nu_omega, fsp.omega )[i, i] - fsp.omega[i] * nu_omega[i]) * sqrt_detg( fsp.omega ) * dx \
@@ -136,7 +136,7 @@ F_omega = (- z * Nabla_v( nu_omega, fsp.omega )[i, i] - fsp.omega[i] * nu_omega[
 
 F_mu = ((H( fsp.omega ) - fsp.mu) * nu_mu) * sqrt_detg( fsp.omega ) * dx
 
-F_nu = (nu[i] * nu_nu[i] + fsp.mu * Nabla_v( nu_nu, fsp.omega )[i, i]) * sqrt_detg( fsp.omega ) * dx \
+F_nu = (fsp.nu[i] * nu_nu[i] + fsp.mu * Nabla_v( nu_nu, fsp.omega )[i, i]) * sqrt_detg( fsp.omega ) * dx \
        - ((n_circle( fsp.omega ))[i] * g( fsp.omega )[i, j] * fsp.mu * nu_nu[j]) * sqrt_deth_circle( fsp.omega, c_r ) * (1.0 / r) * ds_r \
        - ((n_circle( fsp.omega ))[i] * g( fsp.omega )[i, j] * fsp.mu * nu_nu[j]) * sqrt_deth_circle( fsp.omega, c_r ) * (1.0 / R) * ds_R
 
