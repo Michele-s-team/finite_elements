ce5b6180fc97837560304122eeef4c4930068b97
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-02-06 11:56:45 +0100

    Update variational_problem_bc_ring.py

--- steady-state-flow/variational_problem_bc_ring.py
+++ steady-state-flow/variational_problem_bc_ring.py
@@ -106,7 +106,7 @@ F_sigma = (geo.Nabla_v( v, fsp.omega )[i, i] - 2.0 * geo.H( fsp.omega ) * w) * g
 
 F_v = ( \
                     rho * ( \
-                          (v[j] * geo.Nabla_v( v, fsp.omega )[i, j] - 2.0 * v[j] * w * geo.g_c( fsp.omega )[i, k] * b( fsp.omega )[k, j]) * geo.nu_v[i] \
+                          (v[j] * geo.Nabla_v( v, fsp.omega )[i, j] - 2.0 * v[j] * w * geo.g_c( fsp.omega )[i, k] * geo.b( fsp.omega )[k, j]) * geo.nu_v[i] \
                           + 1.0 / 2.0 * (w ** 2) * geo.g_c( fsp.omega )[i, j] * geo.Nabla_f( geo.nu_v, fsp.omega )[i, j] \
                   ) \
                     + (fsp.sigma * geo.g_c( fsp.omega )[i, j] * geo.Nabla_f( geo.nu_v, fsp.omega )[i, j] \
@@ -126,7 +126,7 @@ F_v = ( \
       )
 
 F_w = ( \
-                    rho * (v[i] * v[k] * b( fsp.omega )[k, i]) * geo.nu_w \
+                    rho * (v[i] * v[k] * geo.b( fsp.omega )[k, i]) * geo.nu_w \
                     - rho * w * geo.Nabla_v( vector_times_scalar( v, geo.nu_w ), fsp.omega )[i, i] \
                     + 2.0 * kappa * ( \
                                   - geo.g_c( fsp.omega )[i, j] * ((geo.H( fsp.omega )).dx( i )) * (geo.nu_w.dx( j )) \
@@ -135,7 +135,7 @@ F_w = ( \
                     - ( \
                                   2.0 * fsp.sigma * geo.H( fsp.omega ) \
                                   + 2.0 * eta * (geo.g_c( fsp.omega )[i, k] * geo.Nabla_v( v, fsp.omega )[j, k] *
-                                                 (b( fsp.omega ))[i, j] - 2.0 * w * (2.0 * (geo.H( fsp.omega )) ** 2 - K( fsp.omega )))
+                                                 (geo.b( fsp.omega ))[i, j] - 2.0 * w * (2.0 * (geo.H( fsp.omega )) ** 2 - K( fsp.omega )))
                     ) * geo.nu_w
       ) * geo.sqrt_detg( fsp.omega ) * rmsh.dx \
 + rho * ( \
