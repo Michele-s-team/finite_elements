d1411ddc53e43ddec189382116bc0477f042f9f2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-02-06 14:19:05 +0100

    CHECK OK v, z and omega and mu agree with the exact value!
    - python3 generate_ring_mesh.py 0.05
    - clear; clear; rm -r solution; python3 solve.py /home/fenics/shared/steady-state-flow/mesh /home/fenics/shared/steady-state-flow/solution
    - check-with-analutical-solution-bc-ring.nb

--- steady-state-flow/print_out_bc_ring.py
+++ steady-state-flow/print_out_bc_ring.py
@@ -22,25 +22,14 @@ xdmffile_check.parameters.update( {"functions_share_mesh": True, "rewrite_functi
 # copy the data of the  solution psi into v_output, ..., z_output, which will be allocated or re-allocated here
 v_output, w_output, sigma_output, z_output, omega_output, mu_output = fsp.psi.split( deepcopy=True )
 
+print( "Check of BCs:" )
 
-# print( "\int_{\partial \Omega_r} (n^i \omega_i - psi )^2 dS = ", \
-#    assemble( ( (((n_circle( omega_output ))[i] * omega_output[i] - omega_r) ) ** 2 * ds_r ) )
-#   )
+print( f"\t\t<<(v^i n_i - v_R)^2>>_[partial Omega R] = {col.Fore.RED}{msh.difference_wrt_measure( bgeo.n_circle( omega_output )[i] * geo.g( omega_output )[i, j] * v_output[j], vp.v_R, rmsh.ds_R ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
+
+print( f"\t\t<<(z - phi)^2>>_[partial Omega r] = {col.Fore.RED}{msh.difference_wrt_measure( z_output, vp.z_r, rmsh.ds_r ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
+print( f"\t\t<<(z - phi)^2>>_[partial Omega R] = {col.Fore.RED}{msh.difference_wrt_measure( z_output, vp.z_R, rmsh.ds_R ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
 
 print(
     f"\t\t<<(n^i \omega_i - psi )^2>>_[partial Omega r] = {col.Fore.RED}{msh.difference_wrt_measure( (bgeo.n_circle( omega_output ))[i] * omega_output[i], vp.omega_r, rmsh.ds_r ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
-
-
-# print( "\int_{\partial \Omega_R} (n^i \omega_i - psi )^2 dS = ", \
-#    assemble( ( (((n_circle( omega_output ))[i] * omega_output[i] - omega_R) ) ** 2 * ds_R ) )
-#   )
-
 print(
     f"\t\t<<(n^i \omega_i - psi )^2>>_[partial Omega R] = {col.Fore.RED}{msh.difference_wrt_measure( (bgeo.n_circle( omega_output ))[i] * omega_output[i], vp.omega_R, rmsh.ds_R ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
-
-
-# print( "\int_{\partial \Omega_R} (v^i n_i)^2 dS = ", \
-#    assemble( ( (n_circle( omega_output )[i] * g( omega_output )[i, j] * v_output[j] - v_R) ** 2 * ds_R ) )
-#   )
-
-print( f"\t\t<<(v^i n_i - v_R)^2>>_[partial Omega R] = {col.Fore.RED}{msh.difference_wrt_measure( bgeo.n_circle( omega_output )[i] * geo.g( omega_output )[i, j] * v_output[j], vp.v_R, rmsh.ds_R ):.{io.number_of_decimals}e}{col.Style.RESET_ALL}" )
