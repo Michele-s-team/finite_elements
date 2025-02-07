ac4dad7e97fb8794ec5ec4b8b395d9514ba71731
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-02-07 15:13:47 +0100

    Update variational_problem_bc_square_a.py

--- steady-state-flow/variational_problem_bc_square_a.py
+++ steady-state-flow/variational_problem_bc_square_a.py
@@ -84,21 +84,21 @@ class omega_square_Expression( UserExpression ):
 
 
 # the values of \partial_i z = omega_i on the circle and on the square, to be used in the boundary conditions (BCs) imposed with Nitche's method, in F_N
-omega_circle = interpolate( omega_circle_Expression( element=Q_z.ufl_element() ), Q_z )
-omega_square = interpolate( omega_square_Expression( element=Q_z.ufl_element() ), Q_z )
+omega_circle = interpolate( omega_circle_Expression( element=fsp.Q_z.ufl_element() ), fsp.Q_z )
+omega_square = interpolate( omega_square_Expression( element=fsp.Q_z.ufl_element() ), fsp.Q_z )
 
-v_0.interpolate(TangentVelocityExpression(element=Q_v.ufl_element()))
-w_0.interpolate(NormalVelocityExpression(element=Q_w.ufl_element()))
-sigma_0.interpolate( SurfaceTensionExpression( element=Q_sigma.ufl_element() ))
-omega_0.interpolate( OmegaExpression( element=Q_omega.ufl_element() ))
-z_0.interpolate( ManifoldExpression( element=Q_z.ufl_element() ) )
+fsp.v_0.interpolate(TangentVelocityExpression(element=fsp.Q_v.ufl_element()))
+fsp.w_0.interpolate(NormalVelocityExpression(element=fsp.Q_w.ufl_element()))
+fsp.sigma_0.interpolate( SurfaceTensionExpression( element=fsp.Q_sigma.ufl_element() ))
+fsp.omega_0.interpolate( OmegaExpression( element=fsp.Q_omega.ufl_element() ))
+fsp.z_0.interpolate( ManifoldExpression( element=fsp.Q_z.ufl_element() ) )
 
 #uncomment this if you want to assign to psi the initial profiles stored in v_0, ..., z_0
 # assigner.assign(psi, [v_0, w_0, sigma_0, omega_0, z_0])
 
 
 # CHANGE PARAMETERS HERE
-l_profile_v = Expression( ('v_l', '0'), v_l=v_l, element = Q_v.ufl_element() )
+l_profile_v = Expression( ('v_l', '0'), v_l=v_l, element = fsp.Q_v.ufl_element() )
 # CHANGE PARAMETERS HERE
 
 # boundary conditions (BCs)
@@ -122,27 +122,19 @@ bc_z_square = DirichletBC( Q.sub( 4 ), Expression( '0.0', element=Q.sub( 4 ).ufl
 # all BCs
 bcs = [bc_v_l, bc_w_lr, bc_w_tb, bc_w_circle, bc_sigma, bc_z_circle, bc_z_square]
 
-#set initial profiles
-# v_n_1.interpolate(TangentVelocityExpression(element=Q_v_n.ufl_element()))
-# v_n_2.assign(v_n_1)
-# w_n_1.interpolate(NormalVelocityExpression(element=Q_w_n.ufl_element()))
-# sigma_n_32.interpolate( SurfaceTensionExpression( element=Q_phi.ufl_element() ))
-# z_n_32.interpolate( ManifoldExpression( element=Q_z_n.ufl_element() ) )
-# omega_n_32.interpolate( OmegaExpression( element=Q_omega_n.ufl_element() ))
-
 # Define variational problem : F_v, F_z are related to the PDEs for v, ..., z respectively . F_N enforces the BCs with Nitsche's method.
 # To be safe, I explicitly wrote each term on each part of the boundary with its own normal vector and pull-back of the metric: for example, on the left (l) and on the right (r) sides of the rectangle,
 # the surface elements are ds_l + ds_r, and the normal is n_lr(omega), and the pull-back of the metric is sqrt_deth_lr: this avoids odd interpolations at the corners of the rectangle edges.
 
-F_sigma = (Nabla_v( v, omega )[i, i] - 2.0 * H( omega ) * w) * nu_sigma * sqrt_detg( omega ) * dx
+F_sigma = (geo.Nabla_v( v, omega )[i, i] - 2.0 * H( omega ) * w) * nu_sigma * sqrt_detg( omega ) * dx
 
 F_v = ( \
                     rho * ( \
-                          (v[j] * Nabla_v( v, omega )[i, j] - 2.0 * v[j] * w * g_c( omega )[i, k] * b( omega )[k, j]) * nu_v[i] \
-                          + 1.0 / 2.0 * (w ** 2) * g_c( omega )[i, j] * Nabla_f( nu_v, omega )[i, j] \
+                          (v[j] * geo.Nabla_v( v, omega )[i, j] - 2.0 * v[j] * w * g_c( omega )[i, k] * b( omega )[k, j]) * nu_v[i] \
+                          + 1.0 / 2.0 * (w ** 2) * g_c( omega )[i, j] * geo.Nabla_f( nu_v, omega )[i, j] \
                   ) \
-                    + (sigma * g_c( omega )[i, j] * Nabla_f( nu_v, omega )[i, j] \
-                       + 2.0 * eta * d_c( v, w, omega )[j, i] * Nabla_f( nu_v, omega )[j, i])
+                    + (sigma * g_c( omega )[i, j] * geo.Nabla_f( nu_v, omega )[i, j] \
+                       + 2.0 * eta * d_c( v, w, omega )[j, i] * geo.Nabla_f( nu_v, omega )[j, i])
       ) * sqrt_detg( omega ) * dx \
       - rho / 2.0 * ( \
                     ((w ** 2) * (n_lr( omega ))[i] * nu_v[i]) * sqrt_deth_lr( omega ) * (ds_l + ds_r) \
@@ -164,14 +156,14 @@ F_v = ( \
 
 F_w = ( \
                     rho * (v[i] * v[k] * b( omega )[k, i]) * nu_w \
-                    - rho * w * Nabla_v( vector_times_scalar( v, nu_w ), omega )[i, i] \
+                    - rho * w * geo.Nabla_v( vector_times_scalar( v, nu_w ), omega )[i, i] \
                     + 2.0 * kappa * ( \
                                   - g_c( omega )[i, j] * ((H( omega )).dx( i )) * (nu_w.dx( j )) \
                                   + 2.0 * H( omega ) * ((H( omega )) ** 2 - K( omega )) * nu_w \
                           ) \
                     - ( \
                                   2.0 * sigma * H( omega ) \
-                                  + 2.0 * eta * (g_c( omega )[i, k] * Nabla_v( v, omega )[j, k] *
+                                  + 2.0 * eta * (g_c( omega )[i, k] * geo.Nabla_v( v, omega )[j, k] *
                                                  (b( omega ))[i, j] - 2.0 * w * (2.0 * (H( omega )) ** 2 - K( omega )))
                     ) * nu_w
       ) * sqrt_detg( omega ) * dx \
@@ -192,7 +184,7 @@ F_z = ( \
 
 
 
-F_omega = ( z * Nabla_v( nu_omega, omega )[i, i] + omega[i] * nu_omega[i] ) * sqrt_detg( omega ) * dx \
+F_omega = ( z * geo.Nabla_v( nu_omega, omega )[i, i] + omega[i] * nu_omega[i] ) * sqrt_detg( omega ) * dx \
           - ( \
                         ( (n_lr( omega ))[i] * g( omega )[i, j] * z * nu_omega[j] ) * sqrt_deth_lr( omega ) * (ds_l + ds_r) \
                         + ( (n_tb( omega ))[i] * g( omega )[i, j] * z * nu_omega[j] ) * sqrt_deth_tb( omega ) * (ds_t + ds_b) \
