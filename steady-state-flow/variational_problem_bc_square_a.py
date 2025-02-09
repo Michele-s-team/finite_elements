c949ed88ce413217aa96d78ec1c3f2ed277519b3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-02-09 17:31:34 +0100

    CHECK  OK
    The BCs are satisfied and the remainder goes to zero as the mesh gets finer
    -  python3 generate_square_mesh.py 0.05 [0.01, 0.005]
    - rm -r solution; python3 solve.py /home/fenics/shared/steady-state-flow/mesh /home/fenics/shared/steady-state-flow/solution
    - check.xdmf

--- steady-state-flow/variational_problem_bc_square_a.py
+++ steady-state-flow/variational_problem_bc_square_a.py
@@ -264,4 +264,4 @@ F_pp_nu = (fsp.nu[i] * fsp.nu_nu[i] + fsp.mu * geo.Nabla_v( fsp.nu_nu, fsp.omega
 F_pp_tau = (fsp.nu[i] * geo.g_c( fsp.omega )[i, j] * (fsp.nu_tau.dx( j )) + fsp.tau * fsp.nu_tau) * geo.sqrt_detg( fsp.omega ) * rmsh.dx \
            - ((bgeo.n_lr( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_lr( fsp.omega ) * rmsh.ds_lr \
            - ((bgeo.n_tb( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_tb( fsp.omega ) * rmsh.ds_tb \
-           - ((bgeo.n_circle( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle( fsp.omega, rmsh.c_r ) * (1.0 / rmsh.r) * rmsh.ds_r \
+           - ((bgeo.n_circle( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle( fsp.omega, rmsh.c_r ) * (1.0 / rmsh.r) * rmsh.ds_r
