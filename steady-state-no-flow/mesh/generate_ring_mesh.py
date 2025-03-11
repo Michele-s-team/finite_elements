9da8f65ec5918c3c2a7f3028694d2071343d70aa
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-03-11 15:32:29 +0100

    Added output directory

--- steady-state-no-flow/mesh/generate_ring_mesh.py
+++ steady-state-no-flow/mesh/generate_ring_mesh.py
@@ -14,23 +14,28 @@ import pygmsh
 import argparse
 import sys
 
-#add the path where to find the shared modules
+# add the path where to find the shared modules
 module_path = '/home/fenics/shared/modules'
-sys.path.append(module_path)
+sys.path.append( module_path )
 
 import input_output as io
 import mesh as mesh_module
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("resolution")
-parser.add_argument("output_directory")
+parser.add_argument( "resolution" )
+parser.add_argument( "output_directory" )
 args = parser.parse_args()
 
-#mesh resolution
-resolution = (float)(args.resolution)
+# mesh resolution
+resolution = (float)( args.resolution )
 
-mesh_file = args.output_directory + "/mesh.msh"
+# add '/' to output_directory if it is missing
+output_directory = args.output_directory
+print( f"before = {output_directory}" )
+output_directory = io.add_trailing_slash( output_directory )
+print( f"after = {output_directory}" )
+
+mesh_file = output_directory + "mesh.msh"
 
 # parameters
 r = 1.0
@@ -43,40 +48,44 @@ geometry = pygmsh.geo.Geometry()
 model = geometry.__enter__()
 
 # Add circle
-circle_r = model.add_circle(c_r, r, mesh_size=resolution)
-circle_R = model.add_circle(c_R, R, mesh_size=resolution)
+circle_r = model.add_circle( c_r, r, mesh_size=resolution )
+circle_R = model.add_circle( c_R, R, mesh_size=resolution )
 
-plane_surface = model.add_plane_surface(circle_R.curve_loop, holes=[circle_r.curve_loop])
+plane_surface = model.add_plane_surface( circle_R.curve_loop, holes=[circle_r.curve_loop] )
 
 model.synchronize()
-model.add_physical([plane_surface], "Volume")
+model.add_physical( [plane_surface], "Volume" )
 
-#I will read this tagged element with `ds_circle = Measure("ds", domain=mesh, subdomain_data=mf, subdomain_id=2)`
-model.add_physical(circle_r.curve_loop.curves, "Circle r")
-model.add_physical(circle_R.curve_loop.curves, "Circle R")
+# I will read this tagged element with `ds_circle = Measure("ds", domain=mesh, subdomain_data=mf, subdomain_id=2)`
+model.add_physical( circle_r.curve_loop.curves, "Circle r" )
+model.add_physical( circle_R.curve_loop.curves, "Circle R" )
 
-geometry.generate_mesh(64)
-gmsh.write(mesh_file)
+geometry.generate_mesh( 64 )
+gmsh.write( mesh_file )
+
+mesh_module.write_mesh_to_csv( mesh_file, output_directory + 'line_vertices.csv' )
+
 gmsh.clear()
 geometry.__exit__()
 
-mesh_from_file = meshio.read(mesh_file)
+mesh_from_file = meshio.read( mesh_file )
+
 
 def create_mesh(mesh, cell_type, prune_z=False):
-    cells = mesh.get_cells_type(cell_type)
-    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+    cells = mesh.get_cells_type( cell_type )
+    cell_data = mesh.get_cell_data( "gmsh:physical", cell_type )
     points = mesh.points[:, :2] if prune_z else mesh.points
-    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={
-                           "name_to_read": [cell_data]})
+    out_mesh = meshio.Mesh( points=points, cells={cell_type: cells}, cell_data={
+        "name_to_read": [cell_data]} )
     return out_mesh
 
-line_mesh = create_mesh(mesh_from_file, "line", prune_z=True)
-meshio.write(args.output_directory + "/line_mesh.xdmf", line_mesh)
 
-triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=True)
-meshio.write(args.output_directory + "/triangle_mesh.xdmf", triangle_mesh)
+line_mesh = create_mesh( mesh_from_file, "line", prune_z=True )
+meshio.write( output_directory + "line_mesh.xdmf", line_mesh )
 
+triangle_mesh = create_mesh( mesh_from_file, "triangle", prune_z=True )
+meshio.write( output_directory + "triangle_mesh.xdmf", triangle_mesh )
 
-#print the mesh vertices to file
-mesh = mesh_module.read_mesh(args.output_directory + "/triangle_mesh.xdmf")
-io.print_vertices_to_csv_file(mesh,args.output_directory +  "/vertices.csv" )
+# print the mesh vertices to file
+mesh = mesh_module.read_mesh( output_directory + "triangle_mesh.xdmf" )
+io.print_vertices_to_csv_file( mesh, output_directory + "vertices.csv" )
