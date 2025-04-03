e5445db9782d6024ea342ae4c72182fd2b4bb68c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-03 15:02:03 +0200

    The velocity field is now symmetric !

--- steady-state-flow/read_mesh_square.py
+++ steady-state-flow/read_mesh_square.py
@@ -33,11 +33,11 @@ c_r = [L/2.0, h/2.0]
 
 # test for surface elements
 dx = Measure( "dx", domain=bgeo.mesh, subdomain_data=sf, subdomain_id=1 )
-ds_l = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=2 )
-ds_r = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=3 )
-ds_t = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=4 )
-ds_b = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=5 )
-ds_circle = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=6 )
+ds_l = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=4 )
+ds_r = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=2 )
+ds_t = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=3 )
+ds_b = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=1 )
+ds_circle = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=5 )
 ds_lr = ds_l + ds_r
 ds_tb = ds_t + ds_b
 ds_square = ds_lr + ds_tb
