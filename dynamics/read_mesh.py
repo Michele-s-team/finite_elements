cd3a78a2ffb78fc97eaea95ed073f7bc4c7d5ee0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-05 14:34:03 +0200

    Remplaced strings with f{}

--- dynamics/read_mesh.py
+++ dynamics/read_mesh.py
@@ -48,9 +48,9 @@ import check_mesh_tags
 # CHANGE PARAMETERS HERE
 boundary = 'on_boundary'
 boundary_l = 'near(x[0], 0.0)'
-boundary_r = 'near(x[0], 2.0)'
-boundary_lr = 'near(x[0], 0) || near(x[0], 2.0)'
-boundary_tb = 'near(x[1], 0) || near(x[1], 0.5)'
-boundary_square = 'on_boundary && sqrt(pow(x[0] - 2.0/4.0, 2) + pow(x[1] - 0.5/2.0, 2)) > (0.01 + 0.5/2.0)/2.0'
-boundary_circle = 'on_boundary && sqrt(pow(x[0] - 2.0/4.0, 2) + pow(x[1] - 0.5/2.0, 2)) < (0.01 + 0.5/2.0)/2.0'
+boundary_r = f'near(x[0], {L})'
+boundary_lr = f'near(x[0], 0) || near(x[0], {L})'
+boundary_tb = f'near(x[1], 0) || near(x[1], {h})'
+boundary_square = f'on_boundary && sqrt(pow(x[0] - {c_r[0]}, 2) + pow(x[1] - {c_r[1]}, 2)) > ({r} + {c_r[1]})/2.0'
+boundary_circle = f'on_boundary && sqrt(pow(x[0] - {c_r[0]}, 2) + pow(x[1] - {c_r[1]}, 2)) < ({r} + {c_r[1]})/2.0'
 # CHANGE PARAMETERS HERE
