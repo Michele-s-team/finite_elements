a6004dc33fc9f9cd3ca2a2c1ed4142383a22e4a5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-05 14:25:09 +0200

    Fixed scripts to launch on abacus

--- run-on-abacus.sh
+++ run-on-abacus.sh
@@ -16,6 +16,6 @@ ssh $OUT "mkdir -p "$2"/solution/snapshots/csv"
 rsync -av modules/*.py $OUT:$2
 rsync -av $1/*.py $OUT:$2
 rsync -av /Users/michelecastellana/Documents/finite_elements/script_slurm_abacus.slurm $OUT:$2
-rsync -av --exclude 'mesh.msh' --exclude '.DS_Store' $1/mesh $OUT:$2
+rsync -av --exclude 'mesh.msh' --exclude '.DS_Store' $1/mesh/solution $OUT:$2/mesh
 
 ssh $OUT "cd "$2"; sbatch script_slurm_abacus.slurm"
