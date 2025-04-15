d56742af6a8795903b5aa8aa0c723b542a56f74b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-15 16:16:29 +0200

    Update compare-csv-files.sh

--- compare-csv-files.sh
+++ compare-csv-files.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-
-# run with ./compare-csv-files.sh steady-state-no-flow/solution-old steady-state-no-flow/solution-new
+# Usage: ./compare-csv-files.sh pathA pathB
 PATH_A=$1
 PATH_B=$2
 
-find "$PATH_A" -name '*.csv' -exec bash -c 'diff "$1" "$2/${1#"$3"/}"' _ {} "$PATH_B" "$PATH_A" \;
+find "$PATH_A" -name '*.csv' -exec bash -c '
+  FILE_A="$1"
+  FILE_B="$2/${1#"$3"/}"
+
+  if [ -f "$FILE_B" ]; then
+    if ! diff -q "$FILE_A" "$FILE_B" > /dev/null; then
+      echo "DIFFERS: ${FILE_A#"$3/"}"
+    fi
+  else
+    echo "ONLY in $3: ${FILE_A#"$3/"}"
+  fi
+' _ {} "$PATH_B" "$PATH_A" \;
