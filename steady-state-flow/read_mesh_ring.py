de909cffbdfef079d5733f7223ade9596db5fdf5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-19 19:59:06 +0200

    steady state flow / bc ring OK

--- steady-state-flow/read_mesh_ring.py
+++ steady-state-flow/read_mesh_ring.py
@@ -1,12 +1,8 @@
 from fenics import *
 from mshr import *
-import numpy as np
 
 import boundary_geometry as bgeo
-import geometry as geo
-import mesh as msh
 import runtime_arguments as rarg
-
 
 
 #read the triangles
@@ -32,35 +28,11 @@ c_r = [0, 0]
 c_R = [0, 0]
 #CHANGE PARAMETERS HERE
 
-
-
 # test for surface elements
 dx = Measure( "dx", domain=bgeo.mesh, subdomain_data=sf, subdomain_id=1 )
 ds_r = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=2 )
 ds_R = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=3 )
 ds = ds_r + ds_R
-
-#
-# #a function space used solely to define f_test_ds
-# Q_test = FunctionSpace( bgeo.mesh, 'P', 2 )
-#
-# # f_test_ds is a scalar function defined on the mesh, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
-# f_test_ds = Function( Q_test )
-#
-# #analytical expression for a  scalar function used to test the ds
-# class FunctionTestIntegralsds(UserExpression):
-#     def eval(self, values, x):
-#         c_test = [0.3, 0.76]
-#         r_test = 0.345
-#         values[0] = np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test)**2.0
-#     def value_shape(self):
-#         return (1,)
-#
-# f_test_ds.interpolate( FunctionTestIntegralsds( element=Q_test.ufl_element() ) )
-#
-# msh.test_mesh_integral(2.9021223108952894, f_test_ds, dx, '\int f dx')
-# msh.test_mesh_integral(2.7759459256115657, f_test_ds, ds_r, '\int f ds_r')
-# msh.test_mesh_integral(3.6717505977470717, f_test_ds, ds_R, '\int f ds_R')
 
 import check_mesh_tags_ring
 
