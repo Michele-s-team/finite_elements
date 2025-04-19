271ce75e3056b954e07790d2f48e4081fa8c1fa6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-19 20:52:18 +0200

    /generate-mesh/2d/symmetric-square-circle/circles now tags objects in canonical way

--- generate-mesh/2d/symmetric-square-circle/circles/read_mesh.py
+++ generate-mesh/2d/symmetric-square-circle/circles/read_mesh.py
@@ -14,10 +14,12 @@ from mshr import *
 import numpy as np
 import sys
 
+
 # add the path where to find the shared modules
 module_path = '/home/fenics/shared/modules'
 sys.path.append(module_path)
 
+import calculus as cal
 import geometry as geo
 import input_output as io
 import mesh as msh
@@ -27,10 +29,10 @@ parser.add_argument("input_directory")
 args = parser.parse_args()
 
 # CHANGE PARAMETERS HERE
-L = 1
-h = 1
+L = 0.5
+h = 0.5
 c_r = [L / 2, h / 2]
-r = 0.1
+r = 0.05
 
 c_test = [0.3, 0.76]
 r_test = 0.345
@@ -46,41 +48,22 @@ mesh = msh.read_mesh(args.input_directory + "/triangle_mesh.xdmf")
 vf = msh.read_mesh_components(mesh, 2, args.input_directory + "/triangle_mesh.xdmf")
 # read the lines
 cf = msh.read_mesh_components(mesh, 1, args.input_directory + "/line_mesh.xdmf")
-
-
 # read the vertices
 # sf = msh.read_mesh_components(mesh, 0, args.input_directory + "/vertex_mesh.xdmf")
 
 
-# analytical expression for a  scalar function used to test the ds
-class FunctionTestIntegralExpression(UserExpression):
-    def eval(self, values, x):
-        # values[0] = 1
-        values[0] = function_test_integral_expression(x)
-        # values[0] = x[0]
-
-    def value_shape(self):
-        return (1,)
-
-# analytical expression for a  scalar function used to test the ds
-class FunctionTestSymmetryExpression(UserExpression):
-    def eval(self, values, x):
-        values[0] = x[1] - h / 2
-
-    def value_shape(self):
-        return (1,)
-
-def function_test_integral_expression(x):
-    return np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test) ** 2.0
-    # return 1
-
-
 dx = Measure("dx", domain=mesh, subdomain_data=vf, subdomain_id=surface_id)
-ds_b = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=1)
-ds_r = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=2)
-ds_t = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=3)
-ds_l = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=4)
-ds_circle = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=5)
+ds_l = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=2)
+ds_r = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=3)
+ds_t = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=4)
+ds_b = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=5)
+ds_circle = Measure("ds", domain=mesh, subdomain_data=cf, subdomain_id=6)
+
+ds_lr = ds_l + ds_r
+ds_tb = ds_t + ds_b
+ds_square = ds_lr + ds_tb
+ds = ds_square + ds_circle
+
 
 '''
 dline_12 = Measure( "ds", domain=mesh, subdomain_data=cf, subdomain_id=line_12_id )
@@ -89,31 +72,59 @@ darc_21 = Measure( "ds", domain=mesh, subdomain_data=cf, subdomain_id=arc_21_id 
 dp_2 = Measure( "dP", domain=mesh, subdomain_data=sf, subdomain_id=p_2_id )
 '''
 
-# dp_O = Measure("dP", domain=mesh, subdomain_data=sf, subdomain_id=p_O_id)
 
-Q = FunctionSpace(mesh, 'P', 1)
+# a function space used solely to define function_test_integrals_fenics
+Q_test = FunctionSpace(mesh, 'P', 2)
 
-# f_test_ds is a scalar function defined on the mesh, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
-f_test = Function(Q)
-f_test_symmetry = Function(Q)
-f_test.interpolate(FunctionTestIntegralExpression(element=Q.ufl_element()))
-f_test_symmetry.interpolate(FunctionTestSymmetryExpression(element=Q.ufl_element()))
 
-msh.test_mesh_integral(0.9063111599974194, f_test, dx, '\int dx f_surface')
-# msh.test_mesh_integral(0.7932215226341747, f_test, dp_O, '\int dp f_{p_O}')
-msh.test_mesh_integral(0.7765772342243651, f_test, ds_b, '\int ds_b f')
-msh.test_mesh_integral(0.8056313961280863, f_test, ds_r, '\int ds_r f')
-msh.test_mesh_integral(0.9756236687066221, f_test, ds_t, '\int ds_t f')
-msh.test_mesh_integral(0.9620471547152678, f_test, ds_l, '\int ds_l f')
-msh.test_mesh_integral(0.6251868570121668, f_test, ds_circle, '\int ds_circle f')
+# function_test_integrals_fenics is a function of two variables, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
+def function_test_integrals(x):
+    return (np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test) ** 2.0)
 
-print(
-    f'int f_test_symmetry = {col.Fore.YELLOW}{assemble(f_test_symmetry * dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}')
 
-# msh.test_mesh_integral(0.687851524196998, f_test, ds_aux, '\int ds_{aux} f')
+# function_test_integrals_fenics is the same as function_test_integrals, but in fenics format
+function_test_integrals_fenics = Function(Q_test)
 
-'''
-msh.test_mesh_integral( 0.1588462551091818, f_test, dp_2, '\int dp f_{p_2}' )
-msh.test_mesh_integral( cal.integral_2d_curve( function_test_integral_expression, curve_arc_21 ), f_test, darc_21, '\int dl f_{arc_21}' )
-msh.test_mesh_integral( 0.652012217844941, f_test, dline_34, '\int dl f_{line_34}' )
-'''
+
+# analytical expression for a  scalar function used to test the ds
+class FunctionTestIntegrals(UserExpression):
+    def eval(self, values, x):
+        values[0] = function_test_integrals(x)
+
+    def value_shape(self):
+        return (1,)
+
+
+function_test_integrals_fenics.interpolate(FunctionTestIntegrals(element=Q_test.ufl_element()))
+
+integral_exact_dx = cal.surface_integral_rectangle(function_test_integrals, [0, 0], [L, h]) - \
+                    cal.surface_integral_disk(function_test_integrals, r, c_r)
+
+integral_exact_ds_l = cal.curve_integral_line(function_test_integrals, [0, 0], [0, h])
+integral_exact_ds_r = cal.curve_integral_line(function_test_integrals, [L, 0], [L, h])
+integral_exact_ds_t = cal.curve_integral_line(function_test_integrals, [0, h], [L, h])
+integral_exact_ds_b = cal.curve_integral_line(function_test_integrals, [0, 0], [L, 0])
+
+integral_exact_ds_circle = cal.curve_integral_circle(function_test_integrals, r, c_r)
+
+integral_exact_ds_lr = integral_exact_ds_l + integral_exact_ds_r
+integral_exact_ds_tb = integral_exact_ds_t + integral_exact_ds_b
+
+integral_exact_ds_square = integral_exact_ds_lr + integral_exact_ds_tb
+
+integral_exact_ds = integral_exact_ds_square + integral_exact_ds_circle
+
+
+msh.test_mesh_integral(integral_exact_dx, function_test_integrals_fenics, dx, '\int f dx')
+
+msh.test_mesh_integral(integral_exact_ds_l, function_test_integrals_fenics, ds_l, '\int f ds_l')
+msh.test_mesh_integral(integral_exact_ds_r, function_test_integrals_fenics, ds_r, '\int f ds_r')
+msh.test_mesh_integral(integral_exact_ds_t, function_test_integrals_fenics, ds_t, '\int f ds_t')
+msh.test_mesh_integral(integral_exact_ds_b, function_test_integrals_fenics, ds_b, '\int f ds_b')
+
+msh.test_mesh_integral(integral_exact_ds_lr, function_test_integrals_fenics, ds_lr, '\int f ds_lr')
+msh.test_mesh_integral(integral_exact_ds_tb, function_test_integrals_fenics, ds_tb, '\int f ds_tb')
+
+msh.test_mesh_integral(integral_exact_ds_square, function_test_integrals_fenics, ds_square, '\int f ds_square')
+
+msh.test_mesh_integral(integral_exact_ds, function_test_integrals_fenics, ds, '\int f ds')
