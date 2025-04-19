f70bf6e6b5fdfcf475bd098aa379ae32802398df
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-19 19:12:11 +0200

    Switched to square no circle

--- steady-state-no-flow/read_mesh_ring.py
+++ steady-state-no-flow/read_mesh_ring.py
@@ -1,11 +1,8 @@
 from fenics import *
 from dolfin import *
 from mshr import *
-import numpy as np
 
 import runtime_arguments as rarg
-import mesh as msh
-import geometry as geo
 import boundary_geometry as bgeo
 
 #read the triangles
@@ -40,27 +37,6 @@ ds_R = Measure( "ds", domain=bgeo.mesh, subdomain_data=mf, subdomain_id=3 )
 ds = ds_r + ds_R
 
 import check_mesh_tags_ring
-
-# #a function space used solely to define f_test_ds
-# Q_test = FunctionSpace( bgeo.mesh, 'P', 2 )
-#
-# # f_test_ds is a scalar function defined on the mesh, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
-# f_test_ds = Function( Q_test )
-
-# #analytical expression for a  scalar function used to test the ds
-# class FunctionTestIntegralsds(UserExpression):
-#     def eval(self, values, x):
-#         c_test = [0.3, 0.76]
-#         r_test = 0.345
-#         values[0] = np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test)**2.0
-#     def value_shape(self):
-#         return (1,)
-#
-# f_test_ds.interpolate( FunctionTestIntegralsds( element=Q_test.ufl_element() ) )
-#
-# msh.test_mesh_integral(2.90212, f_test_ds, dx, '\int f dx')
-# msh.test_mesh_integral(2.77595, f_test_ds, ds_r, '\int f ds_r')
-# msh.test_mesh_integral(3.67175, f_test_ds, ds_R, '\int f ds_R')
 
 # Define boundaries and obstacle
 #CHANGE PARAMETERS HERE
--- steady-state-no-flow/read_mesh_square_no_circle.py
+++ steady-state-no-flow/read_mesh_square_no_circle.py
@@ -4,8 +4,8 @@ from mshr import *
 import numpy as np
 
 import runtime_arguments as rarg
-import mesh as msh
-import geometry as geo
+# import mesh as msh
+# import geometry as geo
 import boundary_geometry as bgeo
 
 
@@ -44,36 +44,37 @@ ds_lr = ds_l + ds_r
 ds_tb = ds_t + ds_b
 ds = ds_lr + ds_tb
 
+#
+# #a function space used solely to define f_test_ds
+# Q_test = FunctionSpace( bgeo.mesh, 'P', 2 )
+#
+# # f_test_ds is a scalar function defined on the mesh, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
+# f_test_ds = Function( Q_test )
+#
+# #analytical expression for a  scalar function used to test the ds
+# class FunctionTestIntegralsds(UserExpression):
+#     def eval(self, values, x):
+#         c_test = [0.3, 0.76]
+#         r_test = 0.345
+#         values[0] = np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test)**2.0
+#     def value_shape(self):
+#         return (1,)
+#
+# f_test_ds.interpolate( FunctionTestIntegralsds( element=Q_test.ufl_element() ) )
+#
+# msh.test_mesh_integral(0.9376440450770376, f_test_ds, dx, '\int f dx')
+#
+# msh.test_mesh_integral(3.519879453774341, f_test_ds, ds, '\int f ds')
+#
+# msh.test_mesh_integral(1.7676785508433541, f_test_ds, ds_lr, '\int f ds_lr')
+# msh.test_mesh_integral(1.752200902930987, f_test_ds, ds_tb, '\int f ds_tb')
+#
+# msh.test_mesh_integral(0.9620471547152678, f_test_ds, ds_l, '\int f ds_l')
+# msh.test_mesh_integral(0.8056313961280863, f_test_ds, ds_r, '\int f ds_r')
+# msh.test_mesh_integral(0.9756236687066221, f_test_ds, ds_t, '\int f ds_t')
+# msh.test_mesh_integral(0.7765772342243651, f_test_ds, ds_b, '\int f ds_b')
 
-#a function space used solely to define f_test_ds
-Q_test = FunctionSpace( bgeo.mesh, 'P', 2 )
-
-# f_test_ds is a scalar function defined on the mesh, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
-f_test_ds = Function( Q_test )
-
-#analytical expression for a  scalar function used to test the ds
-class FunctionTestIntegralsds(UserExpression):
-    def eval(self, values, x):
-        c_test = [0.3, 0.76]
-        r_test = 0.345
-        values[0] = np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test)**2.0
-    def value_shape(self):
-        return (1,)
-
-f_test_ds.interpolate( FunctionTestIntegralsds( element=Q_test.ufl_element() ) )
-
-msh.test_mesh_integral(0.9376440450770376, f_test_ds, dx, '\int f dx')
-
-msh.test_mesh_integral(3.519879453774341, f_test_ds, ds, '\int f ds')
-
-msh.test_mesh_integral(1.7676785508433541, f_test_ds, ds_lr, '\int f ds_lr')
-msh.test_mesh_integral(1.752200902930987, f_test_ds, ds_tb, '\int f ds_tb')
-
-msh.test_mesh_integral(0.9620471547152678, f_test_ds, ds_l, '\int f ds_l')
-msh.test_mesh_integral(0.8056313961280863, f_test_ds, ds_r, '\int f ds_r')
-msh.test_mesh_integral(0.9756236687066221, f_test_ds, ds_t, '\int f ds_t')
-msh.test_mesh_integral(0.7765772342243651, f_test_ds, ds_b, '\int f ds_b')
-
+import check_mesh_tags_square_no_circle
 
 # Define boundaries and obstacle
 #CHANGE PARAMETERS HERE
