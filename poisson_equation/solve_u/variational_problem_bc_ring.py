4482087d5d133385497e0874b4a87eeb873226e2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-23 17:28:00 +0200

    test with poisson equation of symmetric ring OK clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh_ring_slice.py 0.1 $SOLUTION_PATH clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh_ring.py  ~/shared/generate-mesh/2d/ring_slice/solution $SOLUTION_PATH ~/shared/poisson_equation/solve_u$ clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir -p $SOLUTION_PATH/nodal_values; python3 solve.py /home/fenics/shared/generate-mesh/2d/symmetric_ring/solution /home/fenics/shared/poisson_equation/solve_u/$SOLUTION_PATH check-the-solution.nb

--- poisson_equation/solve_u/variational_problem_bc_ring.py
+++ poisson_equation/solve_u/variational_problem_bc_ring.py
@@ -13,10 +13,10 @@ i, j = ufl.indices(2)
 class u_exact_expression(UserExpression):
     def eval(self, values, x):
         # test case 1
-        # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
 
         # test case 2
-        values[0] = np.sin(2 * (np.pi) * (x[0] + x[1])) * np.cos(2 * (np.pi) * (x[0] - x[1]) ** 2)
+        # values[0] = np.sin(2 * (np.pi) * (x[0] + x[1])) * np.cos(2 * (np.pi) * (x[0] - x[1]) ** 2)
 
     def value_shape(self):
         return (1,)
@@ -25,14 +25,14 @@ class u_exact_expression(UserExpression):
 class grad_u_expression(UserExpression):
     def eval(self, values, x):
         # test case 1
-        # values[0] = 2.0 * x[0]
-        # values[1] = 4.0 * x[1]
+        values[0] = 2.0 * x[0]
+        values[1] = 4.0 * x[1]
 
         # test case 2
-        values[0] = 2 * (np.pi) * np.cos(2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * np.cos(2 * (np.pi) * ((x[0]) + (x[1]))) + 4 * (np.pi) * (-(x[0]) + (x[1])) * sin(
-            2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * sin(2 * (np.pi) * ((x[0]) + (x[1])))
-        values[1] = 2 * (np.pi) * np.cos(2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * np.cos(2 * (np.pi) * ((x[0]) + (x[1]))) + 4 * (np.pi) * ((x[0]) - (x[1])) * sin(
-            2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * sin(2 * (np.pi) * ((x[0]) + (x[1])))
+        # values[0] = 2 * (np.pi) * np.cos(2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * np.cos(2 * (np.pi) * ((x[0]) + (x[1]))) + 4 * (np.pi) * (-(x[0]) + (x[1])) * sin(
+        #     2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * sin(2 * (np.pi) * ((x[0]) + (x[1])))
+        # values[1] = 2 * (np.pi) * np.cos(2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * np.cos(2 * (np.pi) * ((x[0]) + (x[1]))) + 4 * (np.pi) * ((x[0]) - (x[1])) * sin(
+        #     2 * (np.pi) * ((x[0]) - (x[1])) ** 2) * sin(2 * (np.pi) * ((x[0]) + (x[1])))
 
     def value_shape(self):
         return (2,)
@@ -41,11 +41,11 @@ class grad_u_expression(UserExpression):
 class laplacian_u_expression(UserExpression):
     def eval(self, values, x):
         # test case 1
-        # values[0] = 6.0
+        values[0] = 6.0
 
         # test case 2
-        values[0] = 8 * (np.pi) * (-(np.pi) * (1 + 4 * (x[0] - (x[1])) ** 2) * np.cos(2 * (np.pi) * (x[0] - (x[1])) ** 2) - np.sin(2 * (np.pi) * (x[0] - (x[1])) ** 2)) * np.sin(
-            2 * (np.pi) * (x[0] + (x[1])))
+        # values[0] = 8 * (np.pi) * (-(np.pi) * (1 + 4 * (x[0] - (x[1])) ** 2) * np.cos(2 * (np.pi) * (x[0] - (x[1])) ** 2) - np.sin(2 * (np.pi) * (x[0] - (x[1])) ** 2)) * np.sin(
+        #     2 * (np.pi) * (x[0] + (x[1])))
 
     def value_shape(self):
         return (1,)
@@ -57,30 +57,30 @@ class hess_u_exact_expression(UserExpression):
 
     def eval(self, values, x):
         # test case 1
-        # values[0] = 2
-        # values[1] = 0
-        # values[2] = 0
-        # values[3] = 4
+        values[0] = 2
+        values[1] = 0
+        values[2] = 0
+        values[3] = 4
 
         # test case 2
-        values[0] = 4 * np.pi * (
-                -4 * np.pi * (x[0] - x[1]) * np.cos(2 * np.pi * (x[0] + x[1])) * np.sin(2 * np.pi * (x[0] - x[1]) ** 2)
-                - (np.pi * (1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
-                   + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
-        )
-
-        values[1] = 4 * np.pi * (
-                (np.pi * (-1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
-                 + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
-        )
-
-        values[2] = values[1]
-
-        values[3] = 4 * np.pi * (
-                4 * np.pi * (x[0] - x[1]) * np.cos(2 * np.pi * (x[0] + x[1])) * np.sin(2 * np.pi * (x[0] - x[1]) ** 2)
-                - (np.pi * (1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
-                   + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
-        )
+        # values[0] = 4 * np.pi * (
+        #         -4 * np.pi * (x[0] - x[1]) * np.cos(2 * np.pi * (x[0] + x[1])) * np.sin(2 * np.pi * (x[0] - x[1]) ** 2)
+        #         - (np.pi * (1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
+        #            + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
+        # )
+        #
+        # values[1] = 4 * np.pi * (
+        #         (np.pi * (-1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
+        #          + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
+        # )
+        #
+        # values[2] = values[1]
+        #
+        # values[3] = 4 * np.pi * (
+        #         4 * np.pi * (x[0] - x[1]) * np.cos(2 * np.pi * (x[0] + x[1])) * np.sin(2 * np.pi * (x[0] - x[1]) ** 2)
+        #         - (np.pi * (1 + 4 * x[0] ** 2 - 8 * x[0] * x[1] + 4 * x[1] ** 2) * np.cos(2 * np.pi * (x[0] - x[1]) ** 2)
+        #            + np.sin(2 * np.pi * (x[0] - x[1]) ** 2)) * np.sin(2 * np.pi * (x[0] + x[1]))
+        # )
 
     def value_shape(self):
         return (2, 2)
