ffe5b0ea43f5003f4a1baa075e7ac72df06c46f0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-23 16:08:54 +0200

    Update generate_mesh_ring.py

--- generate-mesh/2d/symmetric_ring/generate_mesh_ring.py
+++ generate-mesh/2d/symmetric_ring/generate_mesh_ring.py
@@ -5,7 +5,7 @@ This code generates a  ring  mesh with radial symmetry: symmetry is obtained by 
 run with
 python3 generate_mesh_ring.py [mesh resolution] [path where to read the ring slice] [path where to store the mesh]
 Example:
-clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh_ring.py 0.1 ~/shared/generate-mesh/2d/ring_slice/solution/ $SOLUTION_PATH
+clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh_ring.py 0.1 ~/shared/generate-mesh/2d/ring_slice/solution $SOLUTION_PATH
 
 '''
 
@@ -41,5 +41,21 @@ r = 1
 R = 2
 c_r = [0, 0]
 c_R = [0, 0]
-N = 16
+# M is the number of times the slice will be replicated
+M = 2
 # N = int(np.round(r * np.pi / resolution))
+
+output_dir = args.output_dir
+input_dir = args.input_dir
+mesh_slice_file = input_dir + "/mesh.msh"
+
+print(f'mesh_slice_file: {mesh_slice_file}')
+
+# Load the mesh slice
+mesh = meshio.read(mesh_slice_file)
+
+
+print('********** Mesh before mirroring: **********')
+msh.print_mesh_element_types(mesh)
+msh.print_mesh_triangles(mesh)
+msh.print_mesh_vertices(mesh)
