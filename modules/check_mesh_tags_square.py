d20d2825089646d09f6096bda6fc92dc9bd017fa
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-25 11:02:57 +0200

    Check - steady-state-flow/bc_ring_1 with symmetric_ring OK - steady-state-flow/bc_square_a with symmetric square circle mesh OK

--- modules/check_mesh_tags_square.py
+++ modules/check_mesh_tags_square.py
@@ -72,5 +72,6 @@ msh.test_mesh_integral(integral_exact_ds_lr, function_test_integrals_fenics, rms
 msh.test_mesh_integral(integral_exact_ds_tb, function_test_integrals_fenics, rmsh.ds_tb, '\int f ds_tb')
 
 msh.test_mesh_integral(integral_exact_ds_square, function_test_integrals_fenics, rmsh.ds_square, '\int f ds_square')
+msh.test_mesh_integral(integral_exact_ds_circle, function_test_integrals_fenics, rmsh.ds_circle, '\int f ds_circle')
 
 msh.test_mesh_integral(integral_exact_ds, function_test_integrals_fenics, rmsh.ds, '\int f ds')
