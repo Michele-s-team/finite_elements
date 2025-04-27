2e77d8b7d1d989bf84d58c8b1587e1ef9e7df2a3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-04-27 19:01:13 +0200

    Revising dynamics

--- dynamics/solve.py
+++ dynamics/solve.py
@@ -7,13 +7,16 @@ and which are stored into finite_elements/mesh/solution
 
 Run with
 clear; clear; rm -rf solution; mkdir solution; python3 solve.py [path where to read the mesh] [path where to store the solution] T k r e v N
-clear; clear; rm -rf solution; mkdir -p /home/fenics/shared/dynamics/solution/snapshots/csv/nodal_values; python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/solution  0.001 1.0 1.0 1.0 1.0 2
-clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir -p /home/fenics/shared/dynamics/$SOLUTION_PATH/snapshots/csv/nodal_values; python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/$SOLUTION_PATH  0.001 1.0 1.0 1.0 1.0 2
-clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir -p /home/fenics/shared/dynamics/$SOLUTION_PATH/snapshots/csv/nodal_values; python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/$SOLUTION_PATH  0.001 0.03 1.0 0.01 100.0 2
-clear; clear; rm -rf solution; mkdir -p /home/fenics/shared/dynamics/solution/snapshots/csv/nodal_values; mpirun -np 6 python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/solution  0.001 1.0 1.0 1.0 1.0 32
-time apptainer exec  /mnt/beegfs/common/containers/singularity/dev/FEniCS/FEniCS.sif python3 solve.py $MESH $SOLUTION $T $k $r $e $v $N
 
-The solution files will be stored in /home/fenics/shared/dynamics/solution
+Example:
+    clear; clear; rm -rf solution; python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/solution  0.001 1.0 1.0 1.0 1.0 2
+    clear; clear; MESH_PATH="/home/fenics/shared/dynamics/mesh/solution"; SOLUTION_PATH="/home/fenics/shared/dynamics/solution"; rm -rf $SOLUTION_PATH; python3 solve.py $MESH_PATH $SOLUTION_PATH  0.001 1.0 1.0 1.0 1.0 2
+    clear; clear; rm -rf solution; mpirun -np 6 python3 solve.py /home/fenics/shared/dynamics/mesh/solution /home/fenics/shared/dynamics/solution  0.001 1.0 1.0 1.0 1.0 32
+    time apptainer exec  /mnt/beegfs/common/containers/singularity/dev/FEniCS/FEniCS.sif python3 solve.py $MESH $SOLUTION $T $k $r $e $v $N
+
+
+
+
 
 Note that all sections of the code which need to be changed when an external parameter (e.g., the inflow velocity, the length of the Rectangle, etc...) is changed are bracketed by
 #CHANGE PARAMETERS HERE
