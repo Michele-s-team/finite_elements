a7849a3e0e9cd3d07acd5cf72b7fb4b33e74b6a7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-01 14:58:49 +0200

    Added poisson_equation/square_no_circle to mesh

--- testing/test.py
+++ testing/test.py
@@ -50,6 +50,14 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh_ring_slice', 0.1, problem_name)
+problem_name = 'square_no_circle'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square_no_circle',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_no_circle_mesh', 0.1, problem_name)
+
 
 
 '''
