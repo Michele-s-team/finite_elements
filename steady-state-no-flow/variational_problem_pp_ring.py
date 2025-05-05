5ece636efde036b463e55eb9e73471ee4e254a85
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-05 23:02:09 +0200

    bc-ring: now I solve for tau by using the pde diractly

--- steady-state-no-flow/variational_problem_pp_ring.py
+++ steady-state-no-flow/variational_problem_pp_ring.py
@@ -1,14 +1,14 @@
+import importlib
 import ufl as ufl
 
 import function_spaces as fsp
-import boundary_geometry as bgeo
 import geometry as geo
+import physics as phys
 import read_mesh_ring as rmsh
+import switch_problem as swi
 
-i, j, k, l = ufl.indices( 4 )
+vp = importlib.import_module(swi.vp)
 
+i, j, k, l = ufl.indices(4)
 
-
-F_pp_tau = (fsp.nu[i] * geo.g_c( fsp.omega )[i, j] * (fsp.nu_tau.dx( j )) + fsp.tau * fsp.nu_tau) * geo.sqrt_detg( fsp.omega ) * rmsh.dx \
-           - ((bgeo.n_circle( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle( fsp.omega, rmsh.c_r ) * (1.0 / rmsh.r) * rmsh.ds_r \
-           - ((bgeo.n_circle( fsp.omega ))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle( fsp.omega, rmsh.c_R ) * (1.0 / rmsh.R) * rmsh.ds_R
+F_pp_tau = phys.lhs_force_balance_equation(vp.kappa, fsp.omega, fsp.mu, fsp.sigma, fsp.tau) * fsp.nu_tau * geo.sqrt_detg(fsp.omega) * rmsh.dx
