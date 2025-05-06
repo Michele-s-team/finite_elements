b21d86b192a42044654c7fdf0f92374840e6b477
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-06 22:44:15 +0200

    Writing pp problem for tau

--- steady-state-flow/variational_problem_pp_ring.py
+++ steady-state-flow/variational_problem_pp_ring.py
@@ -1,15 +1,18 @@
+import importlib
 import ufl as ufl
 
 import function_spaces as fsp
-import boundary_geometry as bgeo
 import geometry as geo
-import read_mesh_ring as rmsh
+import physics as phys
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+vp = importlib.import_module(swi.vp)
+
 
 i, j, k, l = ufl.indices(4)
 
 
-F_pp_tau = (fsp.nu[i] * geo.g_c(fsp.omega)[i, j] * (fsp.nu_tau.dx(j)) + fsp.tau * fsp.nu_tau) * geo.sqrt_detg(fsp.omega) * rmsh.dx \
-           - ((bgeo.n_circle(fsp.omega))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle(fsp.omega, rmsh.c_r) * (1.0 / rmsh.r) * rmsh.ds_r \
-           - ((bgeo.n_circle(fsp.omega))[i] * fsp.nu_tau * fsp.nu[i]) * bgeo.sqrt_deth_circle(fsp.omega, rmsh.c_R) * (1.0 / rmsh.R) * rmsh.ds_R
+F_pp_tau = () * fsp.nu_tau * geo.sqrt_detg(fsp.omega) * rmsh.dx
 
 F_pp_d = ((geo.d(fsp.v, fsp.w, fsp.omega)[i, j] - fsp.d[i, j]) * fsp.nu_d[i, j]) * geo.sqrt_detg(fsp.omega) * rmsh.dx
