13cbd219d66d0664235853e672a63be45dfdc5a3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-06 22:55:51 +0200

    ... done.

--- steady-state-flow/variational_problem_pp_ring.py
+++ steady-state-flow/variational_problem_pp_ring.py
@@ -9,10 +9,13 @@ import switch_problem as swi
 rmsh = importlib.import_module(swi.rmsh)
 vp = importlib.import_module(swi.vp)
 
-
 i, j, k, l = ufl.indices(4)
 
-
-F_pp_tau = () * fsp.nu_tau * geo.sqrt_detg(fsp.omega) * rmsh.dx
+# tau is determined by solving Eq. (3c) in 'Notes'
+F_pp_tau = ( \
+                       - phys.conv_cn_n(fsp.v, fsp.v, fsp.v, fsp.w, fsp.w, fsp.omega, vp.rho) \
+                       + phys.lhs_force_balance_equation(vp.kappa, fsp.omega, fsp.mu, fsp.sigma, fsp.tau) \
+                       + phys.fvisc_n(fsp.v, fsp.w, fsp.omega, fsp.mu, vp.eta) \
+               ) * fsp.nu_tau * geo.sqrt_detg(fsp.omega) * rmsh.dx
 
 F_pp_d = ((geo.d(fsp.v, fsp.w, fsp.omega)[i, j] - fsp.d[i, j]) * fsp.nu_d[i, j]) * geo.sqrt_detg(fsp.omega) * rmsh.dx
