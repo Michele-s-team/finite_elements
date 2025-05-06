d0eb8675ec162f3c082104557fc751e8b048963f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-06 23:30:24 +0200

    bc_ring_1: added print out force on boundary

--- steady-state-flow/print_out_bc_square_b.py
+++ steady-state-flow/print_out_bc_square_b.py
@@ -37,4 +37,4 @@ print(
 print(
     f"\t\t<<(n^i \omega_i - omega_square )^2>>_[partial Omega tb] = {col.Fore.RED}{msh.difference_wrt_measure((bgeo.n_tb(prout.omega_output))[i] * prout.omega_output[i], vp.omega_square, rmsh.ds_tb):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
-import print_out_force_on_boundary_square
+import print_out_force_on_boundary_bc_square
