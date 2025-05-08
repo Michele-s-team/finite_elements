a980ba80493d635a5aba3080dcf30c8ded180de7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-08 18:42:59 +0200

    moved mirror_points to parent folder

--- generate_mesh/2d/square/symmetric_top_bottom/read_mesh_square.py
+++ generate_mesh/2d/square/symmetric_top_bottom/read_mesh_square.py
@@ -2,9 +2,9 @@
 This code reads the mesh generated from generate_mesh_square.py and it creates dvs and dss from labelled components of the mesh
 
 run with
-clear; clear; python3 read_mesh_square.py [path where to find the mesh]
-example:
-clear; clear; python3 read_mesh_square.py solution
+    clear; clear; python3 read_mesh_square.py [path where to find the mesh]
+Example:
+    clear; clear; python3 read_mesh_square.py solution
 '''
 import colorama as col
 from dolfin import *
