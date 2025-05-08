6d6bc36aa20136edb538f6ddbcba7ca1d03f709c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-08 12:18:46 +0200

    commented out temporarly

--- testing/test.py
+++ testing/test.py
@@ -58,7 +58,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', 0.1, problem_name)
-
+'''
 problem_name = 'two_squares_no_circle'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
                                                                      root_path,
@@ -197,7 +197,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                             root_path + case_name,
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_square_mesh', 0.1, problem_name)
-
+'''
 
 cmd.checkout(commit_a)
 
