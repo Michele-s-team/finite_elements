9fb669478719454599cb892d8875db2316ebea9b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-08 19:27:11 +0200

    Multiplied the entire equaiton for tau by dt

--- dynamics/variational_problem_pp_square.py
+++ dynamics/variational_problem_pp_square.py
@@ -12,18 +12,11 @@ vp = importlib.import_module(swi.vp)
 i, j, k, l = ufl.indices(4)
 
 # post-processing variational functional
-
-# F_pp_tau = (fsp.nu_n_12[i] * geo.g_c( fsp.omega_n_12 )[i, j] * (fsp.nu_tau.dx( j )) + fsp.tau_n_12 * fsp.nu_tau) * geo.sqrt_detg( fsp.omega_n_12 ) * rmsh.dx \
-#            - ((bgeo.n_lr( fsp.omega_n_12 ))[i] * fsp.nu_tau * fsp.nu_n_12[i]) * bgeo.sqrt_deth_lr( fsp.omega_n_12 ) * rmsh.ds_lr \
-#            - ((bgeo.n_tb( fsp.omega_n_12 ))[i] * fsp.nu_tau * fsp.nu_n_12[i]) * bgeo.sqrt_deth_tb( fsp.omega_n_12 ) * rmsh.ds_tb \
-#            - ((bgeo.n_circle( fsp.omega_n_12 ))[i] * fsp.nu_tau * fsp.nu_n_12[i]) * bgeo.sqrt_deth_circle( fsp.omega_n_12, rmsh.c_r ) * (1.0 / rmsh.r) * rmsh.ds_r
-
-
 F_pp_tau = ( \
-                       - vp.rho / vp.dt * (fsp.w_bar - fsp.w_n_1) \
-                       - phys.conv_cn_n(fsp.v_bar, fsp.v_n_1, fsp.v_n_2, fsp.w_bar, fsp.w_n_1, fsp.omega_n_12, vp.rho) \
-                       + phys.lhs_force_balance_equation(vp.kappa, fsp.omega_n_12, fsp.mu_n_12, fsp.sigma_n_32, fsp.tau_n_12) \
-                       + phys.fvisc_n(fsp.V, fsp.W, fsp.omega_n_12, fsp.mu_n_12, vp.eta) \
+                       - vp.rho * (fsp.w_bar - fsp.w_n_1) \
+                       - vp.dt * phys.conv_cn_n(fsp.v_bar, fsp.v_n_1, fsp.v_n_2, fsp.w_bar, fsp.w_n_1, fsp.omega_n_12, vp.rho) \
+                       + vp.dt * phys.lhs_force_balance_equation(vp.kappa, fsp.omega_n_12, fsp.mu_n_12, fsp.sigma_n_32, fsp.tau_n_12) \
+                       + vp.dt * phys.fvisc_n(fsp.V, fsp.W, fsp.omega_n_12, fsp.mu_n_12, vp.eta) \
                ) * fsp.nu_tau * geo.sqrt_detg(fsp.omega_n_12) * rmsh.dx
 
 F_pp_d = ((geo.d(fsp.V, fsp.W, fsp.omega_n_12)[i, j] - fsp.d[i, j]) * fsp.nu_d[i, j]) * geo.sqrt_detg(fsp.omega_n_12) * rmsh.dx
