15ff9c93d148ee6184ef05aceae509a630a8579e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-09 18:03:48 +0200

    Added ring test

--- testing/test.py
+++ testing/test.py
@@ -56,6 +56,15 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh_ring_slice', 0.1, problem_name, success)
 
+problem_name = 'ring'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/ring',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_ring_mesh', 0.1, problem_name, success)
+
+
 
 problem_name = 'square_no_circle'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
