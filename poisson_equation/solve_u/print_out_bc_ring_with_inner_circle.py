5d12e00c762e462553184a9910603438b23d6d2b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-09 14:50:38 +0200

    put back print_out_solution

--- poisson_equation/solve_u/print_out_bc_ring_with_inner_circle.py
+++ poisson_equation/solve_u/print_out_bc_ring_with_inner_circle.py
@@ -24,3 +24,5 @@ print("Comparison with exact solution: ")
 print(f"\t\t<<(u - u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u, fsp.u_exact, rmsh.dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 print(
     f"\t\t<<(hess_u - hess_u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_wrt_measure((fsp.hess_u[i, j] - fsp.hess_u_exact[i, j]) * (fsp.hess_u[i, j] - fsp.hess_u_exact[i, j]), Constant(0), rmsh.dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+
+import print_out_solution
