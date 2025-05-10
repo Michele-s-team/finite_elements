4ec57e54800a5db899af35caab3107de4e70aedb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-10 13:39:20 +0200

    now test chekcs tynamics only

--- testing/test.py
+++ testing/test.py
@@ -44,7 +44,7 @@ success = [True]
 
 cmd.run_command('clear; clear', success)
 
-
+'''
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
 
@@ -107,7 +107,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
-'''
+
 
 # Test poisson_equation/solve_u/periodic
 case_name = 'poisson_equation/solve_u/periodic'
@@ -209,7 +209,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                             root_path + case_name,
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_square_mesh', 0.1, problem_name, success)
-
+'''
 
 # Test dynamics
 case_name = 'dynamics'
@@ -229,7 +229,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                             root_path + case_name,
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_square_mesh', 0.1, problem_name, success)
-'''
+
 
 cmd.checkout(commit_a, success)
 
