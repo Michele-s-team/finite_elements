e5d86884f8529fd9d1f6798f6599515ec6b1ae55
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-13 00:20:03 +0200

    Uncommented all tests in test.py

--- testing/test.py
+++ testing/test.py
@@ -100,7 +100,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_two_squares_no_circle_mesh', 0.1, problem_name, success)
-'''
+
 
 problem_name = 'square'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
@@ -110,7 +110,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
-'''
+
 
 # Test poisson_equation/solve_u/periodic
 case_name = 'poisson_equation/solve_u/periodic'
@@ -153,7 +153,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', 0.1, problem_name, success)
 
-'''
+
 problem_name = 'square_a'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
                                                                      root_path,
@@ -170,7 +170,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
 
-'''
+
 
 # Test steady-state-flow
 case_name = 'steady-state-flow'
@@ -207,7 +207,7 @@ checks[case_name + '_' + problem_name + '_symmetric'] = utest.test_problem_and_m
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_ring_mesh', 0.1, problem_name, success)
 
-'''
+
 
 problem_name = 'square_a'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
@@ -264,7 +264,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                             root_path + case_name,
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_square_mesh', 0.1, problem_name, success)
-'''
+
 
 cmd.checkout(commit_a, success)
 
