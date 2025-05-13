0843118e9047ef5a399214df36ead3ab8d7d4f4c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-13 09:10:33 +0200

    Added two instances to test

--- testing/test.py
+++ testing/test.py
@@ -111,6 +111,23 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
 
+problem_name = 'square_symmetric_top_bottom'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square/symmetric_top_bottom',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_mesh', 0.1, problem_name, success)
+
+
+problem_name = 'square_symmetric_left_right_top_bottom'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square/symmetric_left_right_top_bottom',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_mesh', 0.1, problem_name, success)
+
 
 # Test poisson_equation/solve_u/periodic
 case_name = 'poisson_equation/solve_u/periodic'
