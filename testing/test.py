c87284bbee22e6c754ac29b852e6d4fead573a3a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-13 17:31:23 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -170,6 +170,14 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', 0.1, problem_name, success)
 
+problem_name = 'square_no_circle_a_symmetric'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square_no_circle/symmetric',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_no_circle_mesh', 0.1, problem_name, success)
+
 
 problem_name = 'square_a'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
