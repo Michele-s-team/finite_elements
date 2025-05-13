41c93fcd4e2599aa857b1980de489b1d5e9f1dbe
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-13 16:34:27 +0200

    Test now checks only ss no flow

--- testing/test.py
+++ testing/test.py
@@ -44,7 +44,7 @@ success = [True]
 
 cmd.run_command('clear; clear', success)
 
-
+'''
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
 
@@ -140,7 +140,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', 0.05, problem_name, success)
 
-
+'''
 
 # Test steady-state-no-flow
 case_name = 'steady-state-no-flow'
@@ -188,7 +188,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
 
 
 
-
+'''
 # Test steady-state-flow
 case_name = 'steady-state-flow'
 
@@ -281,7 +281,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                             root_path + case_name,
                             mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                             'generate_square_mesh', 0.1, problem_name, success)
-
+'''
 
 cmd.checkout(commit_a, success)
 
