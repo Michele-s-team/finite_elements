e5a94b4535d55455cafad97efa67c15a3c9a8d3f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-15 16:15:37 +0200

    ...

--- fourth_order_pde/dirichlet_bcs/switch_problem.py
+++ fourth_order_pde/dirichlet_bcs/switch_problem.py
@@ -4,12 +4,12 @@ import runtime_arguments as rarg
 
 if rarg.args.problem == 'ring_dirichlet':
     rmsh = 'read_mesh_ring'
-    vp = 'variational_problem_bc_ring'
-    prout_bc = 'print_out_bc_ring'
+    vp = 'variational_problem_bc_ring_dirichlet'
+    prout_bc = 'print_out_bc_ring_dirichlet'
 
-elif rarg.args.problem == 'square':
-    rmsh = 'read_mesh_square'
-    vp = 'variational_problem_bc_square'
-    prout_bc =  'print_out_bc_square'
+elif rarg.args.problem == 'ring_nitsche':
+    rmsh = 'read_mesh_ring'
+    vp = 'variational_problem_bc_ring_nitsche'
+    prout_bc = 'print_out_bc_ring_nitsche'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
