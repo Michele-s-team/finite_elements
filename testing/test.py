5da487e2a485064fcff54d621ee19122ad456189
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-15 22:55:10 +0200

    Added fourth-order-pde/ring_nitsche to test

--- testing/test.py
+++ testing/test.py
@@ -163,6 +163,15 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_ring_mesh', 0.1, problem_name, success)
 
 
+problem_name = 'ring_nitsche'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/ring',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_ring_mesh', 0.1, problem_name, success)
+
+
 
 # Test steady-state-no-flow
 case_name = 'steady-state-no-flow'
