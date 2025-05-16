8a399ae150c4a8dccefa1c91f371c0c041e634fc
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-16 23:16:46 +0200

    Update check-the-solution.nb

--- nitsche_method/one_field/print_out_solution.py
+++ nitsche_method/one_field/print_out_solution.py
@@ -5,7 +5,6 @@ import ufl as ufl
 import function_spaces as fsp
 import input_output as io
 import load_2d_mesh as lmsh
-import runtime_arguments as rarg
 import solution_paths as solpath
 import switch_problem as swi
 
@@ -14,5 +13,6 @@ vp = importlib.import_module(swi.vp)
 
 i, j, k, l = ufl.indices(4)
 
-xdmffile_u = XDMFFile("u.xdmf")
-xdmffile_u.write(fsp.u, 0)
+io.full_print(fsp.u, 'u', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+              solpath.nodal_values_path,
+              lmsh.mesh, 'scalar')
