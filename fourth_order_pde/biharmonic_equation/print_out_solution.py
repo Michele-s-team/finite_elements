f19661a448f13336ba458e58696cef62b3b8ec8b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-16 16:16:07 +0200

    used print_out_solution

--- fourth_order_pde/biharmonic_equation/print_out_solution.py
+++ fourth_order_pde/biharmonic_equation/print_out_solution.py
@@ -13,22 +13,31 @@ i, j, k, l = ufl.indices(4)
 fsp.u_output, fsp.v_output, fsp.w_output = fsp.psi.split(deepcopy=True)
 
 
-xdmffile_u = XDMFFile( (rarg.args.output_directory) + "/u.xdmf" )
-xdmffile_v = XDMFFile( (rarg.args.output_directory) + "/v.xdmf" )
-xdmffile_w = XDMFFile( (rarg.args.output_directory) + "/w.xdmf" )
+# xdmffile_u = XDMFFile( (rarg.args.output_directory) + "/u.xdmf" )
+# xdmffile_v = XDMFFile( (rarg.args.output_directory) + "/v.xdmf" )
+# xdmffile_w = XDMFFile( (rarg.args.output_directory) + "/w.xdmf" )
 
 
 xdmffile_check = XDMFFile( (rarg.args.output_directory) + "/check.xdmf" )
 xdmffile_check.parameters.update( {"functions_share_mesh": True, "rewrite_function_mesh": False} )
 
-xdmffile_u.write( fsp.u_output, 0 )
-xdmffile_v.write( fsp.v_output, 0 )
-xdmffile_w.write( fsp.w_output, 0 )
+# xdmffile_u.write( fsp.u_output, 0 )
+# xdmffile_v.write( fsp.v_output, 0 )
+# xdmffile_w.write( fsp.w_output, 0 )
 
-io.print_scalar_to_csvfile(fsp.u_output, (rarg.args.output_directory) + '/u.csv')
-io.print_scalar_to_csvfile(fsp.v_output, (rarg.args.output_directory) + '/v.csv')
-io.print_scalar_to_csvfile(fsp.w_output, (rarg.args.output_directory) + '/w.csv')
+# io.print_scalar_to_csvfile(fsp.u_output, (rarg.args.output_directory) + '/u.csv')
+# io.print_scalar_to_csvfile(fsp.v_output, (rarg.args.output_directory) + '/v.csv')
+# io.print_scalar_to_csvfile(fsp.w_output, (rarg.args.output_directory) + '/w.csv')
 
+io.full_print(fsp.u_output, 'u', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+              solpath.nodal_values_path,
+              lmsh.mesh, 'scalar')
+io.full_print(fsp.v_output, 'v', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+              solpath.nodal_values_path,
+              lmsh.mesh, 'scalar')
+io.full_print(fsp.w_output, 'w', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+              solpath.nodal_values_path,
+              lmsh.mesh, 'scalar')
 
 
 
--- fourth_order_pde/print_out_solution.py
+++ fourth_order_pde/print_out_solution.py
@@ -11,26 +11,10 @@ i, j, k, l = ufl.indices(4)
 
 fsp.z_output, fsp.omega_output, fsp.mu_output, fsp.rho_output, fsp.tau_output = fsp.psi.split(deepcopy=True)
 
-# xdmffile_z = XDMFFile((rarg.args.output_directory) + "/z.xdmf")
-# xdmffile_omega = XDMFFile((rarg.args.output_directory) + "/omega.xdmf")
-# xdmffile_mu = XDMFFile((rarg.args.output_directory) + "/mu.xdmf")
-# xdmffile_rho = XDMFFile((rarg.args.output_directory) + "/rho.xdmf")
-# xdmffile_tau = XDMFFile((rarg.args.output_directory) + "/tau.xdmf")
 
 xdmffile_check = XDMFFile((rarg.args.output_directory) + "/check.xdmf")
 xdmffile_check.parameters.update({"functions_share_mesh": True, "rewrite_function_mesh": False})
 
-# xdmffile_z.write(fsp.z_output, 0)
-# xdmffile_omega.write(fsp.omega_output, 0)
-# xdmffile_mu.write(fsp.mu_output, 0)
-# xdmffile_rho.write(fsp.rho_output, 0)
-# xdmffile_tau.write(fsp.tau_output, 0)
-
-# io.print_scalar_to_csvfile(fsp.z_output, (rarg.args.output_directory) + '/z.csv')
-# io.print_vector_to_csvfile(fsp.omega_output, (rarg.args.output_directory) + '/omega.csv')
-# io.print_scalar_to_csvfile(fsp.mu_output, (rarg.args.output_directory) + '/mu.csv')
-# io.print_vector_to_csvfile(fsp.rho_output, (rarg.args.output_directory) + '/rho.csv')
-# io.print_vector_to_csvfile(fsp.tau_output, (rarg.args.output_directory) + '/tau.csv')
 
 io.full_print(fsp.z_output, 'z', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path,
