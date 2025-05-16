18b31ad4d8a4e54496ec08455572f2b754570251
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-16 16:00:47 +0200

    ...

--- fourth_order_pde/biharmonic_equation/print_out_bc_ring.py
+++ fourth_order_pde/biharmonic_equation/print_out_bc_ring.py
@@ -16,21 +16,21 @@ rmsh = importlib.import_module(swi.rmsh)
 
 i, j, k, l = ufl.indices(4)
 
-u_output, v_output, w_output = psi.split( deepcopy=True )
+u_output, v_output, w_output = fsp.psi.split( deepcopy=True )
 
 print( "BCs check: " )
-print(f"<<(u - u_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(u_output, u_exact), 'red')}")
-print(f"<<(v - v_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(v_output, v_exact), 'red')}")
-print(f"<<(w - w_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(w_output, w_exact), 'red')}")
+print(f"<<(u - u_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(u_output, fsp.u_exact), 'red')}")
+print(f"<<(v - v_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(v_output, fsp.v_exact), 'red')}")
+print(f"<<(w - w_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(w_output, fsp.w_exact), 'red')}")
 
 print("Check that the PDE is satisfied: ")
-print(f"<<(w - f)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, f), 'green')}")
+print(f"<<(w - f)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, fsp.f), 'green')}")
 
 
 print( "Comparison with exact solution: " )
-print(f"<<(u - u_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(u_output, u_exact), 'blue')}")
-print(f"<<(v - v_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(v_output, v_exact), 'blue')}")
-print(f"<<(w - w_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, w_exact), 'blue')}")
+print(f"<<(u - u_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(u_output, fsp.u_exact), 'blue')}")
+print(f"<<(v - v_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(v_output, fsp.v_exact), 'blue')}")
+print(f"<<(w - w_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, fsp.w_exact), 'blue')}")
 
 
 
--- fourth_order_pde/biharmonic_equation/print_out_solution.py
+++ fourth_order_pde/biharmonic_equation/print_out_solution.py
@@ -4,26 +4,27 @@ import ufl as ufl
 import function_spaces as fsp
 import input_output as io
 import load_2d_mesh as lmsh
+import mesh as msh
 import runtime_arguments as rarg
 import solution_paths as solpath
 
 i, j, k, l = ufl.indices(4)
 
-xdmffile_check = XDMFFile( (args.output_directory) + "/check.xdmf" )
+xdmffile_check = XDMFFile( (rarg.args.output_directory) + "/check.xdmf" )
 xdmffile_check.parameters.update( {"functions_share_mesh": True, "rewrite_function_mesh": False} )
 
-xdmffile_u.write( u_output, 0 )
-xdmffile_v.write( v_output, 0 )
-xdmffile_w.write( w_output, 0 )
+xdmffile_u.write( fsp.u_output, 0 )
+xdmffile_v.write( fsp.v_output, 0 )
+xdmffile_w.write( fsp.w_output, 0 )
 
-io.print_scalar_to_csvfile(u_output, (args.output_directory) + '/u.csv')
-io.print_scalar_to_csvfile(v_output, (args.output_directory) + '/v.csv')
-io.print_scalar_to_csvfile(w_output, (args.output_directory) + '/w.csv')
-
-
+io.print_scalar_to_csvfile(fsp.u_output, (rarg.args.output_directory) + '/u.csv')
+io.print_scalar_to_csvfile(fsp.v_output, (rarg.args.output_directory) + '/v.csv')
+io.print_scalar_to_csvfile(fsp.w_output, (rarg.args.output_directory) + '/w.csv')
 
 
-xdmffile_check.write( project( w_output - f , Q_w ), 0 )
+
+
+xdmffile_check.write( project( fsp.w_output - fsp.f , fsp.Q_w ), 0 )
 xdmffile_check.close()
 
-msh.bulk_points(mesh)
+msh.bulk_points(lmsh.mesh)
