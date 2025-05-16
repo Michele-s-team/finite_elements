67684f19308ea292ed768fd02ba175f56efe14b8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-16 17:31:29 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -171,6 +171,18 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_ring_mesh', 0.1, problem_name, success)
 
+# Test fourth_order_pde/biharmonic_equation
+case_name = 'fourth_order_pde/biharmonic_equation'
+
+problem_name = 'ring'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/ring',
+                                                                     root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_ring_mesh', 0.1, problem_name, success)
+
+
 
 
 # Test steady-state-no-flow
