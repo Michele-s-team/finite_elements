e35ccf168514d773a0e39a5b8304d91a1037cac2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-17 16:50:52 +0200

    Update ft08_navier_stokes_cylinder.py

--- dynamics/channel_with_cylinder_flat_icps/ft08_navier_stokes_cylinder.py
+++ dynamics/channel_with_cylinder_flat_icps/ft08_navier_stokes_cylinder.py
@@ -7,7 +7,9 @@ Scheme (IPCS).
                                  div(u) = 0
 Run with
     python ft08_navier_stokes_cylinder.py [path where to read the mesh] [path where to store the solution]
-    clear; clear; rm -r solution-ft08_navier_stokes_cylinder; python3 ft08_navier_stokes_cylinder.py /home/fenics/shared/channel-with-cylinder-flat-incremental-pressure-correction-scheme/mesh/solution
+
+Example:
+    MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/solution"; SOLUTION_PATH="/home/fenics/shared/dynamics/channel_with_cylinder_flat_icps/solution"; rm -rf $SOLUTION_PATH; python3 solve.py $MESH_PATH $SOLUTION_PATH
 """
 
 from fenics import *
@@ -15,10 +17,9 @@ from mshr import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "input_directory" )
-parser.add_argument( "output_directory" )
+parser.add_argument("input_directory")
+parser.add_argument("output_directory")
 args = parser.parse_args()
-
 
 T = 1.0  # final time
 num_steps = 1024  # number of time steps
@@ -28,15 +29,15 @@ rho = 1  # density
 
 # read mesh
 mesh = Mesh()
-with XDMFFile( (args.input_directory) + "/triangle_mesh.xdmf" ) as infile:
-    infile.read( mesh )
-mvc = MeshValueCollection( "size_t", mesh, 2 )
-with XDMFFile( (args.input_directory) + "/line_mesh.xdmf" ) as infile:
-    infile.read( mvc, "name_to_read" )
+with XDMFFile((args.input_directory) + "/triangle_mesh.xdmf") as infile:
+    infile.read(mesh)
+mvc = MeshValueCollection("size_t", mesh, 2)
+with XDMFFile((args.input_directory) + "/line_mesh.xdmf") as infile:
+    infile.read(mvc, "name_to_read")
 
 # Define function spaces
-V = VectorFunctionSpace( mesh, 'P', 2 )
-Q = FunctionSpace( mesh, 'P', 1 )
+V = VectorFunctionSpace(mesh, 'P', 2)
+Q = FunctionSpace(mesh, 'P', 1)
 
 # Define boundaries
 inflow = 'near(x[0], 0)'
@@ -48,100 +49,100 @@ cylinder = 'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3'
 inflow_profile = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
 
 # Define boundary conditions
-bcu_inflow = DirichletBC( V, Expression( inflow_profile, degree=2 ), inflow )
-bcu_walls = DirichletBC( V, Constant( (0, 0) ), walls )
-bcu_cylinder = DirichletBC( V, Constant( (0, 0) ), cylinder )
-bcp_outflow = DirichletBC( Q, Constant( 0 ), outflow )
+bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
+bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
+bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder)
+bcp_outflow = DirichletBC(Q, Constant(0), outflow)
 bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
 bcp = [bcp_outflow]
 
 # Define trial and test functions
-u = TrialFunction( V )
-v = TestFunction( V )
-p = TrialFunction( Q )
-q = TestFunction( Q )
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
 
 # Define functions for solutions at previous and current time steps
-u_n = Function( V )
-u_ = Function( V )
-p_n = Function( Q )
-p_ = Function( Q )
+u_n = Function(V)
+u_ = Function(V)
+p_n = Function(Q)
+p_ = Function(Q)
 
 # Define expressions used in variational forms
 U = 0.5 * (u_n + u)
-n = FacetNormal( mesh )
-f = Constant( (0, 0) )
-k = Constant( dt )
-mu = Constant( mu )
-rho = Constant( rho )
+n = FacetNormal(mesh)
+f = Constant((0, 0))
+k = Constant(dt)
+mu = Constant(mu)
+rho = Constant(rho)
 
 
 # Define symmetric gradient
 def epsilon(u):
-    return sym( nabla_grad( u ) )
+    return sym(nabla_grad(u))
 
 
 # Define stress tensor
 def sigma(u, p):
-    return 2 * mu * epsilon( u ) - p * Identity( len( u ) )
+    return 2 * mu * epsilon(u) - p * Identity(len(u))
 
 
 # Define variational problem for step 1
-F1 = rho * dot( (u - u_n) / k, v ) * dx \
-     + rho * dot( dot( u_n, nabla_grad( u_n ) ), v ) * dx \
-     + inner( sigma( U, p_n ), epsilon( v ) ) * dx \
-     + dot( p_n * n, v ) * ds - dot( mu * nabla_grad( U ) * n, v ) * ds \
-     - dot( f, v ) * dx
-a1 = lhs( F1 )
-L1 = rhs( F1 )
+F1 = rho * dot((u - u_n) / k, v) * dx \
+     + rho * dot(dot(u_n, nabla_grad(u_n)), v) * dx \
+     + inner(sigma(U, p_n), epsilon(v)) * dx \
+     + dot(p_n * n, v) * ds - dot(mu * nabla_grad(U) * n, v) * ds \
+     - dot(f, v) * dx
+a1 = lhs(F1)
+L1 = rhs(F1)
 
 # Define variational problem for step 2
-a2 = dot( nabla_grad( p ), nabla_grad( q ) ) * dx
-L2 = dot( nabla_grad( p_n ), nabla_grad( q ) ) * dx - (1 / k) * div( u_ ) * q * dx
+a2 = dot(nabla_grad(p), nabla_grad(q)) * dx
+L2 = dot(nabla_grad(p_n), nabla_grad(q)) * dx - (1 / k) * div(u_) * q * dx
 
 # Define variational problem for step 3
-a3 = dot( u, v ) * dx
-L3 = dot( u_, v ) * dx - k * dot( nabla_grad( p_ - p_n ), v ) * dx
+a3 = dot(u, v) * dx
+L3 = dot(u_, v) * dx - k * dot(nabla_grad(p_ - p_n), v) * dx
 
 # Assemble matrices
-A1 = assemble( a1 )
-A2 = assemble( a2 )
-A3 = assemble( a3 )
+A1 = assemble(a1)
+A2 = assemble(a2)
+A3 = assemble(a3)
 
 # Apply boundary conditions to matrices
-[bc.apply( A1 ) for bc in bcu]
-[bc.apply( A2 ) for bc in bcp]
+[bc.apply(A1) for bc in bcu]
+[bc.apply(A2) for bc in bcp]
 
 # Create XDMF files for visualization output
-xdmffile_u = XDMFFile( args.output_directory + '/v_n.xdmf' )
-xdmffile_p = XDMFFile( args.output_directory + '/p_n.xdmf' )
+xdmffile_u = XDMFFile(args.output_directory + '/v_n.xdmf')
+xdmffile_p = XDMFFile(args.output_directory + '/p_n.xdmf')
 
 # Time-stepping
 t = 0
-for n in range( num_steps ):
+for n in range(num_steps):
     # Update current time
     t += dt
 
     # Step 1: Tentative velocity step
-    b1 = assemble( L1 )
-    [bc.apply( b1 ) for bc in bcu]
-    solve( A1, u_.vector(), b1, 'bicgstab', 'hypre_amg' )
+    b1 = assemble(L1)
+    [bc.apply(b1) for bc in bcu]
+    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
 
     # Step 2: Pressure correction step
-    b2 = assemble( L2 )
-    [bc.apply( b2 ) for bc in bcp]
-    solve( A2, p_.vector(), b2, 'bicgstab', 'hypre_amg' )
+    b2 = assemble(L2)
+    [bc.apply(b2) for bc in bcp]
+    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
 
     # Step 3: Velocity correction step
-    b3 = assemble( L3 )
-    solve( A3, u_.vector(), b3, 'cg', 'sor' )
+    b3 = assemble(L3)
+    solve(A3, u_.vector(), b3, 'cg', 'sor')
 
     # Save solution to file (XDMF/HDF5)
-    xdmffile_u.write( u_, t )
-    xdmffile_p.write( p_, t )
+    xdmffile_u.write(u_, t)
+    xdmffile_p.write(p_, t)
 
     # Update previous solution
-    u_n.assign( u_ )
-    p_n.assign( p_ )
+    u_n.assign(u_)
+    p_n.assign(p_)
 
-    print( "\t%.2f %%" % (100.0 * (t / T)), flush=True )
+    print("\t%.2f %%" % (100.0 * (t / T)), flush=True)
