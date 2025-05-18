06b08d45312af570ba85cee1c19d3900cb667315
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-18 11:12:30 +0200

    check_if_exists

--- modules/load_2d_mesh.py
+++ modules/load_2d_mesh.py
@@ -4,6 +4,5 @@ from fenics import *
 import mesh as msh
 import runtime_arguments as rarg
 
-
-#read the mesh
+# read the mesh
 mesh = msh.read_mesh(rarg.args.input_directory + "/triangle_mesh.xdmf")
