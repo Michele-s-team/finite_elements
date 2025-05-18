e5bad6a6b46ae318796d2740e7f6a2ed2c7190fb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-18 11:16:15 +0200

    Update load_2d_mesh.py

--- modules/load_2d_mesh.py
+++ modules/load_2d_mesh.py
@@ -1,8 +1,18 @@
 import dolfin
 from fenics import *
 
+import command as cmd
 import mesh as msh
 import runtime_arguments as rarg
 
-# read the mesh
-mesh = msh.read_mesh(rarg.args.input_directory + "/triangle_mesh.xdmf")
+if cmd.check_if_file_exists(rarg.args.input_directory + "/tetrahedron_mesh.xdmf"):
+    mesh = msh.read_mesh(rarg.args.input_directory + "/tetrahedron_mesh.xdmf")
+    print('3d mesh')
+else:
+    if cmd.check_if_file_exists(rarg.args.input_directory + "/triangle_mesh.xdmf"):
+        mesh = msh.read_mesh(rarg.args.input_directory + "/triangle_mesh.xdmf")
+        print('2d mesh')
+    else:
+        if cmd.check_if_file_exists(rarg.args.input_directory + "/line_mesh.xdmf"):
+            mesh = msh.read_mesh(rarg.args.input_directory + "/line_mesh.xdmf")
+            print('1d mesh')
