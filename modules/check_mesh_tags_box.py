ffccbd831791ac3d6e321b71e744ade166e7b554
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-20 08:30:27 +0200

    all integrals OK

--- modules/check_mesh_tags_box.py
+++ modules/check_mesh_tags_box.py
@@ -46,11 +46,19 @@ function_test_integrals_fenics.interpolate(FunctionTestIntegrals(element=Q.ufl_e
 test_mesh_integral_errors = []
 
 integral_exact_dx = cal.volume_integral_box(function_test_integrals, rmsh.L)
-integral_exact_ds_le = cal.surface_integral_rectangle(lambda r: function_test_integrals([0, r[0], r[1]]), [0,0], [rmsh.L[1], rmsh.L[2]])
-integral_exact_ds_ri = cal.surface_integral_rectangle(lambda r: function_test_integrals([rmsh.L[0], r[0], r[1]]), [0,0], [rmsh.L[1], rmsh.L[2]])
-integral_exact_ds_to = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0],  rmsh.L[1], r[1]]), [0,0], [rmsh.L[0], rmsh.L[2]])
-integral_exact_ds_bo = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0], 0, r[1]]), [0,0], [rmsh.L[0], rmsh.L[2]])
 
+integral_exact_ds_le = cal.surface_integral_rectangle(lambda r: function_test_integrals([0, r[0], r[1]]), [0, 0], [rmsh.L[1], rmsh.L[2]])
+integral_exact_ds_ri = cal.surface_integral_rectangle(lambda r: function_test_integrals([rmsh.L[0], r[0], r[1]]), [0, 0], [rmsh.L[1], rmsh.L[2]])
+integral_exact_ds_to = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0], rmsh.L[1], r[1]]), [0, 0], [rmsh.L[0], rmsh.L[2]])
+integral_exact_ds_bo = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0], 0, r[1]]), [0, 0], [rmsh.L[0], rmsh.L[2]])
+integral_exact_ds_fr = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0], r[1], rmsh.L[2]]), [0, 0], [rmsh.L[0], rmsh.L[1]])
+integral_exact_ds_ba = cal.surface_integral_rectangle(lambda r: function_test_integrals([r[0], r[1], 0]), [0, 0], [rmsh.L[0], rmsh.L[1]])
+
+integral_exact_ds_leri = integral_exact_ds_le + integral_exact_ds_ri
+integral_exact_ds_tobo = integral_exact_ds_to + integral_exact_ds_bo
+integral_exact_ds_frba = integral_exact_ds_fr + integral_exact_ds_ba
+
+integral_exact_ds = integral_exact_ds_leri + integral_exact_ds_tobo + integral_exact_ds_frba
 
 # print out the integrals on the surface elements and compare them with the exact values to double check that the elements are tagged correctly
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_dx, function_test_integrals_fenics, rmsh.dx, '\int_ball f dx'))
@@ -58,5 +66,9 @@ test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_le, fu
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_ri, function_test_integrals_fenics, rmsh.ds_ri, '\int_ri f ds'))
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_to, function_test_integrals_fenics, rmsh.ds_to, '\int_to f ds'))
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_bo, function_test_integrals_fenics, rmsh.ds_bo, '\int_bo f ds'))
+test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_fr, function_test_integrals_fenics, rmsh.ds_fr, '\int_fr f ds'))
+test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_ba, function_test_integrals_fenics, rmsh.ds_ba, '\int_ba f ds'))
+
+test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds, function_test_integrals_fenics, rmsh.ds, '\int f ds'))
 
 print(f'Maximum relative error of mesh integrals = {col.Fore.RED}{max(test_mesh_integral_errors):.{io.number_of_decimals}e}{col.Fore.RESET}')
