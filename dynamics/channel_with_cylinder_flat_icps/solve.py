6aa3381a4c451df29a8e65b803f67a7fe71bd3bb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-26 08:57:22 +0200

    Update solve.py

--- dynamics/channel_with_cylinder_flat_icps/solve.py
+++ dynamics/channel_with_cylinder_flat_icps/solve.py
@@ -13,6 +13,7 @@ Example:
     MESH_PATH="/home/fenics/shared/generate_mesh/3d/box_ball/solution"; SOLUTION_PATH="/home/fenics/shared/dynamics/channel_with_cylinder_flat_icps/solution"; rm -rf $SOLUTION_PATH; python3 solve.py box_ball $MESH_PATH $SOLUTION_PATH
 
 For box_ball problem one gets turbulent behavior with
+    res = 0.05
     L =  [2.2, 0.41, 0.41]
     mu =  0.0001
     T =  1
