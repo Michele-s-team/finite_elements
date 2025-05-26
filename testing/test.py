bc5780ba21c9c59bc1d3f17243d4bbed5e2686a7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-26 16:14:27 +0200

    Added to test

--- testing/test.py
+++ testing/test.py
@@ -338,6 +338,25 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_box_ball_mesh', 0.1, problem_name, success)
 
 
+# Test dynamics/channel_with_cylinder_flat_cn
+case_name = 'dynamics/channel_with_cylinder_flat_cn'
+
+problem_name = 'square_no_circle'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square_no_circle', root_path + case_name,
+                                                                     root_path + 'generate_mesh/2d/square_no_circle', root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_no_circle_mesh', 0.1, problem_name, success)
+
+problem_name = 'square'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/square', root_path + case_name,
+                                                                     root_path + 'generate_mesh/2d/square', root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_mesh', 0.1, problem_name, success)
+
 
 # Test dynamics/channel_with_cylinder_curved_cn
 case_name = 'dynamics/channel_with_cylinder_curved_cn'
