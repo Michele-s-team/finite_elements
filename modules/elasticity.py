05eb4d05a1ff3c526c92c390c5f239c993dec7bd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-05-29 10:28:00 +0200

    Update elasticity.py

--- modules/elasticity.py
+++ modules/elasticity.py
@@ -13,5 +13,17 @@ Input values:
 Return values:
 - F[i][j] = F_{ij}_{Notes fluid-structure interaction}
 '''
+
+
 def F(u):
     return as_tensor(ufl.Identity(len(u))[i, j] + u[i].dx(j), (i, j))
+
+'''
+Green–Lagrange strain tensor
+Input values:
+- 'u': displacement vector field
+Return values:
+- E[i][j] = E_{ij}_{Notes fluid-structure interaction}
+'''
+def E(u):
+    return as_tensor(1.0 / 2.0 * (F(u)[k, i] * F(u)[k, j] - ufl.Identity(len(u))[i, j] ), (i, j))
