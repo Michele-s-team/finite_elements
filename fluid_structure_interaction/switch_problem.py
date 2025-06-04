8a798b2488774fd28f6e69da1e6ab6568529de61
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-04 21:36:42 +0200

    renamed

--- fluid_structure_interaction/switch_problem.py
+++ fluid_structure_interaction/switch_problem.py
@@ -4,7 +4,7 @@ import runtime_arguments as rarg
 
 if rarg.args.problem == 'square_ellipse':
     rmsh = 'read_mesh_square_ellipse'
-    vp = 'variational_problem_bc_square_ellipse'
+    vp = 'variational_problem_u_bc_square_ellipse'
     prout_bc = 'print_out_bc_square_ellipse'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
