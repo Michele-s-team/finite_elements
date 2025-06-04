c0e355fe48af0823d2fa67c06f033b345fb7cd78
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-04 22:26:50 +0200

    Update switch_problem.py

--- fluid_structure_interaction/switch_problem.py
+++ fluid_structure_interaction/switch_problem.py
@@ -7,6 +7,6 @@ if rarg.args.problem == 'square_ellipse':
     vp = 'variational_problem_u_bc_square_ellipse'
     vp_dot = 'variational_problem_u_dot_bc_square_ellipse'
     prout_bc = 'print_out_u_bc_square_ellipse'
-    prout_bc_dot = 'print_out_u_dot_bc_square_ellipse'
+    prout_bc_dot = 'print_out_u_bc_square_ellipse'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
