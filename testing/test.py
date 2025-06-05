118f8084b58cdcd78be0d88a22816e34fb05298a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-05 09:22:00 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -417,8 +417,8 @@ case_name = 'fluid_structure_interaction/mesh_deformation'
 problem_name = 'square_ellipse'
 checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
                                                                      root_path,
-                                                                     root_path + '/home/fenics/shared/generate_mesh/2d/square/ellipse', root_path + case_name,
-                                                                     root_path + '/home/fenics/shared/generate_mesh/2d/square/ellipse', root_path + case_name,
+                                                                     root_path + 'generate_mesh/2d/square/ellipse', root_path + case_name,
+                                                                     root_path + 'generate_mesh/2d/square/ellipse', root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_ellipse_mesh', 0.1, problem_name, success)
 
