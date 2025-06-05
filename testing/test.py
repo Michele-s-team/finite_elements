ffa9ab2c9cb848f6c87f1614ecc47dd07c50aa2a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-05 09:20:36 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -411,6 +411,18 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
 
 
 
+# Test fluid_structure_interaction/mesh_deformation
+case_name = 'fluid_structure_interaction/mesh_deformation'
+
+problem_name = 'square_ellipse'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + '/home/fenics/shared/generate_mesh/2d/square/ellipse', root_path + case_name,
+                                                                     root_path + '/home/fenics/shared/generate_mesh/2d/square/ellipse', root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_ellipse_mesh', 0.1, problem_name, success)
+
+
 cmd.checkout(commit_a, success)
 
 
