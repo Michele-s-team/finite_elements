93fc2fd68e3dedbf3993b224e9dd22a610964734
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-05 09:21:07 +0200

    temp comm out stuff from test

--- testing/test.py
+++ testing/test.py
@@ -41,7 +41,7 @@ success = [True]
 
 cmd.run_command('clear; clear', success)
 
-
+'''
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
 
@@ -408,7 +408,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
-
+'''
 
 
 # Test fluid_structure_interaction/mesh_deformation
