ee5ca5b31ab3025db398a148f14669312e681173
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-09 13:37:06 +0200

    Update switch_problem.py

--- fluid_structure_interaction/switch_problem.py
+++ fluid_structure_interaction/switch_problem.py
@@ -2,20 +2,11 @@ import colorama as col
 
 import runtime_arguments as rarg
 
-if rarg.args.problem == 'square_no_circle':
-    rmsh = 'read_mesh_square_no_circle'
-    vp = 'variational_problem_bc_square_no_circle'
-    vp_pp = 'variational_problem_pp_square_no_circle'
-    prout_bc = 'print_out_bc_square_no_circle'
-    prout_forces_on_boundaries = 'print_out_force_on_boundaries_bc_square_no_circle'
-
-
-
-elif rarg.args.problem == 'square':
-    rmsh = 'read_mesh_square'
-    vp = 'variational_problem_bc_square'
-    vp_pp = 'variational_problem_pp_square'
-    prout_bc = 'print_out_bc_square'
-    prout_forces_on_boundaries = 'print_out_force_on_boundaries_bc_square'
+if rarg.args.problem == 'square_ellipse':
+    rmsh = 'read_mesh_square_ellipse'
+    vp = 'variational_problem_bc_square_ellipse'
+    vp_pp = 'variational_problem_pp_square_ellipse'
+    prout_bc = 'print_out_bc_square_ellipse'
+    prout_forces_on_boundaries = 'print_out_force_on_boundaries_bc_square_ellipse'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
