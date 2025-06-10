aa89ad58d22778de7c9d8b6f8b48fe86fe103e0f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-10 19:01:36 +0200

    added printout of u_n and u_dot_n

--- fluid_structure_interaction/print_out_solution.py
+++ fluid_structure_interaction/print_out_solution.py
@@ -40,12 +40,10 @@ def print_solution(t, step, dt):
                   solpath.snapshots_csv_nodal_values_path,
                   lmsh.mesh, 'vector')
 
-    # print the solution for the fluid problem
     # include the snapshot in xdmf files
-    fi.xdmffile_v_n.write(fsp.v_n, t)
-    fi.xdmffile_v_.write(fsp.v_, t)
-    fi.xdmffile_sigma.write(fsp.sigma_n_12, t - dt / 2.0)
-    fi.xdmffile_phi.write(fsp.phi, t)
+    fi.xdmffile_u_n.write(fsp.u_n, t)
+    fi.xdmffile_u_dot_n.write(fsp.u_dot_n, t)
+
 
     # 3) print the solution of the fluid problem
     io.full_print(fsp.v_, 'v_bar_' + str(step), \
@@ -60,3 +58,9 @@ def print_solution(t, step, dt):
     io.full_print(fsp.phi, 'phi_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, \
                   lmsh.mesh, 'scalar')
+
+    # include the snapshot in xdmf files
+    fi.xdmffile_v_n.write(fsp.v_n, t)
+    fi.xdmffile_v_.write(fsp.v_, t)
+    fi.xdmffile_sigma.write(fsp.sigma_n_12, t - dt / 2.0)
+    fi.xdmffile_phi.write(fsp.phi, t)
