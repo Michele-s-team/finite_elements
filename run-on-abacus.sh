4191b313176b5b4a4dfd252d15315c2d97ebae37
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-10 18:54:54 +0200

    updated abacus scripts

--- run-on-abacus.sh
+++ run-on-abacus.sh
@@ -14,5 +14,7 @@ rsync -av modules/*.py $OUT:$2
 rsync -av $1/*.py $OUT:$2
 rsync -av /Users/michelecastellana/Documents/finite_elements/script_slurm_abacus.slurm $OUT:$2
 rsync -av --exclude 'mesh.msh' --exclude '*.csv' --exclude '.DS_Store' $3 $OUT:$2/mesh
+rsync -av  $1/parameters.csv $OUT:$2
+
 
 ssh $OUT "cd "$2"; sbatch script_slurm_abacus.slurm"
