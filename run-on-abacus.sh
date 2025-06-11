63b10c4164f3a8cf73f486bb6712531a5e05ef23
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-11 12:06:48 +0200

    Revised submission scripts to abacus

--- run-on-abacus.sh
+++ run-on-abacus.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# run with ./run-on-abacus.sh steady_state/no_flow steady_state_no_flow_1 ~/Documents/finite_elements/generate_mesh/2d/ring/symmetric/solution
+# run with ./run-on-abacus.sh read_write read_write_1 ~/Documents/finite_elements/generate_mesh/2d/square/solution
 
 clear
 clear
 
 OUT=mcastel1@abacus
 
+# CHANGE PARAMETERS HERE
+SCRIPT_SLURM="script_slurm_abacus.slurm"
+# SCRIPT_SLURM="script_slurm_abacus_read_write.slurm"
+# CHANGE PARAMETERS HERE
+
 ssh $OUT "rm -rf "$2
 ssh $OUT "mkdir -p "$2"/mesh"
 
 rsync -av modules/*.py $OUT:$2
 rsync -av $1/*.py $OUT:$2
-rsync -av /Users/michelecastellana/Documents/finite_elements/script_slurm_abacus.slurm $OUT:$2
+rsync -av /Users/michelecastellana/Documents/finite_elements/$SCRIPT_SLURM $OUT:$2
 rsync -av --exclude 'mesh.msh' --exclude '*.csv' --exclude '.DS_Store' $3 $OUT:$2/mesh
 
-ssh $OUT "cd "$2"; sbatch script_slurm_abacus.slurm"
+ssh $OUT "cd "$2"; sbatch $SCRIPT_SLURM"
