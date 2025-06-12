a7d7860ca58c6df90954d399cd5282f05ad2f6a6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-12 09:20:32 +0200

    Adding printout of bcs ...

--- fluid_structure_interaction/variational_problem_fluid_bc_square_ellipse.py
+++ fluid_structure_interaction/variational_problem_fluid_bc_square_ellipse.py
@@ -42,7 +42,7 @@ class sigma_expression(UserExpression):
 
 v__profile_l = Expression((f'{rpam.v_l}* 4.0*1.5*x[1]*({rmsh.h} - x[1]) / pow({rmsh.h}, 2)', '0'), element=fsp.Q_v_.ufl_element(), h=rmsh.h)
 bc_v__l = DirichletBC(fsp.Q_v_, v__profile_l, rmsh.boundary_l)
-bc_v__walls = DirichletBC(fsp.Q_v_, Constant((0, 0)), rmsh.boundary_tb)
+bc_v__tb = DirichletBC(fsp.Q_v_, Constant((0, 0)), rmsh.boundary_tb)
 
 v__profile_ellipse = Expression((f'{fsp.omega_n} * (-sin({fsp.theta_n}) * (x[0] - {rmsh.focus[0]}) - cos({fsp.theta_n}) * (x[1] - {rmsh.focus[1]}))', f'{fsp.omega_n} * (cos({fsp.theta_n}) * (x[0] - {rmsh.focus[0]}) - sin({fsp.theta_n}) * (x[1] - {rmsh.focus[1]}))'), element=fsp.Q_v_.ufl_element())
 bc_v__ellipse = DirichletBC(fsp.Q_v_, v__profile_ellipse, rmsh.boundary_ellipse)
@@ -50,7 +50,7 @@ bc_v__ellipse = DirichletBC(fsp.Q_v_, v__profile_ellipse, rmsh.boundary_ellipse)
 bc_phi_r = DirichletBC(fsp.Q_phi, Constant(0), rmsh.boundary_r)
 
 # boundary conditions for the surface_tension p
-bc_v_ = [bc_v__l, bc_v__walls, bc_v__ellipse]
+bc_v_ = [bc_v__l, bc_v__tb, bc_v__ellipse]
 bc_phi = [bc_phi_r]
 
 # Define variational problem for step 1
