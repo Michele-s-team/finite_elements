3a8fc73335ec0fafca562b6bf6ccbd2ac7ab21f5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-18 11:24:53 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -409,7 +409,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_square_mesh', 0.1, problem_name, success)
 
 
-
+'''
 
 # Test fluid_structure_interaction
 case_name = 'fluid_structure_interaction/mesh_deformation'
@@ -421,7 +421,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      root_path + 'generate_mesh/2d/square/ellipse', root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_ellipse_mesh', 0.1, problem_name, success)
-'''
+
 
 case_name = 'fluid_structure_interaction'
 
