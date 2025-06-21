c616b6a501017cf4f766535bc85cdb03aec675e4
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-21 23:39:22 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -46,14 +46,14 @@ cmd.run_command('clear; clear', success)
 case_name = 'poisson_equation/solve_u'
 
 
-# problem_name = 'ring_slice'
-# checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
-#                                                                      root_path,
-#                                                                      root_path + 'generate_mesh/2d/ring/ring_slice', root_path + case_name,
-#                                                                      root_path + 'generate_mesh/2d/ring/ring_slice', root_path + case_name,
-#                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-#                                                                      'generate_mesh_ring_slice', 0.1, problem_name, success)
-#
+problem_name = 'ring_slice'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     root_path + 'generate_mesh/2d/ring/ring_slice', root_path + case_name,
+                                                                     root_path + 'generate_mesh/2d/ring/ring_slice', root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh_ring_slice', 0.1, problem_name, success)
+
 # problem_name = 'half_circle_with_line'
 # checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
 #                                                                      root_path,
