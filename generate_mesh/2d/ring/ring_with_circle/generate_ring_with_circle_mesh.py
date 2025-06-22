5e430f638386c9d2fd7503996b158537cfa05a54
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-22 11:14:29 +0200

    Update generate_ring_with_circle_mesh.py

--- generate_mesh/2d/ring/ring_with_circle/generate_ring_with_circle_mesh.py
+++ generate_mesh/2d/ring/ring_with_circle/generate_ring_with_circle_mesh.py
@@ -77,12 +77,17 @@ geometry.__exit__()
 # Save the line and triangle mesh as usual
 mesh_from_file = meshio.read(mesh_file_name)
 
+# print line mesh
 line_mesh = msh.create_mesh(mesh_from_file, "line", prune_z=True)
 meshio.write(output_directory + "line_mesh.xdmf", line_mesh)
 
+# print triangle mesh
 triangle_mesh = msh.create_mesh(mesh_from_file, "triangle", prune_z=True)
 meshio.write(output_directory + "triangle_mesh.xdmf", triangle_mesh)
 
-# Print the mesh vertices to file
+# Print mesh vertices
 mesh = msh.read_mesh(output_directory + "triangle_mesh.xdmf")
 io.print_mesh_vertices_to_csv(mesh, output_directory + "vertices.csv")
+
+# print mesh metadata
+io.write_parameters_to_csv_file(mesh_metadata_file_name, rpam.parameters)
--- modules/read_mesh_ring_with_circle.py
+++ modules/read_mesh_ring_with_circle.py
@@ -38,6 +38,6 @@ print(f'Module {__file__} called {check_mesh_tags_ring_with_circle.__file__}', f
 # Define boundaries and obstacle
 # CHANGE PARAMETERS HERE
 boundary = 'on_boundary'
-boundary_r = f'on_boundary && sqrt(pow(x[0] - {parameters["c_r"][0]}, 2) + pow(x[1] - {parameters["c_r"][1]}, 2)) < ({parameters.r} + {parameters.R})/2.0'
-boundary_R = f'on_boundary && sqrt(pow(x[0] - {parameters["c_R"][0]}, 2) + pow(x[1] - {parameters["c_R"][1]}, 2)) > ({parameters.r} + {parameters.R})/2.0'
+boundary_r = f'on_boundary && sqrt(pow(x[0] - {parameters["c_r"][0]}, 2) + pow(x[1] - {parameters["c_r"][1]}, 2)) < ({parameters["r"]} + {parameters["R"]})/2.0'
+boundary_R = f'on_boundary && sqrt(pow(x[0] - {parameters["c_R"][0]}, 2) + pow(x[1] - {parameters["c_R"][1]}, 2)) > ({parameters["r"]} + {parameters["R"]})/2.0'
 # CHANGE PARAMETERS HERE
