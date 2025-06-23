dc680cfa421430441ad2745e33e73a1f9569cc76
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-23 17:03:24 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -49,10 +49,11 @@ case_name = 'poisson_equation/solve_u'
 
 # uncomment this after the merge with master - start
 # problem_name = 'disk'
+# generate_mesh_path = root_path + 'generate_mesh/2d/disk'
 # checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
 #                                                                      root_path,
-#                                                                      root_path + 'generate_mesh/2d/disk', root_path + case_name,
-#                                                                      root_path + 'generate_mesh/2d/disk', root_path + case_name,
+#                                                                      generate_mesh_path, root_path + case_name,
+#                                                                      generate_mesh_path, root_path + case_name,
 #                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
 #                                                                      'generate_disk_mesh', 0.1, problem_name, success)
 # uncomment this after the merge with master - end
