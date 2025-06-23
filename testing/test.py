cb9559a9bc2df9b632c6f0a70458c210450a069e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-23 20:25:55 +0200

    revised dynamics

--- testing/test.py
+++ testing/test.py
@@ -368,7 +368,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path + 'high_res_parameters/', 0.01, problem_name, success)
-'''
+
 # Test dynamics/channel_with_cylinder_flat_icps
 case_name = 'dynamics/channel_with_cylinder_flat_icps'
 
@@ -432,7 +432,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, 0.1, problem_name, success)
-
+'''
 # Test dynamics
 case_name = 'dynamics'
 
