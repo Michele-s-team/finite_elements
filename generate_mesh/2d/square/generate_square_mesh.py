63b560648aaf6c18e256f82bb482e9d0a28d8eeb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-23 12:57:43 +0200

    square mesh ...

--- generate_mesh/2d/square/generate_square_mesh.py
+++ generate_mesh/2d/square/generate_square_mesh.py
@@ -1,17 +1,15 @@
 '''
 generate a mesh given by a square with a circular hole in it
 
-run it with
-python3 generate_square_mesh.py [resolution] [output directory]
-example:
-clear; clear; SOLUTION_PATH="solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_square_mesh.py 0.1 $SOLUTION_PATH
+Run it with
+    python3 generate_square_mesh.py [path where to read parameters] [output directory]
+Example:
+    clear; clear; PARAMETERS_PATH="/home/fenics/shared/generate_mesh/2d/square"; SOLUTION_PATH="/home/fenics/shared/generate_mesh/2d/square/solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_square_mesh.py $PARAMETERS_PATH $SOLUTION_PATH
 '''
 
 import meshio
 import gmsh
 import pygmsh
-import argparse
-
 import sys
 
 # add the path where to find the shared modules
@@ -20,35 +18,17 @@ sys.path.append(module_path)
 
 import input_output as io
 import mesh as msh
+import runtime_arguments_generate_mesh as rarg
+import read_parameters_generate_mesh as rpam
 
-parser = argparse.ArgumentParser()
-parser.add_argument("resolution")
-parser.add_argument("output_directory")
-args = parser.parse_args()
-
-# mesh resolution
-resolution = (float)(args.resolution)
+print(f'parameter_directory: {rarg.args.parameter_directory}\noutput_directory: {rarg.args.output_directory}')
 
 # add '/' to output_directory if it is missing
-output_directory = args.output_directory
-output_directory = io.add_trailing_slash(output_directory)
+output_directory = io.add_trailing_slash(rarg.args.output_directory)
 
 mesh_file = output_directory + "mesh.msh"
+mesh_metadata_file_name = rarg.args.output_directory + 'mesh_metadata.csv'
 
-# Channel parameters
-# CHANGE PARAMETERS HERE
-L = 1
-h = 1
-r = 0.25
-c_r = [L / 2, h / 2, 0]
-# CHANGE PARAMETERS HERE
-
-
-print("L = ", L)
-print("h = ", h)
-print("r = ", r)
-print("c_r = ", c_r)
-print("resolution = ", resolution)
 print(f'output_directory = "{output_directory}"')
 
 # Initialize empty geometry using the build in kernel in GMSH
@@ -56,10 +36,10 @@ geometry = pygmsh.geo.Geometry()
 # Fetch model we would like to add data to
 model = geometry.__enter__()
 
-my_points = [model.add_point((0, 0, 0), mesh_size=resolution),
-             model.add_point((L, 0, 0), mesh_size=resolution),
-             model.add_point((L, h, 0), mesh_size=resolution),
-             model.add_point((0, h, 0), mesh_size=resolution)]
+my_points = [model.add_point((0, 0, 0), mesh_size=rpam.parameters["resolution"]),
+             model.add_point((rpam.parameters["L"], 0, 0), mesh_size=rpam.parameters["resolution"]),
+             model.add_point((rpam.parameters["L"], rpam.parameters["h"], 0), mesh_size=rpam.parameters["resolution"]),
+             model.add_point((0, rpam.parameters["h"], 0), mesh_size=rpam.parameters["resolution"])]
 
 # Add lines between all points creating the rectangle
 channel_lines = [model.add_line(my_points[i], my_points[i + 1])
@@ -67,7 +47,7 @@ channel_lines = [model.add_line(my_points[i], my_points[i + 1])
 
 channel_loop = model.add_curve_loop(channel_lines)
 
-circle_r = model.add_circle(c_r, r, mesh_size=resolution)
+circle_r = model.add_circle(rpam.parameters["c_r"], rpam.parameters["r"], mesh_size=rpam.parameters["resolution"])
 
 plane_surface = model.add_plane_surface(channel_loop, holes=[circle_r.curve_loop])
 
@@ -83,19 +63,23 @@ model.add_physical(circle_r.curve_loop.curves, "c")
 geometry.generate_mesh(dim=2)
 gmsh.write(mesh_file)
 
+mesh_from_file = meshio.read(mesh_file)
+
+# write line mesh
+msh.write_mesh_components(mesh_file, output_directory + "line_mesh.xdmf", "line", True)
+
+# write triangle mesh
+msh.write_mesh_components(mesh_file, output_directory + "triangle_mesh.xdmf", "triangle", True)
+
+# print mesh vertices to csv file
+mesh = msh.read_mesh(output_directory + "triangle_mesh.xdmf")
+io.print_mesh_vertices_to_csv(mesh, output_directory + "vertices.csv")
+
+# print mesh lines to csv file
 msh.print_mesh_lines_to_csv(mesh_file, output_directory + 'line_vertices.csv')
+
+# print mesh metadata
+io.write_parameters_to_csv_file(mesh_metadata_file_name, rpam.parameters)
 
 gmsh.clear()
 geometry.__exit__()
-
-mesh_from_file = meshio.read(mesh_file)
-
-line_mesh = msh.create_mesh(mesh_from_file, "line", prune_z=True)
-meshio.write(output_directory + "line_mesh.xdmf", line_mesh)
-
-triangle_mesh = msh.create_mesh(mesh_from_file, "triangle", prune_z=True)
-meshio.write(output_directory + "triangle_mesh.xdmf", triangle_mesh)
-
-# print the mesh vertices to file
-mesh = msh.read_mesh(output_directory + "triangle_mesh.xdmf")
-io.print_mesh_vertices_to_csv(mesh, output_directory + "vertices.csv")
