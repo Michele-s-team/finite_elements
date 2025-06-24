0b663b9f6d2473e2218f4d80edc884cfa617b066
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-24 15:47:19 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -200,15 +200,15 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
 # Test poisson_equation/solve_u/periodic
 case_name = 'poisson_equation/solve_u/periodic'
 
-problem_name = 'square_no_circle'
-generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/symmetric/'
-checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
-                                                                     root_path,
-                                                                     generate_mesh_path, root_path + case_name,
-                                                                     generate_mesh_path, root_path + case_name,
-                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-                                                                     'generate_square_no_circle_mesh', 0.05, problem_name, success)
-
+# problem_name = 'square_no_circle'
+# generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/symmetric/'
+# checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+#                                                                      root_path,
+#                                                                      generate_mesh_path, root_path + case_name,
+#                                                                      generate_mesh_path, root_path + case_name,
+#                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+#                                                                      'generate_square_no_circle_mesh', 0.05, problem_name, success)
+#
 
 # Test nitsche_method
 
