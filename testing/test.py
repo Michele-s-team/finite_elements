35f9bad27e862607db10a3da93ed36df8a87d78a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-24 17:37:58 +0200

    put ball in test

--- testing/test.py
+++ testing/test.py
@@ -169,16 +169,16 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_ellipse_mesh', generate_mesh_path, 0.1, problem_name, success)
 
-#
-# problem_name = 'ball'
-# generate_mesh_path = root_path + 'generate_mesh/3d/ball/'
-# checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
-#                                                                      root_path,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-#                                                                      'generate_ball_mesh', 0.5, problem_name, success)
-#
+
+problem_name = 'ball'
+generate_mesh_path = root_path + 'generate_mesh/3d/ball/'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_ball_mesh', generate_mesh_path, 0.5, problem_name, success)
+
 # problem_name = 'box'
 # generate_mesh_path = root_path + 'generate_mesh/3d/box/'
 # checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
@@ -395,7 +395,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, 0.1, problem_name, success)
-#
+
 # problem_name = 'box_ball'
 # generate_mesh_path =root_path + 'generate_mesh/3d/box_ball'
 # checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
@@ -404,7 +404,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
 #                                                                      generate_mesh_path, root_path + case_name,
 #                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
 #                                                                      'generate_box_ball_mesh', generate_mesh_path, 0.1, problem_name, success)
-#
+
 
 # Test dynamics/channel_with_cylinder_flat_cn
 case_name = 'dynamics/channel_with_cylinder_flat_cn'
