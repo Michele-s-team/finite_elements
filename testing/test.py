897aa7a5cb4f1cc65fb5b6122b6e6abb6a8af497
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-24 12:58:11 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -41,7 +41,7 @@ success = [True]
 
 cmd.run_command('clear; clear', success)
 
-'''
+
 
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
@@ -455,7 +455,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, 0.1, problem_name, success)
-'''
+
 
 # Test fluid_structure_interaction
 case_name = 'fluid_structure_interaction/mesh_deformation'
