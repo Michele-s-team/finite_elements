8913732d257e8c1d082f2df24a82726d1d9480a0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-25 17:07:04 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -46,16 +46,14 @@ cmd.run_command('clear; clear', success)
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
 
-# uncomment this after the merge with master - start
-# problem_name = 'disk'
-# generate_mesh_path = root_path + 'generate_mesh/2d/disk/'
-# checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
-#                                                                      root_path,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-#                                                                      'generate_disk_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
-# uncomment this after the merge with master - end
+problem_name = 'disk'
+generate_mesh_path = root_path + 'generate_mesh/2d/disk/'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_disk_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
 
 
 problem_name = 'ring_slice'
@@ -130,16 +128,14 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
 
-# uncomment this after the merge with master - start
-# problem_name = 'square'
-# generate_mesh_path = root_path + 'generate_mesh/2d/square/lines/'
-# checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
-#                                                                      root_path,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      generate_mesh_path, root_path + case_name,
-#                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-#                                                                      'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
-# uncomment this after the merge with master - end
+problem_name = 'square'
+generate_mesh_path = root_path + 'generate_mesh/2d/square/lines/'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
 
 
 problem_name = 'square_symmetric_top_bottom'
