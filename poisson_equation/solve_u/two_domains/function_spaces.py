26a3d8532a5fbea2a292611a794f3ea53d37db74
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-26 17:28:56 +0200

    Create function_spaces.py

---
+++ poisson_equation/solve_u/two_domains/function_spaces.py
@@ -0,0 +1,31 @@
+import dolfin
+from fenics import *
+import importlib
+
+# import load_mesh as lmsh
+
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+
+
+function_space_degree = 4
+
+Q = FunctionSpace(lmsh.mesh, 'P', function_space_degree)
+V = VectorFunctionSpace(lmsh.mesh, 'P', function_space_degree)
+T = TensorFunctionSpace(lmsh.mesh, 'P', function_space_degree, shape=(lmsh.mesh.topology().dim(), lmsh.mesh.topology().dim()))
+
+# Define variational problem
+u = Function(Q)
+nu_u = TestFunction(Q)
+f = Function(Q)
+grad_u = Function(V)
+J_u = TrialFunction(Q)
+u_exact = Function(Q)
+
+# Define post-processing (pp) variational problem
+# hess_u is a tensor which is the Hessian matrix of u: hess_u[i, j] = \partial_i \partial_j u
+hess_u = Function(T)
+nu_hess_u = TestFunction(T)
+hess_u_exact = Function(T)
+J_hess_u = TrialFunction(T)
