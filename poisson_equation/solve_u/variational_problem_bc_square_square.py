59ea499c747743c7fc313dca5f88fdedaf3feff0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-26 09:41:49 +0200

    created poisson problem to test square square

--- poisson_equation/solve_u/variational_problem_bc_square_square.py
+++ poisson_equation/solve_u/variational_problem_bc_square_square.py
@@ -93,16 +93,20 @@ fsp.f.interpolate(laplacian_u_expression(element=fsp.Q.ufl_element()))
 
 fsp.hess_u_exact.interpolate(hess_u_exact_expression(element=fsp.T.ufl_element()))
 
-bc_u_tb = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_tb)
-bc_u_circle = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_circle)
-bcs = [bc_u_tb, bc_u_circle]
+bc_u_out_tb = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_out_tb)
+bc_u_in_lr = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_in_lr)
+bcs = [bc_u_out_tb, bc_u_in_lr]
 
 # variational functional for the original problem (poisson equation)
-F = (dot(grad(fsp.u), grad(fsp.nu_u)) + fsp.f * fsp.nu_u) * rmsh.dx \
-    - bgeo.facet_normal[i] * fsp.grad_u[i] * fsp.nu_u * rmsh.ds_lr \
-    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_tb \
-    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_circle
+# here the + and - are used to select the inner or outer par of quantities on interiors ds only
+F = (dot(grad(fsp.u), grad(fsp.nu_u)) + fsp.f * fsp.nu_u) * rmsh.dx_out \
+    - bgeo.facet_normal[i] * fsp.grad_u[i] * fsp.nu_u * rmsh.ds_out_lr \
+    - bgeo.facet_normal('+')[i] * fsp.grad_u[i] * fsp.nu_u('+') * rmsh.ds_in_tb \
+    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_out_tb \
+    - bgeo.facet_normal('+')[i] * (fsp.u.dx(i))('+') * fsp.nu_u('+') * rmsh.ds_in_lr
 
 # variational functional for post-processing problem (pp) to obtain the hessian (hess)
-F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
-       - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.ds
+F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx_out \
+       - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.ds_out\
+       - (bgeo.facet_normal('+')[i] * (fsp.u.dx(j))('+') * fsp.nu_hess_u('+')[i, j]) * rmsh.ds_in
+
