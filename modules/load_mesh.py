b7689c284ef0bce6054531f64aae60c86ef1c3d0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-27 13:41:08 +0200

    reading sub meshes

--- modules/load_mesh.py
+++ modules/load_mesh.py
@@ -1,6 +1,17 @@
 from fenics import *
+import os
 
 import mesh as msh
 import runtime_arguments as rarg
 
+# read the mesh
 mesh = msh.read_from_file(rarg.args.input_directory)
+
+
+# look for sub_meshes
+path_to_subfolder = os.path.join(rarg.args.input_directory, 'sub_meshes')
+
+if os.path.isdir(path_to_subfolder):
+    print(f"'{rarg.args.input_directory}' contains a subfolder called sub_meshes")
+else:
+    print(f"No subfolder named sub_meshes inside '{rarg.args.input_directory}'")
