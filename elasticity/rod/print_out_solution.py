059c1f6faccbd03262b06ba7ababfc7cc8d7e727
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-06-30 17:33:03 +0200

    Update print_out_u_bc_square_no_circle.py

--- elasticity/rod/print_out_solution.py
+++ elasticity/rod/print_out_solution.py
@@ -15,13 +15,8 @@ vp = importlib.import_module(swi.vp)
 
 i, j, k, l = ufl.indices(4)
 
-xdmffile_check = XDMFFile(rarg.args.output_directory + "/check.xdmf")
-xdmffile_check.parameters.update({"functions_share_mesh": True, "rewrite_function_mesh": False})
 
 io.full_print(fsp.u, 'u', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
-              solpath.nodal_values_path,
-              lmsh.mesh, 'vector')
-io.full_print(fsp.u_dot, 'u_dot', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path,
               lmsh.mesh, 'vector')
 
--- elasticity/rod/print_out_u_bc_square_no_circle.py
+++ elasticity/rod/print_out_u_bc_square_no_circle.py
@@ -21,7 +21,7 @@ i, j, k, l = ufl.indices(4)
 # check if the boundary conditions (BCs) are satisfied
 print("Check of BCs:")
 print(f"\t\t<<|u|^2>>_[partial Omega l] = {col.Fore.RED}{msh.abs_wrt_measure(fsp.u[i] * fsp.u[i], rmsh.ds_l):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
-print(f"\t\t<<(n_i P_ik)^2>>_[partial Omega r t b] = {col.Fore.RED}{msh.abs_wrt_measure( (bgeo.facet_normal[j] * ela.P(fsp.u, rpam.parameters["K"], rpam.parameters["mu"])[i, j]) * (bgeo.facet_normal[k] * ela.P(fsp.u, rpam.parameters["K"], rpam.parameters["mu"])[i, k]), rmsh.ds_r + rmsh.ds_t + rmsh.ds_b):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t\t<<(n_i P_ik)^2>>_[partial Omega r t b] = {col.Fore.RED}{msh.abs_wrt_measure( (bgeo.facet_normal[j] * ela.P(fsp.u, rpam.parameters['K'], rpam.parameters['mu'])[i, j]) * (bgeo.facet_normal[k] * ela.P(fsp.u, rpam.parameters['K'], rpam.parameters['mu'])[i, k]), rmsh.ds_r + rmsh.ds_t + rmsh.ds_b):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
 
 import print_out_solution
