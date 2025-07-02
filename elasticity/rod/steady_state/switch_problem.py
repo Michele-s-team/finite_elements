4239c189d3dd5388567be3993976982d84f26e4f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-02 13:44:57 +0200

    Creating _b instance

--- elasticity/rod/steady_state/switch_problem.py
+++ elasticity/rod/steady_state/switch_problem.py
@@ -9,4 +9,11 @@ if rarg.args.problem == 'square_no_circle_a':
     prout_bc = 'print_out_u_bc_square_no_circle_a'
     prout_bc_dot = 'print_out_u_dot_bc_square_no_circle_a'
 
+elif rarg.args.problem == 'square_no_circle_b':
+    rmsh = 'read_mesh_square_no_circle'
+    vp = 'variational_problem_u_bc_square_no_circle_b'
+    vp_dot = 'variational_problem_u_dot_bc_square_no_circle_b'
+    prout_bc = 'print_out_u_bc_square_no_circle_b'
+    prout_bc_dot = 'print_out_u_dot_bc_square_no_circle_b'
+
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
