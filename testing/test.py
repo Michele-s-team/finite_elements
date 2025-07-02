cc5f3ce0ac420141b3a0959e278ef5172aa297ea
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-02 23:49:53 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -517,6 +517,18 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
 
+# Test elasticity/rod/steady_state/periodic
+case_name = 'elasticity/rod/steady_state/periodic'
+
+problem_name = 'square_no_circle_a'
+generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/symmetric'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_square_no_circle_mesh', generate_mesh_path, generate_mesh_path, problem_name, success)
+
 
 # Test elasticity/rod/dynamics
 case_name = 'elasticity/rod/dynamics'
