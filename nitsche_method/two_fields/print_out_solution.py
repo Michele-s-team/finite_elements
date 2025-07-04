52bff2bdf1c0949ed6de706edbe7cfbd307a854c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-04 14:26:54 +0200

    nitsches method  /two fields
    Added functionality to write merged parameters to a metadata.csv file in the output directory. This helps track the parameters used for each solution run.

--- nitsche_method/two_fields/print_out_solution.py
+++ nitsche_method/two_fields/print_out_solution.py
@@ -5,6 +5,8 @@ import ufl as ufl
 import function_spaces as fsp
 import input_output as io
 import load_mesh as lmsh
+import read_parameters_solve as rpam
+import runtime_arguments as rarg
 import solution_paths as solpath
 import switch_problem as swi
 
@@ -16,3 +18,7 @@ i, j, k, l = ufl.indices(4)
 io.full_print(fsp.u, 'u', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path,
               lmsh.mesh, 'vector')
+
+io.write_parameters_to_csv_file(io.add_trailing_slash(rarg.args.output_directory) + "metadata.csv", \
+                                io.merge_dictionaries(rmsh.parameters, rpam.parameters))
+
