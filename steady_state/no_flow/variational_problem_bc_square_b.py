9d1cddd1e294d800c3b349a9125def52defc2f76
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-04 16:21:23 +0200

    Revising steady_state/no_flow/bc square b...

--- steady_state/no_flow/variational_problem_bc_square_b.py
+++ steady_state/no_flow/variational_problem_bc_square_b.py
@@ -5,6 +5,7 @@ import ufl as ufl
 import function_spaces as fsp
 import boundary_geometry as bgeo
 import geometry as geo
+import read_parameters_solve as rpam
 import switch_problem as swi
 
 rmsh = importlib.import_module(swi.rmsh)
@@ -12,11 +13,8 @@ rmsh = importlib.import_module(swi.rmsh)
 i, j, k, l = ufl.indices( 4 )
 
 # CHANGE PARAMETERS HERE
-# bending rigidity
 kappa = 1.0
-# Nitche's parameter
 alpha = 1e2
-
 z_square_const = 0.0
 omega_circle_const = 0.5
 n_omega_square_const = 0.0
@@ -24,7 +22,7 @@ n_omega_square_const = 0.0
 
 class SurfaceTensionExpression( UserExpression ):
     def eval(self, values, x):
-        values[0] = 1.0
+        values[0] = rpam.parameters["sigma_const"]
         # values[0] = (x[0] - rmsh.parameters["L"]/2)/(rmsh.parameters["L"]/2)
 
     def value_shape(self):
