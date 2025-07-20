1a2798aee0082c9fa51e02775b2edc5f074f4eac
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-20 23:41:39 +0200

    Update read_mesh_square_square.py

--- modules/read_mesh_square_square.py
+++ modules/read_mesh_square_square.py
@@ -1,25 +1,10 @@
 '''
 Notation:
-- parent_mesh: the total mesh, containing both the outer and the inner part
 - sub_mesh: either of the parts of the total mesh
 
 - sf_sub_mesh: a list of map functions, where sf_sub_mesh[i] is the map function for the triangles of the i-th sub_mesh
 - mf_sub_mesh: a list of map functions, where mf_sub_mesh[i] is the map function for the lines of the i-th sub_mesh
 
-* for the parent_mesh:
-    - dx_parent_mesh: a list of suerface elemetns of the parent mesh: dx_parent_mesh[i] is the surface elements of the i-th part of the parent mesh
-    - ds_parent_mesh_l: a list of line elements of the parent mesh: ds_parent_mesh_l[i] is the line element corresponding to the left boundary of the i-th part of the parent mesh
-    - ... similarly for r, t, b ...
-    - ds_parent_mesh_lr: a list of line elements of the parent mesh: ds_parent_mesh_lr[i] is the line element corresponding to the left + right boundary of the i-th part of the parent mesh
-    - ... similarly for tb, and for ds_parent_mesh ...
-    - ds: the boundary line element of the total parent mesh
-
-* for the sub_mesh:
-    - dx_sub_mesh: a list of surface elements of the sub_mesh: dx_sub_mesh[i] is the surface elements of the i-th sub_mesh
-    - ds_sub_mesh_l: a list of line elements of the sub mesh: ds_sub_mesh_l[i] is the line element corresponding to the left boundary of the i-th  sub_mesh
-    - ... similarly for r, t, b ...
-    - ds_sub_mesh_lr: a list of line elements of the sub_mesh: ds_sub_mesh_lr[i] is the line element corresponding to the left + right boundary of the i-th sub_mesh
-    - ... similarly for tb ...
 '''
 
 from fenics import *
@@ -45,7 +30,7 @@ for sub_mesh in lmsh.sub_meshes:
 
 # radius of the smallest cell in the mesh
 r_mesh = lmsh.mesh.hmin()
-
+'''
 # create line and surface elements for the parent mesh
 dx_parent_mesh, ds_parent_mesh_l, ds_parent_mesh_r, ds_parent_mesh_t, ds_parent_mesh_b, ds_parent_mesh_lr, ds_parent_mesh_tb, ds_parent_mesh_lrtb = [], [], [], [], [], [], [], []
 
@@ -70,22 +55,34 @@ for p in range(len(lmsh.sub_meshes)):
     ds_parent_mesh_lrtb.append(ds_parent_mesh_lr[p] + ds_parent_mesh_tb[p])
 
 ds_parent_mesh = ds_parent_mesh_lrtb[0] + ds_parent_mesh_lrtb[1]
+'''
 
 # create line and surface elements for sub_meshes
-dx_sub_mesh, ds_sub_mesh_l, ds_sub_mesh_r, ds_sub_mesh_t, ds_sub_mesh_b, ds_sub_mesh_lr, ds_sub_mesh_tb, ds_sub_mesh_lrtb = [], [], [], [], [], [], [], []
+dx_sub_mesh = []
 
 for p in range(len(lmsh.sub_meshes)):
     dx_sub_mesh.append(Measure("dx", domain=lmsh.sub_meshes[p], subdomain_data=sf_sub_mesh[p], subdomain_id=parameters[f"sub_mesh_{p}_id"]))
 
-    ds_sub_mesh_l.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_l_id"]))
-    ds_sub_mesh_r.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_r_id"]))
-    ds_sub_mesh_t.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_t_id"]))
-    ds_sub_mesh_b.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_b_id"]))
+ds_sub_mesh = [''] * len(lmsh.sub_meshes)
 
-    ds_sub_mesh_lr.append(ds_sub_mesh_l[p] + ds_sub_mesh_r[p])
-    ds_sub_mesh_tb.append(ds_sub_mesh_t[p] + ds_sub_mesh_b[p])
+ds_sub_mesh[0] = dict([ \
+    ('ds_l', Measure("ds", domain=lmsh.sub_meshes[0], subdomain_data=mf_sub_mesh[0], subdomain_id=parameters[f"line_sub_mesh_{0}_l_id"])), \
+    ('ds_r', Measure("ds", domain=lmsh.sub_meshes[0], subdomain_data=mf_sub_mesh[0], subdomain_id=parameters[f"line_sub_mesh_{0}_r_id"])), \
+    ('ds_t', Measure("ds", domain=lmsh.sub_meshes[0], subdomain_data=mf_sub_mesh[0], subdomain_id=parameters[f"line_sub_mesh_{0}_t_id"])), \
+    ('ds_b', Measure("ds", domain=lmsh.sub_meshes[0], subdomain_data=mf_sub_mesh[0], subdomain_id=parameters[f"line_sub_mesh_{0}_b_id"])) \
+    ])
 
-    ds_sub_mesh_lrtb.append(ds_sub_mesh_lr[p] + ds_sub_mesh_tb[p])
+# for p in range(len(lmsh.sub_meshes)):
+#
+#     ds_sub_mesh_l.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_l_id"]))
+#     ds_sub_mesh_r.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_r_id"]))
+#     ds_sub_mesh_t.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_t_id"]))
+#     ds_sub_mesh_b.append(Measure("ds", domain=lmsh.sub_meshes[p], subdomain_data=mf_sub_mesh[p], subdomain_id=parameters[f"line_sub_mesh_{p}_b_id"]))
+#
+#     ds_sub_mesh_lr.append(ds_sub_mesh_l[p] + ds_sub_mesh_r[p])
+#     ds_sub_mesh_tb.append(ds_sub_mesh_t[p] + ds_sub_mesh_b[p])
+#
+#     ds_sub_mesh_lrtb.append(ds_sub_mesh_lr[p] + ds_sub_mesh_tb[p])
 
 
 import check_mesh_tags_square_square
