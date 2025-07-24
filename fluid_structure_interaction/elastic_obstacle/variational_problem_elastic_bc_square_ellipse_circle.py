e16338e128c219ec49104a65480bdc6f35470f87
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-24 23:31:25 +0200

    Update variational_problem_elastic_bc_square_ellipse_circle.py

--- fluid_structure_interaction/elastic_obstacle/variational_problem_elastic_bc_square_ellipse_circle.py
+++ fluid_structure_interaction/elastic_obstacle/variational_problem_elastic_bc_square_ellipse_circle.py
@@ -110,6 +110,7 @@ F_el_u_dot = (
 F_el_u = (fsp.u_el_n[i] - fsp.u_el_n_1[i] - fsp.u_el_dot_n[i] * dt) * fsp.nu_u[i] * rmsh.dx_sub_mesh[0]
 
 F_N = rpam.alpha / rmsh.r_mesh * ( \
+            # in F I PUT U_N RATHER THAN U_N_1 AS IN THE NOTES -> REVISE THIS
             ((geo.epsilon[j, k] * fsp.dyds_ellipse[k] * ela.P(fsp.u_el_n)[i, j] - ela.var_sigma_tensor(fsp.sigma_n_32, fsp.v_n_1, fsp.u_el_n, rpam.mu_fluid)[i, j] * geo.epsilon[j, k] * ela.F(fsp.u_el_n)[k, l] * fsp.dyds_ellipse[l]) * ()) * rmsh.ds_sub_mesh[0]['ds_ellipse'] \
     )
 
