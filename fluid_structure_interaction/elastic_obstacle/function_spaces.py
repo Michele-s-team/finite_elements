775a1a331f5fae136795e0a53442cfb8833b5ab8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-24 17:05:42 +0200

    Update function_spaces.py

--- fluid_structure_interaction/elastic_obstacle/function_spaces.py
+++ fluid_structure_interaction/elastic_obstacle/function_spaces.py
@@ -5,20 +5,14 @@ import load_mesh as lmsh
 
 '''
 the variables for the problem are
-- 'theta_n', 'theta_n_1' : \theta^n, \theta^{n-1} in notes
-- 'omega_n', 'omega_n_1' : \omega^n, \omega^{n-1} in notes
+- 'u_n', 'u_n_1': u^n, u^{n-1} in notes
+- 'u_dot_n', 'u_dot_n_1', 'u_dot_n_2' : u^n, u^{n-1}, u^{n-2} in notes
 - 'v^n' = \textrm{v}^n_notes
 - 'v_' = \textrm{v}^*_notes
-- 'phi' = phi_notes
+- 'phi' = \varphi_notes
 - 'sigma' = \varsigma_notes
-- 'u', 'u_dot' = u_notes, \dot{u}_notes
 '''
 
-
-theta_n : float
-omega_n : float
-theta_n_1 : float
-omega_n_1 : float
 
 # Define function spaces
 Q_v = VectorFunctionSpace(lmsh.mesh, 'P', 2)
@@ -30,8 +24,6 @@ Q_u_dot = VectorFunctionSpace(lmsh.mesh, 'P', 1)
 # function space for the vector dy(s)/ds which represents the tangent to the ellipse curve
 Q_y = VectorFunctionSpace(lmsh.mesh, 'P', 2)
 Q_dyds = VectorFunctionSpace(lmsh.mesh, 'P', 2)
-
-
 
 # Define functions for solutions at previous and current time steps
 v_n = Function(Q_v)
@@ -66,7 +58,6 @@ nu_v_ = TestFunction(Q_v_)
 nu_phi = TestFunction(Q_phi)
 nu_u = TestFunction(Q_u)
 nu_u_dot = TestFunction(Q_u_dot)
-
 
 # Jacobians
 J_v_ = TrialFunction(Q_v_)
