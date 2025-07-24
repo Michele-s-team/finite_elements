dbb4be41b017ea85c051dc12965a28391863c507
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-24 18:09:50 +0200

    Update read_parameters.py

--- fluid_structure_interaction/elastic_obstacle/read_parameters.py
+++ fluid_structure_interaction/elastic_obstacle/read_parameters.py
@@ -7,6 +7,8 @@ v_l = io.read_parameter_from_csv_file("parameters.csv", "v_l", float)
 T = io.read_parameter_from_csv_file("parameters.csv", "T", float)
 num_steps = io.read_parameter_from_csv_file("parameters.csv", "num_steps", int)
 exponent = io.read_parameter_from_csv_file("parameters.csv", "exponent", float)
+K_elastic = io.read_parameter_from_csv_file("parameters.csv", "K_elastic", float)
+mu_elastic = io.read_parameter_from_csv_file("parameters.csv", "mu_elastic", float)
 
 
 print(f'rho_fluid = {rho_fluid}, mu_fluid = {mu_fluid}, v_l = {v_l}, T = {T},  num_steps = {num_steps}, exponent = {exponent}')
