97379b6c9da06c745c9474bca8f398db05fc1ef8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-24 18:06:00 +0200

    rho_fluid

--- fluid_structure_interaction/elastic_obstacle/read_parameters.py
+++ fluid_structure_interaction/elastic_obstacle/read_parameters.py
@@ -1,7 +1,7 @@
 import input_output as io
 
 # CHANGE PARAMETERS HERE
-rho = io.read_parameter_from_csv_file("parameters.csv", "rho", float)
+rho_fluid = io.read_parameter_from_csv_file("parameters.csv", "rho_fluid", float)
 mu = io.read_parameter_from_csv_file("parameters.csv", "mu", float)
 v_l = io.read_parameter_from_csv_file("parameters.csv", "v_l", float)
 T = io.read_parameter_from_csv_file("parameters.csv", "T", float)
@@ -9,4 +9,4 @@ num_steps = io.read_parameter_from_csv_file("parameters.csv", "num_steps", int)
 exponent = io.read_parameter_from_csv_file("parameters.csv", "exponent", float)
 
 
-print(f'rho = {rho}, mu = {mu}, v_l = {v_l}, T = {T},  num_steps = {num_steps}, exponent = {exponent}')
+print(f'rho_fluid = {rho_fluid}, mu = {mu}, v_l = {v_l}, T = {T},  num_steps = {num_steps}, exponent = {exponent}')
--- fluid_structure_interaction/elastic_obstacle/variational_problem_fluid_bc_square_ellipse_circle.py
+++ fluid_structure_interaction/elastic_obstacle/variational_problem_fluid_bc_square_ellipse_circle.py
@@ -56,7 +56,7 @@ bc_phi = [bc_phi_r]
 # Define variational problem for step 1
 # step 1 for v_
 F_v_ = ( \
-                   rpam.rho * ((fsp.v_[i] - fsp.v_n_1[i]) / dt \
+                   rpam.rho_fluid * ((fsp.v_[i] - fsp.v_n_1[i]) / dt \
                                + (3.0 / 2.0 * (fsp.v_n_1[k] - fsp.u_el_dot_n_1[k]) * ela.G(fsp.u_el_n_1)[j, k] - 1.0 / 2.0 * (fsp.v_n_2[k] - fsp.u_el_dot_n_2[k]) * ela.G(fsp.u_el_n_2)[j, k]) * (fsp.V[i]).dx(j)) * fsp.nu_v_[i] \
                    + fsp.sigma_n_32 * ela.G(fsp.u_el_n_1)[l, i] * (fsp.nu_v_[i]).dx(l) + rpam.mu * ela.G(fsp.u_el_n_1)[k, j] * ((fsp.V[i]).dx(k)) * ela.G(fsp.u_el_n_1)[l, j] * (fsp.nu_v_[i]).dx(l) \
            ) * ela.detF(fsp.u_el_n_1) * rmsh.dx \
@@ -71,9 +71,9 @@ F_v_ = ( \
 # step 2 for phi
 F_phi = ( \
                     - ela.G(fsp.u_el_n_1)[j, i] * (fsp.phi.dx(j)) * ela.G(fsp.u_el_n_1)[l, i] * (fsp.nu_phi.dx(l)) \
-                    - (rpam.rho / dt) * ela.G(fsp.u_el_n_1)[j, i] * ((fsp.v_[i]).dx(j)) * fsp.nu_phi \
+                    - (rpam.rho_fluid / dt) * ela.G(fsp.u_el_n_1)[j, i] * ((fsp.v_[i]).dx(j)) * fsp.nu_phi \
             ) * ela.detF(fsp.u_el_n_1) * rmsh.dx \
         + (ela.G(fsp.u_el_n_1)[l, i] * bgeo.facet_normal[l] * ela.G(fsp.u_el_n_1)[j, i] * (fsp.phi.dx(j)) * fsp.nu_phi) * ela.detF(fsp.u_el_n_1) * rmsh.ds_r
 
 # step 3 for v_n
-F_v_n = (((fsp.v_n[i] - fsp.v_[i]) + (dt / rpam.rho) * ela.G(fsp.u_el_n_1)[l, i] * (fsp.phi.dx(l))) * fsp.nu_v_n[i]) * ela.detF(fsp.u_el_n_1) * rmsh.dx
+F_v_n = (((fsp.v_n[i] - fsp.v_[i]) + (dt / rpam.rho_fluid) * ela.G(fsp.u_el_n_1)[l, i] * (fsp.phi.dx(l))) * fsp.nu_v_n[i]) * ela.detF(fsp.u_el_n_1) * rmsh.dx
