ee388a36072f2a7c275d22723c3c81f5b68e5292
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-07-25 17:12:01 +0200

    implemented natural BC

--- fluid_structure_interaction/elastic_obstacle/variational_problem_elastic_bc_square_ellipse_circle.py
+++ fluid_structure_interaction/elastic_obstacle/variational_problem_elastic_bc_square_ellipse_circle.py
@@ -100,21 +100,14 @@ bc_u_el_circle = DirichletBC(fsp.Q_el.sub(0), fsp.u_el_circle, rmsh.boundary[0][
 bcs_el = [bc_u_el_circle]
 
 # variational functional for the original problem
+# natural BC imposed here
 F_el_u_dot = (
                      fsp.rho_el / dt * (fsp.u_el_dot_n[i] - fsp.u_el_dot_n_1[i]) * fsp.nu_u_el_n[i] \
                      + ela.P(fsp.u_el_n, rpam.K_elastic, rpam.mu_elastic)[i, k] * (fsp.nu_u_el_n[i].dx(k)) \
                  ) * rmsh.dx_sub_mesh[0] \
              - bgeo.sub_mesh_facet_normal[0][k] * ela.P(fsp.u_el_n, rpam.K_elastic, rpam.mu_elastic)[i, k] * fsp.nu_u_el_n[i] * rmsh.ds_sub_mesh[0]['ds_circle'] \
-             - bgeo.sub_mesh_facet_normal[0][k] * ela.P(fsp.u_el_n, rpam.K_elastic, rpam.mu_elastic)[i, k] * fsp.nu_u_el_n[i] * rmsh.ds_sub_mesh[0]['ds_ellipse']
+             - (ela.var_sigma_tensor(fsp.sigma_n_32_on_sub_mesh_0, fsp.v_n_1_on_sub_mesh_0, fsp.u_el_n, rpam.mu_fluid)[i, j] * geo.epsilon[j, k] * ela.F(fsp.u_el_n_1)[k, l] * fsp.dyds_ellipse[l] / sqrt(fsp.dyds_ellipse[m] * fsp.dyds_ellipse[m])) * fsp.nu_u_el_n[i] * rmsh.ds_sub_mesh[0]['ds_ellipse']
 
 F_el_u = (fsp.u_el_n[i] - fsp.u_el_n_1[i] - fsp.u_el_dot_n[i] * dt) * fsp.nu_u_el_dot_n[i] * rmsh.dx_sub_mesh[0]
 
-F_N = rpam.alpha / rmsh.r_mesh * ( \
-    # in F I PUT U_N RATHER THAN U_N_1 AS IN THE NOTES -> REVISE THIS
-        ( \
-                    (geo.epsilon[j, k] * fsp.dyds_ellipse[k] * ela.P(fsp.u_el_n, rpam.K_elastic, rpam.mu_elastic)[i, j] - ela.var_sigma_tensor(fsp.sigma_n_32_on_sub_mesh_0, fsp.v_n_1_on_sub_mesh_0, fsp.u_el_n, rpam.mu_fluid)[i, j] * geo.epsilon[j, k] * ela.F(fsp.u_el_n)[k, l] * fsp.dyds_ellipse[l]) \
-                    * (ela.var_sigma_tensor(fsp.sigma_n_32_on_sub_mesh_0, fsp.v_n_1_on_sub_mesh_0, fsp.u_el_n, rpam.mu_fluid)[i, m] * geo.epsilon[m, n] * fsp.nu_u_el_n[n].dx(o) * fsp.dyds_ellipse[o]) \
-            ) * rmsh.ds_sub_mesh[0]['ds_ellipse'] \
-    )
-
-F_el = (F_el_u_dot + F_el_u) + F_N
+F_el = F_el_u_dot + F_el_u
