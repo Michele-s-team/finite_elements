bd3835cff140777697bcc93eff21ea43ef631f2b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-03 21:55:03 +0200

    added metadata

--- poisson_equation/solve_u_v/print_out_solution.py
+++ poisson_equation/solve_u_v/print_out_solution.py
@@ -5,6 +5,8 @@ import ufl as ufl
 import function_spaces as fsp
 import input_output as io
 import load_mesh as lmsh
+import read_parameters_solve as rpam
+import runtime_arguments as rarg
 import solution_paths as solpath
 import switch_problem as swi
 
@@ -22,3 +24,6 @@ io.full_print(u_output, 'u', solpath.xdmf_file_path, solpath.h5_file_path, solpa
 io.full_print(v_output, 'v', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path,
               lmsh.mesh, 'vector')
+
+io.write_parameters_to_csv_file(io.add_trailing_slash(rarg.args.output_directory) + "metadata.csv", \
+                                io.merge_dictionaries(rmsh.parameters, rpam.parameters))
