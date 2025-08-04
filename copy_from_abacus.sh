fb6e92c58dc5e8a41d47236a2c7b921c2e8b2f81
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-04 15:37:45 +0200

    Update copy_from_abacus.sh

--- copy_from_abacus.sh
+++ copy_from_abacus.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#ths script copies from abacus all files of typs from a path on abacus to a local path, by copying only files matching patterns given by the user
+# This script copies from abacus all files matching one or more patterns,
+# filtering them by numeric suffix (min, max, stride).
 
 # Usage:
-#   ./copy_from_abacus.sh [remote_dir] [pattern1] [pattern2] ... [local_dir]
-#Example
-# ./copy_from_abacus.sh elastic_obstacle_2/solution/snapshots/csv/nodal_values 'def_v_n_*' 'line_mesh_el_n_*' 'line_mesh_msh_n_' 'u_msh_n_*' ~/Desktop
+#   ./copy_from_abacus.sh [remote_dir] [pattern1] [pattern2] ... [local_dir] [min] [max] [stride]
+# Example:
+#   ./copy_from_abacus.sh elastic_obstacle_2/solution/snapshots/csv/nodal_values 'v_n_*' 'u_msh_n_*' ~/Desktop 1 10 3
+
 set -e
 
-if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 [remote_dir] [pattern1] [pattern2] ... [local_dir]"
+if [ "$#" -lt 4 ]; then
+    echo "Usage: $0 [remote_dir] [pattern1] [pattern2] ... [local_dir] [min] [max] [stride]"
     exit 1
 fi
 
+# Parse fixed arguments
 REMOTE_BASE="/mnt/beegfs/home/mcastel1"
 REMOTE_DIR="$1"
-OUT_DIR="${@: -1}"           # Last argument is output directory
-PATTERNS=("${@:2:$#-2}")     # All arguments except first and last are patterns
+OUT_DIR="${@: -4:1}"   # 4th-to-last arg is local output dir
+MIN="${@: -3:1}"       # 3rd-to-last
+MAX="${@: -2:1}"       # 2nd-to-last
+STRIDE="${@: -1:1}"    # last arg
+PATTERNS=("${@:2:$#-5}")  # Everything between remote_dir and local_dir
 
 IN_DIR="$REMOTE_BASE/$REMOTE_DIR"
 
@@ -24,24 +30,42 @@ clear; clear
 echo "Remote directory: $IN_DIR"
 echo "Patterns to match: ${PATTERNS[*]}"
 echo "Local output directory: $OUT_DIR"
+echo "Filter: min=$MIN, max=$MAX, stride=$STRIDE"
 
-# Remove and recreate local output dir
-rm -rf "$OUT_DIR"
-mkdir -p $OUT_DIR/$REMOTE_DIR
+# Clean and recreate output directory
+#uncomment this if you want to remove the existing directory, if any
+#rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR/$REMOTE_DIR"
 
-FIND_CMD="rm -f \"$IN_DIR/file_list.txt\"; "
-FIND_CMD+="cd \"$REMOTE_BASE\" && "
-FIND_CMD+="( "
+# Build the remote command to generate file_list.txt
+REMOTE_CMD=$(cat <<EOF
+cd "$REMOTE_BASE"
+rm -f "$IN_DIR/file_list.txt"
+touch "$IN_DIR/file_list.txt"
+EOF
+)
 
 for pattern in "${PATTERNS[@]}"; do
-  FIND_CMD+="find \"$REMOTE_DIR\" -type f -name \"$pattern\" -printf \"%P\n\"; "
+    REMOTE_CMD+=$'\n'
+    REMOTE_CMD+=$(cat <<EOF
+find "$REMOTE_DIR" -type f -name "$pattern" -printf "%P\n" | \\
+awk -F'[^0-9]*' '{
+    num = -1
+    for (i=NF; i>0; i--) {
+        if (\$i ~ /^[0-9]+\$/) {
+            num = \$i
+            break
+        }
+    }
+    if (num >= $MIN && num <= $MAX && (num - $MIN) % $STRIDE == 0)
+        print \$0
+}' >> "$IN_DIR/file_list.txt"
+EOF
+)
 done
 
-FIND_CMD+=") > \"$IN_DIR/file_list.txt\""
-
-
 echo "Building remote file list..."
-ssh mcastel1@abacus "$FIND_CMD"
+ssh mcastel1@abacus "$REMOTE_CMD"
 
 echo "Copying file list locally..."
 rsync --stats --size-only -P -v -e ssh mcastel1@abacus:"$IN_DIR/file_list.txt" "$OUT_DIR"
