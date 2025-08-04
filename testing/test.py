ac1d6ebe847cd2bf87929776c48378a5aed306b0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-04 14:59:49 +0200

    Merge branch 'revise_solve_u_v' Test OK

--- testing/test.py
+++ testing/test.py
@@ -227,7 +227,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
-'''
+
 
 
 # Test poisson_equation/solve_u_v
@@ -242,7 +242,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_disk_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
-'''
+
 # Test nitsche_method
 
 # Test nitsche_method/one_field
