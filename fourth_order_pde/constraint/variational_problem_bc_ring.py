a41710b488f1f3785bbc0db31b7ff4e45f526ccd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-15 21:32:13 +0200

    copied files

---
+++ fourth_order_pde/constraint/variational_problem_bc_ring.py
@@ -0,0 +1,103 @@
+'''
+    Here the constraint is
+    z - u = g,
+    This is equivalent to solving the  following PDE for z:
+
+    Nabla Nabla \partial_i ((2*z-g) \partial_i (2*z-g)) = f
+
+
+
+'''
+
+from fenics import *
+import importlib
+import ufl as ufl
+
+import boundary_geometry as bgeo
+import function_spaces as fsp
+import read_parameters_solve as rpam
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+
+i, j, k, l = ufl.indices(4)
+
+
+class z_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = (x[0] ** 4 + x[1] ** 4) / 48.0
+
+    def value_shape(self):
+        return (1,)
+
+
+class u_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = (x[0] ** 4 - x[1] ** 4) / 48.0
+
+    def value_shape(self):
+        return (1,)
+
+
+class omega_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = (x[0] ** 3) / 12.0
+        values[1] = (x[1] ** 3) / 12.0
+
+    def value_shape(self):
+        return (2,)
+
+
+class mu_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = 7.0/144.0*x[0]**6
+    def value_shape(self):
+        return (1,)
+
+
+class f_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = 35.0 / 24.0 * x[0] ** 4
+
+    def value_shape(self):
+        return (1,)
+
+
+class g_exact_expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = x[1] ** 4 / 24.0
+
+    def value_shape(self):
+        return (1,)
+
+
+fsp.z_exact.interpolate(z_exact_expression(element=fsp.Q_z.ufl_element()))
+fsp.omega_exact.interpolate(omega_exact_expression(element=fsp.Q_omega.ufl_element()))
+fsp.mu_exact.interpolate(mu_exact_expression(element=fsp.Q_mu.ufl_element()))
+fsp.f.interpolate(f_exact_expression(element=fsp.Q_z.ufl_element()))
+fsp.g.interpolate(g_exact_expression(element=fsp.Q_z.ufl_element()))
+
+bc_z = DirichletBC(fsp.Q.sub(0), fsp.z_exact, rmsh.boundary)
+bc_mu = DirichletBC(fsp.Q.sub(3), fsp.mu_exact, rmsh.boundary)
+
+# here is assign a wrong value to u (f) on purpose to see whether the solver conveges to the right solution
+fsp.assigner.assign(fsp.psi, [fsp.z_exact, fsp.u_exact, fsp.omega_exact, fsp.mu_exact])
+
+F_z = ((fsp.mu.dx(j)) * (fsp.nu_z.dx(j)) + fsp.f * fsp.nu_z) * rmsh.dx \
+      - bgeo.facet_normal[j] * (fsp.mu.dx(j)) * fsp.nu_z * rmsh.ds
+
+F_u = ((fsp.z - fsp.u - fsp.g) * (fsp.nu_z - fsp.nu_u)) * rmsh.dx
+
+F_omega = ((fsp.z + fsp.u) * ((fsp.nu_omega[i]).dx(i)) + fsp.omega[i] * fsp.nu_omega[i]) * rmsh.dx \
+          - bgeo.facet_normal[i] * (fsp.z + fsp.u) * fsp.nu_omega[i] * rmsh.ds
+
+F_mu = ((fsp.z + fsp.u) * fsp.omega[i] * (fsp.nu_mu.dx(i)) + fsp.mu * fsp.nu_mu) * rmsh.dx \
+       - bgeo.facet_normal[i] * (fsp.z + fsp.u) * fsp.omega[i] * fsp.nu_mu * rmsh.ds
+
+F_N = rpam.parameters['alpha'] / rmsh.r_mesh * ( \
+            # (bgeo.facet_normal[i] * fsp.omega[i] - bgeo.facet_normal[i] * fsp.omega_exact[i]) * bgeo.facet_normal[j] * fsp.nu_omega[j] * rmsh.ds + \
+            ((fsp.z - fsp.u - fsp.g) * (fsp.nu_z - fsp.nu_u)) * rmsh.ds
+)
+
+F = (F_z + F_u + F_omega + F_mu) + F_N
+bcs = [bc_z, bc_mu]
