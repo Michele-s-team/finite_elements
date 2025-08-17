449473db23b2a343bbf0ce7026a5bcbfd4e765ab
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-17 15:26:55 +0200

    Update function_spaces.py

--- steady_state/no_flow/dereny_approach/function_spaces.py
+++ steady_state/no_flow/dereny_approach/function_spaces.py
@@ -4,42 +4,52 @@ import load_mesh as lmsh
 import read_parameters_solve as rpam
 
 # Define function spaces
-# finite elements for psi, rho, zeta
+'''
+the fields are
+- psi = psi_notes
+- omega = \partial_1 psi
+- rho = rho_notes
+- zeta = \zeta_notes
+'''
 
 P_psi = FiniteElement('P', triangle, rpam.parameters['degree_function_space'])
+P_omega = FiniteElement('P', triangle, rpam.parameters['degree_function_space'])
 P_rho = FiniteElement('P', triangle, rpam.parameters['degree_function_space'])
 P_zeta = FiniteElement('P', triangle, rpam.parameters['degree_function_space'])
 
-element = MixedElement([P_psi, P_rho, P_zeta])
+element = MixedElement([P_psi, P_omega, P_rho, P_zeta])
 # total function space
 Q = FunctionSpace(lmsh.mesh, element)
-# function spaces for z, omega, eta and theta
 Q_psi = Q.sub(0).collapse()
-Q_rho = Q.sub(1).collapse()
-Q_zeta = Q.sub(2).collapse()
+Q_omega = Q.sub(1).collapse()
+Q_rho = Q.sub(2).collapse()
+Q_zeta = Q.sub(3).collapse()
 
 Q_sigma = FunctionSpace(lmsh.mesh, 'P', 1)
 
 # Define functions
 J_psi = TrialFunction(Q)
 phi = Function(Q)
-nu_psi, nu_rho, nu_zeta = TestFunctions(Q)
+nu_psi, nu_omega, nu_rho, nu_zeta = TestFunctions(Q)
 
 # these functions are used to print the solution to file
 sigma = Function(Q_sigma)
 
 psi_output = Function(Q_psi)
+omega_output = Function(Q_omega)
 rho_output = Function(Q_rho)
 zeta_output = Function(Q_zeta)
 
 psi_exact = Function(Q_psi)
+omega_exact = Function(Q_omega)
 rho_exact = Function(Q_rho)
 zeta_exact = Function(Q_zeta)
 
 # omega_0, z_0 are used to store the initial conditions
 psi_0 = Function(Q_psi)
+omega_0 = Function(Q_omega)
 rho_0 = Function(Q_rho)
 zeta_0 = Function(Q_zeta)
 
-psi, rho, zeta = split(phi)
-assigner = FunctionAssigner(Q, [Q_psi, Q_rho, Q_zeta])
+psi, omega, rho, zeta = split(phi)
+assigner = FunctionAssigner(Q, [Q_psi, Q_omega, Q_rho, Q_zeta])
--- steady_state/no_flow/dereny_approach/variational_problem_bc_square_no_circle.py
+++ steady_state/no_flow/dereny_approach/variational_problem_bc_square_no_circle.py
@@ -10,26 +10,18 @@ import switch_problem as swi
 
 rmsh = importlib.import_module(swi.rmsh)
 
-i, j, k, l = ufl.indices( 4 )
+i, j, k, l = ufl.indices(4)
 
 
-'''
-if you compare with the solution from check-with-analytical-solution-bc-square-no-circle-a.nb:
-    - z_t(b)_const_{here} <-> \[Phi]TOP(BOTTOM)_{check-with-analytical-solution-bc-square-no-circle.nb}
-    - omega_t(b)_const_{here} <-> \[Psi]TOP(BOTTOM)_{check-with-analytical-solution-bc-square-no-circle.nb}
-
-'''
-
-
-class SurfaceTensionExpression( UserExpression ):
+class sigma_Expression(UserExpression):
     def eval(self, values, x):
-        values[0] = 1.0
+        values[0] = rpam.parameters['sigma_const']
 
     def value_shape(self):
         return (1,)
 
 
-class z_exact_Expression( UserExpression ):
+class psi_exact_Expression(UserExpression):
     def eval(self, values, x):
         values[0] = 0
 
@@ -37,40 +29,7 @@ class z_exact_Expression( UserExpression ):
         return (1,)
 
 
-class omega_exact_Expression( UserExpression ):
-    def eval(self, values, x):
-        values[0] = 0
-        values[1] = 0
-
-    def value_shape(self):
-        return (2,)
-
-
-class mu_exact_Expression( UserExpression ):
-    def eval(self, values, x):
-        values[0] = 0
-
-
-    def value_shape(self):
-        return (1,)
-
-class tau_exact_Expression( UserExpression ):
-    def eval(self, values, x):
-        values[0] = 0
-
-
-    def value_shape(self):
-        return (1,)
-
-
-class n_omega_l_Expression(UserExpression):
-    def eval(self, values, x):
-        values[0] = 0
-
-    def value_shape(self):
-        return (1,)
-
-class n_omega_r_Expression(UserExpression):
+class omega_exact_Expression(UserExpression):
     def eval(self, values, x):
         values[0] = 0
 
@@ -78,68 +37,61 @@ class n_omega_r_Expression(UserExpression):
         return (1,)
 
 
-class n_omega_t_Expression(UserExpression):
+class rho_exact_Expression(UserExpression):
     def eval(self, values, x):
-        values[0] = rpam.parameters['n_omega_t_const']
-
-    def value_shape(self):
-        return (1,)
-
-class n_omega_b_Expression(UserExpression):
-    def eval(self, values, x):
-        values[0] = rpam.parameters['n_omega_b_const']
+        values[0] = 0
 
     def value_shape(self):
         return (1,)
 
 
-# the values of \partial_i z = omega_i on the circle and on the square, to be used in the boundary conditions (BCs) imposed with Nitche's method, in F_N
-n_omega_l = interpolate(n_omega_l_Expression(element=fsp.Q_psi.ufl_element()), fsp.Q_psi)
-n_omega_r = interpolate(n_omega_r_Expression(element=fsp.Q_psi.ufl_element()), fsp.Q_psi)
-n_omega_t = interpolate(n_omega_t_Expression(element=fsp.Q_psi.ufl_element()), fsp.Q_psi)
-n_omega_b = interpolate(n_omega_b_Expression(element=fsp.Q_psi.ufl_element()), fsp.Q_psi)
+class zeta_exact_Expression(UserExpression):
+    def eval(self, values, x):
+        values[0] = 0
+
+    def value_shape(self):
+        return (1,)
 
 
-fsp.sigma.interpolate( SurfaceTensionExpression( element=fsp.Q_sigma.ufl_element() ) )
-fsp.psi_0.interpolate(z_exact_Expression(element=fsp.Q_psi.ufl_element()))
-fsp.rho_0.interpolate(omega_exact_Expression(element=fsp.Q_rho.ufl_element()))
-fsp.zeta_0.interpolate(mu_exact_Expression(element=fsp.Q_zeta.ufl_element()))
+fsp.sigma.interpolate(sigma_Expression(element=fsp.Q_sigma.ufl_element()))
 
-fsp.tau_0.interpolate( tau_exact_Expression( element=fsp.Q_tau.ufl_element() ) )
+fsp.psi_0.interpolate(psi_exact_Expression(element=fsp.Q_psi.ufl_element()))
+fsp.omega_0.interpolate(omega_exact_Expression(element=fsp.Q_omega.ufl_element()))
+fsp.rho_0.interpolate(rho_exact_Expression(element=fsp.Q_rho.ufl_element()))
+fsp.zeta_0.interpolate(zeta_exact_Expression(element=fsp.Q_zeta.ufl_element()))
 
-fsp.psi_exact.interpolate(z_exact_Expression(element=fsp.Q_psi.ufl_element()))
-fsp.rho_exact.interpolate(omega_exact_Expression(element=fsp.Q_rho.ufl_element()))
-fsp.zeta_exact.interpolate(mu_exact_Expression(element=fsp.Q_zeta.ufl_element()))
+fsp.psi_exact.interpolate(psi_exact_Expression(element=fsp.Q_psi.ufl_element()))
+fsp.omega_exact.interpolate(omega_exact_Expression(element=fsp.Q_omega.ufl_element()))
+fsp.rho_exact.interpolate(rho_exact_Expression(element=fsp.Q_rho.ufl_element()))
+fsp.zeta_exact.interpolat(zeta_exact_Expression(element=fsp.Q_zeta.ufl_element()))
 
-fsp.tau_exact.interpolate( tau_exact_Expression( element=fsp.Q_tau.ufl_element() ) )
+# uncomment this if you want to assign to psi the initial profiles stored in v_0, ..., z_0
+# fsp.assigner.assign(fsp.phi, [fsp.psi_0, fsp.omega_0, fsp.rho_0, fsp.zeta_0])
 
-#uncomment this if you want to assign to psi the initial profiles stored in v_0, ..., z_0
-fsp.assigner.assign(fsp.phi, [fsp.psi_0, fsp.rho_0, fsp.zeta_0])
 
-# boundary conditions (BCs)
+bc_psi_l = DirichletBC(fsp.Q.sub(0), rpam.parameters['psi_l'], rmsh.boundary_l)
+bc_psi_r = DirichletBC(fsp.Q.sub(0), rpam.parameters['psi_r'], rmsh.boundary_r)
 
-# BCs for z
-#note that here I imposte BCs for z only on l and r because the solution is independent of x, if you consider cases where the solutoion depdends on x, add bc_l, bc_r
-bc_z_t = DirichletBC( fsp.Q.sub( 0 ), rpam.parameters['z_t_const'], rmsh.boundary_t )
-bc_z_b = DirichletBC( fsp.Q.sub( 0 ), rpam.parameters['z_b_const'], rmsh.boundary_b )
+bc_omega_l = DirichletBC(fsp.Q.sub(1), rpam.parameters['omega_l'], rmsh.boundary_l)
 
-# all BCs
-bcs = [bc_z_t, bc_z_b]
+bc_rho_l = DirichletBC(fsp.Q.sub(2), rpam.parameters['rho_l'], rmsh.boundary_l)
+bc_zeta_l = DirichletBC(fsp.Q.sub(3), rpam.parameters['zeta_l'], rmsh.boundary_l)
+
+bcs = [bc_psi_l, bc_psi_r, bc_omega_l, bc_rho_l, bc_zeta_l]
 
 # Define variational problem
 
 F_z = (rpam.parameters["kappa"] * (geo.g_c(fsp.rho)[i, j] * (fsp.zeta.dx(j)) * (fsp.nu_psi.dx(i)) - 2.0 * fsp.zeta * ((fsp.zeta) ** 2 - geo.K(fsp.rho)) * fsp.nu_psi) + fsp.sigma * fsp.zeta * fsp.nu_psi) * geo.sqrt_detg(fsp.rho) * rmsh.dx \
       - ( \
-         # natural BC (bgeo.n_tb(fsp.omega))[i] * (fsp.mu.dx(i)) = 0 on ds_l and ds_r is imposed here
+          # natural BC (bgeo.n_tb(fsp.omega))[i] * (fsp.mu.dx(i)) = 0 on ds_l and ds_r is imposed here
               (rpam.parameters["kappa"] * (bgeo.n_tb(fsp.rho))[i] * fsp.nu_psi * (fsp.zeta.dx(i))) * bgeo.sqrt_deth_tb(fsp.rho) * (rmsh.ds_t + rmsh.ds_b) \
-      )
+          )
 
 F_omega = (- fsp.psi * geo.Nabla_v(fsp.nu_rho, fsp.rho)[i, i] - fsp.rho[i] * fsp.nu_rho[i]) * geo.sqrt_detg(fsp.rho) * rmsh.dx \
           + ((bgeo.n_lr(fsp.rho))[i] * geo.g(fsp.rho)[i, j] * fsp.psi * fsp.nu_rho[j]) * bgeo.sqrt_deth_lr(fsp.rho) * (rmsh.ds_l + rmsh.ds_r) \
           + ((bgeo.n_tb(fsp.rho))[i] * geo.g(fsp.rho)[i, j] * fsp.psi * fsp.nu_rho[j]) * bgeo.sqrt_deth_tb(fsp.rho) * (rmsh.ds_t + rmsh.ds_b) \
-
-
-F_mu = ((geo.H(fsp.rho) - fsp.zeta) * fsp.nu_zeta) * geo.sqrt_detg(fsp.rho) * rmsh.dx
+ \
+    F_mu = ((geo.H(fsp.rho) - fsp.zeta) * fsp.nu_zeta) * geo.sqrt_detg(fsp.rho) * rmsh.dx
 
 F_N = rpam.parameters["alpha"] / rmsh.r_mesh * ( \
             + (((bgeo.n_lr(fsp.rho))[i] * fsp.rho[i] - n_omega_l) * ((bgeo.n_lr(fsp.rho))[k] * geo.g(fsp.rho)[k, l] * fsp.nu_rho[l])) * bgeo.sqrt_deth_lr(fsp.rho) * rmsh.ds_l \
@@ -151,8 +103,7 @@ F_N = rpam.parameters["alpha"] / rmsh.r_mesh * ( \
             + ((geo.H(fsp.rho) - fsp.zeta) * fsp.nu_zeta) * bgeo.sqrt_deth_tb(fsp.rho) * rmsh.ds_tb \
     )
 
-
 # total functional for the mixed problem
-F = ( F_z + F_omega + F_mu) + F_N
+F = (F_z + F_omega + F_mu) + F_N
 
 import variational_problem_pp_square_no_circle as vp_pp
