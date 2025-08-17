764e040a92647fd1d6ea75b0ed729a5e4b067455
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-17 10:33:52 +0200

    ...

--- fourth_order_pde/constraint/grad_u_grad_v/solve.py
+++ fourth_order_pde/constraint/grad_u_grad_v/solve.py
@@ -37,8 +37,8 @@ params = {'nonlinear_solver': 'newton',
               {
                   # 'linear_solver': 'superlu',
                   'linear_solver': 'mumps',
-                  'absolute_tolerance': 1e-12,
-                  'relative_tolerance': 1e-12,
+                  'absolute_tolerance': 1e-6,
+                  'relative_tolerance': 1e-6,
                   'maximum_iterations': 1000000,
                   'relaxation_parameter': 0.95,
               }
