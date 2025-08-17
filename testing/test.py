69c4ace3e8eeb2a3913882734f00bc1cbecf8443
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-17 21:39:33 +0200

    temp comm test

--- testing/test.py
+++ testing/test.py
@@ -41,7 +41,7 @@ success = [True]
 
 cmd.run_command('clear; clear', success)
 
-
+'''
 
 # Test poisson_equation/solve_u
 case_name = 'poisson_equation/solve_u'
@@ -416,6 +416,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+'''
 
 # Test steady_state/no_flow/lagrangian_approach/spherically_symmetric
 case_name = 'steady_state/no_flow/lagrangian_approach/spherically_symmetric'
@@ -430,7 +431,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      'generate_square_no_circle_mesh', generate_mesh_path + 's_theta_parameters/', generate_mesh_path + 's_theta_parameters/', problem_name, problem_name, success)
 
 
-
+'''
 
 # Test steady_state/flow
 case_name = 'steady_state/flow'
@@ -650,7 +651,7 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_no_circle_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
-
+'''
 
 cmd.checkout(commit_a, success)
 
