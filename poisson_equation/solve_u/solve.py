43808656e210c3c02e98920a4a53b684f527e7e7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-20 20:40:58 +0200

    commented out pp problem

--- poisson_equation/solve_u/solve.py
+++ poisson_equation/solve_u/solve.py
@@ -61,12 +61,15 @@ params = {'nonlinear_solver': 'newton',
           }
 solver.parameters.update(params)
 
+'''
 J_pp = derivative(vp.F_pp, fsp.hess_u, fsp.J_hess_u)
 problem_pp = NonlinearVariationalProblem(vp.F_pp, fsp.hess_u, [], J_pp)
 solver_pp = NonlinearVariationalSolver(problem_pp)
-
+'''
 # solve original problem
 solver.solve()
+
+'''
 # solve pp problem
 solver_pp.solve()
 
@@ -77,3 +80,4 @@ solver_pp.solve()
 # import print_out_bc_square
 
 prout_bc = importlib.import_module(swi.prout_bc)
+'''
