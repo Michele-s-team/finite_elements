85cca033bfec3d6916c83eca1a8e3c692d2bc367
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-22 01:02:28 +0200

    test integral ok

--- modules/load_interval_mesh.py
+++ modules/load_interval_mesh.py
@@ -6,3 +6,11 @@ import input_output as io
 parameters = io.read_parameters_from_csv_file("parameters_bc_line.csv")
 
 mesh = IntervalMesh(parameters['N'], parameters['x_min'], parameters['x_max'])
+
+# Tag different regions of the line (1D entities - cells)
+cell_markers = MeshFunction("size_t", mesh, mesh.topology().dim())
+cell_markers.set_all(1)  # Tag entire line as region 1
+
+dx = Measure("dx", domain=mesh, subdomain_data=cell_markers, subdomain_id=1)
+
+print(f'int = {assemble(Constant(1)*dx)}')
