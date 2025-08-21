94bcd1be0a69c0b13c3f27c04d9fa9b9868c11b0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-22 00:45:39 +0200

    commented out F_pp

--- poisson_equation/solve_u/variational_problem_bc_line.py
+++ poisson_equation/solve_u/variational_problem_bc_line.py
@@ -82,5 +82,5 @@ F = (dot(grad(fsp.u), grad(fsp.nu_u)) + fsp.f * fsp.nu_u) * rmsh.dx \
     - bgeo.facet_normal[i] * fsp.grad_u[i] * fsp.nu_u * rmsh.dp_lr
 
 # variational functional for post-processing problem (pp) to obtain the hessian (hess)
-F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
-       - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.dp_lr
+# F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
+#        - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.dp_lr
