5ddebead0671368c9ea8b91aca7f1c510e1eaafd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-22 13:35:50 +0200

    int dx_read work

--- modules/load_mesh/interval/load_interval_mesh.py
+++ modules/load_mesh/interval/load_interval_mesh.py
@@ -31,11 +31,32 @@ for vertex in vertices(mesh):
     if math.isclose(x, parameters['x_r']):
         vf[vertex] = parameters['vertex_r_id']
 
+
+# Save the mesh components to files
+with XDMFFile("line_mesh.xdmf") as outfile:
+    outfile.write(cf)
+
+with XDMFFile("vertex_mesh.xdmf") as outfile:
+    outfile.write(vf)
+
+def read_mesh_components_new(mesh, dim, filename):
+    mf = MeshFunction("size_t", mesh, dim)
+    with XDMFFile(filename) as infile:
+        infile.read(mf)  # Remove the "name_to_read" parameter
+    return mf
+
+cf_read = read_mesh_components_new(mesh, mesh.topology().dim(), "line_mesh.xdmf")
+vf_read = read_mesh_components_new(mesh, mesh.topology().dim()-1, "vertex_mesh.xdmf")
+
+
 dx = Measure("dx", domain=mesh, subdomain_data=cf, subdomain_id=parameters['line_id'])
 ds_l = Measure("ds", domain=mesh, subdomain_data=vf, subdomain_id=parameters['vertex_l_id'])
 ds_r = Measure("ds", domain=mesh, subdomain_data=vf, subdomain_id=parameters['vertex_r_id'])
 
 ds = ds_l + ds_r
+
+dx_read = Measure("dx", domain=mesh, subdomain_data=cf_read, subdomain_id=parameters['line_id'])
+
 
 # a function space used solely to define function_test_integrals_fenics
 Q_test = FunctionSpace(mesh, 'P', 2)
@@ -72,6 +93,7 @@ integral_exact_ds = integral_exact_ds_l + integral_exact_ds_r
 test_mesh_integral_errors = []
 
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_dx, function_test_integrals_fenics, dx, '\int f dx'))
+test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_dx, function_test_integrals_fenics, dx_read, '\int f dx_read'))
 
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_l, function_test_integrals_fenics, ds_l, '\int f ds_l'))
 test_mesh_integral_errors.append(msh.test_mesh_integral(integral_exact_ds_r, function_test_integrals_fenics, ds_r, '\int f ds_r'))
