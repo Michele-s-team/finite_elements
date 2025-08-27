80fbbaec5941654d2adf6031387aae0ea8b64e74
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-28 00:30:53 +0200

    ...

--- modules/boundary_geometry_arc_length_gauge.py
+++ modules/boundary_geometry_arc_length_gauge.py
@@ -8,68 +8,27 @@ import mesh as mesh_module
 # the facet normal vector, which cannot be plotted as a field. It is not a vector in the tangent bundle of \Omega
 facet_normal = FacetNormal(lmsh.mesh)
 
-if ("n_sub_meshes" in lmsh.parameters) and (lmsh.parameters["n_sub_meshes"] > 1):
-    # lmsh loads multiple sub-meshes -> define the facet normal for each sub mesh
-    sub_mesh_facet_normal = []
-    for p in range(lmsh.parameters["n_sub_meshes"]):
-        sub_mesh_facet_normal.append(FacetNormal(lmsh.sub_meshes[p]))
-
 i, j, k, l = ufl.indices(4)
 
 
-# sign
-
 # square root of the determinant of the pull-back of the metric on \partial \Omega_in(out), parametrized with l , given by  x^1 = 0 (L) and x^2 = l, as coordinate for \partial \Omega_in (out)
-def sqrt_deth_lr(omega):
-    return sqrt(geo.g(omega)[1, 1])
+# DOUBLE CHCK THIS - START
+def sqrt_deth_lr(psi):
+    return sqrt(1)
 
 
-# square root of the determinant of the pull-back of the metric on \partial \Omega_W (top or bottom), parametrized with l , given by  x^1 = l and x^2 = 0 (h), as coordinate for \partial \Omega_W
-def sqrt_deth_tb(omega):
-    return sqrt(geo.g(omega)[0, 0])
+# DOUBLE CHCK THIS - END
 
 
 # Nt^i_notes on \partial \Omega_in and out
 def Nt_lr(omega):
     x = ufl.SpatialCoordinate(lmsh.mesh)
-    L = (mesh_module.extremal_coordinates(lmsh.mesh))[0][1]
+    L = (mesh_module.extremal_coordinates(lmsh.mesh))[1]
 
-    N3d = as_tensor([conditional(lt(x[0], L / 2.0), -1.0, 1.0), 0.0, 0.0])
-    return as_tensor(geo.g_c(omega)[i, j] * N3d[k] * geo.e(omega)[j, k], (i))
-
-
-# N_n_notes on \partial \Omega_in and out
-def Nn_lr(omega):
-    x = ufl.SpatialCoordinate(lmsh.mesh)
-    L = (mesh_module.extremal_coordinates(lmsh.mesh))[0][1]
-
-    N3d = as_tensor([conditional(lt(x[0], L / 2.0), -1.0, 1.0), 0.0, 0.0])
-    return (N3d[i] * (normal(omega))[i])
-
-
-# Nt^i_notes on \partisal \Omega_W
-def Nt_tb(omega):
-    x = ufl.SpatialCoordinate(lmsh.mesh)
-    h = (mesh_module.extremal_coordinates(lmsh.mesh))[1][1]
-
-    N3d = as_tensor([0.0, conditional(lt(x[1], h / 2.0), -1.0, 1.0), 0.0])
-    return as_tensor(geo.g_c(omega)[i, j] * N3d[k] * geo.e(omega)[j, k], (i))
-
-
-# N_n_notes on \partial \Omega_top and bottom
-def Nn_tb(omega):
-    x = ufl.SpatialCoordinate(lmsh.mesh)
-    h = (mesh_module.extremal_coordinates(lmsh.mesh))[1][1]
-
-    N3d = as_tensor([0.0, conditional(lt(x[1], h / 2.0), -1.0, 1.0), 0.0])
-    return (N3d[i] * (normal(omega))[i])
+    N2d = as_tensor([conditional(lt(x[0], L / 2.0), -1.0, 1.0), 0.0])
+    return as_tensor(geo.g_c(omega)[i, j] * N2d[k] * geo.e(omega)[j, k], (i))
 
 
 # n^i_notes on \partial \Omega_in and out
 def n_lr(omega):
     return as_tensor((Nt_lr(omega))[k] / sqrt(geo.g(omega)[i, j] * (Nt_lr(omega))[i] * (Nt_lr(omega))[j]), (k))
-
-
-def n_tb(omega):
-    return as_tensor((Nt_tb(omega))[k] / sqrt(geo.g(omega)[i, j] * (Nt_tb(omega))[i] * (Nt_tb(omega))[j]), (k))
-
