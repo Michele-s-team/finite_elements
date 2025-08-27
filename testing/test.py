2645d42104faa3e843070dd013a9faeaf866e7db
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-27 16:20:42 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -242,6 +242,18 @@ checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, c
 
 
 
+problem_name = 'square_no_circle_line'
+generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/line/'
+checks[case_name + '_' + problem_name] = utest.test_problem_and_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
+
+
+
 # Test poisson_equation/solve_u_v
 case_name = 'poisson_equation/solve_u_v'
 
