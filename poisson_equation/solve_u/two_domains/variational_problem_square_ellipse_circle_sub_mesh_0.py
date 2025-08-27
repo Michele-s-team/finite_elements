99dcafb979a824f02da16084a8be9de72899324f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-27 15:41:27 +0200

    Update variational_problem_square_ellipse_circle_sub_mesh_0.py

--- poisson_equation/solve_u/two_domains/variational_problem_square_ellipse_circle_sub_mesh_0.py
+++ poisson_equation/solve_u/two_domains/variational_problem_square_ellipse_circle_sub_mesh_0.py
@@ -71,6 +71,12 @@ fsp.u_exact[0].interpolate(u_exact_sub_mesh_0_expression(element=fsp.Q[0].ufl_el
 fsp.grad_u[0].interpolate(grad_u_exact_sub_mesh_0_expression(element=fsp.V[0].ufl_element()))
 fsp.f[0].interpolate(laplacian_u_exact_sub_mesh_0_expression(element=fsp.Q[0].ufl_element()))
 
+# solve problem 0 by using the solution of problem 1 to specify the BCs
+
+# set the BC at the interface between sub_mesh[0] and sub_mesh[1] according to the solution fsp,u[1] obtained above
+# project fsp.u[1] on fsp.Q[0] and write the result in fsp.u_1_on_0
+fsp.u_1_on_0.assign(project((fsp.u[1]) ** 2, fsp.Q[0]))
+
 bcs = [ \
     DirichletBC(fsp.Q[0], fsp.u_1_on_0, rmsh.mf_sub_mesh[0], rmsh.parameters["ellipse_loop_id"]) \
     ]
