bc6eebedcc4056d72f9280e914da460d1b847389
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-29 18:59:32 +0200

    ...

---
+++ modules/differential_geometry/manifold/gauges/monge_gauge.py
@@ -0,0 +1,7 @@
+'''
+this module contains the differential-geometry definitions
+for a two-dimensional manifold parameterized with a coordinate x^1 = x, x^2 = y in the Monge gauge
+
+all methods specific to one dimension and to the Monge gauge are defined here, while methods independent of the dimension and
+of the gauge are defined in geometry.py
+'''
