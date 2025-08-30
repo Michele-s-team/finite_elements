92964f78d895253c77ddf8574f7275105c9c3a8e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-30 21:12:33 +0200

    ...

--- generate_mesh/3d/submesh_ball/generate_submesh_ball.py
+++ generate_mesh/3d/submesh_ball/generate_submesh_ball.py
@@ -22,7 +22,7 @@ import differential_geometry.manifold.geometry as geo
 import input_output as io
 import mesh.load as lmsh
 import mesh.utils as msh
-import read_parameters_generate_mesh as rpam
+import parameters.read.mesh as rpam
 import runtime_arguments as rarg
 
 # radius of the smallest cell in the mesh
