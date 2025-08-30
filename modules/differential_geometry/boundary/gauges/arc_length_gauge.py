a810082ce88c6128593d72814cfed411ff0e6cfc
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-30 12:07:17 +0200

    ...

--- modules/differential_geometry/boundary/gauges/arc_length_gauge.py
+++ modules/differential_geometry/boundary/gauges/arc_length_gauge.py
@@ -3,7 +3,6 @@ import ufl as ufl
 
 import differential_geometry.manifold.geometry as geo
 import load_mesh as lmsh
-import mesh as mesh_module
 
 i, j, k, l, alpha = ufl.indices(4)
 
@@ -13,14 +12,27 @@ def sqrt_deth_lr(psi):
     return sqrt(1)
 
 
-# Nt^i_notes on \partial \Omega_in and out
+'''
+vector in the tangent bundle of Omega which normal to \partial Omega and points outside \Omega
+Input values:
+- 'psi': the angle psi_here = psi_{Lagrangian approach}
+Return values: 
+- Nt^i_{'Eq. (A9) for a one-dimensional manifold' in notes deserno2004notes}  on partial Omega
+'''
 def Nt_lr(psi):
-    # x = ufl.SpatialCoordinate(lmsh.mesh)
-    # L = (mesh_module.extremal_coordinates(lmsh.mesh))[1]
 
-    # vector in the surrounding two-dimensional Euclidean space
-    N2d = as_tensor(geo.e(psi)[0, alpha], (alpha))
+    # load the coordinate 'x' on the mesh,
+    x = ufl.SpatialCoordinate(lmsh.mesh)
+    # define the middle point of the coordinate 'x'
+    x_middle = (lmsh.parameters['x_l'] + lmsh.parameters['x_r']) / 2.0
 
+    '''
+    vector in the surrounding two-dimensional Euclidean space
+    if x[0] lies in the left (right) half of the interval x_l, x_r, N_2d = e[0] (-e[0]), 
+    '''
+    N2d = as_tensor(conditional(lt(x[0], x_middle), -1.0, 1.0) * geo.e(psi)[0, alpha], (alpha))
+
+    # return the projection of N_2d on the tangent bundle of the manifold
     return as_tensor(geo.g_c(psi)[i, j] * N2d[alpha] * geo.e(psi)[j, alpha], (i))
 
 
