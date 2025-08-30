72674f0e3775000ebf020564ed89d8668f4a1704
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-30 11:56:08 +0200

    revising n_lr ...

--- modules/differential_geometry/boundary/gauges/arc_length_gauge.py
+++ modules/differential_geometry/boundary/gauges/arc_length_gauge.py
@@ -5,7 +5,7 @@ import differential_geometry.manifold.geometry as geo
 import load_mesh as lmsh
 import mesh as mesh_module
 
-i, j, k, l = ufl.indices(4)
+i, j, k, l, alpha = ufl.indices(4)
 
 
 # square root of the determinant of the pull-back of the metric on \partial \Omega_in(out), parametrized with l , given by  x^1 = 0 (L) and x^2 = l, as coordinate for \partial \Omega_in (out)
@@ -14,12 +14,14 @@ def sqrt_deth_lr(psi):
 
 
 # Nt^i_notes on \partial \Omega_in and out
-def Nt_lr(omega):
-    x = ufl.SpatialCoordinate(lmsh.mesh)
-    L = (mesh_module.extremal_coordinates(lmsh.mesh))[1]
+def Nt_lr(psi):
+    # x = ufl.SpatialCoordinate(lmsh.mesh)
+    # L = (mesh_module.extremal_coordinates(lmsh.mesh))[1]
 
-    N2d = as_tensor([conditional(lt(x[0], L / 2.0), -1.0, 1.0), 0.0])
-    return as_tensor(geo.g_c(omega)[i, j] * N2d[k] * geo.e(omega)[j, k], (i))
+    # vector in the surrounding two-dimensional Euclidean space
+    N2d = as_tensor(geo.e(psi)[0, alpha], (alpha))
+
+    return as_tensor(geo.g_c(psi)[i, j] * N2d[alpha] * geo.e(psi)[j, alpha], (i))
 
 
 # n^i_notes on \partial \Omega_in and out
