bb6b650175953b41db3c911b084e5569584868d3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 23:07:39 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -224,7 +224,7 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      success)
 
 
-'''
+
 
 #square_no_circle mesh
 generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/'
@@ -238,6 +238,21 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      'generate_square_no_circle_mesh', 'check_mesh',
                                                                      generate_mesh_path, generate_mesh_path,
                                                                      success)
+'''
+
+#square_no_circle/two_squares_no_circle mesh
+generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/two_squares_no_circle/'
+
+checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     mesh_solution_path_a, mesh_check_solution_path_a,
+                                                                     mesh_solution_path_b, mesh_check_solution_path_b,
+                                                                     'generate_two_squares_no_circle_mesh', 'check_mesh',
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     success)
+
 
 '''
 ################################################################
