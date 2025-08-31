e8e8316b7eaf6d1208f288ddc5ee40b781f6051d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 22:49:10 +0200

    Update check_mesh.py

--- generate_mesh/2d/square_no_circle/check_mesh.py
+++ generate_mesh/2d/square_no_circle/check_mesh.py
@@ -5,6 +5,7 @@ Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
 Example:
     clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square_no_circle/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square_no_circle/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square_no_circle/symmetric/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square_no_circle/symmetric/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 
 from fenics import *
