6f53bdcf6a1470238ffe7b2a2fa3b436ab1bc929
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 17:19:21 +0200

    Disk mesh ...

--- generate_mesh/2d/disk/check_mesh.py
+++ generate_mesh/2d/disk/check_mesh.py
@@ -2,9 +2,9 @@
 This code checks the mesh generated from generate_ring_mesh.py
 
 Run with
-    clear; clear; python3 check_mesh_disk.py [path where to find the mesh]
+    clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
 Example:
-    clear; clear; python3 check_mesh_disk.py solution
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/disk/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/disk/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 import importlib
 import sys
