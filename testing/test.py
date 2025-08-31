efb28ff772633b9cea148aa5f19e37e9add84afb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 21:20:15 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -151,7 +151,7 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      success)
 
 
-'''
+
 
 #ring_with_lines mesh
 generate_mesh_path = root_path + 'generate_mesh/2d/ring/ring_with_lines/'
@@ -166,7 +166,7 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      generate_mesh_path, generate_mesh_path,
                                                                      success)
 
-'''
+
 #ring/symmetric mesh
 generate_mesh_path = root_path + 'generate_mesh/2d/ring/symmetric/'
 
@@ -180,7 +180,7 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      generate_mesh_path, generate_mesh_path,
                                                                      success)
 
-
+'''
 ################################################################
 # Variational problems test
 ################################################################
