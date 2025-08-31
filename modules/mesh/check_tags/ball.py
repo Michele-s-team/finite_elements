ab44498d147e5a2b8cfda07db06c361fdee471b3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 23:12:46 +0200

    ball mesh .OK

--- modules/mesh/check_tags/ball.py
+++ modules/mesh/check_tags/ball.py
@@ -1,12 +1,9 @@
 import colorama as col
 from fenics import *
 import importlib
-import numpy as np
 
 import calculus as cal
-import differential_geometry.manifold.geometry as geo
 import input_output as io
-import mesh.load as lmsh
 import mesh.test_function as tf
 import mesh.utils as msh
 import runtime_arguments as rarg
@@ -15,44 +12,14 @@ rmsh = importlib.import_module('mesh.read.ball')
 
 print(f'Module {__file__} called {rmsh.__file__}', flush=True)
 
-# CHANGE PARAMETERS HERE
-c_test = [0.3, 0.76, 1.23]
-r_test = 0.345
-# CHANGE PARAMETERS HERE
-
-# a function space used solely to define function_test_integrals_fenics
-Q_test = FunctionSpace(lmsh.mesh, 'P', 2)
-
-
-# function_test_integrals_fenics is a function of two variables, that will be used to test whether the boundary elements ds_circle, ds_inflow, ds_outflow, .. are defined correclty . This will be done by computing an integral of f_test_ds over these boundary terms and comparing with the exact result
-def function_test_integrals(x):
-    return (np.cos(geo.my_norm(np.subtract(x, c_test)) - r_test) ** 2.0)
-    # return 1
-
-
-# function_test_integrals_fenics is the same as function_test_integrals, but in fenics format
-function_test_integrals_fenics = Function(Q_test)
-
-
-# analytical expression for a  scalar function used to test the ds
-class FunctionTestIntegrals(UserExpression):
-    def eval(self, values, x):
-        values[0] = function_test_integrals(x)
-
-    def value_shape(self):
-        return (1,)
-
-
-function_test_integrals_fenics.interpolate(FunctionTestIntegrals(element=Q_test.ufl_element()))
+integral_exact_dx = cal.volume_integral_ball(tf.function_test_integrals, rmsh.parameters["r"], rmsh.parameters["c_r"])
+integral_exact_ds = cal.surface_integral_sphere(tf.function_test_integrals, rmsh.parameters["r"], rmsh.parameters["c_r"])
 
 test_mesh_integral_errors = dict([])
 
-integral_exact_dx = cal.volume_integral_ball(function_test_integrals, rmsh.parameters["r"], rmsh.parameters["c_r"])
-integral_exact_ds = cal.surface_integral_sphere(function_test_integrals, rmsh.parameters["r"], rmsh.parameters["c_r"])
-
 # print out the integrals on the surface elements and compare them with the exact values to double check that the elements are tagged correctly
-test_mesh_integral_errors['\int_ball f dx'] = msh.test_mesh_integral(integral_exact_dx, function_test_integrals_fenics, rmsh.dx, '\int_ball f dx')
-test_mesh_integral_errors['\int_sphere f ds'] = msh.test_mesh_integral(integral_exact_ds, function_test_integrals_fenics, rmsh.ds, '\int_sphere f ds')
+test_mesh_integral_errors['\int_ball f dx'] = msh.test_mesh_integral(integral_exact_dx, tf.function_test_integrals_fenics, rmsh.dx, '\int_ball f dx')
+test_mesh_integral_errors['\int_sphere f ds'] = msh.test_mesh_integral(integral_exact_ds, tf.function_test_integrals_fenics, rmsh.ds, '\int_sphere f ds')
 
 # print to file the residuals of the tests of the mesh integrals
 io.write_parameters_to_csv_file(io.add_trailing_slash(rarg.args.output_directory) + 'test_integral_errors.csv', test_mesh_integral_errors)
