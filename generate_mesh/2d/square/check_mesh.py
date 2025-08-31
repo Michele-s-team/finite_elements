4c0554a48b77762e91c78e6d8af0d30d03c5bfda
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 22:41:56 +0200

    Update check_mesh.py

--- generate_mesh/2d/square/check_mesh.py
+++ generate_mesh/2d/square/check_mesh.py
@@ -8,6 +8,7 @@ Example:
     clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
     clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/lines/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/lines/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
     clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_left_right_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_left_right_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 
 import importlib
