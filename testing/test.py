33ae6a9eb648114b3738c270626e40aed2cdeb36
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 18:39:20 +0200

    replaced tf... OK

--- testing/test.py
+++ testing/test.py
@@ -95,7 +95,6 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      generate_mesh_path, generate_mesh_path,
                                                                      success)
 
-'''
 
 #half_circle_with_line mesh
 generate_mesh_path = root_path + 'generate_mesh/2d/half_circle_with_line/'
@@ -107,6 +106,21 @@ checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, c
                                                                      mesh_solution_path_a, mesh_check_solution_path_a,
                                                                      mesh_solution_path_b, mesh_check_solution_path_b,
                                                                      'generate_half_circle_with_line_mesh', 'check_mesh',
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     success)
+
+
+'''
+#ring mesh
+generate_mesh_path = root_path + 'generate_mesh/2d/ring/'
+
+checks[generate_mesh_path] = utest.test_generate_mesh_and_check_mesh(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     generate_mesh_path, generate_mesh_path,
+                                                                     mesh_solution_path_a, mesh_check_solution_path_a,
+                                                                     mesh_solution_path_b, mesh_check_solution_path_b,
+                                                                     'generate_ring_mesh', 'check_mesh',
                                                                      generate_mesh_path, generate_mesh_path,
                                                                      success)
 
