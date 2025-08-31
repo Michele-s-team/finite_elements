0d998526604b37574a073d4c2d71b63aadaef20e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 21:53:00 +0200

    ...  square/square OK

--- generate_mesh/2d/square/square/check_mesh.py
+++ generate_mesh/2d/square/square/check_mesh.py
@@ -4,7 +4,7 @@ This code checks the mesh generated from generate_mesh.py
 Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
 Example:
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/square/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/square/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 import importlib
 import sys
@@ -13,8 +13,6 @@ import sys
 module_path = '/home/fenics/shared/modules'
 sys.path.append(module_path)
 
-import mesh.load as lmsh
-import mesh.utils as msh
 rmsh = importlib.import_module('mesh.read.square_square')
 
 import mesh.check_tags.square_square
