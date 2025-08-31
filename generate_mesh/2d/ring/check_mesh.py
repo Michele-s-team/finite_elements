ed53f795ca9a688383f35f43bb7736d0f8fd05cb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 19:28:00 +0200

    Update check_mesh.py

--- generate_mesh/2d/ring/check_mesh.py
+++ generate_mesh/2d/ring/check_mesh.py
@@ -5,6 +5,7 @@ Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
 Example:
     clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/ring/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/ring/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/ring/symmetric/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/symmetric/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 import importlib
 import sys
