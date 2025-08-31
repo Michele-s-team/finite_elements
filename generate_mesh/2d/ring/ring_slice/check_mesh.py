3d2821f385c29fd3c7c4ffef46b82022c751dfc4
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-08-31 18:45:25 +0200

    replacing runtime arguments ...

--- generate_mesh/2d/ring/ring_slice/check_mesh.py
+++ generate_mesh/2d/ring/ring_slice/check_mesh.py
@@ -2,9 +2,9 @@
 This code checks the mesh generated from generate_mesh_ring_slice.py
 
 Run with
-    clear; clear; python3 check_mesh_ring_slice.py [path where to find the mesh]
+    clear; clear; python3 check_mesh.py [path where to find the mesh]
 Example:
-    clear; clear; python3 check_mesh_ring_slice.py solution
+    clear; clear; python3 check_mesh.py solution
 '''
 from fenics import *
 import sys
--- modules/mesh/check_tags/ring_slice.py
+++ modules/mesh/check_tags/ring_slice.py
@@ -8,6 +8,7 @@ import differential_geometry.manifold.geometry as geo
 import input_output as io
 import mesh.load as lmsh
 import mesh.utils as msh
+import runtime_arguments as rarg
 
 rmsh = importlib.import_module('mesh.read.ring_slice')
 
