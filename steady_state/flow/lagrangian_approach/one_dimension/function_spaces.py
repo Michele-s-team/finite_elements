49607ba440fe0de4842a70e447b2917ea85aacd2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-01 19:16:26 +0200

    Update function_spaces.py

--- steady_state/flow/lagrangian_approach/one_dimension/function_spaces.py
+++ steady_state/flow/lagrangian_approach/one_dimension/function_spaces.py
@@ -13,19 +13,23 @@ mu = H
 X[i] = {X^i}_{Lagrangian approach}
 '''
 
+P_v = VectorElement('P', interval, 2)
+P_w = FiniteElement('P', interval, 1)
+P_sigma = FiniteElement('P', interval, 1)
 P_psi = FiniteElement('P', interval, rpam.parameters['function_space_degree'])
 P_mu = FiniteElement('P', interval, rpam.parameters['function_space_degree'])
 P_X = VectorElement('P', interval, rpam.parameters['function_space_degree'], dim=2)
 
-element = MixedElement([P_psi, P_mu, P_X])
+element = MixedElement([P_v, P_w, P_sigma, P_psi, P_mu, P_X])
 # total function space
 Q = FunctionSpace(lmsh.mesh, element)
 # function spaces for z, omega, eta and theta
-Q_psi = Q.sub(0).collapse()
-Q_mu = Q.sub(1).collapse()
-Q_X = Q.sub(2).collapse()
-
-Q_sigma = FunctionSpace(lmsh.mesh, 'P', 1)
+Q_v = Q.sub(0).collapse()
+Q_w = Q.sub(1).collapse()
+Q_sigma = Q.sub(2).collapse()
+Q_psi = Q.sub(3).collapse()
+Q_mu = Q.sub(4).collapse()
+Q_X = Q.sub(5).collapse()
 
 '''
 function spaces of polynomial order 1 (which should not be changed) which are used to read in functions and assign their nodal values from a list 
@@ -36,31 +40,29 @@ Q_read = FunctionSpace(lmsh.mesh, 'P', 1)
 # Define functions
 J_phi = TrialFunction(Q)
 phi = Function(Q)
-nu_psi, nu_mu, nu_X = TestFunctions(Q)
+nu_v, nu_w, nu_sigma, nu_psi, nu_mu, nu_X = TestFunctions(Q)
 
-# these functions are used to print the solution to file
-sigma = Function(Q_sigma)
-
+v_output = Function(Q_v)
+w_output = Function(Q_w)
+sigma_output = Function(Q_sigma)
 psi_output = Function(Q_psi)
 mu_output = Function(Q_mu)
 X_output = Function(Q_X)
 
+v_exact = Function(Q_v)
+w_exact = Function(Q_w)
+sigma_exact = Function(Q_sigma)
 psi_exact = Function(Q_psi)
 mu_exact = Function(Q_mu)
 X_exact = Function(Q_X)
 
-'''
-# functions used to store the nodal values read from a list or file
-psi_0_read = Function(Q_read)
-mu_0_read = Function(Q_read)
-X_0_r_read = Function(Q_read)
-'''
-
 # omega_0, z_0 are used to store the initial conditions
+v_0 = Function(Q_v)
+w_0 = Function(Q_w)
+sigma_0 = Function(Q_sigma)
 psi_0 = Function(Q_psi)
 mu_0 = Function(Q_mu)
 X_0 = Function(Q_X)
 
-psi, mu, X = split(phi)
-assigner = FunctionAssigner(Q, [Q_psi, Q_mu, Q_X])
-
+v, w, sigma, psi, mu, X = split(phi)
+assigner = FunctionAssigner(Q, [Q_v, Q_w, Q_sigma, Q_psi, Q_mu, Q_X])
