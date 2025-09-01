99fe67767acede94287cd1c31e33c1f206c0b624
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-01 19:18:03 +0200

    Update solve.py

--- steady_state/flow/lagrangian_approach/one_dimension/solve.py
+++ steady_state/flow/lagrangian_approach/one_dimension/solve.py
@@ -5,7 +5,7 @@ Run with
     python3 solve.py [name of variational problem] [path where to read the mesh] [path where to store the solution]
 
 Example:
-    MESH_PATH="/home/fenics/shared/generate_mesh/1d/line/solution"; SOLUTION_PATH="/home/fenics/shared/steady_state/no_flow/lagrangian_approach/one_dimension/solution"; rm -rf $SOLUTION_PATH; python3 solve.py line $MESH_PATH $SOLUTION_PATH;
+    MESH_PATH="/home/fenics/shared/generate_mesh/1d/line/solution"; SOLUTION_PATH="/home/fenics/shared/steady_state/flow/lagrangian_approach/one_dimension/solution"; rm -rf $SOLUTION_PATH; python3 solve.py line $MESH_PATH $SOLUTION_PATH;
 '''
 
 import colorama as col
@@ -53,24 +53,5 @@ params = {'nonlinear_solver': 'newton',
 solver.parameters.update(params)
 
 solver.solve()
-
-'''
-# test n
-import command as cmd
-import differential_geometry.manifold.geometry as geo
-import input_output as io
-import mesh.load as lmsh
-import solution_paths as solpath
-
-cmd.set_gauge('arc_length')
-
-Q_n = VectorFunctionSpace(lmsh.mesh, 'P', 2, dim=2)
-n = Function(Q_n)
-n.assign(project(geo.normal(fsp.psi), Q_n))
-
-io.full_print(n, 'n', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
-              solpath.nodal_values_path, lmsh.mesh,
-              'vector')
-'''
 
 prout_bc = importlib.import_module(swi.prout_bc)
