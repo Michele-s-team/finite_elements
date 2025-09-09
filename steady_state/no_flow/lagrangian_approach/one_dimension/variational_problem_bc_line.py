6731397f10b5ea6d5eb7a6f2af9e22894bd44dd2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-09 10:50:22 +0200

    Revised vp

--- steady_state/no_flow/lagrangian_approach/one_dimension/variational_problem_bc_line.py
+++ steady_state/no_flow/lagrangian_approach/one_dimension/variational_problem_bc_line.py
@@ -45,12 +45,11 @@ bc_psi_l = DirichletBC(fsp.Q.sub(0), Constant(rpam.parameters["psi_l"]), rmsh.bo
 bc_psi_r = DirichletBC(fsp.Q.sub(0), Constant(rpam.parameters["psi_r"]), rmsh.boundary_r)
 
 bc_X_l = DirichletBC(fsp.Q.sub(2), Constant((rpam.parameters["X_l"][0], rpam.parameters["X_l"][1])), rmsh.boundary_l)
-bc_X_r = DirichletBC(fsp.Q.sub(2), Constant((rpam.parameters["X_r"][0], rpam.parameters["X_r"][1])), rmsh.boundary_r)
+bc_X1_r = DirichletBC(fsp.Q.sub(2).sub(0), Constant(rpam.parameters["X_r"][0]), rmsh.boundary_r)
 
 bc_nu_l = DirichletBC(fsp.Q.sub(3), Constant(rpam.parameters["nu_l"]), rmsh.boundary_l)
-bc_nu_r = DirichletBC(fsp.Q.sub(3), Constant(rpam.parameters["nu_r"]), rmsh.boundary_r)
 
-bcs = [bc_psi_l, bc_psi_r, bc_X_l, bc_X_r, bc_nu_l, bc_nu_r]
+bcs = [bc_psi_l, bc_psi_r, bc_X_l, bc_X1_r, bc_nu_l]
 
 # Define variational problem
 
