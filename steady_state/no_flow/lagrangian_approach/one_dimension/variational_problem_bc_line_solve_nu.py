9b902ce60be67489e5f1768d54235dc1482b9481
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-11 13:51:58 +0200

    Added path to no_flow/solve_nu

--- steady_state/no_flow/lagrangian_approach/one_dimension/variational_problem_bc_line_solve_nu.py
+++ steady_state/no_flow/lagrangian_approach/one_dimension/variational_problem_bc_line_solve_nu.py
@@ -2,10 +2,11 @@ from fenics import *
 import importlib
 import ufl as ufl
 
-import differential_geometry.boundary.geometry as bgeo
 import command as cmd
-import function as fu
+import differential_geometry.boundary.geometry as bgeo
 import differential_geometry.manifold.geometry as geo
+import function as fu
+import input_output as io
 import parameters.read.solution as rpam
 import switch_problem as swi
 
@@ -30,10 +31,10 @@ fsp.sigma.interpolate(sigma_Expression(element=fsp.Q_sigma.ufl_element()))
 # uncomment this to set the initial profiles from the ODE soltion
 #
 print("Reading the initial profiles from file ...")
-fu.read_from_file('solution_ode/psi.csv', fsp.psi_0)
-fu.read_from_file('solution_ode/mu.csv', fsp.mu_0)
-fu.read_from_file('solution_ode/X.csv', fsp.X_0)
-fu.read_from_file('solution_ode/nu.csv', fsp.nu_0)
+fu.read_from_file(io.add_trailing_slash(rpam.parameters['solution_ode_path']) + 'psi.csv', fsp.psi_0)
+fu.read_from_file(io.add_trailing_slash(rpam.parameters['solution_ode_path']) + 'mu.csv', fsp.mu_0)
+fu.read_from_file(io.add_trailing_slash(rpam.parameters['solution_ode_path']) + 'X.csv', fsp.X_0)
+fu.read_from_file(io.add_trailing_slash(rpam.parameters['solution_ode_path']) + 'nu.csv', fsp.nu_0)
 
 fsp.assigner.assign(fsp.phi, [fsp.psi_0, fsp.mu_0, fsp.X_0, fsp.nu_0])
 print('... done')
