9c998ca19127f7065627a6c7b4f3d4745dc9ee19
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-19 15:20:12 +0200

    finished replacing in fixed_nu

--- steady_state/no_flow/lagrangian_approach/one_dimension/print_out_solution_solve_nu.py
+++ steady_state/no_flow/lagrangian_approach/one_dimension/print_out_solution_solve_nu.py
@@ -40,10 +40,10 @@ io.full_print(nu_output, 'nu', solpath.xdmf_file_path, solpath.h5_file_path, sol
 
 
 # geo.e(fsp.psi, fsp.nu)[0, alpha]
-io.full_print(project((fsp.X[0].dx(0) - geo.e(fsp.psi, fsp.nu)[0, 0]), fsp.Q_psi), 'err_1', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+io.full_print(project((fsp.u[0].dx(0) - geo.e(fsp.psi, fsp.nu)[0, 0]), fsp.Q_psi), 'err_1', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path, lmsh.mesh,
               'scalar')
-io.full_print(project((fsp.X[1].dx(0) - geo.e(fsp.psi, fsp.nu)[0, 1]), fsp.Q_psi), 'err_2', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+io.full_print(project((fsp.u[1].dx(0) - geo.e(fsp.psi, fsp.nu)[0, 1]), fsp.Q_psi), 'err_2', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
               solpath.nodal_values_path, lmsh.mesh,
               'scalar')
 
