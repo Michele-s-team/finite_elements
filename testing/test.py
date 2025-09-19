f60f359846f02987c761a10e00295e93b7c44a8b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-19 13:08:03 +0200

    Uncommented dynamics from test and added new parameter set to test dynamics

--- testing/test.py
+++ testing/test.py
@@ -8,6 +8,7 @@ Example
     python3 test.py master different
 '''
 
+import os
 import sys
 
 # add the path where to find the shared modules
@@ -728,7 +729,7 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_square_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
-'''
+
 # Test dynamics/lagrangian_approach/one_dimension
 case_name = 'dynamics/lagrangian_approach/one_dimension'
 
@@ -739,8 +740,10 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      generate_mesh_path, root_path + case_name,
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-                                                                     'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
-'''
+                                                                     'generate_mesh', 
+                                                                     os.path.join(generate_mesh_path, 'resolution_0.1'), generate_mesh_path, 
+                                                                     problem_name, problem_name, success)
+
 
 # Test fluid_structure_interaction
 case_name = 'fluid_structure_interaction/mesh_deformation'
