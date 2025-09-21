487fabc612d3b18d85cf53cf1b0ab57342a10815
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-21 23:51:03 +0200

    Revising steady_state/flow ...

--- steady_state/flow/lagrangian_approach/one_dimension/function_spaces_fixed_nu.py
+++ steady_state/flow/lagrangian_approach/one_dimension/function_spaces_fixed_nu.py
@@ -3,24 +3,15 @@ from fenics import *
 import mesh.load as lmsh
 import parameters.read.solution as rpam
 
-'''
-the fields in this problem are
-v[i] = v^i_{Lagrangian approach}
-w = w_{Lagrangian approach}
-sigma = \sigma_{Lagrangian approach}
-psi = psi_{Lagrangian approach}
-mu = H
-X[i] = {X^i}_{Lagrangian approach}
-'''
 
 P_v = VectorElement('P', interval, 2)
 P_w = FiniteElement('P', interval, 1)
 P_sigma = FiniteElement('P', interval, 1)
 P_psi = FiniteElement('P', interval, rpam.parameters['function_space_degree'])
 P_mu = FiniteElement('P', interval, rpam.parameters['function_space_degree'])
-P_X = VectorElement('P', interval, rpam.parameters['function_space_degree'], dim=2)
+P_u = VectorElement('P', interval, rpam.parameters['function_space_degree'], dim=2)
 
-element = MixedElement([P_v, P_w, P_sigma, P_psi, P_mu, P_X])
+element = MixedElement([P_v, P_w, P_sigma, P_psi, P_mu, P_u])
 # total function space
 Q = FunctionSpace(lmsh.mesh, element)
 # function spaces for z, omega, eta and theta
@@ -29,10 +20,11 @@ Q_w = Q.sub(1).collapse()
 Q_sigma = Q.sub(2).collapse()
 Q_psi = Q.sub(3).collapse()
 Q_mu = Q.sub(4).collapse()
-Q_X = Q.sub(5).collapse()
+Q_u = Q.sub(5).collapse()
 
 # function space for the function nu of the arc-length gauge
 Q_nu = FunctionSpace(lmsh.mesh, 'P', rpam.parameters['function_space_degree'])
+Q_X = VectorFunctionSpace(lmsh.mesh, 'P', rpam.parameters['function_space_degree'], dim=2)
 
 
 '''
@@ -44,7 +36,7 @@ Q_read = FunctionSpace(lmsh.mesh, 'P', 1)
 # Define functions
 J_phi = TrialFunction(Q)
 phi = Function(Q)
-nu_v, nu_w, nu_sigma, nu_psi, nu_mu, nu_X = TestFunctions(Q)
+nu_v, nu_w, nu_sigma, nu_psi, nu_mu, nu_u = TestFunctions(Q)
 
 # field for the arc-length gauge
 nu =  Function(Q_nu)
@@ -55,7 +47,7 @@ w_output = Function(Q_w)
 sigma_output = Function(Q_sigma)
 psi_output = Function(Q_psi)
 mu_output = Function(Q_mu)
-X_output = Function(Q_X)
+u_output = Function(Q_u)
 
 
 # omega_0, z_0 are used to store the initial conditions
@@ -64,7 +56,7 @@ w_0 = Function(Q_w)
 sigma_0 = Function(Q_sigma)
 psi_0 = Function(Q_psi)
 mu_0 = Function(Q_mu)
-X_0 = Function(Q_X)
+u_0 = Function(Q_u)
 
 
 # functions used to store the nodal values read from a list or file
@@ -73,7 +65,7 @@ w_0_read = Function(Q_w)
 sigma_0_read = Function(Q_sigma)
 psi_0_read = Function(Q_psi)
 mu_0_read = Function(Q_mu)
-X_0_read = Function(Q_X)
+u_0_read = Function(Q_u)
 
-v, w, sigma, psi, mu, X = split(phi)
-assigner = FunctionAssigner(Q, [Q_v, Q_w, Q_sigma, Q_psi, Q_mu, Q_X])
+v, w, sigma, psi, mu, u = split(phi)
+assigner = FunctionAssigner(Q, [Q_v, Q_w, Q_sigma, Q_psi, Q_mu, Q_u])
