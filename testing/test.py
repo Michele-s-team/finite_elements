e21158b63e60f21bade8eafead30452dcb91f334
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-24 10:22:45 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -741,7 +741,7 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', 
-                                                                     os.path.join(generate_mesh_path, 'resolution_0.1'), generate_mesh_path, 
+                                                                     os.path.join(generate_mesh_path, 'resolution_0.1'), os.path.join(generate_mesh_path, 'resolution_0.1'), 
                                                                      problem_name, problem_name, success)
 
 
