074c98f5bca05099fbba9d6f3ca844ac244610f8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-25 15:04:04 +0200

    Update solve.py

--- poisson_equation/solve_u/print_out_bc_square_half_circle.py
+++ poisson_equation/solve_u/print_out_bc_square_half_circle.py
@@ -16,13 +16,11 @@ i, j, k, l = ufl.indices(4)
 
 # check if the boundary conditions (BCs) are satisfied
 print("Check of BCs:")
-print(f"\t\t<<(u - phi)^2>>_[partial Omega tb + ellipse] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u, fsp.u_exact, rmsh.ds_tb + rmsh.ds_ellipse):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
-print(
-    f"\t\t<<|n^i partial_i u  - n^i grad_u_i|^2>>_[partial Omega lr] = {col.Fore.RED}{msh.difference_wrt_measure(bgeo.facet_normal[i] * (fsp.u.dx(i)), bgeo.facet_normal[i] * fsp.grad_u[i], rmsh.ds_lr):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t\t<<(u - phi)^2>>_[partial Omega] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u, fsp.u_exact, rmsh.ds):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
 print("Comparison with exact solution: ")
 print(f"\t\t<<(u - u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u, fsp.u_exact, rmsh.dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 print(
-    f"\t\t<<(hess_u - hess_u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_wrt_measure((fsp.hess_u[i, j] - fsp.hess_u_exact[i, j]) * (fsp.hess_u[i, j] - fsp.hess_u_exact[i, j]), Constant(0), rmsh.dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+    f"\t\t<<(hess_u - hess_u_exact)^2>>_Omega = {col.Fore.RED}{msh.abs_wrt_measure(sqrt((fsp.hess_u[i, j] - fsp.hess_u_exact[i, j]) * (fsp.hess_u[i, j] - fsp.hess_u_exact[i, j])), rmsh.dx):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
 import print_out_solution
