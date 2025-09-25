9d9d3e4bb7c556de48ecf437537f2c79ef5b3fc3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-25 14:16:49 +0200

    Update square_half_circle.py

--- modules/mesh/read/square_half_circle.py
+++ modules/mesh/read/square_half_circle.py
@@ -17,16 +17,17 @@ r_mesh = lmsh.mesh.hmin()
 parameters = io.read_parameters_from_csv_file(rarg.args.input_directory + "/mesh_metadata.csv")
 
 # test for surface elements
-dx = Measure("dx", domain=lmsh.mesh, subdomain_data=sf, subdomain_id=1)
-ds_l = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=2)
-ds_r = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=3)
-ds_t = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=4)
-ds_b = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=5)
-ds_circle = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=6)
+dx = Measure("dx", domain=lmsh.mesh, subdomain_data=sf, subdomain_id=parameters['surface_id'])
+ds_l = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['line_l_id'])
+ds_r = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['line_r_id'])
+ds_tl = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['line_tl_id'])
+ds_tr = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['line_tr_id'])
+ds_b = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['line_b_id'])
+ds_half_circle = Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters['half_circle_id'])
 ds_lr = ds_l + ds_r
+ds_t = ds_tl + ds_tr + ds_half_circle
 ds_tb = ds_t + ds_b
-ds_square = ds_lr + ds_tb
-ds = ds_square + ds_circle
+ds = ds_lr + ds_tb
 
 import importlib
 check_mesh_module = importlib.import_module('mesh.check_tags.square')
@@ -36,6 +37,7 @@ print(f'Module {__file__} called {check_mesh_module.__file__}', flush=True)
 msh.check_mesh_symmetry(lmsh.mesh, parameters["c_r"])
 
 # Define boundaries and obstacle
+'''
 boundary = 'on_boundary'
 boundary_l = f'near(x[0], 0.0)'
 boundary_r = f'near(x[0], {parameters["L"]})'
@@ -43,3 +45,4 @@ boundary_lr = f'near(x[0], 0) || near(x[0], {parameters["L"]})'
 boundary_tb = f'near(x[1], 0) || near(x[1], {parameters["h"]})'
 boundary_square = f'on_boundary && sqrt(pow(x[0] - {parameters["c_r"][0]}, 2) + pow(x[1] - {parameters["c_r"][1]}, 2)) > {(parameters["r"] + calc.min_dist_c_r_rectangle(parameters["L"], parameters["h"], parameters["c_r"])) / 2}'
 boundary_circle = f'on_boundary && sqrt(pow(x[0] - {parameters["c_r"][0]}, 2) + pow(x[1] - {parameters["c_r"][1]}, 2)) < {(parameters["r"] + calc.min_dist_c_r_rectangle(parameters["L"], parameters["h"], parameters["c_r"])) / 2}'
+'''
