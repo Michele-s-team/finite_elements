1082121e1924231036fcfb86d21ad56c564bd3aa
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-09-25 13:37:54 +0200

    Update check_mesh.py

--- generate_mesh/2d/square/half_circle/check_mesh.py
+++ generate_mesh/2d/square/half_circle/check_mesh.py
@@ -4,12 +4,8 @@ This code checks the mesh generated from generate_mesh.py
 Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
 Example:
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/lines/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/lines/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_left_right_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_left_right_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/symmetric_top_bottom/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
-'''
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/half_circle/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square_circle/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+ '''
 
 import importlib
 import sys
@@ -20,8 +16,7 @@ sys.path.append(module_path)
 
 import mesh.load as lmsh
 import mesh.utils as msh
-rmsh = importlib.import_module('mesh.read.square')
+rmsh = importlib.import_module('mesh.read.square.half_circle')
 
-import mesh.check_tags.square
-msh.check_mesh_symmetry(lmsh.mesh, rmsh.parameters["c_r"])
+import mesh.check_tags.square.half_circle
 
