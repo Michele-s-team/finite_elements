0c1a5e2a4b190cfde00f98b995b2da3128bedb8f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-01 00:45:41 +0200

    setting un line_vertex problem ...

--- fourth_order_pde/switch_problem.py
+++ fourth_order_pde/switch_problem.py
@@ -2,7 +2,12 @@ import colorama as col
 
 import runtime_arguments as rarg
 
-if rarg.args.problem == 'ring_dirichlet':
+if rarg.args.problem == 'line_vertex_dirichlet':
+    rmsh = 'mesh.read.line_vertex'
+    vp = 'variational_problem_bc_line_vertex_dirichlet'
+    prout_bc = 'print_out_bc_line_vertex_dirichlet'
+
+elif rarg.args.problem == 'ring_dirichlet':
     rmsh = 'mesh.read.ring'
     vp = 'variational_problem_bc_ring_dirichlet'
     prout_bc = 'print_out_bc_ring_dirichlet'
