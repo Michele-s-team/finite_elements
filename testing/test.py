09bb01abc20a193ba015a7d22429b03dd41c7488
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-02 09:38:20 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -419,6 +419,24 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
 # Test fourth_order_pde
 case_name = 'fourth_order_pde'
 
+problem_name = 'line_vertex_dirichlet'
+generate_mesh_path = root_path + 'generate_mesh/1d/line/vertex'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
+problem_name = 'line_vertex_nitsche'
+generate_mesh_path = root_path + 'generate_mesh/1d/line/vertex'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
 problem_name = 'ring_dirichlet'
 generate_mesh_path = root_path + 'generate_mesh/2d/ring/'
 checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
