ce56b33644cf5be4cf23845754ea2ab4a6323d74
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-02 11:38:56 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -480,6 +480,17 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
 # Test fourth_order_pde/biharmonic_equation
 case_name = 'fourth_order_pde/biharmonic_equation'
 
+
+problem_name = 'line_vertex'
+generate_mesh_path = root_path + 'generate_mesh/1d/line/vertex'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', os.path.join(generate_mesh_path, 'resolution_0.1'), os.path.join(generate_mesh_path, 'resolution_0.1'), problem_name, problem_name, success)
+
+
 problem_name = 'ring'
 generate_mesh_path = root_path + 'generate_mesh/2d/ring/'
 checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
