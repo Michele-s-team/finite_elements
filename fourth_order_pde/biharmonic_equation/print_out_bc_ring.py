c10e1accbbac71b9e13f29bd78b2ff6add919f25
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-02 09:44:51 +0200

    renamed

--- fourth_order_pde/biharmonic_equation/print_out_bc_ring.py
+++ fourth_order_pde/biharmonic_equation/print_out_bc_ring.py
@@ -1,10 +1,10 @@
 import colorama as col
 from fenics import *
 import importlib
-import termcolor
 import ufl as ufl
 
 import function_spaces as fsp
+import input_output as io
 import mesh.utils as msh
 
 import switch_problem as swi
@@ -16,17 +16,13 @@ i, j, k, l = ufl.indices(4)
 u_output, v_output, w_output = fsp.psi.split(deepcopy=True)
 
 print("BCs check: ")
-print(f"<<(u - u_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(u_output, fsp.u_exact), 'red')}")
-print(f"<<(v - v_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(v_output, fsp.v_exact), 'red')}")
-print(f"<<(w - w_exact)^2>>_partial Omega = {termcolor.colored(msh.difference_on_boundary(w_output, fsp.w_exact), 'red')}")
-
-# print("Check that the PDE is satisfied: ")
-# print(f"<<(w - f)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, fsp.f), 'green')}")
-
+print(f"\t<<(u - u_exact)^2>>_partial Omega = {col.Fore.RED}{msh.difference_on_boundary(u_output, fsp.u_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t<<(v - v_exact)^2>>_partial Omega = {col.Fore.RED}{msh.difference_on_boundary(v_output, fsp.v_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t<<(w - w_exact)^2>>_partial Omega = {col.Fore.RED}{msh.difference_on_boundary(w_output, fsp.w_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
 print("Comparison with exact solution: ")
-print(f"<<(u - u_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(u_output, fsp.u_exact), 'blue')}")
-print(f"<<(v - v_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(v_output, fsp.v_exact), 'blue')}")
-print(f"<<(w - w_exact)^2>>_Omega = {termcolor.colored(msh.difference_in_bulk(w_output, fsp.w_exact), 'blue')}")
+print(f"\t<<(u - u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_in_bulk(u_output, fsp.u_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t<<(v - v_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_in_bulk(v_output, fsp.v_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t<<(w - w_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_in_bulk(w_output, fsp.w_exact):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
 
 import print_out_solution
--- fourth_order_pde/biharmonic_equation/variational_problem_bc_ring.py
+++ fourth_order_pde/biharmonic_equation/variational_problem_bc_ring.py
@@ -42,12 +42,12 @@ fsp.v_exact.interpolate( v_exact_expression( element=fsp.Q_v.ufl_element() ) )
 fsp.w_exact.interpolate( w_exact_expression( element=fsp.Q_w.ufl_element() ) )
 fsp.f.interpolate( w_exact_expression( element=fsp.Q_w.ufl_element() ) )
 
-u_profile = Expression( 'cos(x[0]+x[1]) * sin(x[0]-x[1])', element=fsp.Q.sub( 0 ).ufl_element() )
-v_profile = Expression( '- 4 * cos(x[0])*sin(x[0]) + 4 * cos(x[1])*sin(x[1])', element=fsp.Q.sub( 1 ).ufl_element() )
-w_profile = Expression( '8 * (sin(2*x[0]) - sin(2*x[1]))', element=fsp.Q.sub( 2 ).ufl_element() )
-bc_u = DirichletBC( fsp.Q.sub( 0 ), u_profile, rmsh.boundary )
-bc_v = DirichletBC( fsp.Q.sub( 1 ), v_profile, rmsh.boundary )
-bc_w = DirichletBC( fsp.Q.sub( 2 ), w_profile, rmsh.boundary )
+
+bc_u = DirichletBC( fsp.Q.sub( 0 ), fsp.u_exact, rmsh.boundary )
+bc_v = DirichletBC( fsp.Q.sub( 1 ), fsp.v_exact, rmsh.boundary )
+bc_w = DirichletBC( fsp.Q.sub( 2 ), fsp.w_exact, rmsh.boundary )
+
+bcs = [bc_u, bc_v, bc_w]
 
 F_v = ((fsp.v.dx( i )) * (fsp.nu_v.dx( i )) + fsp.f * fsp.nu_v) * rmsh.dx \
       - bgeo.facet_normal[i] * (fsp.v.dx( i )) * fsp.nu_v * rmsh.ds
@@ -57,4 +57,3 @@ F_w = ((fsp.v.dx( i )) * (fsp.nu_w.dx( i )) + fsp.w * fsp.nu_w) * rmsh.dx \
       - bgeo.facet_normal[i] * (fsp.v.dx( i )) * fsp.nu_w * rmsh.ds
 
 F = F_u + F_v + F_w
-bcs = [bc_u, bc_v, bc_w]
