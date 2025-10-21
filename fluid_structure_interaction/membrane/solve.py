76dd267f67712607e1e4c24a937ecc4aa60d5cd7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-21 15:11:05 +0200

    Update solve.py

--- fluid_structure_interaction/membrane/solve.py
+++ fluid_structure_interaction/membrane/solve.py
@@ -107,7 +107,6 @@ for n in range(rpam.parameters['N']):
     fsp.var_tensor_sigma_fl.assign(project(ela.var_sigma_tensor(fsp.sigma_fl_n_32, fsp.v_fl_n_1, fsp.u_n_1, rpam.parameters['eta_fluid']), fsp.Q_var_tensor_sigma_fl))
     fu.transfer_mesh_to_sub_mesh(fsp.var_tensor_sigma_fl, fsp.var_tensor_sigma_fl_on_mem, rmsh.parameters['h'])
     
-    # sign
 
     vp_membrane = importlib.import_module(swi.vp_membrane)
     
@@ -118,6 +117,8 @@ for n in range(rpam.parameters['N']):
     solver_mem.solve()
 
     print('... done.', flush=True)
+
+    # sign
 
     '''
     # step 2): update u and u_dot (mesh problem)
