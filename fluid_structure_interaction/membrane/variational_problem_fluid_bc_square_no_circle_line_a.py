54263cb19c2e482fd4a02364b21c811c00cd37f5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-21 12:52:28 +0200

    Update variational_problem_fluid_bc_square_no_circle_line_a.py

--- fluid_structure_interaction/membrane/variational_problem_fluid_bc_square_no_circle_line_a.py
+++ fluid_structure_interaction/membrane/variational_problem_fluid_bc_square_no_circle_line_a.py
@@ -71,14 +71,14 @@ alpha, beta, k, delta = ufl.indices(4)
 
 # step 2 for phi
 F_phi = ( \
-                    - ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi.dx(beta)) * ela.G(fsp.u_n_1)[delta, alpha] * (fsp.nu_phi.dx(delta)) \
+                    - ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi_fl.dx(beta)) * ela.G(fsp.u_n_1)[delta, alpha] * (fsp.nu_phi.dx(delta)) \
                     - (rpam.parameters['rho_fluid'] / dt) * ela.G(fsp.u_n_1)[beta, alpha] * ((fsp.v_fl_bar[alpha]).dx(beta)) * fsp.nu_phi \
             ) * ela.detF(fsp.u_n_1) * rmsh.dx \
-        + (ela.G(fsp.u_n_1)[delta, alpha] * bgeo.facet_normal[delta] * ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi.dx(beta)) * fsp.nu_phi) * ela.detF(fsp.u_n_1) * rmsh.ds_r
+        + (ela.G(fsp.u_n_1)[delta, alpha] * bgeo.facet_normal[delta] * ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi_fl.dx(beta)) * fsp.nu_phi) * ela.detF(fsp.u_n_1) * rmsh.ds_r
 
 
 '''
 
 # step 3 for v_n
-F_v_n = (((fsp.v_fl_n[i] - fsp.v_fl_bar[i]) + (dt / rpam.parameters['rho_fluid']) * ela.G(fsp.u_n_1)[l, i] * (fsp.phi.dx(l))) * fsp.nu_v_n[i]) * ela.detF(fsp.u_n_1) * rmsh.dx
+F_v_n = (((fsp.v_fl_n[i] - fsp.v_fl_bar[i]) + (dt / rpam.parameters['rho_fluid']) * ela.G(fsp.u_n_1)[l, i] * (fsp.phi_fl.dx(l))) * fsp.nu_v_n[i]) * ela.detF(fsp.u_n_1) * rmsh.dx
 '''
