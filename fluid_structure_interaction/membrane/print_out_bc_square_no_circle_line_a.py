d56ca7c3e5e895f3f0f0176a95d0f860c088be8d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-11-13 22:05:58 +0100

    added BC 78, now add 80

--- fluid_structure_interaction/membrane/print_out_bc_square_no_circle_line_a.py
+++ fluid_structure_interaction/membrane/print_out_bc_square_no_circle_line_a.py
@@ -30,6 +30,7 @@ fieldnames = [ \
     # membrane problem
     '<<|v_bar - g|^2>>_{ds_{~ l}}',
     '<<|v_bar - g|^2>>_{ds_{~ r}}',
+    '<<w_bar^2>>_{ds_{~ l}}',
 
     
     # mesh problem
@@ -71,42 +72,44 @@ def print_bcs_fl():
             f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_bar - fsp.v_bar_l), rmsh.ds_sub_mesh[1]['ds_l']):.{io.number_of_decimals}e}",
         fieldnames[1]: \
             f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_bar - fsp.v_bar_r), rmsh.ds_sub_mesh[1]['ds_r']):.{io.number_of_decimals}e}",
+        fieldnames[2]: \
+            f"{msh.abs_wrt_measure(fsp.w_bar, rmsh.ds_sub_mesh[1]['ds_l']):.{io.number_of_decimals}e}",
         
         # mesh problem
-        fieldnames[2]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_n), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[3]: \
-            f"{msh.abs_wrt_measure(fsp.u_n[0], rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_n), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[4]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_n - fsp.U_n_12_on_mesh), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(fsp.u_n[0], rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
         fieldnames[5]: \
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_n - fsp.U_n_12_on_mesh), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
+        fieldnames[6]: \
             f"{msh.abs_wrt_measure(fsp.u_n[1].dx(0), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
 
-        fieldnames[6]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_dot_n), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[7]: \
-            f"{msh.abs_wrt_measure(fsp.u_dot_n[0], rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_dot_n), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[8]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_dot_n - fsp.U_dot_n_12_on_mesh), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(fsp.u_dot_n[0], rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
         fieldnames[9]: \
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.u_dot_n - fsp.U_dot_n_12_on_mesh), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
+        fieldnames[10]: \
             f"{msh.abs_wrt_measure(fsp.u_dot_n[1].dx(0), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
             
         # fluid problem
-        fieldnames[10]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar - fsp.v_fl_bar_b), rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[11]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar), rmsh.ds_sub_mesh[0]['ds_l']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar - fsp.v_fl_bar_b), rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[12]: \
-            f"{msh.abs_wrt_measure(abs(fsp.v_fl_bar[0]), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar), rmsh.ds_sub_mesh[0]['ds_l']):.{io.number_of_decimals}e}",
         fieldnames[13]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar - fsp.u_dot_n), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(abs(fsp.v_fl_bar[0]), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
         fieldnames[14]: \
-            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.phi_fl), rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.v_fl_bar - fsp.u_dot_n), rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",
         fieldnames[15]: \
-            f"{msh.abs_wrt_measure(ela.G(fsp.u_n_1)[alpha, 0] * (((fsp.v_fl_n_1[1] + fsp.v_fl_bar[1]) / 2.0).dx(alpha)), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
+            f"{msh.abs_wrt_measure(geo.ufl_norm(fsp.phi_fl), rmsh.ds_sub_mesh[0]['ds_b']):.{io.number_of_decimals}e}",
         fieldnames[16]: \
-            f"{msh.abs_wrt_measure(ela.G(fsp.u_n_1)[gamma, alpha] * (bgeo.sub_mesh_facet_normal[0])[gamma] * ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi_fl.dx(beta)), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",        
+            f"{msh.abs_wrt_measure(ela.G(fsp.u_n_1)[alpha, 0] * (((fsp.v_fl_n_1[1] + fsp.v_fl_bar[1]) / 2.0).dx(alpha)), rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}",
         fieldnames[17]: \
+            f"{msh.abs_wrt_measure(ela.G(fsp.u_n_1)[gamma, alpha] * (bgeo.sub_mesh_facet_normal[0])[gamma] * ela.G(fsp.u_n_1)[beta, alpha] * (fsp.phi_fl.dx(beta)), rmsh.ds_sub_mesh[0]['ds_l'] + rmsh.ds_sub_mesh[0]['ds_t']):.{io.number_of_decimals}e}",        
+        fieldnames[18]: \
             f"{msh.abs_wrt_measure(ela.G(fsp.u_n_1)[beta, 0] * (fsp.phi_fl.dx(beta)) , rmsh.ds_sub_mesh[0]['ds_r']):.{io.number_of_decimals}e}"        
             }])
 
