e961202467e3a63f71846534d091197bf68b4c85
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-01-03 20:08:49 +0100

    revising vp..

--- poisson_equation/solve_u/periodic/variational_problem_bc_line.py
+++ poisson_equation/solve_u/periodic/variational_problem_bc_line.py
@@ -1,7 +1,7 @@
-import switch_problem as swi
-import numpy as np
 from fenics import *
 import importlib
+import numpy as np
+import switch_problem as swi
 import ufl as ufl
 
 import differential_geometry.boundary.geometry as bgeo
@@ -13,8 +13,7 @@ i, j = ufl.indices(2)
 
 class u_exact_expression(UserExpression):
     def eval(self, values, x):
-        values[0] = np.cos(2 * np.pi * x[0] / rmsh.parameters["L"]) * \
-            np.cos(2 * np.pi * x[1] / rmsh.parameters["h"])
+        values[0] = np.cos(2 * np.pi * x[0] / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))
 
     def value_shape(self):
         return (1,)
@@ -22,19 +21,16 @@ class u_exact_expression(UserExpression):
 
 class grad_u_expression(UserExpression):
     def eval(self, values, x):
-        values[0] = - (2 * np.pi / rmsh.parameters["L"]) * np.cos(2 * np.pi * x[1] /
-                                                                  rmsh.parameters["h"]) * np.sin(2 * np.pi * x[0] / rmsh.parameters["L"])
-        values[1] = - (2 * np.pi / rmsh.parameters["h"]) * np.cos(2 * np.pi * x[0] /
-                                                                  rmsh.parameters["L"]) * np.sin(2 * np.pi * x[1] / rmsh.parameters["h"])
+        values[0] = - (2 * np.pi / (rmsh.parameters["x_r"] - rmsh.parameters['x_l'])) * np.sin(2 * np.pi * x[0] / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))
+
 
     def value_shape(self):
-        return (2,)
+        return (1,)
 
 
 class laplacian_u_expression(UserExpression):
     def eval(self, values, x):
-        values[0] = - (4 * np.pi ** 2 * (rmsh.parameters["h"] ** 2 + rmsh.parameters["L"] ** 2) *
-                       np.cos(2 * np.pi * x[0] / rmsh.parameters["L"]) * np.cos(2 * np.pi * x[1] / rmsh.parameters["h"])) / (rmsh.parameters["h"] ** 2 * rmsh.parameters["L"] ** 2)
+        values[0] = -((2 * np.pi / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))**2) * np.cos(2 * np.pi * x[0] / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))
 
     def value_shape(self):
         return (1,)
@@ -45,35 +41,13 @@ class hess_u_exact_expression(UserExpression):
         super().init(**kwargs)
 
     def eval(self, values, x):
-        # Common precomputed terms
-        cos_x = np.cos(2 * np.pi * x[0] / rmsh.parameters["L"])
-        cos_y = np.cos(2 * np.pi * x[1] / rmsh.parameters["h"])
-        sin_x = np.sin(2 * np.pi * x[0] / rmsh.parameters["L"])
-        sin_y = np.sin(2 * np.pi * x[1] / rmsh.parameters["h"])
-        pi2 = 4 * np.pi ** 2
-
+      
         # Matrix components
-        values[0] = - (pi2 * cos_x * cos_y) / \
-            rmsh.parameters["L"] ** 2  # [0, 0]
-        values[1] = (pi2 * sin_x * sin_y) / (rmsh.parameters["h"]
-                                             * rmsh.parameters["L"])  # [0, 1]
-        values[2] = (pi2 * sin_x * sin_y) / (rmsh.parameters["h"]
-                                             * rmsh.parameters["L"])  # [1, 0]
-        values[3] = - (pi2 * cos_x * cos_y) / \
-            rmsh.parameters["h"] ** 2  # [1, 1]
+        values[0] =         values[0] = -((2 * np.pi / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))**2) * np.cos(2 * np.pi * x[0] / (rmsh.parameters["x_r"] - rmsh.parameters['x_l']))
+     
 
     def value_shape(self):
-        return (2, 2)
-
-
-# class u_0_Expression(UserExpression):
-#     def eval(self, values, x):
-#         # test case 1
-#         # values[0] = 1 +  2 * x[1] ** 2
-#         values[0] = 1 + x[0] + 2 * x[1] ** 2
-#
-#     def value_shape(self):
-#         return (1,)
+        return (1, 1)
 
 
 fsp.u_exact.interpolate(u_exact_expression(element=fsp.Q.ufl_element()))
@@ -87,15 +61,13 @@ fsp.hess_u_exact.interpolate(
 # fsp.u.interpolate(u_0_Expression(element=fsp.Q.ufl_element()))
 #
 
-bc_u_l = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_l)
-bc_u_t = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary_t)
-bcs = [bc_u_l, bc_u_t]
+bcs = []
 
 # variational functional for the original problem (poisson equation)
 F = (fsp.u.dx(i) * fsp.nu_u.dx(i) + fsp.f * fsp.nu_u) * rmsh.dx \
-    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_lr \
-    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_tb \
- \
+    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_l\
+    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_r
+ 
     # variational functional for post-processing problem (pp) to obtain the hessian (hess)
 F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
     - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.ds
