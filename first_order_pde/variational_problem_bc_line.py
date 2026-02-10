d481f49e4da567b453016d2428c8387c1b64fb9b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-10 11:26:39 +0100

    set up

---
+++ first_order_pde/variational_problem_bc_line.py
@@ -0,0 +1,71 @@
+from fenics import *
+import importlib
+import numpy as np
+import ufl as ufl
+
+import differential_geometry.boundary.geometry as bgeo
+import function_spaces as fsp
+import mesh.load as lmsh
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+
+i, j = ufl.indices(2)
+
+
+class u_exact_expression(UserExpression):
+    def eval(self, values, x):
+        # test case 1
+        # values[0] = 1 + x[0] ** 2
+
+        # test case 2
+        values[0] = 1 + np.cos(2 * np.pi * x[0]) / (1 + x[0] ** 2)
+
+    def value_shape(self):
+        return (1,)
+
+
+class laplacian_u_expression(UserExpression):
+    def eval(self, values, x):
+        # test case 1
+        # values[0] = 2.0
+
+        # test case 2
+        values[0] = (-2 * (1 - 3 * x[0] ** 2 + 2 * np.pi ** 2 * (1 + x[0] ** 2) ** 2) * np.cos(2 * np.pi * x[0]) + 8 * np.pi * x[0] * (1 + x[0] ** 2) * np.sin(2 * np.pi * x[0])) / (1 + x[0] ** 2) ** 3
+
+    def value_shape(self):
+        return (1,)
+
+
+class hess_u_exact_expression(UserExpression):
+    def init(self, **kwargs):
+        super().init(**kwargs)
+
+    def eval(self, values, x):
+        # test case 1
+        # values[0] = 2
+
+        # test case 2
+        values[0] = (-2 * (1 - 3 * x[0] ** 2 + 2 * np.pi ** 2 * (1 + x[0] ** 2) ** 2) * np.cos(2 * np.pi * x[0]) + 8 * np.pi * x[0] * (1 + x[0] ** 2) * np.sin(2 * np.pi * x[0])) / (1 + x[0] ** 2) ** 3
+
+    def value_shape(self):
+        return (1, 1)
+
+
+fsp.u_exact.interpolate(u_exact_expression(element=fsp.Q.ufl_element()))
+fsp.f.interpolate(laplacian_u_expression(element=fsp.Q.ufl_element()))
+
+fsp.hess_u_exact.interpolate(hess_u_exact_expression(element=fsp.T.ufl_element()))
+
+
+
+bc_u = DirichletBC(fsp.Q, fsp.u_exact, rmsh.boundary)
+bcs = [bc_u]
+
+# variational functional for the original problem (poisson equation)
+F = (fsp.u.dx(i) * fsp.nu_u.dx(i) + fsp.f * fsp.nu_u) * rmsh.dx \
+    - bgeo.facet_normal[i] * fsp.u.dx(i) * fsp.nu_u * rmsh.ds
+
+# variational functional for post-processing problem (pp) to obtain the hessian (hess)
+F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
+       - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.ds
