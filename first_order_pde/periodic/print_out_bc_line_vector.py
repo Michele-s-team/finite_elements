0de9532c8d842e4ba0a4ca99adb9b64256a0330e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-11 14:56:55 +0100

    removed pp problem for now

--- first_order_pde/periodic/print_out_bc_line_vector.py
+++ first_order_pde/periodic/print_out_bc_line_vector.py
@@ -17,16 +17,9 @@ import switch_problem as swi
 fsp = importlib.import_module(swi.fsp)
 rmsh = importlib.import_module(swi.rmsh)
 
-i, j, k, l = ufl.indices(4)
-
 # check if the boundary conditions (BCs) are satisfied
 print("Check of BCs:")
 print(
-    f"\t\tu_[partial Omega l] - u_[partial Omega r] = {col.Fore.RED}{fsp.u(rmsh.parameters['x_l']) - fsp.u(rmsh.parameters['x_r']):.{sys_io.number_of_decimals}e}{col.Style.RESET_ALL}")
-
-
-print("Comparison with exact solution: ")
+    f"\t\tu_[partial Omega l][0] - u_[partial Omega r][0] = {col.Fore.RED}{sqrt((fsp.u(rmsh.parameters['x_l'])[0] - fsp.u(rmsh.parameters['x_r'])[0])):.{sys_io.number_of_decimals}e}{col.Style.RESET_ALL}")
 print(
-    f"\t\t<<(u - u_exact)^2>>_Omega = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u, fsp.u_exact, rmsh.dx):.{sys_io.number_of_decimals}e}{col.Style.RESET_ALL}")
-print(
-    f"\t\t<<(grad_u - grad_u_exact)^2>>_Omega = {col.Fore.RED}{msh.abs_wrt_measure(sqrt((fsp.grad_u[i] - fsp.grad_u_exact[i]) * (fsp.grad_u[i] - fsp.grad_u_exact[i])), rmsh.dx):.{sys_io.number_of_decimals}e}{col.Style.RESET_ALL}")
+    f"\t\tu_[partial Omega l][1] - u_[partial Omega r][1] = {col.Fore.RED}{sqrt((fsp.u(rmsh.parameters['x_l'])[1] - fsp.u(rmsh.parameters['x_r'])[1])):.{sys_io.number_of_decimals}e}{col.Style.RESET_ALL}")
