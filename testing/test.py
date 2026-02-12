de7ca9348a43b735d8f8b502cbe1f89b98e429bc
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-12 14:32:51 +0100

    moved one_dimension -> one_dimension/line

--- testing/test.py
+++ testing/test.py
@@ -848,8 +848,8 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      'generate_square_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
 
-# Test dynamics/lagrangian_approach/one_dimension
-case_name = 'dynamics/lagrangian_approach/one_dimension'
+# Test dynamics/lagrangian_approach/one_dimension/line
+case_name = 'dynamics/lagrangian_approach/one_dimension/line'
 
 problem_name = 'line_a'
 generate_mesh_path =root_path + 'generate_mesh/1d/line'
