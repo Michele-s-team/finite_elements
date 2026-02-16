5566e956c06879bea6f86256ce6f15feed616de4
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-16 15:34:13 +0100

    cu

--- modules/mesh/read/square_square.py
+++ modules/mesh/read/square_square.py
@@ -1,12 +1,3 @@
-'''
-Notation:
-- sub_mesh: either of the parts of the total mesh
-
-- sf_sub_mesh: a list of map functions, where sf_sub_mesh[i] is the map function for the triangles of the i-th sub_mesh
-- mf_sub_mesh: a list of map functions, where mf_sub_mesh[i] is the map function for the lines of the i-th sub_mesh
-
-'''
-
 from fenics import *
 
 import input_output as io
@@ -30,32 +21,7 @@ for sub_mesh in lmsh.sub_meshes:
 
 # radius of the smallest cell in the mesh
 r_mesh = lmsh.mesh.hmin()
-'''
-# create line and surface elements for the parent mesh
-dx_parent_mesh, ds_parent_mesh_l, ds_parent_mesh_r, ds_parent_mesh_t, ds_parent_mesh_b, ds_parent_mesh_lr, ds_parent_mesh_tb, ds_parent_mesh_lrtb = [], [], [], [], [], [], [], []
 
-ds_parent_mesh_l.append(Measure("dS", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_0_l_id"]))
-ds_parent_mesh_l.append(Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_1_l_id"]))
-
-ds_parent_mesh_r.append(Measure("dS", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_0_r_id"]))
-ds_parent_mesh_r.append(Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_1_r_id"]))
-
-ds_parent_mesh_t.append(Measure("dS", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_0_t_id"]))
-ds_parent_mesh_t.append(Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_1_t_id"]))
-
-ds_parent_mesh_b.append(Measure("dS", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_0_b_id"]))
-ds_parent_mesh_b.append(Measure("ds", domain=lmsh.mesh, subdomain_data=mf, subdomain_id=parameters["line_sub_mesh_1_b_id"]))
-
-for p in range(len(lmsh.sub_meshes)):
-    dx_parent_mesh.append(Measure("dx", domain=lmsh.mesh, subdomain_data=sf, subdomain_id=parameters[f"sub_mesh_{p}_id"]))
-
-    ds_parent_mesh_lr.append(ds_parent_mesh_l[p] + ds_parent_mesh_r[p])
-    ds_parent_mesh_tb.append(ds_parent_mesh_t[p] + ds_parent_mesh_b[p])
-
-    ds_parent_mesh_lrtb.append(ds_parent_mesh_lr[p] + ds_parent_mesh_tb[p])
-
-ds_parent_mesh = ds_parent_mesh_lrtb[0] + ds_parent_mesh_lrtb[1]
-'''
 
 # create line and surface elements for sub_meshes
 dx_sub_mesh = []
