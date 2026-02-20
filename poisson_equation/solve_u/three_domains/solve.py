c71d3bf950de7f687289f06d40ce10733ab8e243
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-20 18:30:10 +0100

    there is a discrepancy between the two functions

--- poisson_equation/solve_u/three_domains/solve.py
+++ poisson_equation/solve_u/three_domains/solve.py
@@ -43,7 +43,7 @@ import solution_paths as solpath
 class u_0_0_Expression(UserExpression):
     def eval(self, values, x):
 
-        values[0] = (1 + x[0] ** 2 + 2 * x[1] ** 2)**2
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
 
     def value_shape(self):
         return (1,)
