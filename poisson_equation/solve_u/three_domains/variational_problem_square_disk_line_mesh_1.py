dd06070c97874b5bb65ef64f4bb31615784a93fd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-23 20:05:10 +0100

    Update variational_problem_square_disk_line_mesh_1.py

--- poisson_equation/solve_u/three_domains/variational_problem_square_disk_line_mesh_1.py
+++ poisson_equation/solve_u/three_domains/variational_problem_square_disk_line_mesh_1.py
@@ -19,42 +19,7 @@ class u_exact_mesh_1_expression(UserExpression):
     def value_shape(self):
         return (1,)
 
-
-class grad_u_mesh_1_expression(UserExpression):
-    def eval(self, values, x):
-        values[0] = (4*np.pi*x[0]*np.cos(2*np.pi*x[0]**2/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2))/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2
-
-    def value_shape(self):
-        return (1,)
-
-
-class laplacian_u_mesh_1_expression(UserExpression):
-    def eval(self, values, x):
-        values[0] = (4*np.pi*((rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2*np.cos(2*np.pi*x[0]**2/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2) - 4*np.pi*x[0]**2*np.sin(2*np.pi*x[0]**2/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2)))/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**4
-
-    def value_shape(self):
-        return (1,)
-
-
-class hess_u_exact_mesh_1_expression(UserExpression):
-    def init(self, **kwargs):
-        super().init(**kwargs)
-
-    def eval(self, values, x):
-      
-        # Matrix components
-        values[0] = (4*np.pi*((rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2*np.cos(2*np.pi*x[0]**2/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2) - 4*np.pi*x[0]**2*np.sin(2*np.pi*x[0]**2/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**2)))/(rmsh.lmsh.mesh_parameters[1]['x_r'] - rmsh.lmsh.mesh_parameters[1]['x_l'])**4
-     
-    def value_shape(self):
-        return (1, 1)
-
-
 fsp.u_exact[1].interpolate(u_exact_mesh_1_expression(element=fsp.Q[1].ufl_element()))
-fsp.grad_u[1].interpolate(grad_u_mesh_1_expression(element=fsp.V[1].ufl_element()))
-fsp.f[1].interpolate(laplacian_u_mesh_1_expression(element=fsp.Q[1].ufl_element()))
-
-fsp.hess_u_exact[1].interpolate(
-    hess_u_exact_mesh_1_expression(element=fsp.T[1].ufl_element()))
 '''
 
 # impose a Dirichlet boundary condition on the left vertex in order to set C[1] -> 0
