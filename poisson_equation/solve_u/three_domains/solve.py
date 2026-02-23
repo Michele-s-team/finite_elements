9033e409961de5d4ea6269785ed9e1e5629bb1f6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-23 22:18:14 +0100

    sqitched test case 1 -> 2

--- poisson_equation/solve_u/three_domains/solve.py
+++ poisson_equation/solve_u/three_domains/solve.py
@@ -332,7 +332,7 @@ The three variational problems (VPs) are solved as follows:
     The solution is 
 
       a) For test case 1: u[1](s) = C[1] + r^2 (-2 Cos[s/r] + Sin[s/r])
-      b) For test case 2: u[1](s) = C[1] + 1/12 r^2 (-9 Cos[s/r] + Cos[(3 s)/r] + 4 (5 + Cos[(2 s)/r]) Sin[s/r])
+      b) For test case 2: u[1](s) = C[1] + 1/12 r^4 (-9 Cos[s/r] + Cos[(3 s)/r] + 4 (5 + Cos[(2 s)/r]) Sin[s/r])
     
     where I set C[1] -> 0 by adding a Dirichlet BC on the VP on mesh[1]
 
@@ -341,20 +341,20 @@ The three variational problems (VPs) are solved as follows:
     On the circle 
 
         a) Test case 1: u_1_on_0_0 = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1])
-        b) Test case 2: u_1_on_0_0 =  (9 * r**2  * (cr[0] - x[0]) + (-cr[0] + x[0])**3 + 3 * (cr[0] - x[0]) * (cr[1] - x[1])**2 + 
-                            2 * (cr[1] - x[1])**3 + 18 * r**2  * (-cr[1] + x[1]) + 6 * (cr[0] - x[0])**2 (-cr[1] + x[1]))/(12.0 * r)
+        b) Test case 2: u_1_on_0_0 =  1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
 
 5)  Solve a Poisson problem on sub_mesh[0][0]  with BC on the circle u[0][0] = u_1_on_0_0
 
     The problem has exact solution 
         a) Test case 1:  u[0][0] = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1]),  
-        b) Test case 2:  u[0][0] = (9 * r**2  * (cr[0] - x[0]) + (-cr[0] + x[0])**3 + 3 * (cr[0] - x[0]) * (cr[1] - x[1])**2 + 
-                            2 * (cr[1] - x[1])**3 + 18 * r**2  * (-cr[1] + x[1]) + 6 * (cr[0] - x[0])**2 (-cr[1] + x[1]))/(12.0 * r)
+        b) Test case 2:  u[0][0] = 1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
+
     Obtain 
         a) Test case 1): u[0][0] = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1]) in sub_mesh[0][0]
-        b) Test case 2):  u[0][0] = (9 * r**2  * (cr[0] - x[0]) + (-cr[0] + x[0])**3 + 3 * (cr[0] - x[0]) * (cr[1] - x[1])**2 + 
-                            2 * (cr[1] - x[1])**3 + 18 * r**2 * (-cr[1] + x[1]) + 6 * (cr[0] - x[0])**2 (-cr[1] + x[1]))/(12.0 * r)
-
+        b) Test case 2):  u[0][0] = 1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
 '''
 
 
--- poisson_equation/solve_u/three_domains/variational_problem_square_disk_line_sub_mesh_0_0.py
+++ poisson_equation/solve_u/three_domains/variational_problem_square_disk_line_sub_mesh_0_0.py
@@ -18,10 +18,11 @@ class u_exact_sub_mesh_0_0_expression(UserExpression):
     def eval(self, values, x):
 
         # test case 1
-        values[0] =  - 2 * rmsh.lmsh.parameters['r'] * (x[0] - rmsh.lmsh.parameters['c_r'][0]) + rmsh.lmsh.parameters['r'] * (x[1] - rmsh.lmsh.parameters['c_r'][1])
+        # values[0] =  - 2 * rmsh.lmsh.parameters['r'] * (x[0] - rmsh.lmsh.parameters['c_r'][0]) + rmsh.lmsh.parameters['r'] * (x[1] - rmsh.lmsh.parameters['c_r'][1])
 
         # test case 2
-        # values[0] =  (9.0 * rmsh.lmsh.parameters['r']**2 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) + (-rmsh.lmsh.parameters['c_r'][0] + x[0])**3 + 3.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1])**2 + 2.0 * (rmsh.lmsh.parameters['c_r'][1] - x[1])**3 + 18.0 * rmsh.lmsh.parameters['r']**2 * (-rmsh.lmsh.parameters['c_r'][1] + x[1]) + 6.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 * (-rmsh.lmsh.parameters['c_r'][1] + x[1])) / (12.0 * rmsh.lmsh.parameters['r'])
+       values[0] = (1.0/12.0) * rmsh.lmsh.parameters['r'] * (9.0 * rmsh.lmsh.parameters['r']**2 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) + (-rmsh.lmsh.parameters['c_r'][0] + x[0])**3 + 3.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1])**2 + 2.0 * (rmsh.lmsh.parameters['c_r'][1] - x[1])**3 + 18.0 * rmsh.lmsh.parameters['r']**2 * (-rmsh.lmsh.parameters['c_r'][1] + x[1]) + 6.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 * (-rmsh.lmsh.parameters['c_r'][1] + x[1]))
+
 
     def value_shape(self):
         return (1,)
@@ -31,12 +32,14 @@ class grad_u_exact_sub_mesh_0_0_expression(UserExpression):
     def eval(self, values, x):
 
         # test case 1
-        values[0] = - 2 * rmsh.lmsh.parameters['r']
-        values[1] = rmsh.lmsh.parameters['r']
+        # values[0] = - 2 * rmsh.lmsh.parameters['r']
+        # values[1] = rmsh.lmsh.parameters['r']
 
         # test case 2
-        # values[0] = (-3.0 * rmsh.lmsh.parameters['r']**2 + (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 + 4.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1]) - (rmsh.lmsh.parameters['c_r'][1] - x[1])**2) / (4.0 * rmsh.lmsh.parameters['r'])
-        # values[1] = (3.0 * rmsh.lmsh.parameters['r']**2 + (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 - (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1]) - (rmsh.lmsh.parameters['c_r'][1] - x[1])**2) / (2.0 * rmsh.lmsh.parameters['r'])
+        values[0] = (1.0/4.0) * rmsh.lmsh.parameters['r'] * (-3.0 * rmsh.lmsh.parameters['r']**2 + (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 + 4.0 * (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1]) - (rmsh.lmsh.parameters['c_r'][1] - x[1])**2)
+
+
+        values[1] = (1.0/2.0) * rmsh.lmsh.parameters['r'] * (3.0 * rmsh.lmsh.parameters['r']**2 + (rmsh.lmsh.parameters['c_r'][0] - x[0])**2 - (rmsh.lmsh.parameters['c_r'][0] - x[0]) * (rmsh.lmsh.parameters['c_r'][1] - x[1]) - (rmsh.lmsh.parameters['c_r'][1] - x[1])**2)
 
     def value_shape(self):
         return (2,)
@@ -46,7 +49,7 @@ class laplacian_u_exact_sub_mesh_0_0_expression(UserExpression):
     def eval(self, values, x):
 
         # test case 1
-        values[0] = 0
+        # values[0] = 0
 
         # test case 2
         values[0] = 0
