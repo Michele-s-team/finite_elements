8b0961c7b7ad73193b55116c41c7b9589cbdc013
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-25 13:34:22 +0100

    fixed generate_half_circle_with_line_mesh

--- generate_mesh/2d/half_circle_with_line/check_mesh.py
+++ generate_mesh/2d/half_circle_with_line/check_mesh.py
@@ -1,5 +1,5 @@
 '''
-This code checks the mesh generated from generate_half_circle_with_line_mesh.py
+This code checks the mesh generated from generate_mesh.py
 
 Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
