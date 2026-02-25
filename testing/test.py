427dd74d44f7e10695d2c3371b6f8be5f6935a3b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-02-25 22:40:44 +0100

    box mesh

--- testing/test.py
+++ testing/test.py
@@ -272,7 +272,7 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      generate_mesh_path, root_path + case_name,
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-                                                                     'generate_box_mesh', 'generate_box_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+                                                                     'generate_mesh', 'generate_box_mesh', generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
 problem_name = 'box_ball'
 generate_mesh_path = root_path + 'generate_mesh/3d/box_ball/'
