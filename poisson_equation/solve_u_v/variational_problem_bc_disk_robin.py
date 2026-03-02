ca5e4e4482a320100e4bb3dfbd2b54b8262d6c5a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-02 18:40:22 +0100

    Update variational_problem_bc_disk_robin.py

--- poisson_equation/solve_u_v/variational_problem_bc_disk_robin.py
+++ poisson_equation/solve_u_v/variational_problem_bc_disk_robin.py
@@ -15,9 +15,9 @@ i, j, k, l = ufl.indices(4)
 
 class u_exact_expression( UserExpression ):
     def eval(self, values, x):
-        # values[0] = 1 + cos( x[0] - x[1] ) - sin( x[1] )
-        # values[0] = 1 + (x[0]**2) + 2 * (x[1]**2)
-        values[0] = np.sin(2 * (np.pi) * (x[0] + x[1])) *  np.cos(2 * (np.pi) * (x[0] - x[1])**2)
+
+        # test case 1
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
 
     def value_shape(self):
         return (1,)
@@ -25,10 +25,10 @@ class u_exact_expression( UserExpression ):
 
 class v_exact_expression( UserExpression ):
     def eval(self, values, x):
-        # values[0] = - np.sin( x[0] - x[1] )
-        # values[1] = np.sin( x[0] - x[1] ) - np.cos( x[1] )
-        values[0] =  2 *(np.pi) *np.cos(2 *(np.pi) *((x[0]) - (x[1]))**2) * np.cos(2 *(np.pi) *((x[0]) + (x[1]))) + 4 *(np.pi) *(-(x[0]) + (x[1]))* np.sin(2 *(np.pi) * ((x[0]) - (x[1]))**2) * np.sin(2 * (np.pi) * ((x[0]) + (x[1])))
-        values[1] = 2 * (np.pi) * np.cos(2* (np.pi) * ((x[0]) - (x[1]))**2) * np.cos(2 * (np.pi) * ((x[0]) + (x[1]))) + 4* (np.pi) * ((x[0]) - (x[1])) * np.sin(2 *(np.pi) *((x[0]) - (x[1]))**2) * np.sin(2 * (np.pi)*  ((x[0]) + (x[1])))
+        
+        # test case 1
+        values[0] = 2.0 * x[0]
+        values[1] = 4.0 * x[1]
 
     def value_shape(self):
         return (2,)
@@ -36,8 +36,9 @@ class v_exact_expression( UserExpression ):
 
 class laplacian_u_exact_expression( UserExpression ):
     def eval(self, values, x):
-        # values[0] = -2 * np.cos( x[0] - x[1] ) + np.sin( x[1] )
-        values[0] = 8 *(np.pi)* (-(np.pi)* (1+4* (x[0]-(x[1]))**2) * np.cos(2* (np.pi)* (x[0]-(x[1]))**2)-np.sin(2* (np.pi) *(x[0]-(x[1]))**2))* np.sin(2* (np.pi)* (x[0]+(x[1])))
+    
+        # test case 1
+        values[0] = 6.0
 
     def value_shape(self):
         return (1,)
@@ -49,27 +50,15 @@ fsp.laplacian_u_exact.interpolate(laplacian_u_exact_expression(element=fsp.Q_u.u
 fsp.f.interpolate(laplacian_u_exact_expression(element=fsp.Q_u.ufl_element()))
 
 # define Difichlet boundary conditions
-bc_u = DirichletBC(fsp.Q.sub(0), fsp.u_exact, rmsh.boundary)
-bcs = [bc_u]
-
-# define variational problem
-# F_v = (fsp.v[i] * fsp.nu_v[i] + fsp.u * (fsp.nu_v[i].dx(i))) * rmsh.dx \
-#       - bgeo.facet_normal[i] * fsp.u * fsp.nu_v[i] * rmsh.ds
-# F_u = (fsp.v[i] * (fsp.nu_u.dx(i)) + fsp.f * fsp.nu_u) * rmsh.dx \
-#       - bgeo.facet_normal[i] * fsp.v_exact[i] * fsp.nu_u * rmsh.ds
-#
-# F = F_u + F_v
-
-
-
-#define Difichlet boundary conditions
-bc_u = DirichletBC( fsp.Q.sub( 0 ), fsp.u_exact, rmsh.boundary )
+# bc_u = DirichletBC(fsp.Q.sub(0), fsp.u_exact, rmsh.boundary)
+# bcs = [bc_u]
 
 #define variational problem
-F_v = (fsp.v[i] * fsp.nu_v[i] + fsp.u * (fsp.nu_v[i].dx( i ))) * rmsh.dx \
-      - bgeo.facet_normal[i] * fsp.u * fsp.nu_v[i] * rmsh.ds
 F_u = (fsp.v[i] * (fsp.nu_u.dx( i )) + fsp.f * fsp.nu_u) * rmsh.dx \
       - bgeo.facet_normal[i] * fsp.v[i] * fsp.nu_u * rmsh.ds
+F_v = (fsp.v[i] * fsp.nu_v[i] + fsp.u * (fsp.nu_v[i].dx( i ))) * rmsh.dx \
+      - bgeo.facet_normal[i] * fsp.u * fsp.nu_v[i] * rmsh.ds
+
 F_N = rpam.parameters['alpha'] / rmsh.r_mesh * (bgeo.facet_normal[i] * fsp.v[i] - bgeo.facet_normal[i] * fsp.v_exact[i]) * bgeo.facet_normal[j] * fsp.nu_v[j] * rmsh.ds
 
 F = F_u + F_v + F_N
