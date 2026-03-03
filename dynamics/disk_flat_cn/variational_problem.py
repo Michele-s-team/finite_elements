7ceb8b7f382107f75e93a38345139435ccc5a024
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-03 15:19:07 +0100

    fixed bug with rho

--- dynamics/disk_flat_cn/variational_problem.py
+++ dynamics/disk_flat_cn/variational_problem.py
@@ -52,21 +52,28 @@ fsp.tau.interpolate(tau_Expression(element=fsp.Q_tau.ufl_element()))
 
 # step 1
 F1 = ( \
-                rpam.parameters['rho'] * (
-                    (fsp.v_[alpha] - fsp.v_n_1[alpha]) / dt \
-                    + (3.0 / 2.0 * fsp.v_n_1[beta] - 1.0 / 2.0 * fsp.v_n_2[beta]) * (fsp.V[alpha]).dx(beta) - fsp.f[alpha]
+                ( 
+                    rpam.parameters['rho'] * (
+                        (fsp.v_[alpha] - fsp.v_n_1[alpha]) / dt \
+                        + (3.0 / 2.0 * fsp.v_n_1[beta] - 1.0 / 2.0 * fsp.v_n_2[beta]) * (fsp.V[alpha]).dx(beta)
+                    ) 
+                    - fsp.f[alpha]
                     ) * fsp.nu_v_[alpha] \
                 +  flu.sigma(fsp.V, fsp.sigma_n_32, rpam.parameters['mu'])[alpha, beta] * fsp.nu_v_[alpha].dx(beta)
       ) * rmsh.dx \
       - (fsp.tau[alpha] * fsp.nu_v_[alpha]) * rmsh.ds
 
 # step 2
-F2_phi = ((fsp.phi.dx(alpha)) * (fsp.nu_phi.dx(alpha)) + (rpam.parameters['rho'] / dt) * ((fsp.v_)[alpha].dx(alpha)) * fsp.nu_phi) * rmsh.dx \
+F2_phi = (
+            (fsp.phi.dx(alpha)) * (fsp.nu_phi.dx(alpha)) + (rpam.parameters['rho'] / dt) * ((fsp.v_)[alpha].dx(alpha)) * fsp.nu_phi
+        ) * rmsh.dx \
         - ( bgeo.facet_normal[alpha] * (fsp.phi.dx(alpha)) * fsp.nu_phi) * rmsh.ds
+
 F2_omega = (fsp.omega[alpha] - fsp.phi.dx(alpha)) * (fsp.nu_omega[alpha] - fsp.nu_phi.dx(alpha)) * rmsh.dx
 
 F2_N = rpam.parameters['alpha'] / rmsh.r_mesh * (
-    fsp.phi - rpam.parameters['mu'] * bgeo.facet_normal[alpha] * bgeo.facet_normal[beta] * (fsp.v_[alpha] - fsp.v_n_2[alpha] - dt/rpam.parameters['rho'] * fsp.omega[alpha]).dx(beta)) * \
+        fsp.phi - rpam.parameters['mu'] * bgeo.facet_normal[alpha] * bgeo.facet_normal[beta] * (fsp.v_[alpha] - fsp.v_n_2[alpha] - dt/rpam.parameters['rho'] * fsp.omega[alpha]).dx(beta)
+    ) * \
     (
         fsp.nu_phi + rpam.parameters['mu'] * dt / rpam.parameters['rho'] *  bgeo.facet_normal[gamma] * bgeo.facet_normal[delta] * fsp.nu_omega[gamma].dx(delta)
     ) * rmsh.ds
@@ -74,4 +81,4 @@ F2_N = rpam.parameters['alpha'] / rmsh.r_mesh * (
 F2 = (F2_phi + F2_omega) + F2_N
 
 # step 3
-F3 = (((fsp.v_n[alpha] - fsp.v_[alpha]) + (dt / rpam.parameters['rho']) * (fsp.phi.dx(alpha))) * fsp.nu_v_n[alpha]) * rmsh.dx
+F3 = (fsp.v_n[alpha] - fsp.v_[alpha] + (dt / rpam.parameters['rho']) * (fsp.phi.dx(alpha))) * fsp.nu_v_n[alpha] * rmsh.dx
