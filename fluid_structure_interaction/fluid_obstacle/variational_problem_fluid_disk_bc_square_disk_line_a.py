136304452b7f4050f5db0842c18904fce782003f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-10 10:57:57 +0100

    Update variational_problem_fluid_disk_bc_square_disk_line_a.py

--- fluid_structure_interaction/fluid_obstacle/variational_problem_fluid_disk_bc_square_disk_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/variational_problem_fluid_disk_bc_square_disk_line_a.py
@@ -63,7 +63,6 @@ F_v_disk__ = (
             ) * ela.detF(fsp.u_n_1_di) * rmsh.dx_sub_mesh[0][0] \
        - ( flu.sigma_ale(fsp.v_square_n_1_0_1_on_0_0, fsp.sigma_square_n_32_0_1_on_0_0, fsp.u_n_1_di, rpam.parameters['eta_sq'])[alpha, beta] * ela.G(fsp.u_n_1_di)[gamma, beta] * bgeo.sub_mesh_facet_normal[0][0][gamma] + 1.0 / ela.detF(fsp.u_n_1_di) * f_M(fsp.c_n_1, fsp.U_n_32)[alpha] ) * fsp.nu_v_disk__[alpha] *  ela.detF(fsp.u_n_1_di) * rmsh.ds_sub_mesh[0][0]['ds'] 
 
-# check
 
 
 # step 2 for phi_disk
@@ -73,6 +72,7 @@ F_phi_disk = ( \
             ) * ela.detF(fsp.u_n_1_di) * rmsh.dx_sub_mesh[0][0] \
         + (ela.G(fsp.u_n_1_di)[delta, alpha] * bgeo.sub_mesh_facet_normal[0][0][delta] * ela.G(fsp.u_n_1_di)[beta, alpha] * (fsp.phi_disk.dx(beta)) * fsp.nu_phi_disk) * ela.detF(fsp.u_n_1_di) * rmsh.ds_sub_mesh[0][0]['ds'] 
 
+# check
 
 
 F_omega_disk = ( fsp.omega_disk[alpha] - ela.G(fsp.u_n_1_di)[beta, alpha] * fsp.phi_disk.dx(beta) ) * ( fsp.nu_omega_disk[alpha] - ela.G(fsp.u_n_1_di)[gamma, alpha] * fsp.nu_phi_disk.dx(gamma) ) * ela.detF(fsp.u_n_1_di) * rmsh.dx_sub_mesh[0][0]
