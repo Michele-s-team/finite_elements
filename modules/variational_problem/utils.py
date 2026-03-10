9e86e79dbc718a5999c9fb2fd0f89e102e0de0b2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-10 23:13:36 +0100

    solve_vp

--- modules/variational_problem/utils.py
+++ modules/variational_problem/utils.py
@@ -2,7 +2,32 @@
 this module contains methods used to handle variational problems
 '''
 
+from fenics import *
 
-def solve_vp():
 
-    
+'''
+set up and solve a variational problem
+Input values: 
+    * Mandatory: 
+        - 'F': the variational functional
+        - 'u': the function to solve for
+        - 'J': the Jacobian
+    * Optional:
+        - 'params': a set of parameters for the solver, such as 
+            params = {'nonlinear_solver': 'newton',
+            'newton_solver':
+                ...
+            }
+            'params' is None by default, and if it is !=None, the solver is initialized with parameters 'params'
+'''
+
+def solve_vp(F, u, bcs, J, params=None):
+
+    J_u_sq = derivative(F, u, J)
+    problem_u_sq = NonlinearVariationalProblem(F, u, bcs, J_u_sq)
+    solver_u_sq = NonlinearVariationalSolver(problem_u_sq)
+
+    if params != None:
+        solver_u_sq.parameters.update(params)
+
+    solver_u_sq.solve()
