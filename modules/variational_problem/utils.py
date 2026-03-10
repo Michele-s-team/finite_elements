ac32a30ddf9161a88f6da9ce9ba0f3e697913a65
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-10 23:35:13 +0100

    ...

--- modules/variational_problem/utils.py
+++ modules/variational_problem/utils.py
@@ -13,21 +13,21 @@ Input values:
         - 'u': the function to solve for
         - 'J': the Jacobian
     * Optional:
-        - 'params': a set of parameters for the solver, such as 
-            params = {'nonlinear_solver': 'newton',
+        - 'parameters': a set of parameters for the solver, such as 
+            parameters = {'nonlinear_solver': 'newton',
             'newton_solver':
                 ...
             }
-            'params' is None by default, and if it is !=None, the solver is initialized with parameters 'params'
+            'parameters' is None by default, and if it is !=None, the solver is initialized with parameters 'parameters'
 '''
 
-def solve_vp(F, u, bcs, J, params=None):
+def solve_vp(F, u, bcs, J, parameters=None):
 
     J_der = derivative(F, u, J)
     variational_problem = NonlinearVariationalProblem(F, u, bcs, J_der)
     solver = NonlinearVariationalSolver(variational_problem)
 
-    if params != None:
-        solver.parameters.update(params)
+    if parameters != None:
+        solver.parameters.update(parameters)
 
     solver.solve()
