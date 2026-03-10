be4eeddc4c6bc7e0fc4834a7ceb4a90ac36dca90
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-10 14:01:26 +0100

    Update solve.py

--- fluid_structure_interaction/fluid_obstacle/solve.py
+++ fluid_structure_interaction/fluid_obstacle/solve.py
@@ -104,8 +104,6 @@ io.full_print(fsp.ys, 'ys', \
               lmsh.mesh[1], 'vector')
 
 
-bgeo.facet_normal_smooth(lmsh.sub_meshes[0][0])
-
 
 # FILL IN HERE: set the initial profiles from analytical expressions
 
@@ -122,7 +120,7 @@ class v_sq_expression(UserExpression):
 fsp.v_square_n_1.interpolate(v_sq_expression(element=fsp.Q_v_square.ufl_element()))
 '''
 
-'''
+
 class sigma_di_0_expression(UserExpression):
     def eval(self, values, x):
         values[0] = rpam.parameters['rho_di'] * rpam.parameters['g'] * (x[1] - lmsh.parameters['h'])
@@ -372,4 +370,3 @@ for n in range(rpam.parameters['N']):
     print("\t%.2f %%" % (100.0 * (t / rpam.parameters['T'])), flush=True)
 
 print("... done.", flush=True)
-'''
