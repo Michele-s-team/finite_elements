fa5f4114dd626becc5ba30ac55e992d29dc0b506
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-14 18:37:16 +0100

    setting up

---
+++ poisson_equation/solve_u/variational_problem_bc_square_polygon.py
@@ -0,0 +1,77 @@
+from fenics import *
+import importlib
+import ufl as ufl
+
+import differential_geometry.boundary.geometry as bgeo
+import function_spaces as fsp
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+
+i, j = ufl.indices(2)
+
+
+class u_exact_expression(UserExpression):
+    def eval(self, values, x):
+        # test case 1
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
+
+    def value_shape(self):
+        return (1,)
+
+
+class grad_u_expression(UserExpression):
+    def eval(self, values, x):
+        # test case 1
+        values[0] = 2.0 * x[0]
+        values[1] = 4.0 * x[1]
+
+    def value_shape(self):
+        return (2,)
+
+
+class laplacian_u_expression(UserExpression):
+    def eval(self, values, x):
+        # test case 1
+        values[0] = 6.0
+
+    def value_shape(self):
+        return (1,)
+
+
+class hess_u_exact_expression(UserExpression):
+    def init(self, **kwargs):
+        super().init(**kwargs)
+
+    def eval(self, values, x):
+        # test case 1
+        values[0] = 2
+        values[1] = 0
+        values[2] = 0
+        values[3] = 4
+
+    def value_shape(self):
+        return (2, 2)
+
+
+fsp.u_exact.interpolate(u_exact_expression(element=fsp.Q.ufl_element()))
+fsp.grad_u.interpolate(grad_u_expression(element=fsp.V.ufl_element()))
+fsp.f.interpolate(laplacian_u_expression(element=fsp.Q.ufl_element()))
+
+fsp.hess_u_exact.interpolate(hess_u_exact_expression(element=fsp.T.ufl_element()))
+
+
+bc_u_t = DirichletBC(fsp.Q, fsp.u_exact, rmsh.mf, rmsh.parameters['line_t_id'])
+bc_u_b = DirichletBC(fsp.Q, fsp.u_exact, rmsh.mf, rmsh.parameters['line_b_id'])
+bc_u_polygon = DirichletBC(fsp.Q, fsp.u_exact, rmsh.mf, rmsh.parameters['polygon_id'])
+bcs = [bc_u_t, bc_u_b, bc_u_polygon]
+
+# variational functional for the original problem (poisson equation)
+F = (dot(grad(fsp.u), grad(fsp.nu_u)) + fsp.f * fsp.nu_u) * rmsh.dx \
+    - bgeo.facet_normal[i] * fsp.grad_u[i] * fsp.nu_u * rmsh.ds_lr \
+    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_tb \
+    - bgeo.facet_normal[i] * (fsp.u.dx(i)) * fsp.nu_u * rmsh.ds_polygon
+
+# variational functional for post-processing problem (pp) to obtain the hessian (hess)
+F_pp = (fsp.hess_u[i, j] * fsp.nu_hess_u[i, j] + (fsp.u.dx(j)) * ((fsp.nu_hess_u[i, j]).dx(i))) * rmsh.dx \
+       - (bgeo.facet_normal[i] * (fsp.u.dx(j)) * fsp.nu_hess_u[i, j]) * rmsh.ds
