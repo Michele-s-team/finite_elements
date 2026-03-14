ca778ad347e7c1ea1370f2f33eedb3d4af083e2d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-15 00:01:02 +0100

    mesh generation from solve.py works

--- fluid_structure_interaction/rigid_obstacle/remesh/solve.py
+++ fluid_structure_interaction/rigid_obstacle/remesh/solve.py
@@ -6,7 +6,7 @@ run with:
     rm -r solution; mkdir solution; python3 solve.py [path where to read the mesh] [path where to store the solution]
 
 Examples:
-    MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/ellipse/solution"; SOLUTION_PATH="/home/fenics/shared/fluid_structure_interaction/rigid_obstacle/remesh/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_ellipse $MESH_PATH $SOLUTION_PATH
+    clear; clear; MESH_PARAMETERS_PATH="/home/fenics/shared/generate_mesh/2d/square/polygon"; SOLUTION_PATH="/home/fenics/shared/fluid_structure_interaction/rigid_obstacle/remesh/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_ellipse $MESH_PARAMETERS_PATH $SOLUTION_PATH
 
 Note that all sections of the code which need to be changed when an external parameter (e.g., the inflow velocity, the length of the rectangle, etc...) is changed are bracketed by
 #CHANGE PARAMETERS HERE
@@ -15,19 +15,24 @@ Note that all sections of the code which need to be changed when an external par
 import dolfin
 from fenics import *
 import importlib
+import numpy as np
+import os
+import shutil
 import sys
 
 # add the path where to find the shared modules
 module_path = '/home/fenics/shared/modules'
 sys.path.append(module_path)
 
-import function_spaces as fsp
+# import function_spaces as fsp
+import input_output as io
+import mesh.utils as msh
 import parameters.read.solution as rpam
 import runtime_arguments as rarg
 import switch_problem as swi
 import variational_problem.utils as var_pr
 
-import print_out_solution as pr_sol
+# import print_out_solution as pr_sol
 
 dt = rpam.parameters["T"] / rpam.parameters["num_steps"]  # time step size
 
@@ -43,13 +48,43 @@ params = {'nonlinear_solver': 'newton',
               }
           }
 
+
+######
+mesh_parameters = io.read_parameters_from_csv_file(os.path.join(rarg.args.input_directory, 'mesh_parameters.csv')) 
+mesh_path = os.path.join(rarg.args.input_directory, 'solution')
+
+focus = np.subtract(mesh_parameters["c"], [np.sqrt(mesh_parameters["a"] ** 2 - mesh_parameters["b"] ** 2), 0])
+print(f'list(focus) = {list(focus)}')
+
+# trace the coordinates of polygon vertices
+polygon_coordinates = []
+for i in range(mesh_parameters['N']-1):
+    polygon_coordinates.append(
+        list(np.add(
+            mesh_parameters['c'],
+            [mesh_parameters['a'] * np.cos(2.0*np.pi*i/(mesh_parameters['N']-1)),
+            mesh_parameters['b'] * np.sin(2.0*np.pi*i/(mesh_parameters['N']-1))] 
+            ))
+    )
+print(f'polygon_coordinates = {polygon_coordinates}')
+
+# generate the mesh with the polygon
+if os.path.exists(mesh_path):
+    shutil.rmtree(mesh_path)
+os.mkdir(mesh_path)
+msh.generate_square_polygon_mesh(polygon_coordinates, rarg.args.input_directory, mesh_path)
+
+###### 
+
+
+'''
 # initialize values
 fsp.theta_n = rpam.parameters["theta_0"]
 fsp.omega_n = rpam.parameters["omega_0"]
 fsp.theta_n_1 = rpam.parameters["theta_0"]
 fsp.omega_n_1 = rpam.parameters["omega_0"]
 
-'''
+
 rmsh = importlib.import_module(swi.rmsh)
 ap_ellipse = importlib.import_module(swi.ap_ellipse)
 vp_fluid = importlib.import_module(swi.vp_fluid)
