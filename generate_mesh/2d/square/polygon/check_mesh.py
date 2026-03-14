4432e0630621f4803609d2550bfb88a543c8db00
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-14 11:46:16 +0100

    check mesh square polygon

--- generate_mesh/2d/square/polygon/check_mesh.py
+++ generate_mesh/2d/square/polygon/check_mesh.py
@@ -3,8 +3,9 @@ This code checks the mesh generated from generate_mesh.py
 
 Run with
     clear; clear; python3 check_mesh.py [path where to find the mesh] [path where to write the result of the check]
+
 Example:
-    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/ellipse/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/ellipse/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
+    clear; clear; MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/polygon/solution"; CHECK_PATH="/home/fenics/shared/generate_mesh/2d/square/polygon/check"; rm -rf $CHECK_PATH; mkdir $CHECK_PATH; python3 check_mesh.py $MESH_PATH $CHECK_PATH
 '''
 
 import importlib
@@ -16,8 +17,6 @@ sys.path.append(module_path)
 
 import mesh.load as lmsh
 import mesh.utils as msh
-rmsh = importlib.import_module('mesh.read.square_ellipse')
+rmsh = importlib.import_module('mesh.read.square_polygon')
 
-import mesh.check_tags.square_ellipse
-
-msh.check_mesh_symmetry(lmsh.mesh, rmsh.parameters["c"][:2])
+import mesh.check_tags.square_polygon
