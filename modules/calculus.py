5a1fa3ffa2e2b11affd3dd567b0974789921b2df
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-15 15:43:39 +0100

    Update calculus.py

--- modules/calculus.py
+++ modules/calculus.py
@@ -87,15 +87,15 @@ def circle_arc(r, cr, theta_min, theta_max, t):
             (r * (theta_max - theta_min) * np.array([- np.sin(theta_t), np.cos(theta_t)])).tolist()]
 
 '''
-an ellipse arc
+the arc of an ellipse rotated about the x axis about its center
 Input values:
-- 'a', 'b': the ellipse major and minor axes
-- 'c': the ellipse center (an array of two points)
-- 'phi': the angle by which the major axis is rotated with respect to the x axis
-- 'theta_min', 'theta_max': the minimal and maxmimal values of the polar angles of the arg, respectively
-- 't' : the parametric coordinate of the ellipse arc, 0<=t<1
+    - 'a', 'b': the ellipse major and minor axes
+    - 'c': the ellipse center (an array of two points)
+    - 'phi': the angle by which the major axis is rotated with respect to the x axis
+    - 'theta_min', 'theta_max': the minimal and maxmimal values of the polar angles of the arg, respectively
+    - 't' : the parametric coordinate of the ellipse arc, 0<=t<1
 Return values:
-- the curve position and derivative: [x[0](t), x[1](t)], [x[0]'(t), x[1]'(t)]
+    - the curve position and derivative: [x[0](t), x[1](t)], [x[0]'(t), x[1]'(t)]
 '''
 
 def ellipse_arc(a, b, c, phi, theta_min, theta_max, t):
@@ -106,14 +106,14 @@ def ellipse_arc(a, b, c, phi, theta_min, theta_max, t):
 
 
 '''
-an ellipse
+an ellipse rotated about the x axis about its center
 Input values:
-- 'a', 'b': the ellipse major and minor axes
-- 'c': the ellipse center (an array of two points)
-- 'phi': the angle by which the major axis is rotated with respect to the x axis
-- 't' : the parametric coordinate of the ellipse, 0<=t<1
+    - 'a', 'b': the ellipse major and minor axes
+    - 'c': the ellipse center (an array of two points)
+    - 'phi': the angle by which the major axis is rotated with respect to the x axis
+    - 't' : the parametric coordinate of the ellipse, 0<=t<1
 Return values:
-- the curve position and derivative: [x[0](t), x[1](t)], [x[0]'(t), x[1]'(t)]
+    - the curve position and derivative: [x[0](t), x[1](t)], [x[0]'(t), x[1]'(t)]
 '''
 
 def ellipse(a, b, c, phi, t):
