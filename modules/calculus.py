fixed bug in ellipse derivative

modules/calculus.py
@@ -108,7 +108,7 @@ def ellipse_arc(a, b, c, theta_min, theta_max, t, phi=0):
 
     return [
         np.add(f, np.dot(R(phi), np.subtract(np.add(c, [a * np.cos(theta_t), b * np.sin(theta_t)]), f) ) ).tolist(),
-        ((theta_max - theta_min) * np.dot(R(phi), np.subtract(np.add(c, [- a * np.sin(theta_t), b * np.cos(theta_t)]), f ) )).tolist() 
+        ((theta_max - theta_min) * np.dot(R(phi), [- a * np.sin(theta_t), b * np.cos(theta_t)] )).tolist() 
             ]
 
 
@@ -224,15 +224,16 @@ def curve_integral_circle(f, r, c):
 return the curve integral of a function  along an ellipse rotated with respect to the x axis about its left focal point
 Input values:
     * Mandatory:
-    - 'f': the function f(x[0], x[1])
-    - 'a', 'b': the ellipse minor and major axes
-    - 'c': the circle center (an array of two points)
+        - 'f': the function f(x[0], x[1])
+        - 'a', 'b': the ellipse minor and major axes
+        - 'c': the ellipse center (an array of two points)
     * Optional:
         - 'phi': the angle by which the major axis is rotated with respect to the x axis about the left focal point of the ellipse, it is 0 by default
 Return values: 
     - \int_ellipse f dl
 '''
 def curve_integral_ellipse(f, a, b, c, phi=0):
+
     ellipse_curve = lambda t: ellipse(a, b, c, t, phi)
     return curve_integral(f, ellipse_curve)
 

modules/mesh/check_tags/square_ellipse.py
@@ -28,9 +28,8 @@ integral_exact_ds_b = cal.curve_integral_line(tf.function_test_integrals, [0, 0]
 
 integral_exact_ds_ellipse = cal.curve_integral_ellipse(tf.function_test_integrals,
                                                        rmsh.parameters["a"], rmsh.parameters["b"],
-                                                       # the ellipse here is rotated about its focal point by phi, thus its center is the following
-                                                       np.add(rmsh.focus, np.dot(cal.R_z(rmsh.parameters["phi"]), np.subtract(rmsh.parameters["c"], rmsh.focus)))[:2],
-                                                       rmsh.parameters["phi"])
+                                                       rmsh.parameters["c"][:2],
+                                                       phi=rmsh.parameters["phi"])
 
 integral_exact_ds_lr = integral_exact_ds_l + integral_exact_ds_r
 integral_exact_ds_tb = integral_exact_ds_t + integral_exact_ds_b