34766384022b6d0e7f7fbd34a7fbfbe3022b57c7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-15 15:33:22 +0100

    Update algebraic_problem_polygon_bc_square_polygon.py

--- fluid_structure_interaction/rigid_obstacle/remesh/algebraic_problem_polygon_bc_square_polygon.py
+++ fluid_structure_interaction/rigid_obstacle/remesh/algebraic_problem_polygon_bc_square_polygon.py
@@ -25,6 +25,8 @@ dt = rpam.parameters["T"] / rpam.parameters["num_steps"]  # time step size
 
 focus = np.subtract(rmsh.parameters["c"], [np.sqrt(rmsh.parameters["a"] ** 2 - rmsh.parameters["b"] ** 2), 0])
 
+
+
 i, j, k, l, m, n = ufl.indices(6)
 
 
