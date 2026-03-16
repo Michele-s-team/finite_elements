92fef814371b4f3f324be9e0e04b9914646c91d6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-16 09:50:29 +0100

    Update square_polygon.py

--- modules/mesh/read/square_polygon.py
+++ modules/mesh/read/square_polygon.py
@@ -33,7 +33,7 @@ else:
     #  the polygon shape is a given, parameteric geometrical shape, and it is provided in terms of the parameters of this shape
 
     if parameters['polygon_format'] == 'ellipse':
-    # the polygon shape is an ellipse -> obtain polygon_cordinates from the ellipse parameters
+    # the polygon shape is an ellipse -> obtain polygon_coordinates from the ellipse parameters
         print('The polygon shape is an ellipse.')
         polygon_coordinates = cal.points_ellipse(parameters['a'], parameters['b'], parameters['c'], parameters['N'],
                                                  phi=parameters['phi'])
