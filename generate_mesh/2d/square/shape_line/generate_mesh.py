b9fa9b86f111e7a90cedb2dd3ab63377a7b5e457
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-20 12:48:13 +0100

    polygon -> shape

--- generate_mesh/2d/square/shape_line/generate_mesh.py
+++ generate_mesh/2d/square/shape_line/generate_mesh.py
@@ -1,5 +1,5 @@
 '''
-This code generates a 2d mesh (A) given by a square with a polygonal shape (the polygon is meshed inside), plus a one-dimensional mesh (B) given by a line. The line mesh B corresponds to the polygon boundary of A, stretched on a line. 
+This code generates a 2d mesh (A) given by a square with an arbitrary shape (the shape is meshed inside), plus a one-dimensional mesh (B) given by a line. The line mesh B corresponds to the shape boundary of A, stretched on a line. 
 
 Here 
     - A is mesh_0
@@ -14,7 +14,7 @@ and B has no sub_meshes
 Run it with
     python3 generate_mesh.py [path where to read parameters] [output directory]
 Example:
-    clear; clear; PARAMETERS_PATH="/home/fenics/shared/generate_mesh/2d/square/shape/"; SOLUTION_PATH="/home/fenics/shared/generate_mesh/2d/square/shape/solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh.py $PARAMETERS_PATH $SOLUTION_PATH
+    clear; clear; PARAMETERS_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/"; SOLUTION_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; rm -rf $SOLUTION_PATH; mkdir $SOLUTION_PATH; python3 generate_mesh.py $PARAMETERS_PATH $SOLUTION_PATH
 '''
 
 import colorama as col
@@ -37,14 +37,14 @@ import parameters.read.mesh as rpam
 
 shape_parametric_form = io.read_function_expresssion(rpam.parameters['shape_parametric_form'])
 
-polygon_coordinates = []
+shape_coordinates = []
 for i in range(rpam.parameters['N']):
 
     coordinate = shape_parametric_form(i/rpam.parameters['N'])
 
     if cal.point_in_box(coordinate, [[0, rpam.parameters['L']], [0, rpam.parameters['h']]]):
 
-        polygon_coordinates.append(shape_parametric_form(i/rpam.parameters['N']))
+        shape_coordinates.append(shape_parametric_form(i/rpam.parameters['N']))
 
     else: 
 
@@ -53,7 +53,7 @@ for i in range(rpam.parameters['N']):
 
 
 
-print(f'polygon_coordinates = {polygon_coordinates}')
+print(f'shape_coordinates = {shape_coordinates}')
 
 # mesh A will be stored in output_directory_square_mesh
 output_directory_mesh_0 = io.add_trailing_slash(os.path.join(rarg.args.output_directory, 'mesh_0'))
@@ -64,8 +64,8 @@ os.mkdir(output_directory_mesh_1)
 
 mesh_0_file = os.path.join(output_directory_mesh_0, "mesh.msh")
 
-# total length of the polygon boundary
-polygon_length = cal.polygon_length(polygon_coordinates)
+# total length of the shape boundary
+shape_length = cal.polygon_length(shape_coordinates)
 
 #write metadata for ensemble mesh
 mesh_metadata = rpam.parameters.copy()
@@ -76,7 +76,7 @@ mesh_0_metadata['L'] = rpam.parameters['L']
 mesh_0_metadata['h'] = rpam.parameters['h']
 mesh_0_metadata['resolution'] = rpam.parameters['resolution']
 mesh_0_metadata['n_sub_meshes'] = rpam.parameters['n_sub_meshes_0']
-mesh_0_metadata['polygon_coordinates'] = polygon_coordinates
+mesh_0_metadata['shape_coordinates'] = shape_coordinates
 
 mesh_0_metadata['sub_mesh_0_dim'] = rpam.parameters['sub_mesh_0_0_dim']
 mesh_0_metadata['sub_mesh_1_dim'] = rpam.parameters['sub_mesh_0_1_dim']
@@ -88,7 +88,7 @@ mesh_0_metadata['line_l_id'] = rpam.parameters['line_l_id']
 mesh_0_metadata['line_r_id'] = rpam.parameters['line_r_id']
 mesh_0_metadata['line_t_id'] = rpam.parameters['line_t_id']
 mesh_0_metadata['line_b_id'] = rpam.parameters['line_b_id']
-mesh_0_metadata['polygon_id'] = rpam.parameters['polygon_id']
+mesh_0_metadata['shape_id'] = rpam.parameters['shape_id']
 
 mesh_0_metadata['file_format'] = 'xdmf'
 
@@ -96,16 +96,16 @@ mesh_0_metadata['file_format'] = 'xdmf'
 # write metadata for mesh 1
 mesh_1_metadata = {}
 
-mesh_1_metadata['L'] = polygon_length
+mesh_1_metadata['L'] = shape_length
 mesh_1_metadata['x_l'] = 0
 mesh_1_metadata['x_r'] = mesh_1_metadata['L']
 mesh_1_metadata['N'] = rpam.parameters['N']
-mesh_1_metadata['polygon_coordinates'] = polygon_coordinates
+mesh_1_metadata['shape_coordinates'] = shape_coordinates
 
 
 mesh_1_metadata['vertex_l_id'] = rpam.parameters['vertex_l_id']
 mesh_1_metadata['vertex_r_id'] = rpam.parameters['vertex_r_id']
-mesh_1_metadata['line_id'] = rpam.parameters['polygon_id']
+mesh_1_metadata['line_id'] = rpam.parameters['shape_id']
 
 mesh_1_metadata['file_format'] = 'h5'
 
@@ -134,43 +134,43 @@ square_loop = gmsh.model.geo.addCurveLoop([square_line_b, square_line_r, square_
 gmsh.model.geo.synchronize()
 
 
-#2. add polygon
+#2. add shape
 
 
-polygon_points = [gmsh.model.geo.addPoint(polygon_coordinates[0][0], polygon_coordinates[0][1], 0)]
+shape_points = [gmsh.model.geo.addPoint(shape_coordinates[0][0], shape_coordinates[0][1], 0)]
 gmsh.model.geo.synchronize()
 
-polygon_lines = []
+shape_lines = []
 
-print(f'Added point with coordinates {polygon_coordinates[-1]}')
+print(f'Added point with coordinates {shape_coordinates[-1]}')
 
-print("Starting loop over polygon ... ")
+print("Starting loop over shape ... ")
 
 for i in range(1, rpam.parameters['N']):
 
-    polygon_points.append(gmsh.model.geo.addPoint(polygon_coordinates[i][0], polygon_coordinates[i][1], 0))
+    shape_points.append(gmsh.model.geo.addPoint(shape_coordinates[i][0], shape_coordinates[i][1], 0))
     gmsh.model.geo.synchronize()
 
-    polygon_lines.append(gmsh.model.geo.addLine(polygon_points[-2], polygon_points[-1]))
+    shape_lines.append(gmsh.model.geo.addLine(shape_points[-2], shape_points[-1]))
     gmsh.model.geo.synchronize()
 
 print("... done.")
 
-polygon_lines.append(gmsh.model.geo.addLine(polygon_points[-1], polygon_points[0]))
+shape_lines.append(gmsh.model.geo.addLine(shape_points[-1], shape_points[0]))
 gmsh.model.geo.synchronize()
 
 
 
-polygon_loop = gmsh.model.geo.addCurveLoop(polygon_lines)
+shape_loop = gmsh.model.geo.addCurveLoop(shape_lines)
 gmsh.model.geo.synchronize()
 
-square_minus_polygon_surface = gmsh.model.geo.addPlaneSurface([square_loop, polygon_loop])
+square_minus_shape_surface = gmsh.model.geo.addPlaneSurface([square_loop, shape_loop])
 gmsh.model.geo.synchronize()
 
-gmsh.model.mesh.embed(1, polygon_lines, 2, square_minus_polygon_surface)
+gmsh.model.mesh.embed(1, shape_lines, 2, square_minus_shape_surface)
 gmsh.model.geo.synchronize()
 
-polygon_surface = gmsh.model.geo.addPlaneSurface([polygon_loop])
+shape_surface = gmsh.model.geo.addPlaneSurface([shape_loop])
 gmsh.model.geo.synchronize()
 
 
@@ -184,23 +184,23 @@ msh.tag_physical_object(lines[1], rpam.parameters['line_r_id'], gmsh.model, 'lin
 msh.tag_physical_object(lines[2], rpam.parameters['line_t_id'], gmsh.model, 'line_t')
 msh.tag_physical_object(lines[3], rpam.parameters['line_l_id'], gmsh.model, 'line_l')
 
-#add polygon lines
-msh.tag_physical_object([lines[i] for i in range(4, 4 + rpam.parameters['N'])], rpam.parameters['polygon_id'], gmsh.model, 'polygon_loop')
+#add shape lines
+msh.tag_physical_object([lines[i] for i in range(4, 4 + rpam.parameters['N'])], rpam.parameters['shape_id'], gmsh.model, 'shape_loop')
 
 
 
 # add 2-dimensional objects
 surfaces = gmsh.model.getEntities(dim=2)
 
-msh.tag_physical_object(surfaces[0], rpam.parameters['sub_mesh_0_1_id'], gmsh.model, 'square_minus_polygon_surface')
-msh.tag_physical_object(surfaces[1], rpam.parameters['sub_mesh_0_0_id'], gmsh.model, 'polygon_surface')
+msh.tag_physical_object(surfaces[0], rpam.parameters['sub_mesh_0_1_id'], gmsh.model, 'square_minus_shape_surface')
+msh.tag_physical_object(surfaces[1], rpam.parameters['sub_mesh_0_0_id'], gmsh.model, 'shape_surface')
 
 
 # set the resolution
 # se resolution equal to parameters["resolution"] at a distance 0 from surface_in, and  at distance max(rpam.parameters["L"],rpam.parameters["h"]) from sub_mesh_0_1_id
 distance = gmsh.model.mesh.field.add("Distance")
 
-gmsh.model.mesh.field.setNumbers(distance, "FacesList", [polygon_loop])
+gmsh.model.mesh.field.setNumbers(distance, "FacesList", [shape_loop])
 
 threshold = gmsh.model.mesh.field.add("Threshold")
 gmsh.model.mesh.field.setNumber(threshold, "IField", distance)
@@ -225,42 +225,49 @@ msh.generate_sub_mesh(output_directory_mesh_0, os.path.join(output_directory_mes
 msh.generate_sub_mesh(output_directory_mesh_0, os.path.join(output_directory_mesh_0, 'sub_meshes', 'sub_mesh_1'), rpam.parameters["sub_mesh_0_1_id"])
 
 
-# print the boundary points of the boundary given by the polygon
+# print the boundary points of the boundary given by the shape
 msh.sorted_boundary_points(
     msh.read_mesh(os.path.join(output_directory_mesh_0, 'triangle_mesh.xdmf')), 
     output_directory_mesh_0, 
-    [rpam.parameters['polygon_id']],
-    os.path.join(output_directory_mesh_0, 'boundary_points_id_' + str(rpam.parameters['polygon_id']) + '.csv'))
+    [rpam.parameters['shape_id']],
+    os.path.join(output_directory_mesh_0, 'boundary_points_id_' + str(rpam.parameters['shape_id']) + '.csv'))
 
 
 # check that the number of mesh vertices on the circle matches N and if it does not, abort. 
 mesh_0 = msh.read_mesh(os.path.join(output_directory_mesh_0, 'triangle_mesh.xdmf'))
 mf_mesh_0 = msh.read_mesh_components(mesh_0, mesh_0.topology().dim() - 1, os.path.join(output_directory_mesh_0, 'line_mesh.xdmf'))
 
-# collect unique vertex indices touched by facets tagged with polygon_id
-polygon_vertex_ids = set()
+# collect unique vertex indices touched by facets tagged with shape_id
+shape_vertex_ids = set()
 
 for facet in facets(mesh_0):
     #run through all facets of mesh_0 
 
-    if mf_mesh_0[facet] == rpam.parameters['polygon_id']:
-        # the facet under consideration belongs to the polygon
+    if mf_mesh_0[facet] == rpam.parameters['shape_id']:
+        # the facet under consideration belongs to the shape
 
         for v in vertices(facet):
-            # run through the vertices of the facet under consideration, and ad them to polygon_vertex_ids
+            # run through the vertices of the facet under consideration, and ad them to shape_vertex_ids
 
-            polygon_vertex_ids.add(v.index())
+            shape_vertex_ids.add(v.index())
 
-n_vertices_on_polygon = len(polygon_vertex_ids)
-print(f'Number of vertices on polygon = {n_vertices_on_polygon}')
+n_vertices_on_shape = len(shape_vertex_ids)
+
+if n_vertices_on_shape != rpam.parameters['N']:
+    # the meshing algorithm has added additional vertices on the shape, while I want the number of vertices on the shape to match N, and thus the number of vertices in the line mesh -> print an error message
+
+    print(f"{col.Fore.RED}{'Error: the number of vertices on shape does not match the number of vertices of the 1d mesh!!! Aborting...'}{col.Style.RESET_ALL}")
+    print(f'Number of vertices on shape = {n_vertices_on_shape}\nNumber of vertices on line = {rpam.parameters["N"]}')
+
+    sys.exit()
 
 
 # B) mesh B (line)
 
 
-# generate the line mesh corresponding to the polygon
-msh.genereate_line_mesh(0, polygon_length, rpam.parameters['N'],
-                        rpam.parameters['polygon_id'], rpam.parameters['vertex_l_id'], rpam.parameters['vertex_r_id'],
+# generate the line mesh corresponding to the shape
+msh.genereate_line_mesh(0, shape_length, rpam.parameters['N'],
+                        rpam.parameters['shape_id'], rpam.parameters['vertex_l_id'], rpam.parameters['vertex_r_id'],
                         x_m=None,
                         vertex_m_id=None,
                         output_directory=output_directory_mesh_1, 
