44b5de2aac1158149d4ee89d8cc767d4a9e33431
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-23 16:30:14 +0100

    Update switch_problem.py

--- poisson_equation/solve_u/three_domains/transfer_test/switch_problem.py
+++ poisson_equation/solve_u/three_domains/transfer_test/switch_problem.py
@@ -2,12 +2,12 @@ import colorama as col
 
 import runtime_arguments as rarg
 
-if rarg.args.problem == 'square_disk_line':
-    rmsh = 'mesh.read.square_disk_line'
-    vp_sub_mesh_0_0 = 'variational_problem_square_disk_line_sub_mesh_0_0'
-    vp_sub_mesh_0_1 = 'variational_problem_square_disk_line_sub_mesh_0_1'
-    vp_mesh_1 = 'variational_problem_square_disk_line_mesh_1'
-    prout_bc = 'print_out_square_disk_line'
+if rarg.args.problem == 'square_shape_line':
+    rmsh = 'mesh.read.square_shape_line'
+    vp_sub_mesh_0_0 = 'variational_problem_square_shape_line_sub_mesh_0_0'
+    vp_sub_mesh_0_1 = 'variational_problem_square_shape_line_sub_mesh_0_1'
+    vp_mesh_1 = 'variational_problem_square_shape_line_mesh_1'
+    prout_bc = 'print_out_square_shape_line'
 
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
