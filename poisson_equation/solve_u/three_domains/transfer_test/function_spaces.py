089313357f94f4a78baf1de3f60fa005246ba798
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-23 16:29:50 +0100

    set up

---
+++ poisson_equation/solve_u/three_domains/transfer_test/function_spaces.py
@@ -0,0 +1,141 @@
+from fenics import *
+import importlib
+
+import mesh.load as lmsh
+import parameters.read.solution as rpam
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+
+'''
+here Q[i][j] is the scalar function space for the j-th submesh of the i-th mesh, and similarly of other spaces
+'''
+
+# This enforces periodic boundary conditions which map the l vertex into the r vertex or mesh 1
+class PeriodicBoundary(SubDomain):
+    # Identify the "target domain": the left vertex
+    def inside(self, x, on_boundary):
+        return near(x[0], lmsh.mesh_parameters[1]['x_l']) and on_boundary
+
+    # Map the other boundaries to the "target domain"
+    def map(self, x, y):
+        if near(x[0], lmsh.mesh_parameters[1]['x_r']):
+            # right vertex → left vertex
+            y[0] = lmsh.mesh_parameters[1]['x_l']
+        else:
+            # Required: set unmapped points to identity
+            y[0] = x[0]
+            
+
+periodic_boundary = PeriodicBoundary()
+
+Q, V, T = [], [], []
+u, nu_u, f, grad_u, J_u, u_exact, hess_u, nu_hess_u, hess_u_exact, J_hess_u = [], [], [], [], [], [], [], [], [], []
+
+for i in range(len(lmsh.mesh)):
+
+    if "n_sub_meshes" not in lmsh.mesh_parameters[i]:
+        # mesh i has no sub-meshes 
+
+        Q.append(FunctionSpace(lmsh.mesh[i], 'P', rpam.parameters['function_space_degree'], constrained_domain=periodic_boundary))
+        V.append(VectorFunctionSpace(lmsh.mesh[i], 'P', rpam.parameters['function_space_degree'], constrained_domain=periodic_boundary))
+        T.append(TensorFunctionSpace(lmsh.mesh[i], 'P', rpam.parameters['function_space_degree'], shape=(lmsh.mesh[i].topology().dim(), lmsh.mesh[i].topology().dim()), constrained_domain=periodic_boundary))
+
+        # Define variational problem
+        u.append(Function(Q[i]))
+        nu_u.append(TestFunction(Q[i]))
+        f.append(Function(Q[i]))
+        grad_u.append(Function(V[i]))
+        J_u.append(TrialFunction(Q[i]))
+        u_exact.append(Function(Q[i]))
+
+        # Define post-processing (pp) variational problem
+        # hess_u is a tensor which is the Hessian matrix of u: hess_u[i, j] = \partial_i \partial_j u
+        hess_u.append(Function(T[i]))
+        nu_hess_u.append(TestFunction(T[i]))
+        hess_u_exact.append(Function(T[i]))
+        J_hess_u.append(TrialFunction(T[i]))
+
+
+    else:
+        # mesh i has sub-meshes -> run through all sub-meshes and define function spaces and fields
+
+        Q.append([])
+        V.append([])
+        T.append([])
+
+        u.append([])
+        nu_u.append([])
+        f.append([])
+        grad_u.append([])
+        J_u.append([])
+        u_exact.append([])
+        hess_u.append([])
+        nu_hess_u.append([])
+        hess_u_exact.append([])
+        J_hess_u.append([])
+
+        for j in range(len(lmsh.sub_meshes[i])):
+
+            Q[i].append(FunctionSpace(lmsh.sub_meshes[i][j], 'P', rpam.parameters['function_space_degree']))
+            V[i].append(VectorFunctionSpace(lmsh.sub_meshes[i][j], 'P', rpam.parameters['function_space_degree']))
+            T[i].append(TensorFunctionSpace(lmsh.sub_meshes[i][j], 'P', rpam.parameters['function_space_degree'], shape=(lmsh.sub_meshes[i][j].topology().dim(), lmsh.sub_meshes[i][j].topology().dim())))
+
+            # Define variational problem
+            u[i].append(Function(Q[i][j]))
+            nu_u[i].append(TestFunction(Q[i][j]))
+            f[i].append(Function(Q[i][j]))
+            grad_u[i].append(Function(V[i][j]))
+            J_u[i].append(TrialFunction(Q[i][j]))
+            u_exact[i].append(Function(Q[i][j]))
+
+            # Define post-processing (pp) variational problem
+            # hess_u is a tensor which is the Hessian matrix of u: hess_u[i, j] = \partial_i \partial_j u
+            hess_u[i].append(Function(T[i][j]))
+            nu_hess_u[i].append(TestFunction(T[i][j]))
+            hess_u_exact[i].append(Function(T[i][j]))
+            J_hess_u[i].append(TrialFunction(T[i][j]))
+
+
+u[0][1].set_allow_extrapolation(True)
+
+
+# a function which allows to bridge between sub_mesh[0][1] and mesh[1], and thus to impose the BCs for problem on mesh[1] in terms of the solution of the problem on sub_mesh[0][1]
+u_0_1_on_1 = Function(Q[1])
+# a function which allows to bridge between mesh[1] and sub_mesh[0][0], and thus to impose the BCs for problem on sub_mesh[0][0] in terms of the solution of the problem on mesh[1]
+u_1_on_0_0 = Function(Q[0][0])
+
+
+#  for testing trasnfer - start
+# scalar
+Q_sub_mesh_0_0 = FunctionSpace(lmsh.sub_meshes[0][0], 'P', rpam.parameters['function_space_degree'])
+Q_sub_mesh_0_1 = FunctionSpace(lmsh.sub_meshes[0][1], 'P', rpam.parameters['function_space_degree'])
+Q_mesh_1 = FunctionSpace(lmsh.mesh[1], 'P', rpam.parameters['function_space_degree'], constrained_domain=periodic_boundary)
+
+f_sub_mesh_0_0 = Function(Q_sub_mesh_0_0)
+f_sub_mesh_0_1 = Function(Q_sub_mesh_0_1)
+f_mesh_1 = Function(Q_mesh_1)
+
+
+# vector
+V_sub_mesh_0_0 = VectorFunctionSpace(lmsh.sub_meshes[0][0], 'P', rpam.parameters['function_space_degree'])
+V_sub_mesh_0_1 = VectorFunctionSpace(lmsh.sub_meshes[0][1], 'P', rpam.parameters['function_space_degree'])
+V_mesh_1 = VectorFunctionSpace(lmsh.mesh[1], 'P', rpam.parameters['function_space_degree'], constrained_domain=periodic_boundary, dim=2)
+
+v_sub_mesh_0_0 = Function(V_sub_mesh_0_0)
+v_sub_mesh_0_1 = Function(V_sub_mesh_0_1)
+v_mesh_1 = Function(V_mesh_1)
+
+
+# tensor
+T_sub_mesh_0_0 = TensorFunctionSpace(lmsh.sub_meshes[0][0], 'P', rpam.parameters['function_space_degree'], shape=(2,3))
+T_sub_mesh_0_1 = TensorFunctionSpace(lmsh.sub_meshes[0][1], 'P', rpam.parameters['function_space_degree'], shape=(2,3))
+T_mesh_1 = TensorFunctionSpace(lmsh.mesh[1], 'P', rpam.parameters['function_space_degree'], constrained_domain=periodic_boundary, shape=(2,3))
+
+t_sub_mesh_0_0 = Function(T_sub_mesh_0_0)
+t_sub_mesh_0_1 = Function(T_sub_mesh_0_1)
+t_mesh_1 = Function(T_mesh_1)
+
+#  for testing trasnfer - end
+
+
---
+++ poisson_equation/solve_u/three_domains/transfer_test/solve.py
@@ -0,0 +1,317 @@
+'''
+This code solves the Poisson equation in two sub_meshes, sub_mesh[0] and sub_mesh[1], which share one boundary
+The problem is first solved in sub_mesh[1], and the solution u[1] is then used to specify the BCs of the problem of sub_mesh[0]
+
+Run with
+    clear; clear; python3 solve.py [name of the variational problem to solve] [path where to read the mesh generated from generate_mesh.py] [path where to store the solution]
+
+Examples:
+     MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/disk_line/solution"; SOLUTION_PATH="/home/fenics/shared/poisson_equation/solve_u/three_domains/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_disk_line $MESH_PATH $SOLUTION_PATH
+ '''
+
+from fenics import *
+import importlib
+import os
+import sys
+
+# add the path where to find the shared modules
+module_path = '/home/fenics/shared/modules'
+sys.path.append(module_path)
+
+import function_spaces as fsp
+import input_output as io
+import mesh.utils as msh
+import runtime_arguments as rarg
+import solution_paths as solpath
+import switch_problem as swi
+import variational_problem.utils as var_pr
+
+rmsh = importlib.import_module(swi.rmsh)
+vp_mesh_0 = ['','']
+
+# set the solver parameters here
+params = {'nonlinear_solver': 'newton',
+          'newton_solver':
+              {
+                  'linear_solver': 'superlu',
+                  'absolute_tolerance': 1e-6,
+                  'relative_tolerance': 1e-6,
+                  'maximum_iterations': 1000000,
+                  'relaxation_parameter': 0.95,
+              }
+          }
+
+
+
+
+
+####################
+# test transfer function
+
+import numpy as np
+'''
+# 1 transfer scalar
+
+# 1.1 transfer from 2d to 1d 
+
+class f_0__Expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = 1 + 3 * x[0] ** 2 + 2 * x[1] ** 2
+
+    def value_shape(self):
+        return (1,)
+    
+fsp.f_sub_mesh_0_1.interpolate(f_0__Expression(element=fsp.Q[0][1].ufl_element()))
+
+msh.transfer_2d_to_1d(fsp.f_sub_mesh_0_1, fsp.f_mesh_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.f_sub_mesh_0_1, f'u_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.f_mesh_1, f'u_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+    
+
+
+
+# 1.2 transfer from 1d  to 2d
+
+# here one needs to choose a periodic analytical expression, because f_mesh_1 is defined on a periodic space Q[1]
+class f_1_Expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = np.cos(2.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+
+    def value_shape(self):
+        return (1,)
+    
+fsp.f_mesh_1.interpolate(f_1_Expression(element=fsp.Q[1].ufl_element()))
+
+
+msh.transfer_1d_to_2d(fsp.f_mesh_1, fsp.f_sub_mesh_0_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.f_mesh_1, f'u_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.f_sub_mesh_0_1, f'u_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+
+'''
+
+# 2 transfer vector
+
+'''
+# 2.1 transfer from 2d to 1d
+class v_sub_mesh_0__Expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = 1 - x[0] + 2 * x[1] ** 2
+        values[1] = 1 - 4 * x[0] ** 2 + 2 * x[1] ** 2
+
+    def value_shape(self):
+        return (2,)
+    
+fsp.v_sub_mesh_0_0.interpolate(v_sub_mesh_0__Expression(element=fsp.V_sub_mesh_0_0.ufl_element()))
+fsp.v_sub_mesh_0_1.interpolate(v_sub_mesh_0__Expression(element=fsp.V_sub_mesh_0_1.ufl_element()))
+
+
+msh.transfer_2d_to_1d(fsp.v_sub_mesh_0_0, fsp.v_mesh_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.v_sub_mesh_0_0, f'v_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.v_mesh_1, f'v_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)    
+
+ 
+
+# 2.2 transfer from 1d  to 2d  
+class v_mesh_1_Expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = np.cos(2.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+        values[1] = np.sin(4.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+
+    def value_shape(self):
+        return (2,)
+    
+fsp.v_mesh_1.interpolate(v_mesh_1_Expression(element=fsp.V_mesh_1.ufl_element()))
+
+msh.transfer_1d_to_2d(fsp.v_mesh_1, fsp.v_sub_mesh_0_0, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.v_mesh_1, f'v_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.v_sub_mesh_0_0, f'v_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+
+'''
+
+# 3 transfer tensor
+'''
+# 3.1 transfer from 2d to 1d
+
+class t_sub_mesh_0__Expression(UserExpression):
+    def init(self, **kwargs):
+        super().init(**kwargs)
+
+    def eval(self, values, x):
+        # test case 1
+        values[0] = np.cos(2* np.pi * (x[0]*x[1]))
+        values[1] = np.sin(2* np.pi * x[0]) - np.sin(2* np.pi * x[1])
+        values[2] = (np.sin(2* np.pi * x[0]) - np.sin(2* np.pi * x[1]))**2
+        values[3] = np.cos(2* np.pi * x[0])**2 - np.sin(2* np.pi * x[1])
+        values[4] = np.cos(2* np.pi * x[0])**3 - np.sin(2* np.pi * (x[0]+x[1]))
+        values[5] = (np.cos(2* np.pi * x[0])**3 - np.sin(2* np.pi * (x[0]+x[1])))**2
+
+    def value_shape(self):
+        return (2, 3)
+    
+
+fsp.t_sub_mesh_0_1.interpolate(t_sub_mesh_0__Expression(element=fsp.T_sub_mesh_0_1.ufl_element()))
+
+
+msh.transfer_2d_to_1d(fsp.t_sub_mesh_0_1, fsp.t_mesh_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.t_sub_mesh_0_1, f't_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.t_mesh_1, f't_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path) 
+
+'''
+'''
+# 3.2 transfer from line 1d to 2d
+class t_mesh_1_Expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = np.cos(2.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+        values[1] = np.sin(4.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+        values[2] = np.sin(2.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))**2
+        values[3] = np.sin(4.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))**2
+        values[4] = np.sin(6.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))
+        values[5] = np.sin(6.0*np.pi*x[0]/(rmsh.lmsh.mesh_parameters[1]['x_r']-rmsh.lmsh.mesh_parameters[1]['x_l']))**2
+
+    def value_shape(self):
+        return (6,)
+    
+fsp.t_mesh_1.interpolate(t_mesh_1_Expression(element=fsp.T_mesh_1.ufl_element()))
+
+
+msh.transfer_1d_to_2d(fsp.t_mesh_1, fsp.t_sub_mesh_0_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.t_mesh_1, f't_1d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(fsp.t_sub_mesh_0_1, f't_2d', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+'''
+####################
+
+
+
+
+'''
+here J[i][j] is the Jacobian of the functional for the j-th submesh of the i-th mesh, and similarly for problem, solver, ... 
+'''
+
+''''
+A) For square_disk_line problem
+
+The three variational problems (VPs) are solved as follows:
+1)  Solve Poisson VP on sub_mesh[0][1] for u[0][1] ->
+    Obtain 
+        a) For test case 1: u[0][1] = (x[0] - cr[0]) + 2 * (x[1] - cr[1])
+        b) For test case 2: u[0][1] = 2 * (x[0] - cr[0])**3 + (x[1] - cr[1])**3
+
+2)  Transfer u[0][1] on mesh[1] -> u_0_1_on_1
+    r theta = s
+    L1 = 2 pi r 
+    Given that 
+        x[0] = cr[0] + r * cos(s/r), 
+        x[1] = cr[1] + r * sin(s/r), 
+        where s is the coordinate along mesh[1] and along the circle, we have 
+
+        a) For test case 1: u_0_1_on_1(s)  = (r * cos(s/r)) + 2 * (r * sin(s/r))
+        b) For test case 2: u_0_1_on_1(s)  = 2 * (r * cos(s/r))**3 + (r * sin(s/r))**3
+
+3)  Solve on mesh[1] the VP
+
+        u[1]'(s) = u_0_1_on_1(s)
+
+    The solution is 
+
+      a) For test case 1: u[1](s) = C[1] + r^2 (-2 Cos[s/r] + Sin[s/r])
+      b) For test case 2: u[1](s) = C[1] + 1/12 r^4 (-9 Cos[s/r] + Cos[(3 s)/r] + 4 (5 + Cos[(2 s)/r]) Sin[s/r])
+    
+    where I set C[1] -> 0 by adding a Dirichlet BC on the VP on mesh[1]
+
+
+4)  Transfer u[1](s) to sub_mesh[0][0] and write it in u_1_on_0_0. 
+    On the circle 
+
+        a) Test case 1: u_1_on_0_0 = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1])
+        b) Test case 2: u_1_on_0_0 =  1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
+
+5)  Solve a Poisson problem on sub_mesh[0][0]  with BC on the circle u[0][0] = u_1_on_0_0
+
+    The problem has exact solution 
+        a) Test case 1:  u[0][0] = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1]),  
+        b) Test case 2:  u[0][0] = 1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
+
+    Obtain 
+        a) Test case 1): u[0][0] = - 2 r * (x[0] - cr[0]) + r * (x[1] - cr[1]) in sub_mesh[0][0]
+        b) Test case 2):  u[0][0] = 1/12 r (9 r^2 (crx - x) + (-crx + x)^3 + 3 (crx - x) (cry - y)^2 + 
+   2 (cry - y)^3 + 18 r^2 (-cry + y) + 6 (crx - x)^2 (-cry + y))
+
+
+   '''
+
+
+
+
+
+J, problem, solver, vp = [[None]*2, None], [[None]*2, None], [[None]*2, None], [[None]*2, None]
+
+# solve the variational problem in sub_mesh[0][1], and obtain the solution 
+print('Solving the problem in sub_mesh[0][1]...')
+
+vp[0][1] = importlib.import_module(swi.vp_sub_mesh_0_1)
+
+var_pr.solve_vp(vp[0][1].F, fsp.u[0][1], vp[0][1].bcs, fsp.J_u[0][1])
+
+print('...done.')
+
+
+print(f'Transferring solution on sub_mesh[0][1] to mesh[1] ...')
+
+msh.transfer_2d_to_1d(fsp.u[0][1], fsp.u_0_1_on_1, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+print(f'... done.')
+
+# solve the variational problem on mesh[1]
+print('Solving the problem in mesh[1]...')
+
+# use the solution obtained for sub_mesh[0][1] in the variational problem on mesh[1]
+vp[1] = importlib.import_module(swi.vp_mesh_1)
+
+var_pr.solve_vp(vp[1].F, fsp.u[1], vp[1].bcs, fsp.J_u[1])
+
+print('...done.')
+
+
+print(f'Transferring solution on mesh[1] to sub_mesh[0][0] ...')
+
+msh.transfer_1d_to_2d(fsp.u[1], fsp.u_1_on_0_0, os.path.join(rarg.args.input_directory, f'mesh_{0}'), rmsh.lmsh.parameters['shape_id'])
+
+print(f'... done.')
+
+
+
+print('Solving the problem in sub_mesh[0][0]...')
+
+vp[0][0] = importlib.import_module(swi.vp_sub_mesh_0_0)
+
+var_pr.solve_vp(vp[0][0].F, fsp.u[0][0], vp[0][0].bcs, fsp.J_u[0][0])
+
+print('...done.')
+
+
+prout_bc = importlib.import_module(swi.prout_bc)
