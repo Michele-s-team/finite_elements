f730b2e5ba7b96e6dcfbd13edca23a68758f68c0
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-24 16:55:59 +0100

    cu

--- poisson_equation/solve_u/three_domains/solve.py
+++ poisson_equation/solve_u/three_domains/solve.py
@@ -21,9 +21,9 @@ import input_output as io
 import mesh.utils as msh
 import solution_paths as solpath
 import switch_problem as swi
+import variational_problem as var_pr
 
 rmsh = importlib.import_module(swi.rmsh)
-vp_mesh_0 = ['','']
 
 # set the solver parameters here
 params = {'nonlinear_solver': 'newton',
@@ -97,49 +97,61 @@ The three variational problems (VPs) are solved as follows:
 
 J, problem, solver, vp = [[None]*2, None], [[None]*2, None], [[None]*2, None], [[None]*2, None]
 
-# solve the variational problem in sub_mesh[0][1], and obtain the solution 
+#1. solve the variational problem in sub_mesh[0][1], and obtain the solution 
 print('Solving the problem in sub_mesh[0][1]...')
+
 vp[0][1] = importlib.import_module(swi.vp_sub_mesh_0_1)
 J[0][1] = derivative(vp[0][1].F, fsp.u[0][1], fsp.J_u[0][1])
 problem[0][1] = NonlinearVariationalProblem(vp[0][1].F, fsp.u[0][1], vp[0][1].bcs, J[0][1])
 solver[0][1] = NonlinearVariationalSolver(problem[0][1])
-
 solver[0][1].solve()
+
 print('...done.')
 
-print(f'Transferring solution on sub_mesh[0][1] to mesh[1] ...')
-msh.transfer_circle_to_line(fsp.u[0][1], fsp.u_0_1_on_1, rmsh.lmsh.mesh_parameters[0]['c_r'], rmsh.lmsh.mesh_parameters[0]['r'], rmsh.lmsh.mesh_parameters[0]['N'])
-print(f'... done.')
+# 2. transfer solution on sub_mesh[0][1] to mesh[1]
 
+print(f'Transferring solution on sub_mesh[0][1] to mesh[1] ...')
+
+msh.transfer_circle_to_line(fsp.u[0][1], fsp.u_0_1_on_1, rmsh.lmsh.mesh_parameters[0]['c_r'], rmsh.lmsh.mesh_parameters[0]['r'], rmsh.lmsh.mesh_parameters[0]['N'])
 
 io.full_print(fsp.u_0_1_on_1, f'u_0_1_on_1', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
                   solpath.nodal_values_path)
 
 
+print(f'... done.')
 
-# solve the variational problem on mesh[1]
+
+# 3. solve the variational problem on mesh[1]
+
 print('Solving the problem in mesh[1]...')
+
 # use the solution obtained for sub_mesh[0][1] in the variational problem on mesh[1]
 vp[1] = importlib.import_module(swi.vp_mesh_1)
 J[1] = derivative(vp[1].F, fsp.u[1], fsp.J_u[1])
 problem[1] = NonlinearVariationalProblem(vp[1].F, fsp.u[1], vp[1].bcs, J[1])
 solver[1] = NonlinearVariationalSolver(problem[1])
-
 solver[1].solve()
+
 print('...done.')
 
+# 4. transfer the solution on mesh[1] to sub_mesh[0][0]
+
 print(f'Transferring solution on mesh[1] to sub_mesh[0][0] ...')
+
 msh.transfer_line_to_circle(fsp.u[1], fsp.u_1_on_0_0, rmsh.lmsh.mesh_parameters[0]['c_r'], rmsh.lmsh.mesh_parameters[0]['r'], rmsh.lmsh.mesh_parameters[0]['N'])
+
 print(f'... done.')
 
+# 5. solve the problem on sub_mesh[0][0]
+
+print('Solving the problem in sub_mesh[0][0]...')
 
 vp[0][0] = importlib.import_module(swi.vp_sub_mesh_0_0)
 J[0][0] = derivative(vp[0][0].F, fsp.u[0][0], fsp.J_u[0][0])
 problem[0][0] = NonlinearVariationalProblem(vp[0][0].F, fsp.u[0][0], vp[0][0].bcs, J[0][0])
 solver[0][0] = NonlinearVariationalSolver(problem[0][0])
-
-print('Solving the problem in sub_mesh[0][0]...')
 solver[0][0].solve()
+
 print('...done.')
 
 
