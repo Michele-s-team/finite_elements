69b3e05742832308ce984d677b55f5d5d4aaaed8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-25 16:49:09 +0100

    replaced ys

--- fluid_structure_interaction/rigid_obstacle/remesh/algebraic_problem_shape_bc_square_polygon.py
+++ fluid_structure_interaction/rigid_obstacle/remesh/algebraic_problem_shape_bc_square_polygon.py
@@ -11,8 +11,6 @@ import elasticity as ela
 import fluid as flu
 import function_spaces as fsp
 import differential_geometry.manifold.geometry as geo
-import input_output as io
-import mesh.load as lmsh
 import parameters.read.solution as rpam
 import switch_problem as swi
 from calculus import atan_quad
@@ -22,28 +20,15 @@ vp_fluid = importlib.import_module(swi.vp_fluid)
 
 dt = rpam.parameters["T"] / rpam.parameters["num_steps"]  # time step size
 
-focus = cal.ellipse_focal_points(rmsh.parameters['a'], rmsh.parameters['b'], rmsh.parameters['c'])[0]
-
-
-
 i, j, k, l, m, n = ufl.indices(6)
 
 
 
 class ys_shape_expression(UserExpression):
     def eval(self, values, x):
-        # s = 1 / (2 * np.pi) * atan_quad([rmsh.parameters["b"] * (x[0] - rmsh.parameters["c"][0]), rmsh.parameters["a"] * (x[1] - rmsh.parameters["c"][1])])
 
-        # compute the ellipse parameteric coordinate 's' corresdponding to the point 'x'
-        s = cal.parameteric_coordinate_ellipse(x, rmsh.parameters['a'], rmsh.parameters['b'], rmsh.parameters['c'], 
-                                               phi=rmsh.parameters['phi'])
-
-        # compute the ellipse curve y_s at 's'
-        t = cal.ellipse(rmsh.parameters["a"], rmsh.parameters["b"], rmsh.parameters["c"][:2], s, 
-                        phi=rmsh.parameters['phi'])[0]
-
-        values[0] = t[0]
-        values[1] = t[1]
+        values[0] = x[0]
+        values[1] = x[1]
 
     def value_shape(self):
         return (2,)
@@ -74,5 +59,5 @@ fsp.dyds_shape.interpolate(dyds_shape_expression(element=fsp.Q_dyds.ufl_element(
 
 # momentum of forces exerted by the fluid on the ellipse
 M_ellipse = assemble( \
-    (geo.epsilon[i, j] * (fsp.ys_shape[i] + fsp.u_n_1[i] - (Constant(focus))[i]) * flu.sigma_ale(fsp.v_n_1, fsp.sigma_n_32, fsp.u_n_1, rpam.parameters["mu"])[j, k] * geo.epsilon[k, m] * ela.F(fsp.u_n_1)[m, l] * fsp.dyds_shape[l]) \
+    (geo.epsilon[i, j] * (fsp.ys_shape[i] + fsp.u_n_1[i] - (Constant(rmsh.parameters['c']))[i]) * flu.sigma_ale(fsp.v_n_1, fsp.sigma_n_32, fsp.u_n_1, rpam.parameters["mu"])[j, k] * geo.epsilon[k, m] * ela.F(fsp.u_n_1)[m, l] * fsp.dyds_shape[l]) \
     / sqrt(fsp.dyds_shape[n] * fsp.dyds_shape[n]) * rmsh.ds_poly)
