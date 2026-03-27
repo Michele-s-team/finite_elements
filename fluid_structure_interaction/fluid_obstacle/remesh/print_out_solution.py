0d2a50db5a61d02c132ceded6a9e0e1cd5a23aee
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-27 22:17:38 +0100

    removed useless printouts

--- fluid_structure_interaction/fluid_obstacle/remesh/print_out_solution.py
+++ fluid_structure_interaction/fluid_obstacle/remesh/print_out_solution.py
@@ -65,7 +65,7 @@ def print_solution_I(t, step):
             solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
 
     # include the snapshot in xdmf files
-    fi.xdmffile_U_n_12.write(fsp.U_n_12, t)
+    # fi.xdmffile_U_n_12.write(fsp.U_n_12, t)
 
 
 # print the solution for D 
@@ -78,8 +78,8 @@ def print_solution_D(t, step):
     io.full_print(fsp.u_n_di_dot, 'u_n_di_dot_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
     
-    fi.xdmffile_u_n_di.write(fsp.u_n_di, t)
-    fi.xdmffile_u_n_di_dot.write(fsp.u_n_di_dot, t)
+    # fi.xdmffile_u_n_di.write(fsp.u_n_di, t)
+    # fi.xdmffile_u_n_di_dot.write(fsp.u_n_di_dot, t)
 
     # Write the deformed sub_mesh[0][0] to file
     deformed_sub_mesh_0_0 = msh.deform_mesh(lmsh.sub_meshes[0][0], fsp.u_n_di)
@@ -97,13 +97,13 @@ def print_solution_D(t, step):
     io.full_print(fsp.u_n_sq_dot, 'u_n_sq_dot_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
     
-    fi.xdmffile_u_n_sq.write(fsp.u_n_sq, t)
-    fi.xdmffile_u_n_sq_dot.write(fsp.u_n_sq_dot, t)
+    # fi.xdmffile_u_n_sq.write(fsp.u_n_sq, t)
+    # fi.xdmffile_u_n_sq_dot.write(fsp.u_n_sq_dot, t)
 
     # Write the deformed sub_mesh[0][1] to file
     deformed_sub_mesh_0_1 = msh.deform_mesh(lmsh.sub_meshes[0][1], fsp.u_n_sq)
-    with XDMFFile(solpath.snapshots_path + 'sub_mesh_0_1_msh_n_' + str(step) + '.xdmf') as xdmf:
-        xdmf.write(deformed_sub_mesh_0_1)
+    # with XDMFFile(solpath.snapshots_path + 'sub_mesh_0_1_msh_n_' + str(step) + '.xdmf') as xdmf:
+    #     xdmf.write(deformed_sub_mesh_0_1)
     io.print_mesh_vertices_to_csv(deformed_sub_mesh_0_1, solpath.snapshots_csv_path + 'vertex_sub_mesh_0_1_msh_n_' + str(step) + '.csv')
     io.print_mesh_lines_to_csv(deformed_sub_mesh_0_1, solpath.snapshots_csv_path + 'line_sub_mesh_0_1_msh_n_' + str(step) + '.csv')
 
@@ -130,8 +130,8 @@ def print_solution_di_fluid(t, step):
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
 
 
-    fi.xdmffile_v_di__.write(fsp.v_disk__, t)
-    fi.xdmffile_v_di_n.write(fsp.v_disk_n, t)
+    # fi.xdmffile_v_di__.write(fsp.v_disk__, t)
+    # fi.xdmffile_v_di_n.write(fsp.v_disk_n, t)
 
 
 
@@ -154,7 +154,7 @@ def print_solution_di_fluid(t, step):
     io.full_print_deformed(fsp.sigma_disk_n_12, fsp.u_n_di, 'sigma_disk_n_12_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
     
-    fi.xdmffile_sigma_di_n_12.write(fsp.sigma_disk_n_12, t)
+    # fi.xdmffile_sigma_di_n_12.write(fsp.sigma_disk_n_12, t)
 
 
 # print the solution for square fluid 
@@ -176,8 +176,8 @@ def print_solution_sq_fluid(t, step):
     io.full_print_deformed(fsp.v_square_n, fsp.u_n_sq, 'v_square_n_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
 
-    fi.xdmffile_v_sq__.write(fsp.v_square__, t)
-    fi.xdmffile_v_sq_n.write(fsp.v_square_n, t)
+    # fi.xdmffile_v_sq__.write(fsp.v_square__, t)
+    # fi.xdmffile_v_sq_n.write(fsp.v_square_n, t)
 
 
 
@@ -198,7 +198,7 @@ def print_solution_sq_fluid(t, step):
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
 
 
-    fi.xdmffile_sigma_sq_n_12.write(fsp.sigma_square_n_12, t)
+    # fi.xdmffile_sigma_sq_n_12.write(fsp.sigma_square_n_12, t)
 
 
 
@@ -213,7 +213,7 @@ def print_solution_M(t, step):
     io.full_print_deformed(fsp.c_n, fsp.u_n_sq, 'c_n_' + str(step), \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path)
     
-    fi.xdmffile_c_n.write(fsp.c_n, t)
+    # fi.xdmffile_c_n.write(fsp.c_n, t)
 
 
 # print solution for all sectors
