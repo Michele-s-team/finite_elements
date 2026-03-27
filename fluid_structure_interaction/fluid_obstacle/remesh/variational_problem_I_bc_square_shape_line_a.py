d65cc987dab7d826ff753ab2db5c2821042cbe3e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-27 19:01:17 +0100

    test 1d_to_2d OK

--- fluid_structure_interaction/fluid_obstacle/remesh/variational_problem_I_bc_square_shape_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/remesh/variational_problem_I_bc_square_shape_line_a.py
@@ -31,7 +31,7 @@ dt = rpam.parameters['T'] / rpam.parameters['N']
 class ys_expression(UserExpression):
     def eval(self, values, x):
 
-        p = msh.map_1d_to_2d(x[0], rmsh.lmsh.mesh[0], rmsh.mf[0], rmsh.lmsh.mesh_parameters[0]['shape_coorindates'], rmsh.lmsh.mesh_parameters[0]['shape_id'])
+        p = msh.map_1d_to_2d(x[0], rmsh.lmsh.mesh[0], rmsh.mf[0], rmsh.lmsh.mesh_parameters[0]['shape_coordinates'], rmsh.lmsh.mesh_parameters[0]['shape_id'])
 
         values[0] = p[0]
         values[1] = p[1]
