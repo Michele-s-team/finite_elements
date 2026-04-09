119876fc6e4d0cbc29b5659e17b06854694c070d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-09 18:29:29 +0200

    switched to square_shape_line mesh

--- poisson_equation/solve_u/two_domains/monolithic/solve.py
+++ poisson_equation/solve_u/two_domains/monolithic/solve.py
@@ -1,16 +1,15 @@
 '''
-This code solves the Poisson equation in two sub_meshes, sub_mesh[0] and sub_mesh[1], which share one boundary
-The problem is solved with a monolithic approach, where the solution on the mesh given by the union of sub_mesh[0] and sub_mesh[1] is solved with a single variational problem 
+This code solves the Poisson equation in mesh[0] which is composed of two sub_meshes, sub_mesh[0][0] and sub_mesh[0][1], which share one boundary. The problem is solved with the monolithic approach, by defining a field u which lives on the full mesh[0]. 
 
 Run with
     clear; clear; python3 solve.py [name of the variational problem to solve] [path where to read the mesh generated from generate_mesh.py] [path where to store the solution]
 Examples:
-     MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/ellipse_circle/solution"; SOLUTION_PATH="/home/fenics/shared/poisson_equation/solve_u/two_domains/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_ellipse_circle $MESH_PATH $SOLUTION_PATH
-
-'''
+     MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; SOLUTION_PATH="/home/fenics/shared/poisson_equation/solve_u/two_domains/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_shape_line $MESH_PATH $SOLUTION_PATH
+ '''
 
 from fenics import *
 import importlib
+import os
 import sys
 
 # add the path where to find the shared modules
@@ -18,11 +17,14 @@ module_path = '/home/fenics/shared/modules'
 sys.path.append(module_path)
 
 import function_spaces as fsp
+import input_output as io
+import mesh.utils as msh
+import runtime_arguments as rarg
+import solution_paths as solpath
 import switch_problem as swi
 import variational_problem.utils as var_pr
 
 rmsh = importlib.import_module(swi.rmsh)
-vp = ['','']
 
 # set the solver parameters here
 params = {'nonlinear_solver': 'newton',
@@ -36,19 +38,41 @@ params = {'nonlinear_solver': 'newton',
               }
           }
 
-J = [None] * len(rmsh.lmsh.sub_meshes)
-problem = [None] * len(rmsh.lmsh.sub_meshes)
-solver = [None] * len(rmsh.lmsh.sub_meshes)
 
-# solve problem on sub_mesh[1]
-vp[1] = importlib.import_module(swi.vp_sub_mesh_1)
 
-var_pr.solve_vp(vp[1].F, fsp.u[1],  vp[1].bcs,  fsp.J_u[1], parameters=params)
 
-# solve problem on sub_mesh[0] by using the solution above on sub_mesh[1] as a BC
-vp[0] = importlib.import_module(swi.vp_sub_mesh_0)
+J, problem, solver, vp = [[None]*2, None], [[None]*2, None], [[None]*2, None], [[None]*2, None]
 
-var_pr.solve_vp(vp[0].F, fsp.u[0], vp[0].bcs, fsp.J_u[0], parameters=params)
+#1. solve the variational problem in sub_mesh[0][1], and obtain the solution 
+print('Solving the problem in sub_mesh[0][1]...')
+
+vp[0][1] = importlib.import_module(swi.vp_sub_mesh_0_1)
+var_pr.solve_vp(vp[0][1].F, fsp.u[0][1], vp[0][1].bcs, fsp.J_u[0][1])
+
+print('...done.')
+
+# 2. transfer solution on sub_mesh[0][1] to mesh[1]
+
+print(f'Transferring solution on sub_mesh[0][1] to mesh[1] ...')
+
+msh.transfer_2d_to_1d(fsp.u[0][1], fsp.u_0_1_on_1, rmsh.lmsh.mesh[0], rmsh.mf[0], rmsh.lmsh.mesh_parameters[0]['shape_coordinates'], rmsh.lmsh.parameters['shape_id'])
+
+io.full_print(fsp.u_0_1_on_1, f'u_0_1_on_1', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+
+
+print(f'... done.')
+
+
+
+# 5. solve the problem on sub_mesh[0][0]
+
+print('Solving the problem in sub_mesh[0][0]...')
+
+vp[0][0] = importlib.import_module(swi.vp)
+var_pr.solve_vp(vp[0][0].F, fsp.u[0][0], vp[0][0].bcs, fsp.J_u[0][0])
+
+print('...done.')
 
 
 prout_bc = importlib.import_module(swi.prout_bc)
--- poisson_equation/solve_u/two_domains/monolithic/switch_problem.py
+++ poisson_equation/solve_u/two_domains/monolithic/switch_problem.py
@@ -2,11 +2,9 @@ import colorama as col
 
 import runtime_arguments as rarg
 
-if rarg.args.problem == 'square_ellipse_circle':
-    rmsh = 'mesh.read.square_ellipse_circle'
-    vp_sub_mesh_0 = 'variational_problem_square_ellipse_circle_sub_mesh_0'
-    vp_sub_mesh_1 = 'variational_problem_square_ellipse_circle_sub_mesh_1'
-    prout_bc = 'print_out_square_ellipse_circle'
-
+if rarg.args.problem == 'square_shape_line':
+    rmsh = 'mesh.read.square_shape_line'
+    vp = 'variational_problem_square_shape_line'
+    prout_bc = 'print_out_square_shape_line'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
