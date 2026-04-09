650750137dbe6fb73f0341c72475bb895279cdaf
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-09 14:05:34 +0200

    removed disk_parameters

--- testing/test.py
+++ testing/test.py
@@ -1053,8 +1053,7 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', 'generate_mesh', 
-                                                                     os.path.join(generate_mesh_path, 'disk_parameters'),  
-                                                                     os.path.join(generate_mesh_path, 'disk_parameters'),         
+                                                                     generate_mesh_path, generate_mesh_path,         
                                                                      problem_name, problem_name, success)
 
 
