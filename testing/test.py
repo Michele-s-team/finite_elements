566694b1d5beb0766f7e61b943c77f80d318e0e7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-12 18:11:34 +0200

    added to test poisson_equation/solve_u/two_domains/discontinuous
    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -367,6 +367,20 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
 
+# Test poisson_equation/solve_u/two_domains/discontinuous
+case_name = 'poisson_equation/solve_u/two_domains/discontinuous'
+
+problem_name = 'square_shape_line_a'
+generate_mesh_path = root_path + 'generate_mesh/2d/square/shape_line/'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
+
+
 # Test poisson_equation/solve_u/three_domains
 case_name = 'poisson_equation/solve_u/three_domains'
 
