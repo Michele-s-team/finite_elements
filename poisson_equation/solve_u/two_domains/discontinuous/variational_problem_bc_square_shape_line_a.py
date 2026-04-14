9be7b093aa08f9201ca2f24ff782e6f3a4126ef4
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-14 11:54:42 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- poisson_equation/solve_u/two_domains/discontinuous/variational_problem_bc_square_shape_line_a.py
+++ poisson_equation/solve_u/two_domains/discontinuous/variational_problem_bc_square_shape_line_a.py
@@ -32,57 +32,6 @@ else:
     print(f'The "+" domain is the region inside the shape.')
 '''
 
-'''
-class u_exact_shape_expression(UserExpression):
-    def eval(self, values, x):
-
-        # test case 1
-        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2 + rpam.parameters['A'] * ((x[0]-rmsh.lmsh.parameters['c'][0])**2 + (x[1]-rmsh.lmsh.parameters['c'][1])**2 - rmsh.lmsh.parameters['r']**2)
-
-    def value_shape(self):
-        return (1,)
-
-
-class u_exact_square_expression(UserExpression):
-    def eval(self, values, x):
-
-        # test case 1
-        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
-
-    def value_shape(self):
-        return (1,)
-
-
-class laplacian_u_shape_expression(UserExpression):
-    def eval(self, values, x):
-
-        # test case 1
-        values[0] = 6.0 + rpam.parameters['A'] * 4.0
-
-    def value_shape(self):
-        return (1,)
-
-
-class laplacian_u_square_expression(UserExpression):
-    def eval(self, values, x):
-
-        # test case 1
-        values[0] = 6.0
-
-    def value_shape(self):
-        return (1,)
-    
-class d_expression(UserExpression):
-    def eval(self, values, x):
-
-
-
-        # test case 1
-        values[0] = rpam.parameters['A'] * 2.0 * rmsh.lmsh.parameters['r']
-
-    def value_shape(self):
-        return (1,)
-'''
 
 # test case 1
 
