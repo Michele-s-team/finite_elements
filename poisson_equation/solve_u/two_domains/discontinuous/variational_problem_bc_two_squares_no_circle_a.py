bc63e1e3d0e85ffeb2f93310e2a5ec8e3c47f2a5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-14 08:14:19 +0200

    substituted set_dg_scalar_field in case 2, it works

--- poisson_equation/solve_u/two_domains/discontinuous/variational_problem_bc_two_squares_no_circle_a.py
+++ poisson_equation/solve_u/two_domains/discontinuous/variational_problem_bc_two_squares_no_circle_a.py
@@ -50,10 +50,10 @@ class u_exact_l_expression(UserExpression):
         # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2 + rpam.parameters['A'] * (x[0]-rmsh.lmsh.parameters['L_m'])
 
         # test case 2
-        # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 + rpam.parameters['A'] * (x[0]-rmsh.lmsh.parameters['L_m'])
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 + rpam.parameters['A'] * (x[0]-rmsh.lmsh.parameters['L_m'])
 
         # test case 3
-        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 + rpam.parameters['A'] * np.sin(2 * np.pi / rmsh.lmsh.parameters['L'] * (x[0] - rmsh.lmsh.parameters['L_m'])) * np.cos(4 * np.pi * (x[0] + x[1]) / rmsh.lmsh.parameters['L'])
+        # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 + rpam.parameters['A'] * np.sin(2 * np.pi / rmsh.lmsh.parameters['L'] * (x[0] - rmsh.lmsh.parameters['L_m'])) * np.cos(4 * np.pi * (x[0] + x[1]) / rmsh.lmsh.parameters['L'])
 
     def value_shape(self):
         return (1,)
@@ -66,10 +66,10 @@ class u_exact_r_expression(UserExpression):
         # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
 
         # test case 2
-        # value`s[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 
 
         # test case 3
-        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 
+        # values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 4 
 
     def value_shape(self):
         return (1,)
@@ -83,18 +83,15 @@ class d_expression(UserExpression):
         # values[0] = rpam.parameters['A']
 
         # test case 2
-        # values[0] = rpam.parameters['A']
+        values[0] = rpam.parameters['A']
 
         # test case 3
-        values[0] = (2 * rpam.parameters['A'] * np.pi * np.cos((4 * np.pi * (rmsh.lmsh.parameters['L_m'] + x[1])) / rmsh.lmsh.parameters['L'])) / rmsh.lmsh.parameters['L']
+        # values[0] = (2 * rpam.parameters['A'] * np.pi * np.cos((4 * np.pi * (rmsh.lmsh.parameters['L_m'] + x[1])) / rmsh.lmsh.parameters['L'])) / rmsh.lmsh.parameters['L']
 
     def value_shape(self):
         return (1,)
 
 
-# build a UFL expression that contains the expression for the laplacians in both surface_l and surface_r
-
-# x_  = SpatialCoordinate(rmsh.lmsh.mesh)
 
 
 '''
@@ -102,30 +99,35 @@ class d_expression(UserExpression):
 f = 6.0
 '''
 
-'''
+
 # test case 2
-f = 2.0 + 24.0 * x_[1]**2
+def f_l(x): 
+    return 2.0 + 24.0 * x[1]**2
+
+def f_r(x):
+    return f_l(x)
+
 '''
-
-
-
 # test case 3
-'''
-L = Constant(rmsh.lmsh.parameters['L'])
-L_m = Constant(rmsh.lmsh.parameters['L_m'])
-A   = Constant(rpam.parameters['A'])
-pi = Constant(np.pi)
 
-f = conditional(le(x_[0], L_m),
-                    2.0 + 24.0 * x_[1]**2 + 2 * A * pi**2/ L**2 * (13 * sin((2 * pi * (L_m - 3 * x_[0] - 2 * x_[1])) / L) +  5 * sin((2 * pi * (L_m + x_[0] + 2 * x_[1])) / L) ),
-                    2.0 + 24.0 * x_[1]**2)
-'''
+# build a UFL expression that contains the expression for the laplacians in both surface_l and surface_r
+# x_  = SpatialCoordinate(rmsh.lmsh.mesh)
+# L = Constant(rmsh.lmsh.parameters['L'])
+# L_m = Constant(rmsh.lmsh.parameters['L_m'])
+# A   = Constant(rpam.parameters['A'])
+# pi = Constant(np.pi)
+
+# f = conditional(le(x_[0], L_m),
+#                     2.0 + 24.0 * x_[1]**2 + 2 * A * pi**2/ L**2 * (13 * sin((2 * pi * (L_m - 3 * x_[0] - 2 * x_[1])) / L) +  5 * sin((2 * pi * (L_m + x_[0] + 2 * x_[1])) / L) ),
+#                     2.0 + 24.0 * x_[1]**2)
+
 
 def f_l(x):
     return 2.0 + 24.0 * x[1]**2 + 2 * rpam.parameters['A'] * np.pi**2/ rmsh.lmsh.parameters['L']**2 * (13 * np.sin((2 * np.pi * (rmsh.lmsh.parameters['L_m'] - 3 * x[0] - 2 * x[1])) / rmsh.lmsh.parameters['L']) +  5 * np.sin((2 * np.pi * (rmsh.lmsh.parameters['L_m'] + x[0] + 2 * x[1])) / rmsh.lmsh.parameters['L']) )
 
 def f_r(x):
     return 2.0 + 24.0 * x[1]**2
+'''
 
 msh.set_dg_scalar_field(fsp.f, f_l, rmsh.sf, rmsh.lmsh.parameters['l_surface_id'])
 msh.set_dg_scalar_field(fsp.f, f_r, rmsh.sf, rmsh.lmsh.parameters['r_surface_id'])
