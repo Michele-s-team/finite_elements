d41c5cced00a946ceac17ba6747f2aac9627b7a8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-16 14:17:35 +0200

    added _b cases to ttest

--- testing/test.py
+++ testing/test.py
@@ -379,7 +379,25 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
+problem_name = 'square_shape_line_b'
+generate_mesh_path = root_path + 'generate_mesh/2d/square/shape_line/'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
 problem_name = 'two_squares_no_circle_a'
+generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/two_squares_no_circle/'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
+problem_name = 'two_squares_no_circle_b'
 generate_mesh_path = root_path + 'generate_mesh/2d/square_no_circle/two_squares_no_circle/'
 checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
                                                                      root_path,
