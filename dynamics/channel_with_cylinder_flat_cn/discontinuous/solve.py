32d1d31bf66d8fea1ae36266afaa1a621c3ddd9a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-18 18:22:43 +0200

    quadrature degree

--- dynamics/channel_with_cylinder_flat_cn/discontinuous/solve.py
+++ dynamics/channel_with_cylinder_flat_cn/discontinuous/solve.py
@@ -28,11 +28,8 @@ rmsh = importlib.import_module(swi.rmsh)
 vp = importlib.import_module(swi.vp)
 pr_bc = importlib.import_module(swi.prout_bc)
 
-dolfin.parameters["form_compiler"]["quadrature_degree"] = 10
+dolfin.parameters["form_compiler"]["quadrature_degree"] = rpam.parameters['quadrature_degree']
 
-print("Input directory", rarg.args.input_directory)
-print("Output directory", rarg.args.output_directory)
-print(f"Radius of mesh cell = {col.Fore.BLUE}{rmsh.r_mesh}{col.Style.RESET_ALL}")
 
 
 # set the initial profiles
