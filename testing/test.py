ec5fe25db627b4c7a0610d7d917f61e810ed0c61
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-04-20 14:59:08 +0200

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -449,6 +449,14 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
+problem_name = 'disk_vertices_a'
+generate_mesh_path = root_path + 'generate_mesh/2d/disk/vertices'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
 
 # Test poisson_equation/solve_u_v
