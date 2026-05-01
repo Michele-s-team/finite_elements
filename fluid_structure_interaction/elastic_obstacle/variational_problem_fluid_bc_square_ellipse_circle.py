3601ba3cc9f8d0373b21cdbae04661dd098fee7c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-01 18:03:35 +0200

    vp_fluid check OK

--- fluid_structure_interaction/elastic_obstacle/variational_problem_fluid_bc_square_ellipse_circle.py
+++ fluid_structure_interaction/elastic_obstacle/variational_problem_fluid_bc_square_ellipse_circle.py
@@ -72,7 +72,7 @@ F_phi = ( \
             ) * ela.detF(fsp.u_msh_n_1) * rmsh.dx_sub_mesh[1] \
         + dt / rpam.parameters['rho_fluid'] * (ela.G(fsp.u_msh_n_1)[l, i] * bgeo.sub_mesh_facet_normal[1][l] * ela.G(fsp.u_msh_n_1)[j, i] * (fsp.phi.dx(j)) * fsp.nu_phi) * ela.detF(fsp.u_msh_n_1) * rmsh.ds_sub_mesh[1]['ds_r']
 
-# sign
 
 # step 3 for v_n
-F_v_n = ( ( (fsp.v_n[i] - fsp.v_[i]) + (dt / rpam.parameters['rho_fluid']) * ela.G(fsp.u_msh_n_1)[l, i] * (fsp.phi.dx(l)) ) * fsp.nu_v_n[i] ) * ela.detF(fsp.u_msh_n_1) * rmsh.dx_sub_mesh[1]
+F_v_n = ( ( (fsp.v_n[i] - fsp.v_[i]) + (dt / rpam.parameters['rho_fluid']) * ela.G(fsp.u_msh_n_1)[j, i] * (fsp.phi.dx(j)) ) * fsp.nu_v_n[i] ) * ela.detF(fsp.u_msh_n_1) * rmsh.dx_sub_mesh[1]
+
