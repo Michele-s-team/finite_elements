6a6e04678ddacda600454b9a42811a54401dc33c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-12 19:02:44 +0200

    finished revision of printouts

--- fluid_structure_interaction/elastic_obstacle/monolithic/print_out_data.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/print_out_data.py
@@ -22,7 +22,7 @@ import switch_problem as swi
 rmsh = importlib.import_module(swi.rmsh)
 vp = importlib.import_module(swi.vp)
 
-i, j = ufl.indices(2)
+i, j, k = ufl.indices(3)
 
 # create the path for the csv file if it does not exist
 filename_data = os.path.join(rarg.args.output_directory, 'data.csv')
@@ -32,20 +32,21 @@ csvfile = open(filename_data, 'a', newline='')
 fieldnames = [ \
     '<<|u_n|^2>>_{partial Omega ellipse}',
     '<<sigma_n^2>>_{partial Omega ellipse}',
+    '<<varsigma_no_pressure_{ij} varsigma_no_pressure_{ij}>>_{partial Omega ellipse}',
     ]
 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 writer.writeheader()
 
 
-
-# this function prints out the residuals of BCs
 def print_data():
 
     writer.writerows([{
         fieldnames[0]: \
             f"{sqrt(assemble(msh.average(fsp.u_n[i]*fsp.u_n[i]) * rmsh.dS_ellipse)):.{io.number_of_decimals}e}",
         fieldnames[1]: \
-            f"{sqrt(assemble((fsp.sigma_n(vp.sub_mesh_1_label))**2 * rmsh.dS_ellipse)):.{io.number_of_decimals}e}"
+            f"{sqrt(assemble((fsp.sigma_n(vp.sub_mesh_1_label))**2 * rmsh.dS_ellipse)):.{io.number_of_decimals}e}",
+        fieldnames[2]: \
+            f"{sqrt(assemble(flu.sigma_ale_no_pressure(fsp.v_n(vp.sub_mesh_1_label), Constant(0), fsp.u_n(vp.sub_mesh_1_label), rpam.parameters['mu_fluid'])[i, k] * flu.sigma_ale_no_pressure(fsp.v_n(vp.sub_mesh_1_label), Constant(0), fsp.u_n(vp.sub_mesh_1_label), rpam.parameters['mu_fluid'])[i, k] * rmsh.dS_ellipse)):.{io.number_of_decimals}e}",
         }])
 
     csvfile.flush()
