353e9cfb6a49720774a654bf610dc430a55ab854
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-18 19:28:38 +0200

    replaced with Constant()

--- fluid_structure_interaction/elastic_obstacle/monolithic/solve.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/solve.py
@@ -229,14 +229,14 @@ for n in range(rpam.parameters['num_steps']):
 
     if step <= rpam.parameters['n_hold']:
         
-        cont.pressure_scale = 0.0
+        cont.pressure_scale = Constant(0.0)
     
     elif step <= rpam.parameters['n_hold'] + rpam.parameters['n_ramp']:
     
-        cont.pressure_scale = (step - rpam.parameters['n_hold']) / rpam.parameters['n_ramp']
+        cont.pressure_scale = Constant((step - rpam.parameters['n_hold']) / rpam.parameters['n_ramp'])
     
     else:
-        cont.pressure_scale = 1.0
+        cont.pressure_scale = Constant(1.0)
         
     vp = importlib.reload(importlib.import_module(swi.vp))  # rebuilds F with new pressure_scale
 
