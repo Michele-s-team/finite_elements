578b2d001cbca56f0a822e0a6c4a58043303bc13
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-20 19:05:04 +0200

    added u_0

--- fluid_structure_interaction/elastic_obstacle/monolithic/function_spaces_square_shape_line.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/function_spaces_square_shape_line.py
@@ -15,6 +15,8 @@ the variables for the problem are
     - v_n, v_n_1 : \textrm{v}^n_notes, \textrm{v}^{n-1}_notes
     - 'sigma_n' = \varsigma^n_notes
 
+    - 'u_0' = u_0_{Decomposition of deformation field}
+
 all fields are defined from a mixed function space
 '''
 
@@ -60,12 +62,14 @@ v_n, sigma_n, u_n, u_dot_n = split(psi)
 v_n_1 = Function(Q_v_n)
 
 u_n_1 = Function(Q_u_n)
-
 u_dot_n_1 = Function(Q_u_dot_n)
 
 rho_el = Function(Q_rho_el)
 
 sigma_r = Function(Q_sigma_n)
+
+u_0 = Function(Q_u_n)
+
 
 # y is the identity function that, given the coordinates y_i in the reference configuration, returns y_i
 y = Function(Q_u_n)
