b663610c1b9ac69d77cc1aafa6cb9701760bd690
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-20 12:41:28 +0200

    Update decompose_u.py

--- fluid_structure_interaction/elastic_obstacle/monolithic/decompose_u.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/decompose_u.py
@@ -8,7 +8,15 @@ import switch_problem as swi
 fsp = importlib.import_module(swi.fsp)
 rmsh = importlib.import_module(swi.rmsh)
 
-# `c` are the coordinates of the center of mass of the elastic body
+'''
+quantitties definitions
+
+c[i] = {c_i}_{Decomposition of deformation field}
+t[i] = {t_i}_{Decomposition of deformation field}
+C[i][j] = {C_{ij}}_{Decomposition of deformation field}
+
+chi = \chi_{Decomposition of deformation field}
+'''
 
 class y_expression(UserExpression):
     def eval(self, values, x):
@@ -21,25 +29,25 @@ class y_expression(UserExpression):
 
 msh.interpolate_dg(fsp.y, y_expression())
 
-# 1. compute c[i] = {c_i}_notes
+# 1. compute c
 c = [msh.average_wrt_measure(fsp.y[i], rmsh.dx_mesh[0]['dx_shape']) for i in range(2)]
 print(f'*** c = {c}')
 
 # sign
 
-# 2. compute C[i][j]= {C_{ij}}_notes
+# 2. compute C
 C = [[msh.average_wrt_measure((fsp.y[i] + fsp.u_n[i]) * (fsp.y[j] - c[j]), rmsh.dx_mesh[0]['dx_shape']) for j in range(2)] for i in range(2)]
 
 print(f'*** C = {C}')
 
 
-# 3. compute t[i] = {t_i}_notes
+# 3. compute t
 t = [msh.average_wrt_measure(fsp.u_n[i], rmsh.dx_mesh[0]['dx_shape']) for i in range(2)]
 
 print(f'*** t = {t}')
 
 # 4. compute chi = \chi_notes and theta = \theta_notes
 chi = (C[1][0] - C[0][1]) / (C[0][0] + C[1][1])
-theta = np.arcsin(chi/sqrt(1.0 + chi**2))
+theta = np.arcsin(chi/np.sqrt(1.0 + chi**2))
 
 print(f'*** chi = {chi}\ntheta = {theta}')
