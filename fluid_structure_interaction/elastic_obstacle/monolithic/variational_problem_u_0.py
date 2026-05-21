52a10f3aea5c416302ab22fa42916bc28816455d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-21 10:47:22 +0200

    .

--- fluid_structure_interaction/elastic_obstacle/monolithic/variational_problem_u_0.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/variational_problem_u_0.py
@@ -36,21 +36,21 @@ F_u_0 = msh.ufl_conditional_form(
                                         rmsh.lmsh.mesh[0],
                                         rmsh.sf[0], 
                                         0, 
-                                        - ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] * (fsp.nu_u_n[k].dx(i)), 
+                                        - ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] * (fsp.nu_u_0[k].dx(i)), 
                                         rmsh.lmsh.parameters['sub_mesh_0_0_id'],
                                         rmsh.lmsh.parameters['sub_mesh_0_1_id']
                                 ) * rmsh.dx_mesh[0]['dx'] \
         + (\
-            msh.jump(fsp.nu_u_n[k], bgeo.facet_normal[0])[i] * msh.average( ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] )   
+            msh.jump(fsp.nu_u_0[k], bgeo.facet_normal[0])[i] * msh.average( ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] )   
         ) * rmsh.ds_mesh[0]['dS_I_square'] \
-        + bgeo.facet_normal[0][i] * ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] * fsp.nu_u_n[k] * rmsh.ds_mesh[0]['ds'] \
-        + bgeo.facet_normal[0](sub_mesh_1_label)[i] * ela.P(fsp.u_0(sub_mesh_1_label), ela.K(fsp.u_0(sub_mesh_1_label), rpam.parameters['exponent']), ela.mu(fsp.u_0(sub_mesh_1_label), rpam.parameters['exponent']))[k, i] * fsp.nu_u_n(sub_mesh_1_label)[k] * rmsh.ds_mesh[0]['dS_shape'] \
+        + bgeo.facet_normal[0][i] * ela.P(fsp.u_0, ela.K(fsp.u_0, rpam.parameters['exponent']), ela.mu(fsp.u_0, rpam.parameters['exponent']))[k, i] * fsp.nu_u_0[k] * rmsh.ds_mesh[0]['ds'] \
+        + bgeo.facet_normal[0](sub_mesh_1_label)[i] * ela.P(fsp.u_0(sub_mesh_1_label), ela.K(fsp.u_0(sub_mesh_1_label), rpam.parameters['exponent']), ela.mu(fsp.u_0(sub_mesh_1_label), rpam.parameters['exponent']))[k, i] * fsp.nu_u_0(sub_mesh_1_label)[k] * rmsh.ds_mesh[0]['dS_shape'] \
         + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
-            msh.jump(fsp.u_0[i], bgeo.facet_normal[0])[j] * msh.jump(fsp.nu_u_n[i], bgeo.facet_normal[0])[j] * rmsh.ds_mesh[0]['dS_I_square'] \
-            + fsp.u_0[i] * fsp.nu_u_n[i] * rmsh.ds_mesh[0]['ds'] \
+            msh.jump(fsp.u_0[i], bgeo.facet_normal[0])[j] * msh.jump(fsp.nu_u_0[i], bgeo.facet_normal[0])[j] * rmsh.ds_mesh[0]['dS_I_square'] \
+            + fsp.u_0[i] * fsp.nu_u_0[i] * rmsh.ds_mesh[0]['ds'] \
         ) \
         + rpam.parameters['alpha_ellipse']/rmsh.r_mesh[0] * (\
-            msh.jump(fsp.u_0[i], bgeo.facet_normal[0])[j] * msh.jump(fsp.nu_u_n[i], bgeo.facet_normal[0])[j] * rmsh.ds_mesh[0]['dS_shape'] \
+            msh.jump(fsp.u_0[i], bgeo.facet_normal[0])[j] * msh.jump(fsp.nu_u_0[i], bgeo.facet_normal[0])[j] * rmsh.ds_mesh[0]['dS_shape'] \
         ) \
 
 
