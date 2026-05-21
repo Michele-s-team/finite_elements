05da3998f6a73f2ce145a0b339fc2d9f7ca089f6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-21 14:20:24 +0200

    printed out additional quantities relative to u_0

--- fluid_structure_interaction/elastic_obstacle/monolithic/print_out_solution_square_shape_line.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/print_out_solution_square_shape_line.py
@@ -50,12 +50,24 @@ def print_solution(t, step, dt):
     io.full_print(u_dot_n_dummy, 'u_dot_n_' + str(step), \
                 solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
 
+
     # 3.2 current configuration
+
+    # 3.2.1 deformed with u_n
     io.full_print_deformed(v_n_dummy, u_n_dummy, 'v_n_' + str(step), \
                            solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
     
     io.full_print_deformed(sigma_n_dummy, u_n_dummy, 'sigma_n_' + str(step), \
                            solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+    
+    # 3.2.2 deformed with u_0
+    io.full_print_deformed(v_n_dummy, fsp.u_0, 'v_0_n_' + str(step), \
+                           solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+    
+    io.full_print_deformed(sigma_n_dummy, fsp.u_0, 'sigma_0_n_' + str(step), \
+                           solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+    
+
     
     # 3.3 average of det(F)
     io.full_print(det_F_n, 'det_F_n_' + str(step), \
@@ -82,6 +94,8 @@ def print_solution(t, step, dt):
 
 
     #4. Write the deformed mesh to file
+
+    # 4.1 write the mesh deformed according to u_n
     deformed_mesh = msh.deform_mesh(rmsh.lmsh.mesh[0], u_n_dummy)
 
     with XDMFFile(solpath.snapshots_path + 'mesh_n_' + str(step) + '.xdmf') as xdmf:
@@ -90,7 +104,19 @@ def print_solution(t, step, dt):
     io.print_mesh_vertices_to_csv(deformed_mesh, solpath.snapshots_csv_path + 'vertex_mesh_n_' + str(step) + '.csv')
     io.print_mesh_lines_to_csv(deformed_mesh, solpath.snapshots_csv_path + 'line_mesh_n_' + str(step) + '.csv')
 
-    # 4.2 write shape vertices 
+
+    # 4.2 write the mesh deformed according to u_0
+    deformed_mesh_0 = msh.deform_mesh(rmsh.lmsh.mesh[0], fsp.u_0)
+
+    with XDMFFile(solpath.snapshots_path + 'mesh_0_n_' + str(step) + '.xdmf') as xdmf:
+        xdmf.write(deformed_mesh_0)
+
+    io.print_mesh_vertices_to_csv(deformed_mesh_0, solpath.snapshots_csv_path + 'vertex_mesh_0_n_' + str(step) + '.csv')
+    io.print_mesh_lines_to_csv(deformed_mesh_0, solpath.snapshots_csv_path + 'line_mesh_0_n_' + str(step) + '.csv')
+
+
+    # 5 write shape vertices 
+
     input_path = os.path.join(rarg.args.input_directory, f"mesh_0/boundary_points_id_{rmsh.parameters['shape_id']}.csv")
     output_path = os.path.join(rarg.args.output_directory, f"snapshots/csv/boundary_points_id_{rmsh.parameters['shape_id']}_n_{step}.csv")
     os.system(f'cp {input_path} {output_path}')
