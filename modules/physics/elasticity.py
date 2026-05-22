7fd154636d8da340774ef712d633ba82249255b8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-22 17:19:08 +0200

    ..

--- modules/physics/elasticity.py
+++ modules/physics/elasticity.py
@@ -244,4 +244,4 @@ Return values;
 
 def psi(u, K, mu):
 
-    return (1.0/2.0 * (mu * ( detF(u)**(-2.0/len(u))) * C(u)[i, i] - len(u)) + K * (1.0/2.0 * (detF(u)**2 - 1.0) - log(detF(u))))
+    return (1.0/2.0 * (mu * ( detF(u)**(-2.0/len(u))) * C(u)[i, i] - len(u) ) + K * (1.0/2.0 * (detF(u)**2 - 1.0) - log(detF(u))))
