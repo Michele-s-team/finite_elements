07916dc3018ef16e30823c0185cbd135e231f9fa
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-23 00:09:06 +0200

    added to test square_shape_line case

--- testing/test.py
+++ testing/test.py
@@ -1194,6 +1194,15 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
                                                                      'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
 
+problem_name = 'square_shape_line'
+generate_mesh_path =root_path + 'generate_mesh/2d/square/shape_line'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
 
 case_name = 'fluid_structure_interaction/membrane'
 
