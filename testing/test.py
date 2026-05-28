c332145567696d30e080af3297c7e79a4666d4e9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-28 12:45:39 +0200

    put the right parameters into test

--- testing/test.py
+++ testing/test.py
@@ -1192,7 +1192,10 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      generate_mesh_path, root_path + case_name,
                                                                      generate_mesh_path, root_path + case_name,
                                                                      mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
-                                                                     'generate_mesh', 'generate_mesh',  generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+                                                                     'generate_mesh', 'generate_mesh',  
+                                                                     os.path.join(generate_mesh_path, 'elastic_obstacle_monolithic_square_shape_line_a_parameters'),
+                                                                     os.path.join(generate_mesh_path, 'elastic_obstacle_monolithic_square_shape_line_a_parameters'), 
+                                                                     problem_name, problem_name, success)
 
 problem_name = 'square_shape_line_b'
 generate_mesh_path =root_path + 'generate_mesh/2d/square/shape_line'
