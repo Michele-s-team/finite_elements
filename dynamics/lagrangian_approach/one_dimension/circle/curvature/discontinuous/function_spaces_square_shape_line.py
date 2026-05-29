0b80bae6135cb5ff263c2b399a722ee6b87608f7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-29 20:01:02 +0200

    Update function_spaces_square_shape_line.py

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/function_spaces_square_shape_line.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/function_spaces_square_shape_line.py
@@ -9,29 +9,17 @@ rmsh = importlib.import_module(swi.rmsh)
 
 '''
 the variables for the problem are
-
-    - 'u_n': u^n in notes
-    - 'u_dot_n', 'u_dot_n_1': \dot{u}^n, \dot{u}^{n-1} in notes
-    - v_n, v_n_1 : \textrm{v}^n_notes, \textrm{v}^{n-1}_notes
-    - 'sigma_n' = \varsigma^n_notes
-
-    - 'u_0' = {u_0}_{Decomposition of deformation field}
-    - 'phi_0' = {phi_0}_{Decomposition of deformation field}
-
-all fields are defined from a mixed function space
+    - 'u': deformation field of the shape and mesh
 '''
 
 #1 define elements 
 
 #1.1 fluid 
-D_v_n = VectorElement('DG', triangle, 2)
-D_sigma_n = FiniteElement('DG', triangle, 1)
 
 #1.2 elastic body and mesh
 D_u = VectorElement('DG', triangle, rpam.parameters['u_function_space_degree'])
-D_u_dot = VectorElement('DG', triangle, rpam.parameters['u_dot_function_space_degree'])
 
-element = MixedElement([D_v_n, D_sigma_n, D_u, D_u_dot])
+element = MixedElement([D_u])
 
 
 
@@ -41,77 +29,37 @@ element = MixedElement([D_v_n, D_sigma_n, D_u, D_u_dot])
 Q = FunctionSpace(lmsh.mesh[0], element)
 
 #2.2 collapsed function spaces
-Q_v_n = Q.sub(0).collapse()
-Q_sigma_n = Q.sub(1).collapse()
 
-Q_u_n = Q.sub(2).collapse()
-Q_u_dot_n = Q.sub(3).collapse()
-
-Q_rho_el = FunctionSpace(lmsh.mesh[0], 'DG', 1)
-Q_det_F = FunctionSpace(lmsh.mesh[0], 'DG', 1)
-
+Q_u = Q.sub(0).collapse()
 
 
 #3 define fields
 
 # 3.1 psi contains all fields
 psi = Function(Q)
-v_n, sigma_n, u_n, u_dot_n = split(psi)
+u = split(psi)
 
 
 # 3.2 auxiliary fields
-v_n_1 = Function(Q_v_n)
-
-u_n_1 = Function(Q_u_n)
-u_dot_n_1 = Function(Q_u_dot_n)
-
-rho_el = Function(Q_rho_el)
-
-sigma_t = Function(Q_sigma_n)
-
-phi_0 = Function(Q_u_n)
-u_0 = Function(Q_u_n)
-
-f_fluid = Function(Q_v_n)
-f_ela = Function(Q_u_n)
-
-
-# y is the identity function that, given the coordinates y_i in the reference configuration, returns y_i
-y = Function(Q_u_n)
-
-y.set_allow_extrapolation(True)
-
-
-# 3.2.1 fields to store initial condition read from file
-
-v_input = Function(Q_v_n)
-sigma_input = Function(Q_sigma_n)
-u_input = Function(Q_u_n)
-u_dot_input = Function(Q_u_dot_n)
 
 
 
-# velocity profiles for the BCs
-v_l = Function(Q_v_n)
-v_tb = Function(Q_v_n)
-v_lrtb = Function(Q_v_n)
+
+
+
 
 
 
 
 # 3.3 test functions
-nu_v_n, nu_sigma_n, nu_u_n, nu_u_dot_n = TestFunctions(Q)
-nu_u_0 = TestFunction(Q_u_n)
-
-
+nu_u = TestFunctions(Q)
 
 # 3.4 jacobian
 J_psi = TrialFunction(Q)
-J_u_0 = TrialFunction(Q_u_n)
 
 # 3.5 function assigner
 
-assigner = FunctionAssigner(Q, [Q_v_n, Q_sigma_n, Q_u_n, Q_u_dot_n])
+assigner = FunctionAssigner(Q, [Q_u])
 
 
 
