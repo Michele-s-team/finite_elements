b12e0682db5863c8d26302926b803d2eec22a1e7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-29 20:49:36 +0200

    Update solve.py

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/solve.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/solve.py
@@ -6,7 +6,7 @@ run with:
 
 Examples:
 
-    MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; SOLUTION_PATH="/home/fenics/shared/fluid_structure_interaction/elastic_obstacle/monolithic/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_shape_line_a $MESH_PATH $SOLUTION_PATH
+    MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; SOLUTION_PATH="/home/fenics/shared/dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_shape_line_a $MESH_PATH $SOLUTION_PATH
 
 """
 
