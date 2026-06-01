9d49808feadcfd458e8a68ac6a803529b9cc620a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-01 13:53:28 +0200

    n works

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/print_out_solution_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/print_out_solution_square_shape_line_a.py
@@ -1,7 +1,9 @@
 from fenics import *
+import importlib
 import ufl
 
-import importlib
+import differential_geometry.boundary.geometry as bgeo
+import physics.elasticity as ela
 import input_output as io
 import solution_paths as solpath
 import switch_problem as swi
@@ -9,15 +11,23 @@ import switch_problem as swi
 fsp = importlib.import_module(swi.fsp)
 rmsh = importlib.import_module(swi.rmsh)
 
-i, j, k, l, m = ufl.indices(5)
+i, j, k, l, m, n, o, p, q, r, s, t, u = ufl.indices(13)
 
 mu_dummy, grad_u_dummy = fsp.psi.split( deepcopy=True )
 
 
 
-io.full_print(fsp.n, 'n', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
 
 io.full_print(project(as_tensor((fsp.e[i] + grad_u_dummy[i, k] * fsp.e[k]) , (i)), fsp.V), 'e_cur', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
+
+io.full_print(
+    project(as_tensor(  
+        (- sqrt( dot(fsp.e, fsp.e) / (ela.F(fsp.u)[p, q] * ela.F(fsp.u)[p, r] * fsp.e[q] * fsp.e[r] ) ) \
+               * bgeo.epsilon[i, s] * ela.F(fsp.u)[s, t] * bgeo.epsilon[t, u] * fsp.n[u] ), \
+        (i)), fsp.V), \
+    'n_cur', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
+
+
 
 io.full_print(\
     project( (fsp.e[i] + grad_u_dummy[i, k] * fsp.e[k]) * (fsp.e[i] + grad_u_dummy[i, l] * fsp.e[l]), fsp.Q_mu), \
