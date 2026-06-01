094dae04c69e35752172dd39f567f1f683cf5e33
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-01 13:43:47 +0200

    g works

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/print_out_solution_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/print_out_solution_square_shape_line_a.py
@@ -17,7 +17,10 @@ mu_dummy, grad_u_dummy = fsp.psi.split( deepcopy=True )
 
 io.full_print(fsp.n, 'n', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
 io.full_print(fsp.e, 't', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
-io.full_print(project(fsp.e[i]*fsp.e[i], fsp.Q_mu), 'g', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
+io.full_print(\
+    project( (fsp.e[i] + grad_u_dummy[i, k] * fsp.e[k]) * (fsp.e[i] + grad_u_dummy[i, l] * fsp.e[l]), fsp.Q_mu), \
+    'g', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0]\
+    )
 
 io.full_print(project(fsp.e[i].dx(j) * fsp.e[j] * fsp.n[i], fsp.Q_mu), 'b', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path, solpath.nodal_values_path,  rmsh.sf[0])
 
