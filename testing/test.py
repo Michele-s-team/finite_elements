03f361dea8bd886dce94e06387e97c9aed5ed805
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-02 18:59:09 +0200

    added to test

--- testing/test.py
+++ testing/test.py
@@ -1132,6 +1132,19 @@ checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(comm
                                                                      problem_name, problem_name, success)
 
 
+case_name = 'dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous'
+
+problem_name = 'square_shape_line_a'
+generate_mesh_path =root_path + 'generate_mesh/2d/square/shape_line'
+checks[case_name + '_' + problem_name] = utest.test_generate_mesh_and_solve(commit_a, commit_b,
+                                                                     root_path,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     generate_mesh_path, root_path + case_name,
+                                                                     mesh_solution_path_a, problem_solution_path_a, mesh_solution_path_b, problem_solution_path_b,
+                                                                     'generate_mesh', 'generate_mesh',    
+                                                                     generate_mesh_path, generate_mesh_path, problem_name, problem_name, success)
+
+
 
 # Test fluid_structure_interaction
 case_name = 'fluid_structure_interaction/mesh_deformation'
