6496bfb85ed8a343541c6325d47c52f603dd5db5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-03 23:22:57 +0200

    updated rotation in shape_a case

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
@@ -13,5 +13,13 @@ Return values;
     - 'y_s', 'd y_s/ds': y(s) and d y(s) / ds
 '''
 def y_s_dy_ds(s):
-    return np.add(rmsh.parameters['c'], [rmsh.parameters['a'] * np.cos(2.0 * np.pi * s)/(2.0 + np.cos(2.0 * np.pi * s)),  rmsh.parameters['b'] * np.sin(2.0 * np.pi * s)]), np.array([-4.0*np.pi * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2, 2.0*np.pi * rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) ])
+
+    x = rmsh.parameters['a'] * np.cos(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))
+    y = rmsh.parameters['b'] * np.sin(2.0 * np.pi * s)
+
+    dx = -4.0 * np.pi * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2
+    dy = 2.0 * np.pi * rmsh.parameters['b'] * np.cos(2.0 * np.pi * s)
+    
+    return np.add(rmsh.parameters['c'], [(x - y) / np.sqrt(2.0), (x + y) / np.sqrt(2.0)]), \
+           np.array([(dx - dy) / np.sqrt(2.0), (dx + dy) / np.sqrt(2.0)])
 
