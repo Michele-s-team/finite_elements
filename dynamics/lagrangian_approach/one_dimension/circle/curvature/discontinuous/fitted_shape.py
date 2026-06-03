7ea91641bd2371788e34df0e07a80338bbcb60ca
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-03 17:31:35 +0200

    Update fitted_shape.py

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/fitted_shape.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/fitted_shape.py
@@ -69,5 +69,5 @@ Return values;
     - 'y_s', 'd y_s/ds': y(s) and d y(s) / ds
 '''
 def y_s_dy_ds(s):
-    return np.add(rmsh.parameters['c'], [rmsh.parameters['a'] * np.cos(2.0 * np.pi * s)/(2.0 + np.cos(2.0 * np.pi * s)),  rmsh.parameters['b'] * np.sin(2.0 * np.pi * s)]), np.array([-4.0*np.pi * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2, 2.0*np.pi * rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) ])
+    return f(s), df(s)
 
