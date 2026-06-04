094c9427fbc608de87d500c9f8db6bba2fc52380
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-04 12:49:53 +0200

    it works for case a too

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
@@ -14,12 +14,12 @@ Return values;
 '''
 def y_s_dy_ds(s):
 
-    x = rmsh.parameters['a'] * np.cos(2.0 * np.pi * s) / (np.sqrt(2.0) * (2.0 + np.cos(2.0 * np.pi * s)))
-    y = rmsh.parameters['b'] * np.sin(2.0 * np.pi * s) / np.sqrt(2.0)
+    x = -2.0 * rmsh.parameters['a'] / (2.0 + np.cos(2.0 * np.pi * s))
+    y = rmsh.parameters['b'] * np.sin(2.0 * np.pi * s)
 
-    dx = -2.0 * np.pi * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) * np.sqrt(2.0) / (2.0 + np.cos(2.0 * np.pi * s))**2
-    dy = 2.0 * np.pi * rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) * np.sqrt(2.0)
+    dx = -4.0 * np.pi * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2
+    dy = 2.0 * np.pi * rmsh.parameters['b'] * np.cos(2.0 * np.pi * s)
 
-    return np.add(rmsh.parameters['c'], [x + y, -x + y]), \
-           np.array([dx + dy, -dx + dy])
+    return np.array([rmsh.parameters['a'] + rmsh.parameters['c'][0] + x, rmsh.parameters['c'][1] + y]), \
+           np.array([dx, dy])
 
