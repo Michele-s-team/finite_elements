827095d6849da1bde268f4fc15469f154957e35d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-04 14:53:59 +0200

    Test with rotation_angle = pi - pi/4 OK

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
@@ -13,7 +13,7 @@ Return values;
     - 'y_s', 'd y_s/ds': y(s) and d y(s) / ds
 '''
 
-
+'''
 # case rotation_angle = 0
 def y_s_dy_ds(s):
 
@@ -23,6 +23,7 @@ def y_s_dy_ds(s):
                       2.0 * rmsh.parameters['b'] * np.pi * np.cos(2.0 * np.pi * s)])
 
 '''
+
 # case rotation_angle = pi - pi/4
 def y_s_dy_ds(s):
 
@@ -34,4 +35,3 @@ def y_s_dy_ds(s):
                np.sqrt(2.0) * np.pi * (- rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) + 2.0 * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2),
                np.sqrt(2.0) * np.pi * (- rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) - 2.0 * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2)
            ])
-'''
