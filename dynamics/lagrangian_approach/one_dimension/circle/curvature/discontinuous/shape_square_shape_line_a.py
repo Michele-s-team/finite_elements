f81933e694ec703799d50160afa8ffa0f38b9dac
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-04 14:07:22 +0200

    Test rotating_angle = pi - pi /4 OK

--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/shape_square_shape_line_a.py
@@ -12,10 +12,25 @@ Input values:
 Return values; 
     - 'y_s', 'd y_s/ds': y(s) and d y(s) / ds
 '''
+
+'''
+# case rotation_angle = 0
 def y_s_dy_ds(s):
 
     return np.array([rmsh.parameters['a'] + rmsh.parameters['c'][0] - 2.0 * rmsh.parameters['a'] / (2.0 + np.cos(2.0 * np.pi * s)),
                      rmsh.parameters['c'][1] + rmsh.parameters['b'] * np.sin(2.0 * np.pi * s)]), \
            np.array([-4.0 * rmsh.parameters['a'] * np.pi * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2,
                       2.0 * rmsh.parameters['b'] * np.pi * np.cos(2.0 * np.pi * s)])
+'''
 
+# case rotation_angle = pi - pi/4
+def y_s_dy_ds(s):
+
+    return np.add(rmsh.parameters['c'], [
+               - rmsh.parameters['a'] * np.cos(2.0 * np.pi * s) / (np.sqrt(2.0) * (2.0 + np.cos(2.0 * np.pi * s))) - rmsh.parameters['b'] * np.sin(2.0 * np.pi * s) / np.sqrt(2.0),
+               + rmsh.parameters['a'] * np.cos(2.0 * np.pi * s) / (np.sqrt(2.0) * (2.0 + np.cos(2.0 * np.pi * s))) - rmsh.parameters['b'] * np.sin(2.0 * np.pi * s) / np.sqrt(2.0)
+           ]), \
+           np.array([
+               np.sqrt(2.0) * np.pi * (- rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) + 2.0 * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2),
+               np.sqrt(2.0) * np.pi * (- rmsh.parameters['b'] * np.cos(2.0 * np.pi * s) - 2.0 * rmsh.parameters['a'] * np.sin(2.0 * np.pi * s) / (2.0 + np.cos(2.0 * np.pi * s))**2)
+           ])
--- dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/variational_problem_bc_square_shape_line.py
+++ dynamics/lagrangian_approach/one_dimension/circle/curvature/discontinuous/variational_problem_bc_square_shape_line.py
@@ -46,7 +46,7 @@ print(f'Solving for s_0 ... ')
 
 print(f'\n\tf_L = {np.arctan((sh.y_s_dy_ds(0)[0][1] - rmsh.parameters["c"][1]) / (sh.y_s_dy_ds(0)[0][0] - rmsh.parameters["c"][0]))}')
 print(f'\n\tf_R = {np.arctan((sh.y_s_dy_ds(0.25)[0][1] - rmsh.parameters["c"][1]) / (sh.y_s_dy_ds(0.25)[0][0] - rmsh.parameters["c"][0]))}')
-s_0 = brentq(lambda s:  np.arctan((sh.y_s_dy_ds(s)[0][1] - rmsh.parameters['c'][1]) / (sh.y_s_dy_ds(s)[0][0] - rmsh.parameters['c'][0])), a=0.0, b=0.25 )
+s_0 = brentq(lambda s:  np.arctan((sh.y_s_dy_ds(s)[0][1] - rmsh.parameters['c'][1]) / (sh.y_s_dy_ds(s)[0][0] - rmsh.parameters['c'][0])), a=0.0, b=0.75 )
 
 
 print(f'... done. ')
