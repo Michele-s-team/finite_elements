cac70e9642ecb4aaa08558f52bd8f016014a0d52
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-05 13:46:40 +0200

    added F_mu + F_grad_u

--- fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/variational_problem_bc_square_shape_line_a.py
@@ -5,8 +5,11 @@ this module solves for the fields v_n, sigma_n, u_n, u_dot_n which define the st
 from fenics import *
 import importlib
 import numpy as np
+from scipy.optimize import brentq
 import ufl as ufl
 
+import calculus as cal
+import constants.utils as const
 import continuation as cont
 import differential_geometry.boundary.geometry as bgeo
 import mesh.utils as msh
@@ -20,7 +23,7 @@ fsp = importlib.import_module(swi.fsp)
 rmsh = importlib.import_module(swi.rmsh)
 sh = importlib.import_module(swi.sh)
 
-i, j, k, l, m = ufl.indices(5)
+i, j, k, l, m, n, o, p, q, r, s, t, u = ufl.indices(13)
 
 dt = rpam.parameters['T'] / rpam.parameters['num_steps']  # time step size
 
@@ -38,6 +41,138 @@ io.full_print(n_1, 'n_1', \
                   solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
 '''
 
+
+
+# 
+def theta(s):
+    return cal.atan_quad([ sh.y_s_dy_ds(s)[0][0] - rmsh.parameters['c'][0], sh.y_s_dy_ds(s)[0][1] - rmsh.parameters['c'][1] ])
+
+
+
+
+# s_0 is the value of the curvilinear coordinate at which the polar angle of y(s) with respect to c is 0
+theta_0 = theta(0)
+print(f'*** theta(0) = {theta_0 * const.rad_to_deg}')
+
+# 1. Determine for s_0
+# 1.1
+
+if theta_0 == 0.0:
+    # here s_0 = 1
+
+    print(f'Setting s_0 = 1')
+
+    s_0 = 1
+
+else:
+    # theta_0 != 0 -> need to solve for s_0
+
+    print(f'Solving for s_0 ... ')
+
+    # 1.1 find two values of s, s_0_L and s_0_R, that bracket s_0, by looking for a pair of values of s, s_i and s_i_plus_1 such that theta(s_i_plus_1) < theta(s_i). This is done by dividing the interval 0 < s < 1 into ~ N_scan interals. Note that N_scan needs to be large enough to resolve the root. 
+    for i_s in range(rpam.parameters['N_scan']+1):
+        
+        if theta((i_s+1)/(rpam.parameters['N_scan']-1)) < theta(i_s/(rpam.parameters['N_scan']-1)): 
+            break
+
+    s_0_L = i_s/(rpam.parameters["N_scan"]-1)
+    s_0_R = (i_s+1)/(rpam.parameters["N_scan"]-1)
+
+    print(f's_0 lies betwee {s_0_L} and {s_0_R}')
+
+    # 1.2 solve for s_0 by using s_0_L and s_0_R
+    s_0 = brentq(lambda s:  np.arctan((sh.y_s_dy_ds(s)[0][1] - rmsh.parameters['c'][1]) / (sh.y_s_dy_ds(s)[0][0] - rmsh.parameters['c'][0])), a=s_0_L, b=s_0_R)
+
+    print(f's_0 = {s_0}\t theta(s_0) = {theta(s_0)}')
+
+    print(f'... done. ')
+# 
+
+
+'''
+return the parameter `s` of the curve `y(s)` corresponding to a point `y` on the plane
+Input values: 
+    - `y`: the point in the reference configuration
+Return values: 
+    - `s`: the value of the parametric coordinate of the curve `y(s)` such that the point y(s) forms the same polar angle as `y` with respect to `c`
+'''
+def s_y(y):
+
+    # the polar angle that `y` forms with respect to `c``, theta_y is in [0, 2 pi]
+    theta_y = cal.atan_quad([ y[0] - rmsh.parameters['c'][0], y[1] - rmsh.parameters['c'][1] ])
+
+    if (abs(theta_y) < const.epsilon) or (abs(theta_y - 2.0 * np.pi) < const.epsilon): 
+        # theta_y takes the special values 0 or 2 pi -> set directly s = s_0
+
+        s = s_0
+
+    else:
+        # theta_y is not equal to 0 nor to 2 pi -> solve for s
+
+        if 0 < theta_y <= theta_0:
+            
+            # 0 < theta_y <= theta_0: the solution s must lie in the interval s_0 < s <= 1.0. 
+            
+
+            print(f'Case I\n\ts_0 = {s_0}\n\ttheta_y = {theta_y} \n\ttheta_L = {theta(s_0+const.epsilon)}\n\ttheta_R = {theta(1)}', flush=True)
+
+            s = brentq(\
+                    lambda s: theta(s) - theta_y, \
+                    a=s_0 + const.epsilon, b=1 + const.epsilon \
+                )
+
+        else:
+            # theta_0 <= theta_y < 2 pi,  the solution s must lie in the interval 0 <= s < s_0
+
+
+            print(f'Case II \n\ts_0 = {s_0}\n\ttheta_y = {theta_y} \n\ttheta_L = {theta(0)}\n\ttheta_R = {theta(s_0)}', flush=True)
+
+            s = brentq(\
+                lambda s: theta(s) - theta_y, 
+                a=0, b=s_0 - const.epsilon \
+            )
+ 
+    print(f'\t s = {s}')
+
+    return s
+
+
+# define expressions for  curvature computation
+
+
+class f_expression(UserExpression):
+    def eval(self, values, x):
+
+        # obtain the value of the parametric coordinate `s` corresponding to `x`
+        s = s_y(x)
+
+        values[0] = sh.y_s_dy_ds(s)[1][0]
+        values[1] = sh.y_s_dy_ds(s)[1][1]
+
+    def value_shape(self):
+        return (2,)
+    
+
+
+    
+class nu_expression(UserExpression):
+    def eval(self, values, x):
+
+        # obtain the value of `t` corresponding to `x`
+        s = s_y(x)
+
+        _, d_y_s_ds = sh.y_s_dy_ds(s)
+
+        norm = np.linalg.norm(d_y_s_ds)
+
+        values[0] = d_y_s_ds[1] / norm
+        values[1] = - d_y_s_ds[0] / norm
+
+    def value_shape(self):
+        return (2,)
+
+msh.interpolate_dg(fsp.f, f_expression())
+msh.interpolate_dg(fsp.nu, nu_expression())
 
 # 1. define expressions for BCs
 
@@ -68,18 +203,6 @@ class rho_el_expression(UserExpression):
     def value_shape(self):
         return (1,)
     
-'''    
-class f_expression(UserExpression):
-    def eval(self, values, x):
-
-        values[0] = 1.0
-        values[1] = 0.0
-
-    def value_shape(self):
-        return (2,)
-
-msh.interpolate_dg(fsp.f, f_expression())
-'''
 
 msh.interpolate_dg(fsp.v_l, v_l_expression())
 msh.interpolate_dg(fsp.v_tb, v_tb_circle_expression())
@@ -220,4 +343,24 @@ F_u_dot_n = msh.ufl_conditional_form(
             ) \
             
 
-F = F_v_n + F_sigma_n + F_u_n + F_u_dot_n
+# 2.3 variational problems for curvature computation
+
+F_mu = (\
+        (fsp.mu \
+        - (fsp.f[i] + fsp.grad_u[i, k] * fsp.f[k]).dx(j) * fsp.f[j] \
+        * ( sqrt( dot(fsp.f, fsp.f) / (ela.F(fsp.u)[p, q] * ela.F(fsp.u)[p, r] * fsp.f[q] * fsp.f[r] ) ) \
+        * bgeo.epsilon[i, s] * ela.F(fsp.u)[s, t] * bgeo.epsilon[t, u] * fsp.nu[u] )  \
+        / (2.0 * (fsp.f[m] + fsp.grad_u[m, n] * fsp.f[n]) * (fsp.f[m] + fsp.grad_u[m, o] * fsp.f[o]) ) \
+        ) * fsp.nu_mu \
+    ) * rmsh.dx_mesh[0]['dx'] \
+    + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
+        msh.jump(fsp.mu, bgeo.facet_normal[0])[i] *  msh.jump(fsp.nu_mu, bgeo.facet_normal[0])[i] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
+    )
+
+F_grad_u = ( (fsp.grad_u[i, j] - fsp.u[i].dx(j)) * fsp.nu_grad_u[i, j] ) * rmsh.dx_mesh[0]['dx'] \
+    + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
+        msh.jump(fsp.grad_u[i, j], bgeo.facet_normal[0])[k] *  msh.jump(fsp.nu_grad_u[i, j], bgeo.facet_normal[0])[k] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
+    )
+
+
+F = F_v_n + F_sigma_n + F_u_n + F_u_dot_n + F_mu + F_grad_u
