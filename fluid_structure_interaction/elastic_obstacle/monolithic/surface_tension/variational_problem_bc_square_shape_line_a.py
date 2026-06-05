f6783c7adef371f5940d43bc17d0934bc97a2006
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-05 14:01:26 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/variational_problem_bc_square_shape_line_a.py
@@ -351,15 +351,15 @@ F_mu = (\
         * ( sqrt( dot(fsp.f, fsp.f) / (ela.F(fsp.u_n)[p, q] * ela.F(fsp.u_n)[p, r] * fsp.f[q] * fsp.f[r] ) ) \
         * bgeo.epsilon[i, s] * ela.F(fsp.u_n)[s, t] * bgeo.epsilon[t, u] * fsp.nu[u] )  \
         / (2.0 * (fsp.f[m] + fsp.grad_u_n[m, n] * fsp.f[n]) * (fsp.f[m] + fsp.grad_u_n[m, o] * fsp.f[o]) ) \
-        ) * fsp.nu_mu \
+        ) * fsp.nu_mu_n \
     ) * rmsh.dx_mesh[0]['dx'] \
     + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
-        msh.jump(fsp.mu_n, bgeo.facet_normal[0])[i] *  msh.jump(fsp.nu_mu, bgeo.facet_normal[0])[i] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
+        msh.jump(fsp.mu_n, bgeo.facet_normal[0])[i] *  msh.jump(fsp.nu_mu_n, bgeo.facet_normal[0])[i] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
     )
 
-F_grad_u = ( (fsp.grad_u_n[i, j] - fsp.u_n[i].dx(j)) * fsp.nu_grad_u[i, j] ) * rmsh.dx_mesh[0]['dx'] \
+F_grad_u = ( (fsp.grad_u_n[i, j] - fsp.u_n[i].dx(j)) * fsp.nu_grad_u_n[i, j] ) * rmsh.dx_mesh[0]['dx'] \
     + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
-        msh.jump(fsp.grad_u_n[i, j], bgeo.facet_normal[0])[k] *  msh.jump(fsp.nu_grad_u[i, j], bgeo.facet_normal[0])[k] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
+        msh.jump(fsp.grad_u_n[i, j], bgeo.facet_normal[0])[k] *  msh.jump(fsp.nu_grad_u_n[i, j], bgeo.facet_normal[0])[k] * (rmsh.ds_mesh[0]['dS_I_shape'] + rmsh.ds_mesh[0]['dS_I_square'] + rmsh.ds_mesh[0]['dS_shape']) \
     )
 
 
