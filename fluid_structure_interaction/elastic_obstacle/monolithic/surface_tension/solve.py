ccdb423396c7e572b64a4f1117ef032c6a570972
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-10 15:13:31 +0200

    removed b case

--- fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/solve.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/solve.py
@@ -9,7 +9,7 @@ run with:
 Examples:
 
     MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; SOLUTION_PATH="/home/fenics/shared/fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_shape_line_a $MESH_PATH $SOLUTION_PATH
-    MESH_PATH="/home/fenics/shared/generate_mesh/2d/square/shape_line/solution"; SOLUTION_PATH="/home/fenics/shared/fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/solution"; rm -rf $SOLUTION_PATH; python3 solve.py square_shape_line_b $MESH_PATH $SOLUTION_PATH
+
 """
 
 import colorama as col
--- fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/switch_problem.py
+++ fluid_structure_interaction/elastic_obstacle/monolithic/surface_tension/switch_problem.py
@@ -16,16 +16,4 @@ if rarg.args.problem == 'square_shape_line_a':
     vp = 'variational_problem_bc_square_shape_line_a'
 
 
-elif rarg.args.problem == 'square_shape_line_b':
-
-    fi = 'files_square_shape_line_b'
-    fsp = 'function_spaces_square_shape_line'
-    prout_bc = 'print_out_bc_square_shape_line_b'
-    prout_ic = 'print_out_ic_square_shape_line'
-    prout_da = 'print_out_data_square_shape_line'
-    prout_sol = 'print_out_solution_square_shape_line'
-    rmsh = 'mesh.read.square_shape_line'
-    sh = 'shape_square_shape_line'
-    vp = 'variational_problem_bc_square_shape_line_b'
-
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
