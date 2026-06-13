d0b61c7fde80c0c66c56905d338a9a8083555ccf
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-13 17:12:11 +0200

    .. substituted solve_vp ...

--- dynamics/solve.py
+++ dynamics/solve.py
@@ -38,11 +38,9 @@ sys.path.append(module_path)
 
 
 import function_spaces as fsp
-import input_output as io
 import parameters.read.solution as rpam
-import runtime_arguments as rarg
 import switch_problem as swi
-
+import variational_problem.utils as var_pr
 
 prout_bc = importlib.import_module(swi.prout_bc)
 rmsh = importlib.import_module(swi.rmsh)
@@ -103,26 +101,31 @@ for step in range(rpam.parameters['N']):
     vp = importlib.import_module(swi.vp)
 
     # solve the variational problem
-    J = derivative( vp.F, fsp.psi, fsp.J_psi )
-    problem = NonlinearVariationalProblem( vp.F, fsp.psi, vp.bcs, J )
-    solver = NonlinearVariationalSolver( problem )
+    # J = derivative( vp.F, fsp.psi, fsp.J_psi )
+    # problem = NonlinearVariationalProblem( vp.F, fsp.psi, vp.bcs, J )
+    # solver = NonlinearVariationalSolver( problem )
+    # solver.parameters.update(params)
+    # solver.solve()
 
+    var_pr.solve_vp(vp.F, fsp.psi, vp.bcs, fsp.J_psi, parameters=params)
+
+    
     # the post-processing ('pp') variational problem used to compute tau, ...
-    J_pp_tau = derivative( vp.vp_pp.F_pp_tau, fsp.tau_n_12, fsp.J_pp_tau )
-    J_pp_d = derivative( vp.vp_pp.F_pp_d, fsp.d, fsp.J_pp_d )
-    problem_pp_tau = NonlinearVariationalProblem( vp.vp_pp.F_pp_tau, fsp.tau_n_12, [], J_pp_tau )
-    problem_pp_d = NonlinearVariationalProblem( vp.vp_pp.F_pp_d, fsp.d, [], J_pp_d )
-    solver_pp_tau = NonlinearVariationalSolver( problem_pp_tau )
-    solver_pp_d = NonlinearVariationalSolver( problem_pp_d )
+    # J_pp_tau = derivative( vp.vp_pp.F_pp_tau, fsp.tau_n_12, fsp.J_pp_tau )
+    # problem_pp_tau = NonlinearVariationalProblem( vp.vp_pp.F_pp_tau, fsp.tau_n_12, vp.vp_pp.bcs_tau, J_pp_tau )
+    # solver_pp_tau = NonlinearVariationalSolver( problem_pp_tau )
+    # solver_pp_tau.parameters.update(params)
+    # solver_pp_tau.solve()
 
-    solver.parameters.update(params)
-    solver_pp_tau.parameters.update(params)
-    solver_pp_d.parameters.update(params)
+    var_pr.solve_vp(vp.vp_pp.F_pp_tau, fsp.tau_n_12, vp.vp_pp.bcs_tau, fsp.J_pp_tau, parameters=params)
 
-    solver.solve()
+    # J_pp_d = derivative( vp.vp_pp.F_pp_d, fsp.d, fsp.J_pp_d )
+    # problem_pp_d = NonlinearVariationalProblem( vp.vp_pp.F_pp_d, fsp.d, vp.vp_pp.bcs_d, J_pp_d )
+    # solver_pp_d = NonlinearVariationalSolver( problem_pp_d )
+    # solver_pp_d.parameters.update(params)
+    # solver_pp_d.solve()
 
-    solver_pp_tau.solve()
-    solver_pp_d.solve()
+    var_pr.solve_vp(vp.vp_pp.F_pp_d, fsp.d, vp.vp_pp.bcs_d, fsp.J_pp_d, parameters=params)
 
     #update previous solution:
     #get the solution and write it to file
