0763d3801725d4f42fcba356d87bb99d170c6c9a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-17 17:43:52 +0200

    Update switch_problem.py

--- dynamics/switch_problem.py
+++ dynamics/switch_problem.py
@@ -6,7 +6,7 @@ if rarg.args.problem == 'square_a':
     rmsh = 'mesh.read.square'
     vp = 'variational_problem_bc_square_a'
     prout_bc =  'print_out_bc_square_a'
-    prout_da =  'print_out_data_bc_square_b'
+    prout_da =  'print_out_data_bc_square_a'
 
 elif rarg.args.problem == 'square_b':
     rmsh = 'mesh.read.square'
