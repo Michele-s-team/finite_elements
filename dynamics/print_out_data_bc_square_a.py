e4142e4d3e1ce11691577bde630a6893b2ef3ff5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-06-17 17:41:52 +0200

    setting up printout of data

--- dynamics/print_out_data_bc_square_a.py
+++ dynamics/print_out_data_bc_square_a.py
@@ -6,10 +6,13 @@ import importlib
 from fenics import *
 import ufl as ufl
 
+import differential_geometry.boundary.geometry as bgeo
+import differential_geometry.manifold.geometry as geo
 import parameters.read.solution as rpam
 import switch_problem as swi
 
 import files as fi
+import function_spaces as fsp
 
 rmsh = importlib.import_module(swi.rmsh)
 vp = importlib.import_module(swi.vp)
@@ -23,7 +26,9 @@ def print_data(step):
 
     fi.writer_data.writerows([{
         fi.fieldnames_data[0]: \
-            f"{step:.{rpam.parameters['print_out_digits']}e}"
+            f"{step:.{rpam.parameters['print_out_digits']}e}",\
+        fi.fieldnames_data[1]: \
+            f"{assemble((fsp.v_bar[i] * geo.g( fsp.omega_n_12 )[i, j] * (bgeo.n_circle( fsp.omega_n_12 ))[j])**2 * bgeo.sqrt_deth_circle( fsp.omega_n_12, rmsh.parameters["c_r"] ) * (1.0 / rmsh.parameters["r"]) * rmsh.ds_circle):.{rpam.parameters['print_out_digits']}e}"      
         }])
 
     fi.csvfile_data.flush()
