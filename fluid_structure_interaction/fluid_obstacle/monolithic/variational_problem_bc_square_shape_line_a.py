5d11309e476d8a04b0295c3d8199d358d8624cc3
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-05 17:36:13 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
@@ -78,12 +78,14 @@ bcs = []
 F_v_n = msh.ufl_conditional_form(
                                         rmsh.lmsh.mesh[0],
                                         rmsh.sf[0], 
+                                        # checked - start
                                         ( \
                                             (rpam.parameters['rho_circle'] * ( (fsp.v_n[i] - fsp.v_n_1[i]) / dt \
                                             + (fsp.v_n[k] - fsp.u_dot_n[k]) * ela.G(fsp.u_n)[j, k] * (fsp.v_n[i]).dx(j) ) - fsp.f_circle[i] ) * fsp.nu_v_n[i] \
                                             + ela.G(fsp.u_n)[k, j] * flu.sigma_ale(fsp.v_n, fsp.sigma_n, fsp.u_n, rpam.parameters['mu_circle'])[i, j] * (fsp.nu_v_n[i]).dx(k) \
-                                        ) * ela.detF(fsp.u_n),
-                                        # sign 
+                                        ) * ela.detF(fsp.u_n)
+                                        # checked - end
+                                        ,
                                         ( \
                                             rpam.parameters['rho_fluid'] * ( (fsp.v_n[i] - fsp.v_n_1[i]) / dt \
                                             + (fsp.v_n[k] - fsp.u_dot_n[k]) * ela.G(fsp.u_n)[j, k] * (fsp.v_n[i]).dx(j) ) * fsp.nu_v_n[i] \
@@ -92,6 +94,17 @@ F_v_n = msh.ufl_conditional_form(
                                         rmsh.lmsh.parameters['sub_mesh_0_0_id'],
                                         rmsh.lmsh.parameters['sub_mesh_0_1_id']
                                 ) * rmsh.dx_mesh[0]['dx'] \
+# checked - start
+        - (\
+            msh.jump(fsp.nu_v_n[i], bgeo.facet_normal[0])[k] * msh.average( ela.detF(fsp.u_n) * ela.G(fsp.u_n)[k, j] * flu.sigma_ale(fsp.v_n, fsp.sigma_n, fsp.u_n, rpam.parameters['mu_circle'])[i, j] ) \
+        ) * rmsh.ds_mesh[0]['dS_I_circle'] \
+        - ( \
+                ( \
+                    ela.detF(fsp.u_n(sub_mesh_0_label)) * bgeo.facet_normal[0](sub_mesh_0_label)[k] * ela.G(fsp.u_n(sub_mesh_1_label))[k, j] * flu.sigma_ale(fsp.v_n(sub_mesh_1_label), fsp.sigma_n(sub_mesh_1_label), fsp.u_n(sub_mesh_1_label), rpam.parameters['mu_square'])[i, j] \
+                    + f_shape(fsp.c_n(sub_mesh_1_label), fsp.u_n(sub_mesh_1_label))[i] \
+                )* fsp.nu_v_n(sub_mesh_0_label)[i]
+        ) * rmsh.ds_mesh[0]['dS_shape'] \
+# checked - end
         - (\
             msh.jump(fsp.nu_v_n[i], bgeo.facet_normal[0])[k] * msh.average( ela.detF(fsp.u_n) * ela.G(fsp.u_n)[k, j] * flu.sigma_ale(fsp.v_n, fsp.sigma_n, fsp.u_n, rpam.parameters['mu_fluid'])[i, j] ) \
         ) * rmsh.ds_mesh[0]['dS_I_square'] \
