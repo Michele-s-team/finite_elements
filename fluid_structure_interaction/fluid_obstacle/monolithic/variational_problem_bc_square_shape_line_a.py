002786529054af8e6d42af3ca7ad1ffaa1882e78
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-05 21:56:09 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
@@ -198,8 +198,8 @@ F_u_n = msh.ufl_conditional_form(
             msh.jump(fsp.nu_u_n[k], bgeo.facet_normal[0])[i] * msh.average( ela.P(fsp.u_n, ela.K(fsp.u_n, rpam.parameters['exponent']), ela.mu(fsp.u_n, rpam.parameters['exponent']))[k, i] )  
         ) * rmsh.ds_mesh[0]['dS_I_square'] \
         + bgeo.facet_normal[0][i] * ela.P(fsp.u_n, ela.K(fsp.u_n, rpam.parameters['exponent']), ela.mu(fsp.u_n, rpam.parameters['exponent']))[k, i] * fsp.nu_u_n[k] * rmsh.ds_mesh[0]['ds'] \
-# checked - end
         + bgeo.facet_normal[0](sub_mesh_1_label)[i] * ela.P(fsp.u_n(sub_mesh_1_label), ela.K(fsp.u_n(sub_mesh_1_label), rpam.parameters['exponent']), ela.mu(fsp.u_n(sub_mesh_1_label), rpam.parameters['exponent']))[k, i] * fsp.nu_u_n(sub_mesh_1_label)[k] * rmsh.ds_mesh[0]['dS_shape'] \
+# checked - end
         + rpam.parameters['alpha']/rmsh.r_mesh[0] * (\
             msh.jump(fsp.u_n[i], bgeo.facet_normal[0])[j] * msh.jump(fsp.nu_u_n[i], bgeo.facet_normal[0])[j] * rmsh.ds_mesh[0]['dS_I_square'] \
             + fsp.u_n[i] * fsp.nu_u_n[i] * rmsh.ds_mesh[0]['ds'] \
