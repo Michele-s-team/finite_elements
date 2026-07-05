05030a046e2e194a348e2e3c52a712c51e7aabeb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-05 22:11:19 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
@@ -206,7 +206,7 @@ F_u_n = msh.ufl_conditional_form(
 # checked - end
         + rpam.parameters['alpha_ellipse']/rmsh.r_mesh[0] * (\
             ( \
-               ( \
+                ( \
                    fsp.u_n(sub_mesh_1_label)[i] \
                     - ( \
                         fsp.u_n_1(sub_mesh_1_label)[i] + ( fsp.v_n(sub_mesh_1_label)[j] * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[j] ) * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[i] * dt  \
