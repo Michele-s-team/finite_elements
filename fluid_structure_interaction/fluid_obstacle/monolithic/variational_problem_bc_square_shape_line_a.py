42108b5519749a59dd80a6eb965ec39a6680bc39
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-06 14:25:46 +0200

    Update variational_problem_bc_square_shape_line_a.py

--- fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
+++ fluid_structure_interaction/fluid_obstacle/monolithic/variational_problem_bc_square_shape_line_a.py
@@ -212,7 +212,11 @@ F_u_n = msh.ufl_conditional_form(
                         fsp.u_n_1(sub_mesh_1_label)[i] + ( fsp.v_n(sub_mesh_1_label)[j] * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[j] ) * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[i] * dt  \
                     ) \
                 ) \
-                * ()
+                * ( fsp.nu_u_n(sub_mesh_1_label)[i] \
+                   - ( \
+                        ( fsp.nu_v_n(sub_mesh_1_label)[k] * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[k] ) * bgeo.n_cur(bgeo.facet_normal[0](sub_mesh_0_label), fsp.u_n(sub_mesh_1_label), fsp.dyds(sub_mesh_1_label))[i]
+                     ) * dt \
+                )
             ) * rmsh.ds_mesh[0]['dS_shape'] \
         ) \
 
