424cf3bfe068d602a6f9cf06ef3867d03a8c72d6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-07 21:07:09 +0200

    added print out of snapshots

--- fluid_structure_interaction/fluid_obstacle/monolithic/print_out_solution_square_shape_line.py
+++ fluid_structure_interaction/fluid_obstacle/monolithic/print_out_solution_square_shape_line.py
@@ -1,6 +1,8 @@
 from fenics import *
 import importlib
 
+import input_output as io
+import solution_paths as solpath
 import switch_problem as swi
 
 fi = importlib.import_module(swi.fi)
@@ -25,6 +27,25 @@ def print_solution(t, step, dt):
     fi.xdmffile_u_dot_n.write(u_dot_n_dummy, t)
 
     fi.xdmffile_c_n.write(c_n_dummy, t)
+
+     # 3 write snapshots
+
+    # 3.1 reference configuration and deformation fields
+    io.full_print(v_n_dummy, 'v_n_' + str(step), \
+                  solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])    
+    io.full_print(sigma_n_dummy, 'sigma_n_' + str(step), \
+                  solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+
+    
+    io.full_print(u_n_dummy, 'u_n_' + str(step), \
+                solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+    io.full_print(u_dot_n_dummy, 'u_dot_n_' + str(step), \
+                solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+    
+    io.full_print(c_n_dummy, 'c_n_' + str(step), \
+                solpath.snapshots_path, solpath.snapshots_h5_path, solpath.snapshots_csv_path, solpath.snapshots_csv_nodal_values_path, rmsh.sf[0])
+
+
 
 
 
