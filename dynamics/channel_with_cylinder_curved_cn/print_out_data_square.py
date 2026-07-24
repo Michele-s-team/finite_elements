72ce43ec1463ee8c89ddab86e8fd2b716ce83fa9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-07-24 18:31:53 +0200

    Started channel_with_cylinder_curved_cn ...

---
+++ dynamics/channel_with_cylinder_curved_cn/print_out_data_square.py
@@ -0,0 +1,59 @@
+import csv
+import importlib
+from fenics import *
+import os
+import ufl as ufl
+
+import differential_geometry.boundary.geometry as bgeo
+import function_spaces as fsp
+import parameters.read.solution as rpam
+import physics.fluid_mechanics as flu
+import runtime_arguments as rarg
+import switch_problem as swi
+
+rmsh = importlib.import_module(swi.rmsh)
+vp = importlib.import_module(swi.vp)
+
+
+i, j, k, l = ufl.indices( 4 )
+
+# create the path for the csv file if it does not exist
+filename_data = rarg.args.output_directory + '/data.csv'
+os.makedirs(os.path.dirname(filename_data), exist_ok=True)
+
+csvfile = open(filename_data, 'a', newline='' )
+fieldnames = [ \
+    'step',
+    'dTdt_L',
+    'dTdt_R',
+    '(dTdt_L - dTdt_R)/|(dTdt_L + dTdt_R)/2.0|'
+    ]
+writer = csv.DictWriter( csvfile, fieldnames=fieldnames )
+writer.writeheader()
+
+
+# this function prints out some useful data
+def print_data(step):
+
+    dTdt_L = assemble( ( \
+                rpam.parameters['rho']/2.0 * (fsp.v_n[i] * fsp.v_n[i] - fsp.v_n_1[i] * fsp.v_n_1[i])/vp.dt \
+                + fsp.v_n[i] * ( rpam.parameters['rho']/2.0 * fsp.v_n[j] * fsp.v_n[j] ).dx(i)
+            ) * rmsh.dx)
+
+    dTdt_R = (assemble( ( \
+                            bgeo.facet_normal[i] * fsp.v_n[j] * flu.sigma(fsp.v_n, fsp.sigma_n_12, rpam.parameters['mu'])[i, j]
+                        ) * rmsh.ds) \
+                        - assemble( ( rpam.parameters['mu']/2.0 * (fsp.v_n[i].dx(j) + fsp.v_n[j].dx(i)) * (fsp.v_n[j].dx(i) + fsp.v_n[i].dx(j)) ) * rmsh.dx))
+    
+    writer.writerows( [{ \
+        fieldnames[0]: \
+            step,\
+        fieldnames[1]: \
+            dTdt_L,\
+        fieldnames[2]: \
+            dTdt_R,\
+        fieldnames[3]: \
+            (dTdt_L - dTdt_R)/abs((dTdt_L + dTdt_R)/2.0),\
+        }] )
+
+    csvfile.flush()
--- dynamics/channel_with_cylinder_curved_cn/switch_problem.py
+++ dynamics/channel_with_cylinder_curved_cn/switch_problem.py
@@ -7,6 +7,7 @@ if rarg.args.problem == 'square_no_circle':
     vp = 'variational_problem_bc_square_no_circle'
     vp_pp = 'variational_problem_pp_square_no_circle'
     prout_bc = 'print_out_bc_square_no_circle'
+    prout_da = 'print_out_data_square_no_circle'
     prout_forces_on_boundaries = 'print_out_force_on_boundaries_bc_square_no_circle'
 
 
@@ -16,6 +17,7 @@ elif rarg.args.problem == 'square':
     vp = 'variational_problem_bc_square'
     vp_pp = 'variational_problem_pp_square'
     prout_bc = 'print_out_bc_square'
+    prout_da = 'print_out_data_square'
     prout_forces_on_boundaries = 'print_out_force_on_boundaries_bc_square'
 
 print(f'{col.Fore.CYAN}Loaded {rarg.args.problem} problem{col.Style.RESET_ALL}')
