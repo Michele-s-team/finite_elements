378996e1a2620a14b6e85a701286ca7e9ac796f7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-08-31 15:13:21 +0200

    mesh_parameters

--- generate_mesh/2d/square_no_circle/line/generate_mesh.py
+++ generate_mesh/2d/square_no_circle/line/generate_mesh.py
@@ -16,7 +16,24 @@ import sys
 module_path = '/home/fenics/shared/modules'
 sys.path.append(module_path)
 
+import input_output as io
 import mesh.utils as msh
+import parameters.read.mesh as rpam
 import runtime_arguments_generate_mesh as rarg
 
-msh.generate_square_no_circle_curve_mesh([], rarg.args.parameter_directory, rarg.args.output_directory)
+curve_coordinates = None
+
+if rpam.parameters['curve_format'] == 'coordinates':
+    # the  curve is provided directly as a sequence of coordinates of the curve points -> set curve_coordinates to these coordinates
+
+    print('The curve is provided as a set of coordinates.')
+
+    curve_coordinates = rpam.parameters['curve_coordinates']
+
+elif rpam.parameters['curve_format'] == 'parametric':
+    #  the curve is a given, parametric geometrical curve, and it is provided in terms of the parameters of this curve
+
+    curve_parametric_form = io.read_function_expresssion(rpam.parameters['curve_parametric_form'])
+    curve_coordinates = [curve_parametric_form(i/rpam.parameters['N']) for i in range(rpam.parameters['N'])]
+
+msh.generate_square_no_circle_curve_mesh(curve_coordinates, rarg.args.parameter_directory, rarg.args.output_directory)
