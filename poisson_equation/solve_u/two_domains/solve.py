6f005e5b722dc2f3e56d3f1532696be8a57fc97c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-01 23:13:41 +0200

    test for scalar for a line OK

--- poisson_equation/solve_u/two_domains/solve.py
+++ poisson_equation/solve_u/two_domains/solve.py
@@ -25,6 +25,37 @@ import variational_problem.utils as var_pr
 rmsh = importlib.import_module(swi.rmsh)
 vp = ['','']
 
+# test transfer_sub_mesh_to_mesh - start
+import function as fu
+import input_output as io
+import runtime_arguments as rarg
+import solution_paths as solpath
+
+class u_1_expression(UserExpression):
+    def eval(self, values, x):
+
+        values[0] = x[0]**2
+
+    def value_shape(self):
+        return (1,)
+
+u_0 = Function(fsp.Q[0])
+u_1 = Function(fsp.Q[1])
+
+u_1.interpolate(u_1_expression(element=fsp.Q[1].ufl_element()))
+
+
+fu.transfer_sub_mesh_to_mesh(u_1, u_0, rarg.args.input_directory)
+
+io.full_print(u_0, 'u_0_test', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+io.full_print(u_1, 'u_1_test', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
+                  solpath.nodal_values_path)
+# test transfer_sub_mesh_to_mesh - end
+
+
+
+
 # set the solver parameters here
 params = {'nonlinear_solver': 'newton',
           'newton_solver':
