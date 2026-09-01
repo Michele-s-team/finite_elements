e79d22737110f81d5e9b9f7471c0262b0da7f53b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-01 23:37:15 +0200

    test transfer_sub_mesh_to_mesh OK

--- poisson_equation/solve_u/two_domains/solve.py
+++ poisson_equation/solve_u/two_domains/solve.py
@@ -25,6 +25,7 @@ import variational_problem.utils as var_pr
 rmsh = importlib.import_module(swi.rmsh)
 vp = ['','']
 
+'''
 # test transfer_sub_mesh_to_mesh - start
 import function as fu
 import input_output as io
@@ -39,12 +40,14 @@ class u_1_expression(UserExpression):
 
         values[0] = x[0]**2
         values[1] = x[0]**3
+        values[2] = x[0]**4
+        values[3] = x[0]**5
 
     def value_shape(self):
-        return (2,)
+        return (2, 2)
 
-Q_0 = VectorFunctionSpace(lmsh.sub_meshes[0], 'P', 2)
-Q_1 = VectorFunctionSpace(lmsh.sub_meshes[1], 'P', 2, dim=2)
+Q_0 = TensorFunctionSpace(lmsh.sub_meshes[0], 'P', 2, shape=(2,2))
+Q_1 = TensorFunctionSpace(lmsh.sub_meshes[1], 'P', 2, shape=(2,2))
 
 u_0 = Function(Q_0)
 u_1 = Function(Q_1)
@@ -59,8 +62,7 @@ io.full_print(u_0, 'u_0_test', solpath.xdmf_file_path, solpath.h5_file_path, sol
 io.full_print(u_1, 'u_1_test', solpath.xdmf_file_path, solpath.h5_file_path, solpath.csv_files_path,
                   solpath.nodal_values_path)
 # test transfer_sub_mesh_to_mesh - end
-
-
+'''
 
 
 # set the solver parameters here
