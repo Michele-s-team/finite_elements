105a36997eb57e84b6d49d039a3ae839d1acd459
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-05 13:22:31 +0200

    Update solve.py

--- fluid_structure_interaction/rigid_obstacle/remesh/solve.py
+++ fluid_structure_interaction/rigid_obstacle/remesh/solve.py
@@ -334,7 +334,6 @@ for n in range(rpam.parameters["num_steps"]):
         # set fsp.u_dot_n_2(y') =  u_dot_n_2_old(phi_n_old^{-1}(y')), where phi_n_old(y) = y + u_n_old(y)
         msh.transfer(u_dot_n_2_old, fsp.u_dot_n_2, u_n_old)
 
-        # sign
 
 
         # 8 clean up
