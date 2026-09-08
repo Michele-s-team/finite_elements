4c0d96137fb18b464d196565b92918c1a1b7ea52
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-08 17:32:16 +0200

    Update solve.py

--- fluid_structure_interaction/membrane/solve.py
+++ fluid_structure_interaction/membrane/solve.py
@@ -607,11 +607,12 @@ for n in range(rpam.parameters['N']):
         # set fsp.u_dot_n_2(y') =  u_dot_n_2_old(phi_n_old^{-1}(y')), where phi_n_old(y) = y + u_n_old(y)
         msh.transfer(u_dot_n_2_old, fsp.u_dot_n_2, u_n_old)
 
-        # sign
 
 
         # 4.4.3 transfer fluid fields
+
         msh.transfer(v_fl_n_old, fsp.v_fl_n, u_n_old)
+        # sign
         msh.transfer(v_fl_n_1_old, fsp.v_fl_n_1, u_n_old)
 
         msh.transfer(phi_fl_old, fsp.phi_fl, u_n_old)
