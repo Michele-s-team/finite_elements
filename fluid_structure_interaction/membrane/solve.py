3cbaabb6d21f9b9179cfe8d2a8875f89fcb0ff6e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-09 12:13:03 +0200

    Update solve.py

--- fluid_structure_interaction/membrane/solve.py
+++ fluid_structure_interaction/membrane/solve.py
@@ -505,15 +505,17 @@ for n in range(rpam.parameters['N']):
         phi = sigma_n_32 - sigma_n_12
         To obtain the transferred value of `phi`, we transfer sigma_n_32 and sigma_n_12 separately, and then take the difference. 
         '''
-        # after this call, fsp.sigma_n_12_dummy(x_1') = sigma_n_12_old((phi_n_12_old)^{-1}(x_1')). 
-        fu.transfer_1d_to_1d_curve(sigma_n_12_old, fsp.sigma_n_12_dummy, u_n_12_old, pre_remesh_path)      
+        # after this call, fsp.sigma_n_12_dummy(x_1') = sigma_n_12_old((phi_n_old)^{-1}(x_1')). 
+        fu.transfer_1d_to_1d_curve(sigma_n_12_old, fsp.sigma_n_12_dummy, u_n_old, pre_remesh_path)      
 
-        # after this call, fsp.sigma_n_32_dummy(x_1') = sigma_n_32_old((phi_n_32_old)^{-1}(x_1')). 
-        fu.transfer_1d_to_1d_curve(sigma_n_32_old, fsp.sigma_n_32_dummy, u_n_32_old, pre_remesh_path)   
+        # after this call, fsp.sigma_n_32_dummy(x_1') = sigma_n_32_old((phi_n_old)^{-1}(x_1')). 
+        fu.transfer_1d_to_1d_curve(sigma_n_32_old, fsp.sigma_n_32_dummy, u_n_old, pre_remesh_path)   
 
         fsp.phi_output.assign(fsp.sigma_n_32_dummy - fsp.sigma_n_12_dummy)   
 
         # 4.4.1.3 transfer v_n, w_n
+
+        # sign
 
         # after this call, fsp.v_n_output(x_1') = v_n_old((phi_n_old)^{-1}(x_1')). 
         fu.transfer_1d_to_1d_curve(v_n_old, fsp.v_n_output, u_n_old, pre_remesh_path)
