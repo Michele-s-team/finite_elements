bc2bedab775e623b29efb249d27cfa4cffce4fad
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-13 20:36:45 +0200

    Update solve.py

--- fluid_structure_interaction/rigid_obstacle/remesh/solve.py
+++ fluid_structure_interaction/rigid_obstacle/remesh/solve.py
@@ -175,9 +175,6 @@ for n in range(rpam.parameters["num_steps"]):
     # step 3.3: velocity step
     var_pr.solve_vp(vp_fluid.F_v_n, fsp.v_n, vp_fluid.bc_v_n, fsp.J_v_n)
 
-    # write into sigma_n_12
-    fsp.sigma_n_12.assign(fsp.sigma_n_32 - fsp.phi)
-
     print('... done.', flush=True)
 
 
