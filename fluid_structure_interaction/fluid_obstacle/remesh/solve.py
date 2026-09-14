a7cd88e77fc0ef5e37934780be020753850c9d69
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-14 18:18:13 +0200

    Update solve.py

--- fluid_structure_interaction/fluid_obstacle/remesh/solve.py
+++ fluid_structure_interaction/fluid_obstacle/remesh/solve.py
@@ -783,6 +783,23 @@ for n in range(rpam.parameters['N']):
 
         # 7.4.1.2 transfer U_n_32
 
+        # WARNING: here I am assuming that U_n_32 can be approximated by interpolating linearly the values at n-1/2 and n-3/2 - start
+
+        # after this call, fsp.U_a(x_1') = U_n_12_old((phi_n_old)^{-1}(x_1')). 
+        fu.transfer_1d_to_1d_curve(
+            U_n_12_old, fsp.U_a, u_n_di_old, os.path.join(pre_remesh_path, 'mesh_0'), 
+            closed=True
+            )
+        
+        # after this call, fsp.U_b(x_1') = U_n_32_old((phi_n_old)^{-1}(x_1')). 
+        fu.transfer_1d_to_1d_curve(
+            U_n_32_old, fsp.U_b, u_n_di_old, os.path.join(pre_remesh_path, 'mesh_0'), 
+            closed=True
+            )
+
+        fsp.U_n_32.assign(-3.0*(fsp.U_a - fsp.U_b)/2.0)
+        # WARNING: here I am assuming that U_n_32 can be approximated by interpolating linearly the values at n-1/2 and n-3/2 - end
+
 
   
         #7.4.2 given that psi_0 has been recreated from scratch, it is set to 0 -> re-set the correct profile in it
