2d8853fda599bdc5fd7dce050b5d6c263e5ababe
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-14 18:32:26 +0200

    Update solve.py

--- fluid_structure_interaction/fluid_obstacle/remesh/solve.py
+++ fluid_structure_interaction/fluid_obstacle/remesh/solve.py
@@ -897,7 +897,7 @@ for n in range(rpam.parameters['N']):
     fsp.v_disk_n_2.assign(fsp.v_disk_n_1)
     fsp.v_disk_n_1.assign(fsp.v_disk_n)
 
-    fsp.sigma_disk_n_12.assign( fsp.sigma_disk_n_32 - fsp.phi_disk_aux )
+    fsp.sigma_disk_n_12.assign( fsp.sigma_disk_n_32 - project(fsp.phi_disk_aux, fsp.Q_sigma_disk) )
     fsp.sigma_disk_n_32.assign(fsp.sigma_disk_n_12)
 
 
