ee463e5fb35d141797fb21a114df1e4e308e5b67
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-17 11:35:17 +0200

    substitutes arc_length_tab

--- modules/mesh/utils.py
+++ modules/mesh/utils.py
@@ -2913,12 +2913,8 @@ def read_sub_meshes(mesh, sf, mesh_medatada, input_directory):
                     3. compute the arc length along the one-dimensional manifold, store the cumulative arc length at each vertex of it in `line_mesh_coordinates`: these will be the one-dimensional coordinates of the line, 1d mesh that will be generated. 
                     Thi 1d mesh is obtained by lying flat the one-dimensional manifold above. 
                     '''
-                    line_mesh_coordinates = [0]
-                    arc_length = 0
-                    for i in range(1, len(sub_mesh_vertices)):
-
-                        arc_length += np.linalg.norm(np.subtract(sub_mesh_vertices[i].point().array()[:2], sub_mesh_vertices[i-1].point().array()[:2])) 
-                        line_mesh_coordinates.append(arc_length)     
+                    sub_mesh_vertices_point_array = [sub_mesh_vertices[i].point().array()[:2] for i in range(len(sub_mesh_vertices))]
+                    line_mesh_coordinates = geo_u.arc_length_tab(sub_mesh_vertices_point_array)
 
 
                     # generate the one-dimensional submesh and return its cell mesh function and vertex mesh function
