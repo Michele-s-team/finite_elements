3654531786c9386541f5691bc9e2c2e8e8615c3f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-17 11:10:55 +0200

    substituted acr_length_tab

--- modules/function.py
+++ modules/function.py
@@ -252,12 +252,7 @@ def transfer_1d_to_2d_curve(u_1d, u_2d, mesh_path,
     '''
     compute the arc length along the 1d mesh: arc_length_tab[i] = [cumulative arc length along the sub mesh curve obtained from its beginning until sub_mesh_vertices included]
     '''
-    arc_length = 0
-    arc_length_tab = [0]
-    for i in range(1, len(mesh_1d_vertices)):
-
-        arc_length += np.linalg.norm(np.subtract(mesh_1d_vertices[i], mesh_1d_vertices[i-1]))
-        arc_length_tab.append(arc_length)
+    arc_length_tab = geo.arc_length_tab(mesh_1d_vertices)
 
     num_components = n_components(Q_2d)
 
