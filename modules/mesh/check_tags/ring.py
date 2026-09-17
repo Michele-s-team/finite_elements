71150eebd8c3b8085d345fc9cc7e9c46947b5037
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-17 13:29:53 +0200

    same for ring mesh

--- modules/mesh/check_tags/ring.py
+++ modules/mesh/check_tags/ring.py
@@ -22,6 +22,9 @@ integral_exact_ds_R = cal.curve_integral_circle(tf.function_test_integrals, rmsh
 
 integral_exact_ds = integral_exact_ds_r + integral_exact_ds_R
 
+integral_exact_dS = cal.curve_integral_dS(rmsh.lmsh.mesh, tf.function_test_integrals)
+
+
 test_mesh_integral_errors = dict([])
 
 test_mesh_integral_errors['\int f dx'] = msh.test_mesh_integral(integral_exact_dx, tf.function_test_integrals_fenics, rmsh.dx, '\int f dx')
@@ -30,6 +33,9 @@ test_mesh_integral_errors['\int f ds_r'] = msh.test_mesh_integral(integral_exact
 test_mesh_integral_errors['\int f ds_R'] = msh.test_mesh_integral(integral_exact_ds_R, tf.function_test_integrals_fenics, rmsh.ds_R, '\int f ds_R')
 
 test_mesh_integral_errors['\int f ds'] = msh.test_mesh_integral(integral_exact_ds, tf.function_test_integrals_fenics, rmsh.ds, '\int f ds')
+
+test_mesh_integral_errors['\int f dS'] = msh.test_mesh_integral(integral_exact_dS, tf.function_test_integrals_fenics, rmsh.dS, '\int f dS')
+
 
 # print to file the residuals of the tests of the mesh integrals
 io.write_parameters_to_csv_file(io.add_trailing_slash(rarg.args.output_directory) + 'test_integral_errors.csv', test_mesh_integral_errors)
