9285a7d89c63523e3f5834a2b2a94751770f68c9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-18 11:01:39 +0200

    .

--- poisson_equation/solve_u/two_domains/print_out_square_no_circle_line_b.py
+++ poisson_equation/solve_u/two_domains/print_out_square_no_circle_line_b.py
@@ -3,6 +3,7 @@ from fenics import *
 import importlib
 import ufl as ufl
 
+import differential_geometry.boundary.geometry as bgeo
 import input_output as io
 import mesh.utils as msh
 
@@ -18,9 +19,8 @@ i, j, k, l = ufl.indices(4)
 print(f"\t- Check of BCs:")
 
 print(f"\t\tBCs for mesh {0}:")
-print(f"\t\t\t<<(u - u_exact)^2>>_[partial Omega {0} lr] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u[0], fsp.u_exact[0], rmsh.ds_mesh[0]['ds_lr']):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
-print(f"\t\t\t<<(u - u_exact)^2>>_[partial Omega {0} t] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u[0], fsp.u_1_on_0, rmsh.ds_mesh[0]['ds_t']):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
-print(f"\t\t\t<<(u - u_exact)^2>>_[partial Omega {0} b] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u[0], fsp.u_exact[0], rmsh.ds_mesh[0]['ds_b']):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+print(f"\t\t\t<<|n^i partial_i u  - n^i grad_u_i|^2>>_[partial Omega{0}] = {col.Fore.RED}{msh.difference_wrt_measure(bgeo.facet_normal[0][i] * (fsp.u[0].dx(i)), bgeo.facet_normal[0][i] * fsp.grad_u[0][i], rmsh.ds_mesh[0]['ds']):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
+
 
 print(f"\t\tBCs for mesh {1}:")
 print(f"\t\t\t<<(u - u_exact)^2>>_[partial Omega{1}] = {col.Fore.RED}{msh.difference_wrt_measure(fsp.u[1], fsp.u_exact[1], rmsh.ds_mesh[1]['ds']):.{io.number_of_decimals}e}{col.Style.RESET_ALL}")
--- poisson_equation/solve_u/two_domains/variational_problem_square_no_circle_line_b_sub_mesh_0.py
+++ poisson_equation/solve_u/two_domains/variational_problem_square_no_circle_line_b_sub_mesh_0.py
@@ -17,10 +17,10 @@ i, j = ufl.indices(2)
 # exact expression for mesh 0: here I choose an expression for u_exact which matches fsp.u[1] on the top edge of mesh[0]
 class u_exact_mesh_0_expression(UserExpression):
     def eval(self, values, x):
-        
-        # test case 1
-        values[0] = (1 + x[0]**2 + 2 * x[1]**2) * np.cos(2 * np.pi * x[1] / rmsh.parameters['shape_coordinates'][0][1])
-    
+
+        values[0] = 1 + x[0] ** 2 + 2 * x[1] ** 2
+  
+      
     def value_shape(self):
         return (1,)
 
@@ -28,10 +28,9 @@ class u_exact_mesh_0_expression(UserExpression):
 class grad_u_exact_mesh_0_expression(UserExpression):
     def eval(self, values, x):
         
-        # test case 1
-        values[0] = 2 * x[0] * np.cos((2 * np.pi * x[1]) / rmsh.parameters['shape_coordinates'][0][1])
-        values[1] = 4 * x[1] * np.cos((2 * np.pi * x[1]) / rmsh.parameters['shape_coordinates'][0][1]) - (2 * np.pi * (1 + x[0]**2 + 2 * x[1]**2) * np.sin((2 * np.pi * x[1]) / rmsh.parameters['shape_coordinates'][0][1])) / rmsh.parameters['shape_coordinates'][0][1]
-
+        values[0] = 2.0 * x[0]
+        values[1] = 4.0 * x[1]
+   
     def value_shape(self):
         return (2,)
 
@@ -39,42 +38,19 @@ class grad_u_exact_mesh_0_expression(UserExpression):
 class laplacian_u_exact_mesh_0_expression(UserExpression):
     def eval(self, values, x):
         
-        # test case 1
-        values[0] = -((2 * ((-3 * rmsh.parameters['shape_coordinates'][0][1]**2 + 2 * np.pi**2 * (1 + x[0]**2 + 2 * x[1]**2)) * np.cos((2 * np.pi * x[1]) / rmsh.parameters['shape_coordinates'][0][1]) + 8 * rmsh.parameters['shape_coordinates'][0][1] * np.pi * x[1] * np.sin((2 * np.pi * x[1]) / rmsh.parameters['shape_coordinates'][0][1]))) / rmsh.parameters['shape_coordinates'][0][1]**2)
+        values[0] = 6.0
 
     def value_shape(self):
         return (1,)
 
-
-# v_expression is assigned from the values of fsp.u[1], to transfer the solution of problem on mesh[1] to mesh[0]
-class v_Expression(UserExpression):
-    def eval(self, values, x):
-        # test case 1
-        values[0] = ((fsp.u)[1])(x[0]) 
-
-    def value_shape(self):
-        return (1,)
 
 
 fsp.u_exact[0].interpolate(u_exact_mesh_0_expression(element=fsp.Q[0].ufl_element()))
 fsp.grad_u[0].interpolate(grad_u_exact_mesh_0_expression(element=fsp.V[0].ufl_element()))
 fsp.f[0].interpolate(laplacian_u_exact_mesh_0_expression(element=fsp.Q[0].ufl_element()))
 
-# variational problem
-# solve problem 0 by using the solution of problem 1 to specify the BCs
-# set the BC at the interface between mesh[0] and mesh[1] according to the solution fsp.u[1] obtained above
-# impose the BCs for problem on mesh[0], on the t boundary of mesh[0], in terms of fsp.u_1_on_0, and solve problem on mesh[0]
-# force reload vp to update bc[0], because u_1_on_0 has changed
-fsp.v.interpolate(v_Expression(element=fsp.Q[1].ufl_element()))
-# set u_1_on_0 to be equal to v = u[1]**2 + cos(2 pi (x[0] - h))**2 on the top edge of mesh[0]
-fu.transfer_1d_to_2d_curve(fsp.v, fsp.u_1_on_0, rarg.args.input_directory)
 
-bcs = [ \
-    DirichletBC(fsp.Q[0], fsp.u_exact[0], rmsh.mf[0], rmsh.parameters["line_l_id"]), \
-    DirichletBC(fsp.Q[0], fsp.u_exact[0], rmsh.mf[0], rmsh.parameters["line_r_id"]), \
-    DirichletBC(fsp.Q[0], fsp.u_1_on_0, rmsh.mf[0], rmsh.parameters["mesh_1_id"]), \
-    DirichletBC(fsp.Q[0], fsp.u_exact[0], rmsh.mf[0], rmsh.parameters["line_b_id"])
-]
+bcs = []
 
 F = (fsp.u[0].dx(i) * fsp.nu_u[0].dx(i) + fsp.f[0] * fsp.nu_u[0]) * rmsh.dx_mesh[0] \
-    - bgeo.facet_normal[0][i] * (fsp.u[0].dx(i)) * fsp.nu_u[0] * rmsh.ds_mesh[0]['ds']
+    - bgeo.facet_normal[0][i] * fsp.grad_u[0][i] * fsp.nu_u[0] * rmsh.ds_mesh[0]['ds']
