41d84efbf1ab48ddb06c1f9b03cc3d1d2c5e0799
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-23 20:38:19 +0200

    Update variational_problem_fluid_bc_square_no_circle_line_a.py

--- fluid_structure_interaction/membrane/variational_problem_fluid_bc_square_no_circle_line_a.py
+++ fluid_structure_interaction/membrane/variational_problem_fluid_bc_square_no_circle_line_a.py
@@ -9,6 +9,7 @@ import ufl as ufl
 import differential_geometry.boundary.geometry as bgeo
 import phi_lb as phi_lb
 import physics.elasticity as ela
+import physics.fluid_mechanics as flu
 import function_spaces as fsp
 import parameters.read.solution as rpam
 import switch_problem as swi
@@ -33,6 +34,7 @@ class v_fl_bar_b_Expression(UserExpression):
 
 fsp.v_fl_bar_b.interpolate(v_fl_bar_b_Expression(element=fsp.Q_v_fl_bar.ufl_element()))
 
+# checked - start
 # BCs
 # 1) for step 1
 bc_v_fl_bar_b = DirichletBC(fsp.Q_v_fl_bar, fsp.v_fl_bar_b, rmsh.mf[0], rmsh.parameters["line_b_id"])
@@ -42,7 +44,7 @@ bc_v_fl_bar_t = DirichletBC(fsp.Q_v_fl_bar, fsp.u_dot_n_1, rmsh.mf[0], rmsh.para
 
 bc_v_fl_bar = [bc_v_fl_bar_b, bc_v_fl_bar_l, bc_v_fl_bar_0_r, bc_v_fl_bar_t]
 
-# sign
+# checked - end
 
 # 2) for step 2
 '''
@@ -58,25 +60,16 @@ bc_phi_fl = [bc_phi_fl_vertex_lb]
 
 
 # step 1 for v_fl_bar
+# checked - start
 F_v_fl_bar = ( \
                    rpam.parameters['rho_fluid'] * (
                                                 (fsp.v_fl_bar[alpha] - fsp.v_fl_n_1[alpha]) / dt \
                                                 + (3.0 / 2.0 * (fsp.v_fl_n_1[gamma] - fsp.u_dot_n_1[gamma]) * ela.G(fsp.u_n_1)[beta, gamma] - 1.0 / 2.0 * (fsp.v_fl_n_2[gamma] - fsp.u_dot_n_2[gamma]) * ela.G(fsp.u_n_2)[beta, gamma]) * (fsp.V_fl[alpha]).dx(beta)
                                                 ) * fsp.nu_v_fl_bar[alpha] \
-                    + fsp.sigma_fl_n_32 * ela.G(fsp.u_n_1)[beta, alpha] * (fsp.nu_v_fl_bar[alpha]).dx(beta) \
-                    + rpam.parameters['eta_fluid'] * ela.G(fsp.u_n_1)[gamma, beta] * ((fsp.V_fl[alpha]).dx(gamma)) * ela.G(fsp.u_n_1)[delta, beta] * (fsp.nu_v_fl_bar[alpha]).dx(delta) \
+                    + ela.G(fsp.u_n_1)[gamma, beta] * flu.sigma_ale(fsp.V_fl, fsp.sigma_fl_n_32, fsp.u_n_1, rpam.parameters['eta_fluid'])[alpha, beta] * (fsp.nu_v_fl_bar[alpha]).dx(gamma) \
             ) * ela.detF(fsp.u_n_1) * rmsh.dx_mesh[0] \
-            - (ela.G(fsp.u_n_1)[beta, alpha] * (bgeo.facet_normal[0])[beta] * fsp.sigma_fl_n_32 * fsp.nu_v_fl_bar[alpha]) * ela.detF(fsp.u_n_1) * rmsh.ds_mesh[0]['ds'] \
-            - ( \
-                   rpam.parameters['eta_fluid'] * ela.G(fsp.u_n_1)[delta, beta] * (bgeo.facet_normal[0])[delta] * ela.G(fsp.u_n_1)[gamma, beta] * (fsp.V_fl[alpha].dx(gamma)) * fsp.nu_v_fl_bar[alpha] * ela.detF(fsp.u_n_1) * rmsh.ds_mesh[0]['ds_l'] + \
-                   rpam.parameters['eta_fluid'] * ela.G(fsp.u_n_1)[delta, beta] * (bgeo.facet_normal[0])[delta] * ela.G(fsp.u_n_1)[gamma, beta] * (fsp.V_fl[alpha].dx(gamma)) * fsp.nu_v_fl_bar[alpha] * ela.detF(fsp.u_n_1) * rmsh.ds_mesh[0]['ds_tb'] + \
-                   #natural BC imposed here
-                   rpam.parameters['eta_fluid'] * (bgeo.facet_normal[0])[delta] * (
-                                                                                ela.G(fsp.u_n_1)[delta, 0] * ela.G(fsp.u_n_1)[gamma, 0] * (fsp.V_fl[0].dx(gamma)) * fsp.nu_v_fl_bar[0] + \
-                                                                                ela.G(fsp.u_n_1)[delta, 1] * ela.G(fsp.u_n_1)[gamma, 1] * (fsp.V_fl[0].dx(gamma)) * fsp.nu_v_fl_bar[0] + \
-                                                                                ela.G(fsp.u_n_1)[delta, 1] * ela.G(fsp.u_n_1)[gamma, 1] * (fsp.V_fl[1].dx(gamma)) * fsp.nu_v_fl_bar[1] 
-                                                                            ) * ela.detF(fsp.u_n_1) * rmsh.ds_mesh[0]['ds_r'] \
-            )
+            - (ela.G(fsp.u_n_1)[gamma, beta] * (bgeo.facet_normal[0])[gamma] * flu.sigma_ale(fsp.V_fl, fsp.sigma_fl_n_32, fsp.u_n_1, rpam.parameters['eta_fluid'])[alpha, beta] * fsp.nu_v_fl_bar[alpha]) * ela.detF(fsp.u_n_1) * rmsh.ds_mesh[0]['ds'] \
+            # checked - end
 
 
 # step 2 for phi
