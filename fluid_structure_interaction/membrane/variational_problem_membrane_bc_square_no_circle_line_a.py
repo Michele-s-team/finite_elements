7dda5060e351d446d89f53f11a48da2ee482a237
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-09-23 22:52:31 +0200

    Update variational_problem_membrane_bc_square_no_circle_line_a.py

--- fluid_structure_interaction/membrane/variational_problem_membrane_bc_square_no_circle_line_a.py
+++ fluid_structure_interaction/membrane/variational_problem_membrane_bc_square_no_circle_line_a.py
@@ -74,9 +74,11 @@ F_v_bar = ( \
                                                              fsp.nu_n_12)[i] * fsp.nu_v_bar[i]\
                             )
           ) * geo.sqrt_detg( fsp.psi_n_12, fsp.nu_n_12 ) * rmsh.dx_mesh[1]  \
+# checked - start
           - dt * rpam.parameters['rho'] / 2.0 * ( \
                       ((fsp.W ** 2) * (bgeo.n_lr( fsp.psi_n_12, fsp.nu_n_12,  lmsh.mesh[1]))[i] * fsp.nu_v_bar[i]) * bgeo.sqrt_deth_lr( fsp.psi_n_12 ) * rmsh.ds_mesh[1]['ds'] \
           ) \
+# checked - end
           - dt * ( \
                       (fsp.sigma_n_32 * (bgeo.n_lr( fsp.psi_n_12, fsp.nu_n_12,  lmsh.mesh[1]))[i] * fsp.nu_v_bar[i]) * bgeo.sqrt_deth_lr( fsp.psi_n_12 ) * rmsh.ds_mesh[1]['ds_r'] \
            ) \
